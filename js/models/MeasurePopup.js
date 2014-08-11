0c768b3b6e3cdae7507a739095aba7473d08413d
sebadu <sebastian.duden@gv.hamburg.de>
2014-08-11 10:18:35 +0200

    1.Version Measure

---
+++ js/models/MeasurePopup.js
@@ -0,0 +1,61 @@
+define([
+    'underscore',
+    'backbone',
+    'eventbus',
+    'openlayers'
+], function (_, Backbone, EventBus, ol) {
+
+    var MeasurePopup = Backbone.Model.extend({
+        initialize: function () {
+            EventBus.on('activateDraw', this.setGeometrieType, this);
+            this.set('source', new ol.source.Vector());
+            this.set('layer', new ol.layer.Vector({
+                source: this.get('source')
+            }));
+        },
+        setGeometrieType: function (value) {
+            this.set('draw', new ol.interaction.Draw({
+                source: this.get('source'),
+                type: value
+            }));
+            EventBus.trigger('activateClick', 'measure');
+            this.get('draw').on('drawstart', function (evt) {
+                var features = this.get('source').getFeatures();
+                if (features.length > 0) {
+                    this.get('source').removeFeature(features[0]);
+                }
+            }, this);
+            this.get('draw').on('drawend', function (evt) {
+                if (evt.feature.getGeometry().getType() === 'LineString') {
+                    this.formatLength(evt.feature.getGeometry())
+                }
+                else {
+                    this.formatArea(evt.feature.getGeometry())
+                }
+            }, this);
+        },
+        formatLength: function (line) {
+            var length = Math.round(line.getLength() * 100) / 100;
+            var output;
+            if (length > 100) {
+                output = (Math.round(length / 1000 * 100) / 100) + ' ' + 'km';
+            }
+            else {
+                output = (Math.round(length * 100) / 100) + ' ' + 'm';
+            }
+            this.set('result', 'Länge: ' + output);
+        },
+        formatArea: function (polygon) {
+            var area = polygon.getArea();
+            var output;
+            if (area > 10000) {
+                output = (Math.round(area / 1000000 * 100) / 100) + ' ' + 'km<sup>2</sup>';
+            } else {
+                output = (Math.round(area * 100) / 100) + ' ' + 'm<sup>2</sup>';
+            }
+            this.set('result', 'Fläche: ' + output);
+        }
+    });
+
+    return new MeasurePopup();
+});
