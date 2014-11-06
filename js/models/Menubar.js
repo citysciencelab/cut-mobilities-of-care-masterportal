1fd70857ba91a7ce9ca038131cbc7c9a80221a9d
sebadu <sebastian.duden@gv.hamburg.de>
2014-11-06 13:30:46 +0100

    Menue-Eintrag fuer Werkzeuge wird nur bei min. zwei Tools erzeugt

--- js/models/Menubar.js
+++ js/models/Menubar.js
@@ -7,6 +7,16 @@ define([
     var Menubar = Backbone.Model.extend({
         initialize: function () {
             _.each(Config.menu, this.setAttributes, this);
+            // Wenn nur ein Tool aktiviert ist, wird der Menüeintrag Werkzeuge nicht erzeugt. --> Abfrage im template
+            var oneTool = _.filter(_.values(Config.tools), function (value) {
+                return value == true;
+            });
+            if (oneTool.length === 1) {
+                this.set('oneTool', true);
+            }
+            else {
+                this.set('oneTool', false);
+            }
         },
         setAttributes: function (value, key) {
             this.set(key, value);
