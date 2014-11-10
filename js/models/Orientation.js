5930db1069b1e6cdca3d8b8d3bed80d48c2c4152
sebadu <sebastian.duden@gv.hamburg.de>
2014-11-10 14:39:29 +0100

    Struktur POIs

--- js/models/Orientation.js
+++ js/models/Orientation.js
@@ -10,23 +10,24 @@ define([
     proj4.defs("EPSG:25832","+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
 
     var Orientation = Backbone.Model.extend({
-        defaults: {
-            poiContent: []
-        },
+//        defaults: {
+//            poiContent: []
+//        },
         initialize: function () {
             EventBus.on('setOrientation', this.setOrientation, this);
-            EventBus.on('getPOI', this.getPOI, this);
-            EventBus.on('getPOIParams', this.getPOIParams, this);
+//            EventBus.on('getPOI', this.getPOI, this);
+//            EventBus.on('getPOIParams', this.getPOIParams, this);
+            EventBus.on('sendVisibleWFSLayer', this.getPOIParams, this);
         },
         setOrientation: function (btn) {
             proj4326=ol.proj.get('EPSG:4326');
-            var positions = new ol.geom.LineString([],
-                /** @type {ol.geom.GeometryLayout} */ ('XYZM'));
+//            var positions = new ol.geom.LineString([],
+//                /** @type {ol.geom.GeometryLayout} */ ('XYZM'));
             var geolocation = new ol.Geolocation(/** @type {olx.GeolocationOptions} */ ({
               projection: proj4326,
               tracking: true
             }));
-            var position
+            var position;
             geolocation.on('change', function(evt) {
               position = geolocation.getPosition();
               var newCenter = proj4(proj4('EPSG:4326'), proj4('EPSG:25832'), position);
@@ -59,18 +60,21 @@ define([
             circleExtent[3]=botrightUTM[1];
             var circleCoord = circle.getCoordinates();
             this.set('circleExtent', circleExtent);
-            EventBus.trigger('setPOIParams', this);
+            EventBus.trigger('getVisibleWFSLayer', this);
         },
-        getPOIParams: function(params){
-            var featureArray= new Array();
-            for(var i =0;i<params.length;i++){
-                psource=params[i].source;
-                psource.forEachFeatureInExtent(this.get('circleExtent'), function addFeature(feature){featureArray.push(feature)});
-            }
-            if(featureArray.length>0){
-                this.set('poiContent', featureArray);
-            }
-            EventBus.trigger('showPOIModal', this.get('poiContent'),StyleList);
+        getPOIParams: function(visibleWFSLayers){
+            var featureArray = [];
+            _.each(visibleWFSLayers, function (layer) {
+                layer.get('source').forEachFeatureInExtent(this.get('circleExtent'), function (feature) {
+                    featureArray.push(feature);
+                    EventBus.trigger('setModel', feature, StyleList);
+                });
+            }, this);
+            // Hier jetzt model poi erstellen
+//            if(featureArray.length>0){
+//                this.set('poiContent', featureArray);
+//            }
+            EventBus.trigger('showPOIModal');
 
         }
     });
--- js/views/PointOfInterestView.js
+++ js/views/PointOfInterestView.js
@@ -3,42 +3,20 @@ define([
     'underscore',
     'backbone',
     'text!templates/PointOfInterest.html',
-    'models/PointOfInterestModal',
+    'models/PointOfInterest',
     'eventbus'
-], function ($, _, Backbone, PointOfInterestTemplate, PointOfInterestModal, EventBus) {
+], function ($, _, Backbone, PointOfInterestTemplate, PointOfInterest, EventBus) {
 
-    var MeasurePopupView = Backbone.View.extend({
-        model: PointOfInterestModal,
-        id: 'base-modal',
-        className: 'modal fade in',
+    var PointOfInterestView = Backbone.View.extend({
+        model: PointOfInterest,
+        tagName: "tr",
         template: _.template(PointOfInterestTemplate),
-        events: {
-          'click .poiRow': 'onPOIClick'
-        },
-        initialize: function () {
-            EventBus.on('showPOIModal', this.show, this);
-        },
-        onPOIClick: function (evt) {
-            this.$el.modal('hide');
-            newCenter=evt.target.title.split(',');
-            newCenter.pop();
-            EventBus.trigger('setCenter', [parseInt(newCenter[0],10),parseInt(newCenter[1],10)]);
-        },
-        show: function (poiContent, StyleList) {
-            this.model.setAttributions(poiContent, StyleList);
-            this.model.set('poiContent', poiContent);
-            this.model.set('StyleList', StyleList);
-            this.render();
-            this.$el.modal({
-                backdrop: 'static',
-                show: true
-            });
-        },
         render: function () {
             var attr = this.model.toJSON();
             this.$el.html(this.template(attr));
+            return this;
         }
     });
 
-    return MeasurePopupView;
+    return PointOfInterestView;
 });
