675e531c6fa8977eadde42d1e22dff59c3eb13e4
Christopher Vornfeld <christopher.vornfeld@gv.hamburg.de>
2015-01-15 09:37:57 +0100

    geotourismus config angepasst

--- portale/geotourismus/config.js
+++ portale/geotourismus/config.js
@@ -32,14 +32,14 @@ define(function () {
              ],
              name: 'Verkehrsbelastung auf Autobahnen', visible: true
             },
-            {id: '352', visible: true, style: ['5','6','7','8','352_cluster'], clusterDistance: 0, styleField :'Kategorie'},
-            {id: '353', visible: true, style: ['32','33','34','35','353_cluster'], clusterDistance: 30, styleField :'Kategorie'},
-            {id: '354', visible: true, style: ['10','354_cluster'], clusterDistance: 30, styleField :'Kategorie'},
-            {id: '355', visible: true, style: ['11','12','13','14','15','16','17','355_cluster'], clusterDistance: 30, styleField :'Kategorie'},
-            {id: '356', visible: true, style: ['3', '9','356_cluster'], clusterDistance: 30, styleField :'Kategorie'},
-            {id: '357', visible: true, style: ['18','19','20','21','22','357_cluster'], clusterDistance: 30, styleField :'Kategorie'},
-            {id: '358', visible: true, style: ['23','24','25','26','27','28','358_cluster'], clusterDistance: 30, styleField :'Kategorie'},
-            {id: '359', visible: true, style: ['29','30','31','359_cluster'], clusterDistance: 30, styleField :'Kategorie'}
+            {id: '352', visible: true, style: '352', clusterDistance: 30, styleField :'Kategorie'},
+            {id: '353', visible: true, style: '353', clusterDistance: 30, styleField :'Kategorie'},
+            {id: '354', visible: true, style: '354', clusterDistance: 30, styleField :'Kategorie'},
+            {id: '355', visible: true, style: '355', clusterDistance: 30, styleField :'Kategorie'},
+            {id: '356', visible: true, style: '356', clusterDistance: 30, styleField :'Kategorie'},
+            {id: '357', visible: true, style: '357', clusterDistance: 30, styleField :'Kategorie'},
+            {id: '358', visible: true, style: '358', clusterDistance: 30, styleField :'Kategorie'},
+            {id: '359', visible: true, style: '359', clusterDistance: 30, styleField :'Kategorie'}
         ],
         styleConf: '../../style.json',
         menubar: true,
@@ -53,9 +53,20 @@ define(function () {
             tools: true,
             treeFilter: false,
             wfsFeatureFilter: false,
-            legend: true
+            legend: true,
+            routing: false
         },
-        gazetteerURL: 'http://wscd0096/dog_hh/services/wfs?service=WFS&request=GetFeature&version=2.0.0',
+        searchBar: {
+            placeholder: "Adresssuche",
+            gazetteerURL: function () {
+                if (window.location.host === "wscd0096" || window.location.host === "wscd0095") {
+                    return locations.host + "/dog_hh/services/wfs?service=WFS&request=GetFeature&version=2.0.0";
+                }
+                else {
+                    return "http://geodienste-hamburg.de/HH_WFS_DOG?service=WFS&request=GetFeature&version=2.0.0";
+                }
+            }
+        },
         tools: {
             gfi: true,
             measure: false,
@@ -63,7 +74,18 @@ define(function () {
             coord: false,
             active: 'gfi'
         },
-        printURL: 'http://wscd0096:8680/mapfish_print_2.0/pdf6/info.json',
+        print: {
+            url: function () {
+                if (window.location.host === "wscd0096" || window.location.host === "wscd0095") {
+                    return locations.host + ":8680/mapfish_print_2.0/";
+                }
+                else {
+                    return locations.host + "/mapfish_print_2.0/";
+                }
+            },
+            title: 'Geotourismus',
+            gfi: false
+        },
         proxyURL: 'http://wscd0096/cgi-bin/proxy.cgi',
         orientation: true,
         poi: true
