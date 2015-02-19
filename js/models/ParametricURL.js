f734b41f8da522d30ec740d61bcdf1f8827c12a7
Michael Bieler <michael.bieler@gv.hamburg.de>
2015-02-19 11:34:18 +0100

    Eingabe der Parameter in URL unabhängig von groß-kleinschreibung

--- js/models/ParametricURL.js
+++ js/models/ParametricURL.js
@@ -8,18 +8,17 @@ define([
 
         initialize: function () {
             // Parsen des parametrisierten Aufruf --> http://wscd0096/libs/lgv/portale/master?layerIDs=453,1346&center=555874,5934140&zoomLevel=4&isMenubarVisible=false
-            var query = location.search.substr(1); // URL --> alles nach ? wenn vorhanden
+            var query = location.search.substr(1).toUpperCase(); // URL --> alles nach ? wenn vorhanden
             var result = {};
             query.split("&").forEach(function (keyValue) {
                 var item = keyValue.split("=");
                 result[item[0]] = decodeURIComponent(item[1]); // item[0] = key; item[1] = value;
             });
-
             /**
              * Gibt die initiale Zentrumskoordinate zurück.
              * Ist der Parameter 'center' vorhanden wird dessen Wert zurückgegeben, ansonsten der Standardwert.
              */
-            if (_.has(result, 'center')) {
+            if (_.has(result, 'CENTER')) {
                 var values = _.values(_.pick(result, 'center'))[0].split(',');
                 _.each(values, function(value, index, list) {
                     value = parseInt(value);
@@ -32,8 +31,8 @@ define([
              * Gibt die LayerIDs für die Layer zurück, die initial sichtbar sein sollen.
              * Ist der Parameter 'layerIDs' vorhanden werden dessen IDs zurückgegeben, ansonsten die konfigurierten IDs.
              */
-            if (_.has(result, 'layerIDs')) {
-                var values = _.values(_.pick(result, 'layerIDs'))[0].split(',');
+            if (_.has(result, 'LAYERIDS')) {
+                var values = _.values(_.pick(result, 'LAYERIDS'))[0].split(',');
                 var newLayerIDs = Config.layerIDs;
                 newLayerIDs.forEach(function(layerID){
                     if (_.find(values, function (value) {return value == layerID.id})) {
@@ -50,8 +49,8 @@ define([
              * Gibt die initiale Resolution (Zoomlevel) zurück.
              * Ist der Parameter 'zoomLevel' vorhanden wird die passende Resolution zurückgegeben, ansonsten der Standardwert.
              */
-            if (_.has(result, 'zoomLevel')) {
-                var value = _.values(_.pick(result, 'zoomLevel'))[0];
+            if (_.has(result, 'ZOOMLEVEL')) {
+                var value = _.values(_.pick(result, 'ZOOMLEVEL'))[0];
                 var resolutions = {
                     '1': 66.14614761460263,  // 1:250:000
                     '2': 26.458319045841044, // 1:100.000
@@ -71,8 +70,8 @@ define([
             * Ist der Parameter 'isMenubarVisible' vorhanden, wird dieser zurückgegeben, ansonsten der Standardwert.
             *
             */
-            if (_.has(result, 'isMenubarVisible')) {
-                var value = _.values(_.pick(result, 'isMenubarVisible'))[0].toUpperCase();
+            if (_.has(result, 'ISMENUBARVISIBLE')) {
+                var value = _.values(_.pick(result, 'ISMENUBARVISIBLE'))[0].toUpperCase();
                 if (value ==  'TRUE') {
                     Config.isMenubarVisible = true;
                 }
@@ -86,24 +85,24 @@ define([
             * Ist der Parameter 'isMenubarVisible' vorhanden, wird dieser zurückgegeben, ansonsten der Standardwert.
             *
             */
-            if (_.has(result, 'startUpModul')) {
-                var value = _.values(_.pick(result, 'startUpModul'))[0].toUpperCase();
+            if (_.has(result, 'STARTUPMODUL')) {
+                var value = _.values(_.pick(result, 'STARTUPMODUL'))[0].toUpperCase();
                 Config.startUpModul = value;
             }
 
             /**
             *
             */
-            if (_.has(result, 'bplanfest')) {
-                var value = _.values(_.pick(result, 'bplanfest'))[0];
+            if (_.has(result, 'BPLANFEST')) {
+                var value = _.values(_.pick(result, 'BPLANFEST'))[0];
                 Config.searchBar.initString = value;
             }
 
             /**
             *
             */
-            if (_.has(result, 'bplaniv')) {
-                var value = _.values(_.pick(result, 'bplaniv'))[0];
+            if (_.has(result, 'BPLANIV')) {
+                var value = _.values(_.pick(result, 'BPLANIV'))[0];
                 Config.searchBar.initString = value;
             }
         }
