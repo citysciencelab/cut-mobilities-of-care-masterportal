2f9bc9b8e89fb8cd8160ac494f77d53d800e9cab
sebadu <sebastian.duden@gv.hamburg.de>
2015-03-16 13:19:55 +0100

    fix Layerbaum wird beim Zoomen der Karte "gerendert"

--- js/views/TreeListView.js
+++ js/views/TreeListView.js
@@ -1,29 +1,28 @@
 define([
-    'jquery',
-    'underscore',
-    'backbone',
-    'collections/TreeList',
-    'views/TreeNodeView',
-    'eventbus',
-    'bootstrap'
-    ], function ($, _, Backbone, TreeList, TreeNodeView, EventBus) {
+    "jquery",
+    "underscore",
+    "backbone",
+    "collections/TreeList",
+    "views/TreeNodeView",
+    "eventbus",
+    "bootstrap"
+    ], function ($, _, Backbone, TreeList, TreeNodeView) {
 
         var TreeListView = Backbone.View.extend({
             collection: TreeList,
-            el: '#tree',
+            el: "#tree",
             initialize: function () {
-                EventBus.on("currentResolution", this.render, this);
                 this.collection.on("add", this.render, this);
                 this.render();
             },
             render: function () {
-                this.$el.html('');
+                this.$el.html("");
                 this.collection.forEach(this.addTreeNode, this);
             },
             addTreeNode: function (node) {
                 if (node.get("layerList").length > 0) {
                     var treeNodeView = new TreeNodeView({model: node});
-                    $('#tree').append(treeNodeView.render().el);
+                    $("#tree").append(treeNodeView.render().el);
                 }
             }
         });
