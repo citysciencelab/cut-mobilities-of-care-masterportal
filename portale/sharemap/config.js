98c0defb59ab338fdc74df3cbec8461d1b93727b
sebadu <sebastian.duden@gv.hamburg.de>
2015-03-23 13:13:43 +0100

    add Sharemap Portal

---
+++ portale/sharemap/config.js
@@ -0,0 +1,71 @@
+define(function () {
+    var config = {
+        allowParametricURL: true,
+        view: {
+            center: [565874, 5934140], // Rathausmarkt
+            resolution: 15.874991427504629, // 1:60.000
+            scale: 60000 // für print.js benötigt
+        },
+        layerConf: locations.baseUrl + (locations.fhhnet ? '../diensteapiFHHNET.json' : '../diensteapiINTERNET.json'),
+        layerIDs:
+        [
+            {id: "453", visible: true},
+            {id: "94", visible: false, name: "Luftbilder"},
+        ],
+        styleConf: locations.baseUrl + "../style.json",
+        menubar: true,
+        mouseHover: false,
+        scaleLine: true,
+        isMenubarVisible: true,
+        menu: {
+            viewerName: "GeoViewer",
+            searchBar: true,
+            layerTree: true,
+            helpButton: false,
+            contactButton: true,
+            tools: true,
+            treeFilter: false,
+            wfsFeatureFilter: false,
+            legend: false,
+            routing: false
+        },
+        startUpModul: '',
+        // gazetteerURL: locations.host + "/dog_hh/services/wfs?service=WFS&request=GetFeature&version=2.0.0",
+        searchBar: {
+            placeholder: "Suche Adresse, Stadtteil",
+            gazetteerURL: function () {
+                if (locations.fhhnet) {
+                    return locations.host + "/geofos/dog_hh/services/wfs?service=WFS&request=GetFeature&version=2.0.0";
+                }
+                else {
+                    return locations.host + "/geodienste-hamburg/HH_WFS_DOG?service=WFS&request=GetFeature&version=2.0.0";
+                }
+            }
+        },
+        // bPlanURL: locations.host + "/fachdaten_public/services/wfs_hh_bebauungsplaene",
+        tools: {
+            gfi: true,
+            measure: true,
+            print: true,
+            coord: true,
+            draw: false,
+            active: "gfi"
+        },
+        orientation: true,
+        poi: false,
+        print: {
+            url: function () {
+                if (locations.fhhnet) {
+                    return locations.host + ":8680/mapfish_print_2.0/";
+                }
+                else {
+                    return "http://geoportal-hamburg.de/mapfish_print_2.0/";
+                }
+            },
+            title: 'Hamburg',
+            gfi: false
+        },
+        proxyURL: "/cgi-bin/proxy.cgi"
+    }
+    return config;
+});
