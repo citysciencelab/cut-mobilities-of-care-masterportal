86aa5d43d1f935950945df9f48f5d718a43dfbc5
Sebastian Schmitz <sebastian.schmitz@gv.hamburg.de>
2015-04-20 22:53:24 +0200

    add beispiel-portal für lokale entwicklung

---
+++ portale/local/config.js
@@ -0,0 +1,67 @@
+define(function () {
+
+    var config = {
+        allowParametricURL: true,
+        view: {
+            center: [565874, 5934140], // Rathausmarkt
+            resolution: 15.874991427504629, // 1:60.000
+            scale: 60000 // für print.js benötigt
+        },
+        layerConf: '../config/diensteapiInternet.json',
+        categoryConf: '../config/category.json',
+        layerIDs:
+        [
+        {id: "453", visible: true},
+        {id: "94", visible: false},
+        {id: "1554", visible: false, minScale: "10000"},
+        {id: "1555", visible: false, minScale: "10000"},
+        {id: "1519", visible: false, minScale: "10000"},
+        // {id: "1703", visible: false},
+        {id: "1568,1569,1570", visible: false, name: "Schutzwürdige Böden", minScale: "10000"},
+        {id: "1702", visible: true}
+        ],
+        styleConf: "../config/style.json",
+        menubar: true,
+        mouseHover: false,
+        scaleLine: true,
+        isMenubarVisible: true,
+        menu: {
+            viewerName: "GeoViewer",
+            searchBar: true,
+            layerTree: true,
+            helpButton: false,
+            contactButton: true,
+            tools: true,
+            treeFilter: false,
+            wfsFeatureFilter: false,
+            legend: true,
+            routing: false
+        },
+        startUpModul: "",
+        searchBar: {
+            placeholder: "Suche Adresse, Stadtteil",
+            gazetteerURL: function () {
+                return "/geofos/dog_hh/services/wfs?service=WFS&request=GetFeature&version=2.0.0";
+            }
+        },
+        tools: {
+            gfi: true,
+            measure: true,
+            print: false,
+            coord: true,
+            draw: true,
+            active: "gfi"
+        },
+        orientation: true,
+        poi: false,
+        print: {
+            url: function () {
+                    return "/mapfish/mapfish_print_2.0/";
+                }
+            ,
+            title: "Bodenschutz-Portal",
+            gfi: false
+        }
+    }
+    return config;
+});
