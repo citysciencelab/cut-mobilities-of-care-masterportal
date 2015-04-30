f63cf669c3434fd4128042be632e71633104fe8b
sebadu <sebastian.duden@gv.hamburg.de>
2015-04-30 14:08:20 +0200

    update Strassenbaum-online Konfiguration

--- portale/strassenbaumkataster/config.js
+++ portale/strassenbaumkataster/config.js
@@ -6,23 +6,20 @@ define(function () {
             resolution: 5.2916638091682096,
             scale: 20000 // für print.js benötigt
         },
-        layerConf: locations.baseUrl + (locations.fhhnet ? '../diensteapiFHHNET.json' : '../diensteapiINTERNET.json'),
+        layerConf: "../components/lgv-config/services-fhhnet.json",
+        categoryConf: "../components/lgv-config/category.json",
         layerIDs: [
-            {id: '453', visible: true},
-            {id: '8', visible: false},
-            // NOTE wenn displayInTree auf false steht, ist auch keine GFI-Abfrage möglich
-            {id: '182', visible: false, styles: "strassenbaumkataster_grau", displayInTree: false},
-            {id: '182', visible: true, styles: "strassenbaumkataster", name: "Straßenbäume"},
-            {id: '5181', visible: false, displayInTree: false},
-            // ,
-            // {id: '5183', visible: false, displayInTree: false}
+            {id: "453", visible: true},
+            {id: "8", visible: false},
+            {id: "5182", visible: false, displayInTree: false},
+            {id: "5183", visible: false, displayInTree: false}
         ],
-        styleConf: locations.baseUrl + '../style.json',
+        styleConf: "../components/lgv-config/style.json",
         menubar: true,
         scaleLine: false,
         isMenubarVisible: true,
         menu: {
-            viewerName: 'GeoViewer',
+            viewerName: "GeoViewer",
             searchBar: true,
             layerTree: true,
             helpButton: false,
@@ -33,27 +30,12 @@ define(function () {
             legend: false,
             routing: false
         },
-        startUpModul: '',
+        startUpModul: "",
         // gazetteerURL: locations.host + '/dog_hh/services/wfs?service=WFS&request=GetFeature&version=2.0.0',
         searchBar: {
-            placeholder: "Adresssuche",
+            placeholder: "Suche Adresse, Stadtteil",
             gazetteerURL: function () {
-                if (locations.fhhnet) {
-                    return locations.host + "/geofos/dog_hh/services/wfs?service=WFS&request=GetFeature&version=2.0.0";
-                }
-                else {
-                    return locations.host + "/geodienste-hamburg/HH_WFS_DOG?service=WFS&request=GetFeature&version=2.0.0";
-                }
-            }
-        },
-        bPlan: {
-            url: function () {
-                if (locations.fhhnet) {
-                    return locations.host + "/fachdaten_public/services/wfs_hh_bebauungsplaene";
-                }
-                else {
-                    return locations.host + "/geodienste-hamburg.de/HH_WFS_Bebauungsplaene";
-                }
+                return "/geofos/dog_hh/services/wfs?service=WFS&request=GetFeature&version=2.0.0";
             }
         },
         tools: {
@@ -63,21 +45,16 @@ define(function () {
             coord: true,
             draw: false,
             orientation: false,
-            active: 'gfi'
+            active: "gfi"
         },
         print: {
             url: function () {
-                if (locations.fhhnet) {
-                    return locations.host + ":8680/mapfish_print_2.0/";
-                }
-                else {
-                    return "http://geoportal-hamburg.de/mapfish_print_2.0/";
-                }
+                return "http://wscd0096:8680/mapfish_print_2.0/";
             },
-            title: 'Straßenbaumkataster',
-            gfi: true
+            title: "Strassenbaum-Online",
+            gfi: false
         },
-        proxyURL: '/cgi-bin/proxy.cgi'
+        proxyURL: "/cgi-bin/proxy.cgi"
     }
 
     return config;
