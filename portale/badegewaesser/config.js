27b614cf5c40c60061775781f03b8bbce5bc3c71
sebadu <sebastian.duden@gv.hamburg.de>
2015-05-06 17:00:54 +0200

    add Konfiguration Badegewässer-Portal.

---
+++ portale/badegewaesser/config.js
@@ -0,0 +1,62 @@
+define(function () {
+    var config = {
+        allowParametricURL: true,
+        view: {
+            center: [565874, 5934140], // Rathausmarkt
+            resolution: 15.874991427504629, // 1:60.000
+            scale: 60000 // für print.js benötigt
+        },
+        layerConf: "../components/lgv-config/services-fhhnet.json",
+        categoryConf: "../components/lgv-config/category.json",
+        styleConf: "../components/lgv-config/style.json",
+        print: {
+            url: function () {
+                return "http://wscd0096:8680/mapfish_print_2.0/";
+            },
+            title: "Planportal",
+            gfi: false
+        },
+        proxyURL: "/cgi-bin/proxy.cgi",
+        layerIDs:
+        [
+        {id: "453", visible: true},
+        {id: "94", visible: false, name: "Luftbilder"},
+        // {id: "1452", visible: true},
+        {id: "1728", visible: true, style: "1728", clusterDistance: 0, searchField: "", mouseHoverField: "name", filterOptions: [], styleLabelField: "", styleField: "eg_einstufung", name: "Badegewässer"}
+        ],
+        menubar: true,
+        mouseHover: true,
+        scaleLine: true,
+        isMenubarVisible: true,
+        menu: {
+            viewerName: "GeoViewer",
+            searchBar: true,
+            layerTree: true,
+            helpButton: false,
+            contactButton: true,
+            tools: true,
+            treeFilter: false,
+            wfsFeatureFilter: false,
+            legend: true,
+            routing: false
+        },
+        startUpModul: "",
+        searchBar: {
+            placeholder: "Suche Adresse, Stadtteil",
+            gazetteerURL: function () {
+                return "/geofos/dog_hh/services/wfs?service=WFS&request=GetFeature&version=2.0.0";
+            }
+        },
+        tools: {
+            gfi: true,
+            measure: true,
+            print: true,
+            coord: true,
+            draw: false,
+            active: "gfi"
+        },
+        orientation: true,
+        poi: false
+    }
+    return config;
+});
