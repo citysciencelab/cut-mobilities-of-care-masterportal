f18bc5d8b8d0ebb2f37bb98c687df5dec5c2d3d7
sebadu <sebastian.duden@gv.hamburg.de>
2015-05-07 13:09:06 +0200

    refactor main.js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,39 @@
 require.config({
     waitSeconds: 60,
     paths: {
-        openlayers: '../_libs/openlayers/js/ol-debug',
-        jquery: '../components/jquery/dist/jquery',
-        jqueryui: '../components/jquery-ui/jquery-ui.min',
-        underscore: '../components/underscore/underscore',
-        backbone: '../components/backbone/backbone',
-        text: '../components/requirejs-text/text',
-        bootstrap: '../components/bootstrap/dist/js/bootstrap',
-        proj4: '../components/proj4/dist/proj4',
-        eventbus: 'EventBus',
-        views: 'views',
-        models: 'models',
-        collections: 'collections',
-        config: window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/') +1 ) + 'config',
-        app: 'app',
-        templates: '../templates',
-        modules: '../modules',
-        videojs: '../_libs/video-js/video.dev'
+        openlayers: "../_libs/openlayers/js/ol-debug",
+        jquery: "../components/jquery/dist/jquery",
+        jqueryui: "../components/jquery-ui/jquery-ui.min",
+        underscore: "../components/underscore/underscore",
+        backbone: "../components/backbone/backbone",
+        text: "../components/requirejs-text/text",
+        bootstrap: "../components/bootstrap/dist/js/bootstrap",
+        proj4: "../components/proj4/dist/proj4",
+        eventbus: "EventBus",
+        views: "views",
+        models: "models",
+        collections: "collections",
+        config: window.location.pathname.substring(0, window.location.pathname.lastIndexOf("/") +1 ) + "config",
+        app: "app",
+        templates: "../templates",
+        modules: "../modules",
+        videojs: "../_libs/video-js/video.dev"
     },
     shim: {
-        bootstrap: {
-            deps: ['jquery']
-        },
-        openlayers: {
-            exports: 'ol'
-        },
         jqueryui: {
             exports: "$",
-            deps: ['jquery']
+            deps: ["jquery"]
+        },
+        bootstrap: {
+            deps: ["jquery"]
+        },
+        openlayers: {
+            exports: "ol"
         }
     },
-    urlArgs: 'bust=' +  (new Date()).getTime()
+    urlArgs: "bust=" +  (new Date()).getTime()
 });
 
-define(['app'], function (App) {
-    console.log('Application initialized');
+define(["app"], function () {
+    console.log("Application initialized");
 });
