043b31d2e8a71933ef3e327f103843a9ace9c758
sebadu <sebastian.duden@gv.hamburg.de>
2015-05-22 11:35:24 +0200

    add module treeconfig

---
+++ modules/treeconfig/model.js
@@ -0,0 +1,72 @@
+define([
+    "backbone"
+], function (Backbone) {
+
+    var model = Backbone.Model.extend({
+        /**
+         * {Array} layers - Aufbereitete Layer-Objeke für die Config
+         */
+        defaults: {
+            layers: []
+        },
+
+        /**
+         * [initialize description]
+         * @return {[type]} [description]
+         */
+        initialize: function () {
+            if (this.has("childnodes")) {
+                this.setNodeChildLayer();
+            }
+            if (this.has("layerIDs")) {
+                this.setNodeLayer();
+            }
+        },
+
+        /**
+         * Fügt die Layer der ersten Ebene(nodeLayer) dem Attribut "layers" hinzu.
+         */
+        setNodeLayer: function () {
+            _.each(this.get("layerIDs"), function (element) {
+                this.push("layers", {id: element, kategorieCustom: this.get("node")});
+            }, this);
+        },
+
+        /**
+         * Fügt die Layer der zweiten Ebene(nodeChildLayer) dem Attribut "layers" hinzu.
+         */
+        setNodeChildLayer: function () {
+            _.each(this.get("childnodes"), function (subfolder) {
+                _.each(subfolder.layerIDs, function (ele) {
+                    // if (_.has(subfolder, "node")) {
+                        this.push("layers", {
+                            id: ele,
+                            subfolder: subfolder.node,
+                            kategorieCustom: this.get("node")
+                        });
+                    // }
+                    // else {
+                    //     this.push("layers", {
+                    //         id: ele,
+                    //         kategorieCustom: this.get("node")
+                    //     });
+                    // }
+                }, this);
+            }, this);
+        },
+
+        /**
+         * Hilfsmethode um ein Attribut vom Typ Array zu setzen.
+         * {String} attribute - Das Attribut das gesetzt werden soll
+         * {whatever} value - Der Wert des Attributs
+         */
+        push: function (attribute, value) {
+            var clonedAttribute = _.clone(this.get(attribute));
+
+            clonedAttribute.push(value);
+            this.set(attribute, clonedAttribute);
+        }
+    });
+
+    return model;
+});
