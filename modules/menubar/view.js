20d0267c192d2dd8be88994c2927b645aadcb708
sebadu <sebastian.duden@gv.hamburg.de>
2015-06-11 11:50:16 +0200

    fix Layer (CustromTree) initial laden funktioniert nach dem "bauen" nicht mehr.

--- modules/menubar/view.js
+++ modules/menubar/view.js
@@ -1,50 +1,50 @@
 define([
-    'jquery',
-    'underscore',
-    'backbone',
-    'text!modules/menubar/template.html',
-    'views/LayerListView',
-    'modules/menubar/model',
-    'config',
-    'eventbus'
-], function ($, _, Backbone, MenubarTemplate, LayerListView, Menubar, Config, EventBus) {
+    "backbone",
+    "text!modules/menubar/template.html",
+    "views/LayerListView",
+    "modules/menubar/model",
+    "config",
+    "eventbus"
+], function (Backbone, MenubarTemplate, LayerListView, Menubar, Config, EventBus) {
 
     var MenubarView = Backbone.View.extend({
         model: Menubar,
-        tagName: 'nav',
-        className: 'navbar navbar-default navbar-fixed-top',
-        attributes: {"role": "navigation"},
+        tagName: "nav",
+        className: "navbar navbar-default navbar-fixed-top",
+        attributes: {role: "navigation"},
         template: _.template(MenubarTemplate),
         initialize: function () {
-            EventBus.on('appendItemToMenubar', this.appendItemToMenubar, this);
+            EventBus.on("appendItemToMenubar", this.appendItemToMenubar, this);
             this.render();
-            $('#tree').on({
-                "click": function (e) {
+            $("#tree").on({
+                click: function (e) {
                     e.stopPropagation();
                 }
             });
         },
         events: {
-            'click .filterTree': 'activateFilterTree',
-            'click .filterWfsFeature': 'activateWfsFilter',
-            'click .legend': 'activateLegend',
-            'click .routingModul': 'activateRoutingModul',
-            'click .wfsFeatureFilter': 'activateWfsFeatureFilter'
+            "click .filterTree": "activateFilterTree",
+            "click .filterWfsFeature": "activateWfsFilter",
+            "click .legend": "activateLegend",
+            "click .routingModul": "activateRoutingModul",
+            "click .wfsFeatureFilter": "activateWfsFeatureFilter"
         },
         render: function () {
             var attr = this.model.toJSON();
-            $('body').append(this.$el.append(this.template(attr)));
-            if(Config.isMenubarVisible === false) {
-                $('#navbarRow').css('display', 'none');
+
+            $("body").append(this.$el.append(this.template(attr)));
+            if (Config.isMenubarVisible === false) {
+                $("#navbarRow").css("display", "none");
             }
             if (_.has(Config, "tree") === true) {
-                require(["modules/layercatalog/listView", "modules/layertree/view", "modules/layerselection/listView", "modules/baselayercatalog/listView"], function (TreeListView, LayerTreeView, LayerSelectionListView, BaseLayerListView) {
+                require(["modules/layerselection/listView", "modules/layercatalog/listView", "modules/layertree/view", "modules/baselayercatalog/listView"], function (LayerSelectionListView, TreeListView, LayerTreeView, BaseLayerListView) {
+                    new LayerSelectionListView();
                     new LayerTreeView();
                     new TreeListView();
-                    new LayerSelectionListView();
                     new BaseLayerListView();
                 });
-            } else {
+            }
+            else {
                 new LayerListView();
             }
             if (_.has(Config, "title") === true) {
@@ -55,27 +55,28 @@ define([
             // new OpenDataTreeList();
         },
         appendItemToMenubar: function (obj) {
-            var html = '<li>';
-            html += '<a href="#" class="menuitem ' + obj.classname + '">';
-            html += '<span class="' + obj.symbol + '"></span>&nbsp;' + obj.title;
-            html += '</a>';
-            html += '</li>';
+            var html = "<li>";
+
+            html += "<a href='#' class='menuitem ' + obj.classname + ''>";
+            html += "<span class='' + obj.symbol + ''></span>&nbsp;" + obj.title;
+            html += "</a>";
+            html += "</li>";
             $(".menubarlgv").append(html);
-            $("." + obj.classname).on('click', function (evt) {
-                EventBus.trigger("toggleWin", [evt.target.className.split(' ')[1], evt.target.text, evt.target.children[0].className]);
+            $("." + obj.classname).on("click", function (evt) {
+                EventBus.trigger("toggleWin", [evt.target.className.split(" ")[1], evt.target.text, evt.target.children[0].className]);
             });
         },
         activateFilterTree: function () {
-            EventBus.trigger('toggleWin', ['treefilter', 'Filtereinstellungen', 'glyphicon-filter']);
+            EventBus.trigger("toggleWin", ["treefilter", "Filtereinstellungen", "glyphicon-filter"]);
         },
         activateWfsFilter: function () {
-            EventBus.trigger('toggleFilterWfsWin');
+            EventBus.trigger("toggleFilterWfsWin");
         },
         activateLegend: function () {
-            EventBus.trigger('toggleLegendWin');
+            EventBus.trigger("toggleLegendWin");
         },
         activateRoutingModul: function () {
-            EventBus.trigger('toggleWin', ['routing', 'Routenplaner', 'glyphicon-road']);
+            EventBus.trigger("toggleWin", ["routing", "Routenplaner", "glyphicon-road"]);
         },
         activateWfsFeatureFilter: function () {
             EventBus.trigger("toggleWin", ["wfsfeaturefilter", "Filter", "glyphicon-filter"]);
