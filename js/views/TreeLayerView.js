3d14f28b4d5c3f21f39f44952fdac136971a0b3c
sebadu <sebastian.duden@gv.hamburg.de>
2015-06-18 11:16:35 +0200

    refactor treelayerview

--- js/views/TreeLayerView.js
+++ js/views/TreeLayerView.js
@@ -1,30 +1,28 @@
 define([
-    'jquery',
-    'underscore',
-    'backbone',
-    'text!../../templates/TreeLayer.html',
-    'text!../../templates/TreeLayerSetting.html',
-    'eventbus'
-    ], function ($, _, Backbone, TreeLayerTemplate, TreeLayerSettingTemplate, EventBus) {
+    "backbone",
+    "text!../../templates/TreeLayer.html",
+    "text!../../templates/TreeLayerSetting.html",
+    "eventbus"
+    ], function (Backbone, TreeLayerTemplate, TreeLayerSettingTemplate, EventBus) {
 
         var TreeLayerView = Backbone.View.extend({
-            className : 'list-group-item',
-            tagName: 'li',
+            className: "list-group-item",
+            tagName: "li",
             template: _.template(TreeLayerTemplate),
             templateSetting: _.template(TreeLayerSettingTemplate),
             events: {
-                'click .plus': 'upTransparence',
-                'click .minus': 'downTransparence',
-                'click .info': 'getMetadata',
-                'click .check, .unchecked, small': 'toggleVisibility',
-                'click .up, .down': 'moveLayer',
-                'click .refresh': 'toggleSettings'
+                "click .plus": "upTransparence",
+                "click .minus": "downTransparence",
+                "click .info": "getMetadata",
+                "click .check, .unchecked, small": "toggleVisibility",
+                "click .up, .down": "moveLayer",
+                "click .refresh": "toggleSettings"
             },
             render: function (model) {
                 this.stopListening();
-                this.listenToOnce(this.model, 'change:visibility', this.render);
-                this.listenToOnce(this.model, 'change:transparence', this.render);
-                this.listenToOnce(this.model, 'change:settings', this.render);
+                this.listenToOnce(this.model, "change:visibility", this.render);
+                this.listenToOnce(this.model, "change:transparence", this.render);
+                this.listenToOnce(this.model, "change:settings", this.render);
                 this.delegateEvents();
                 var attr = this.model.toJSON();
 
@@ -50,30 +48,31 @@ define([
                 return this;
             },
             animateView: function (template) {
-                this.$el.animate({width: '10%'}, 500, function () {
-                    $(this).html(template).animate({width: '100%'}, 500);
+                this.$el.animate({width: "10%"}, 500, function () {
+                    $(this).html(template).animate({width: "100%"}, 500);
                 });
             },
             moveLayer: function (evt) {
                 var className = evt.currentTarget.className;
-                if (className.search('down') !== -1) {
-                    EventBus.trigger('moveLayer', [-1, this.model.get('layer')]);
+
+                if (className.search("down") !== -1) {
+                    EventBus.trigger("moveLayer", [-1, this.model.get("layer")]);
                 }
-                else if (className.search('up') !== -1) {
-                    EventBus.trigger('moveLayer', [1, this.model.get('layer')]);
+                else if (className.search("up") !== -1) {
+                    EventBus.trigger("moveLayer", [1, this.model.get("layer")]);
                 }
             },
-            upTransparence: function (evt) {
+            upTransparence: function () {
                 this.model.setUpTransparence(10);
             },
-            downTransparence: function (evt) {
+            downTransparence: function () {
                 this.model.setDownTransparence(10);
             },
-            toggleVisibility: function (evt) {
+            toggleVisibility: function () {
                 this.model.toggleVisibility();
             },
             getMetadata: function () {
-                window.open('http://hmdk.de/trefferanzeige?docuuid=' + this.model.get('metaID'), "_blank");
+                this.model.openMetadata();
             },
             toggleSettings: function () {
                 this.model.toggleSettings();
