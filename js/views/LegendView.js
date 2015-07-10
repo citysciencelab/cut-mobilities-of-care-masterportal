88b5fe584587050c1ec58a5c8a387eac104f4040
sebadu <sebastian.duden@gv.hamburg.de>
2015-07-10 11:52:08 +0200

    fix Legende bei Grouplayern fehlerhaft. refactor LegendView.

--- js/views/LegendView.js
+++ js/views/LegendView.js
@@ -1,45 +1,44 @@
 define([
-    'jquery',
-    'underscore',
-    'backbone',
-    'text!templates/Legend.html',
-    'models/Legend',
-    'eventbus'
-], function ($, _, Backbone, LegendTemplate, Legend, EventBus) {
+    "backbone",
+    "text!templates/Legend.html",
+    "models/Legend",
+    "eventbus"
+], function (Backbone, LegendTemplate, Legend, EventBus) {
 
     var LegendView = Backbone.View.extend({
         model: Legend,
-        id: 'base-modal-legend',
-        className: 'modal bs-example-modal-sm legend fade in',
+        id: "base-modal-legend",
+        className: "modal bs-example-modal-sm legend fade in",
         template: _.template(LegendTemplate),
         initialize: function () {
             this.getVisibleLayer();
             this.render();
-            EventBus.on('toggleLegendWin', this.toggleLegendWin, this);
-            EventBus.on('changeView', this.changeView, this);
-            EventBus.on('setMap', this.setMap, this);
-            EventBus.trigger('getMap', this);
+            EventBus.on("toggleLegendWin", this.toggleLegendWin, this);
+            EventBus.on("changeView", this.changeView, this);
+            EventBus.on("setMap", this.setMap, this);
+            EventBus.trigger("getMap", this);
 
-            //EventBus.on('sendAllVisibleLayer', this.getVisibleLayer, this);
-            EventBus.on('aftercollapse', this.aftercollapse, this);
-            $(document.body).on('hide.bs.modal', '#base-modal-legend', this, function(evt) {
-                EventBus.trigger('changeView', this);
+            // EventBus.on("sendAllVisibleLayer", this.getVisibleLayer, this);
+            EventBus.on("aftercollapse", this.aftercollapse, this);
+            $(document.body).on("hide.bs.modal", "#base-modal-legend", this, function () {
+                EventBus.trigger("changeView", this);
             });
 
         },
         events: {
             "click .list-group-item.type": "collapseHits"
         },
-        show: function (params) {
+        show: function () {
             this.render();
             this.$el.modal({
                 backdrop: true,
                 show: true
             });
-            this.model.set('visibLegend', '');
+            this.model.set("visibLegend", "");
         },
         render: function () {
             var attr = this.model.toJSON();
+
             this.$el.html(this.template(attr));
         },
 
@@ -54,117 +53,119 @@ define([
             });
             return visibleLayer;
         },
-        getVisibleLayer: function(evt){
-
-            layers = this.getAllVisibleLayer();
-            var legendParams=[], groupArray=[],layeridarray=[];
+        getVisibleLayer: function () {
+            var layers = this.getAllVisibleLayer(),
+                legendParams = [],
+                groupArray = [],
+                layeridarray = [];
 
             _.each(layers, function (element) {
-                if (element.get('typ') === 'WFS') {
-                    layeridarray.push(element.get('id'));
+                if (element.get("typ") === "WFS") {
+                    layeridarray.push(element.get("id"));
 
                     legendParams.push({
-                        typ: 'WFS',
-                        layerID:element.get('id'),
-                        source: element.get('url'),
-                        name: element.get('name'),
+                        typ: "WFS",
+                        layerID: element.get("id"),
+                        source: element.get("url"),
+                        name: element.get("name"),
 
-                        styleField: element.get('styleField'),
-                        legendURL: element.get('legendURL'),
+                        styleField: element.get("styleField"),
+                        legendURL: element.get("legendURL")
                     });
                 }
-                else if (element.get('typ') === 'WMS') {
-                    layeridarray.push(element.get('id'));
+                else if (element.get("typ") === "WMS") {
+                    if (element.get("displayInTree") === true) {
+                        layeridarray.push(element.get("id"));
+                        legendParams.push({
+                            typ: "WMS",
+                            layerID: element.get("id"),
+                            source: element.get("url"),
+                            name: element.get("name"),
+                            legendURL: element.get("legendURL"),
 
-                    legendParams.push({
-                        typ:'WMS',
-                        layerID:element.get('id'),
-                        source:element.get('url'),
-                        name: element.get('name'),
-                        legendURL: element.get('legendURL'),
-
-                        layers: element.get('layers'),
-                        styles:element.get('styles')
-
-                    })
-                }
-                else if (element.get('typ') === 'GROUP') {
-                    groupArray=[];
-
-                    _.each(element.get("layer").getLayers().getArray(), function (layerarray, indexarray){
-                        layeridarray.push(layerarray.id);
-                        groupArray.push({
-                            typ:'GROUP',
-                            layerID:layerarray.id,
-                            source:layerarray.getSource().getUrls()[0],
-                            name: layerarray.get('name'),
-                            legendURL: layerarray.getProperties().legendURL,
-                            layers: layerarray.getSource().getParams().LAYERS,
-                            styles:layerarray.getSource().getParams().STYLES
-
+                            layers: element.get("layers"),
+                            styles: element.get("styles")
                         });
-                    })
+                    }
+                }
+                else if (element.get("typ") === "GROUP") {
+                    groupArray = [];
+                    _.each(element.get("layer").getLayers().getArray(), function (layerarray) {
+                        layeridarray.push(layerarray.id);
+                        // die Abfrage muss irgendwie sein verstehe nicht so ganz warum SD
+                        if (layerarray.getSource().getUrls) {
+                            groupArray.push({
+                                typ: "GROUP",
+                                layerID: layerarray.id,
+                                source: layerarray.getSource().getUrls()[0],
+                                name: layerarray.get("name"),
+                                legendURL: layerarray.getProperties().legendURL,
+                                layers: layerarray.getSource().getParams().LAYERS,
+                                styles: layerarray.getSource().getParams().STYLES
+
+                            });
+                        }
+                    });
                     legendParams.push({
-                        typ:'GROUP',
-
-                        layerID:element.get('id'),
-
-                        name: element.get('name'),
-                        layers:groupArray})
+                        typ: "GROUP",
+                        layerID: element.get("id"),
+                        name: element.get("name"),
+                        layers: groupArray
+                    });
                 }
             });
 
-            this.model.set('layeridArray',layeridarray);
-            this.model.set('params',legendParams);
+            this.model.set("layeridArray", layeridarray);
+            this.model.set("params", legendParams);
             this.model.setAttributions();
 
         },
-        toggleLegendWin: function (){
-            if(this.model.get('visibLegend')===''){
-                EventBus.trigger('getAllVisibleLayer', this);
+        toggleLegendWin: function () {
+            if (this.model.get("visibLegend") === "") {
+                EventBus.trigger("getAllVisibleLayer", this);
             }
             this.show();
         },
         setMap: function (map) {
-            this.model.set('map', map);
+            this.model.set("map", map);
         },
-        "collapseHits": function (evt) {
-            $(".list-group-item.type + div").hide("fast");  // schließt alle Reiter
+        collapseHits: function (evt) {
+            $(".list-group-item.type + div").hide("fast"); // schließt alle Reiter
             if ($(evt.currentTarget.nextElementSibling).css("display") === "block") {
                 $(evt.currentTarget.nextElementSibling).hide("fast");
             }
             else {
-                var legendObject={
-                    layernameLi:$(evt.currentTarget).height(),
-                    heightDIVLi:$(evt.currentTarget.nextElementSibling.children),
+                var legendObject = {
+                    layernameLi: $(evt.currentTarget).height(),
+                    heightDIVLi: $(evt.currentTarget.nextElementSibling.children),
+                    legendDIVLiListLength: $(evt.currentTarget.parentNode).children("li").length,
+                    currentTargetLi: evt.currentTarget.nextElementSibling.nextElementSibling.innerHTML * 1,
+                    maxHeightLegendDIV: $(evt.currentTarget.parentNode).css("max-height").split("p")[0] * 1
+                };
 
-
-                    legendDIVLiListLength:$(evt.currentTarget.parentNode).children('li').length,
-                    currentTargetLi:evt.currentTarget.nextElementSibling.nextElementSibling.innerHTML*1,
-                    maxHeightLegendDIV:$(evt.currentTarget.parentNode).css("max-height").split('p')[0]*1
-                }
-                $.when( $(evt.currentTarget.nextElementSibling).show("fast") ).then(function(){
-                    EventBus.trigger('aftercollapse',legendObject);
+                $.when($(evt.currentTarget.nextElementSibling).show("fast")).then(function () {
+                    EventBus.trigger("aftercollapse", legendObject);
                 });
             }
         },
-        aftercollapse: function (legendObject){
-            var heightlegendbody=$('#legendbody').css("height").split('p')[0]*1;
-            _.each($('.legenddiv'), function(element, index){
-                element.style.maxHeight=(legendObject.maxHeightLegendDIV-(8*legendObject.layernameLi))+"px";
+        aftercollapse: function (legendObject) {
+            var heightlegendbody = $("#legendbody").css("height").split("p")[0] * 1;
+
+            _.each($(".legenddiv"), function (element) {
+                element.style.maxHeight = (legendObject.maxHeightLegendDIV - (8 * legendObject.layernameLi)) + "px";
             });
 
-            if(legendObject.currentTargetLi+1===legendObject.legendDIVLiListLength){
-                this.model.set('scrollHeight',heightlegendbody);
+            if (legendObject.currentTargetLi + 1 === legendObject.legendDIVLiListLength) {
+                this.model.set("scrollHeight", heightlegendbody);
             }
-            else{
-            this.model.set('scrollHeight',(legendObject.layernameLi*legendObject.currentTargetLi)/(heightlegendbody/(heightlegendbody+$(legendObject.heightDIVLi[0]).css('height').split('p')[0]*1)));
+            else {
+                this.model.set("scrollHeight", (legendObject.layernameLi * legendObject.currentTargetLi) / (heightlegendbody / (heightlegendbody + $(legendObject.heightDIVLi[0]).css("height").split("p")[0] * 1)));
             }
-            $('#legendbody').scrollTop(this.model.get('scrollHeight'));
+            $("#legendbody").scrollTop(this.model.get("scrollHeight"));
 
         },
-        changeView: function(){
-            this.model.set('visibLegend', $('.legendbody img:visible'));
+        changeView: function () {
+            this.model.set("visibLegend", $(".legendbody img:visible"));
         }
     });
 
