1d822886f9cb7963bf9fea35f881f17eaa1bd24c
sebadu <sebastian.duden@gv.hamburg.de>
2015-07-17 09:52:05 +0200

    add GeoAPI - externer EventBus

--- js/GeoAPI.js
+++ js/GeoAPI.js
@@ -1,7 +1,39 @@
-define([
-    "backboneradio"
-], function (Radio) {
-    var GeoAPI = this.GeoAPI = Radio.channel("GeoAPI");
+define("GeoAPI", [
+    "backbone",
+    "eventbus"
+], function (Backbone, EventBus) {
 
-    return GeoAPI; // http://underscorejs.org/#extend
+    var GeoAPI = this.GeoAPI = _.extend({}, Backbone.Events);
+
+    /**
+     * [on description]
+     * @param  {[type]} "addFeatures" [description]
+     * @param  {[type]} function      (name,        features [description]
+     * @return {[type]}               [description]
+     */
+    GeoAPI.on("addFeatures", function (name, features) {
+        EventBus.trigger("addFeatures", name, features);
+    });
+
+    /**
+     * [on description]
+     * @param  {[type]} "removeFeatures" [description]
+     * @param  {[type]} function         (name         [description]
+     * @return {[type]}                  [description]
+     */
+    GeoAPI.on("removeFeatures", function (name) {
+        EventBus.trigger("removeFeatures", name);
+    });
+
+    /**
+     * [on description]
+     * @param  {[type]} "getDrawendCoords" [description]
+     * @param  {[type]} function           (coordinates  [description]
+     * @return {[type]}                    [description]
+     */
+    EventBus.on("getDrawendCoords", function (coordinates) {
+        GeoAPI.trigger("getDrawendCoords", coordinates);
+    });
+
+    return GeoAPI;
 });
