7c5d995f681bb71682e5ec2f99129bc418a84d72
sebadu <sebastian.duden@gv.hamburg.de>
2015-08-13 10:49:33 +0200

    update Konfiguration FHH-Atlas

--- portale/fhh-atlas/config.js
+++ portale/fhh-atlas/config.js
@@ -10,11 +10,12 @@ define(function () {
                 ["368", "369", "370", "371", "372", "373", "374", "375", "376", "377", "378", "379", "380", "381", "382", "383", "384", "385", "386", "387", "388", "389", "390", "391", "392", "393", "394", "395", "396", "397"]
             ],
             // "DC71F8A1-7A8C-488C-AC99-23776FA7775E" "DFDA2969-A041-433B-BD65-4CDA9F830A55",
-            groupLayerByID: ["38575F13-7FA2-4F26-973F-EDED24D937E5", "757A328B-415C-4E5A-A696-353ABDC80419", "335B680C-CA3E-4FE9-BC05-641BA565E366", "3EE8938B-FF9E-467B-AAA2-8534BB505580", "19A39B3A-2D9E-4805-A5E6-56A5CA3EC8CB"]
+            groupLayerByID: ["B6A59A2B-2D40-4676-9094-0EB73039ED34", "38575F13-7FA2-4F26-973F-EDED24D937E5", "757A328B-415C-4E5A-A696-353ABDC80419", "335B680C-CA3E-4FE9-BC05-641BA565E366", "3EE8938B-FF9E-467B-AAA2-8534BB505580", "19A39B3A-2D9E-4805-A5E6-56A5CA3EC8CB"]
         },
         baseLayerIDs: [
             {id: "453", visibility: true, minResolution: 0.2645838625010583, minScale: "1000"},
             {id: "8", visibility: false},
+            {id: "717", visibility: false},
             {id: "368", visibility: false, name: "ALKIS farbig"},
             {id: "149", visibility: false, name: "ALKIS grau-blau"}
         ],
