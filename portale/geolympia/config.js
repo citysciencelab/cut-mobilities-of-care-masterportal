7bc2a3b38fa1567151d9b0368fd6f2ac8d6eb757
sebadu <sebastian.duden@gv.hamburg.de>
2015-08-31 10:44:56 +0200

    update Bürgerportal Konfiguration.

--- portale/geolympia/config.js
+++ portale/geolympia/config.js
@@ -81,13 +81,13 @@ define(function () {
             },
             getFeatures: [
                 {
-                    url: "/wscd0096/fachdaten_public/services/wfs_hh_olympiastandorte?service=WFS&request=GetFeature&version=2.0.0",
+                    url: "/geofos/fachdaten_public/services/wfs_hh_olympiastandorte?service=WFS&request=GetFeature&version=2.0.0",
                     typeName: "olympia_sportarten_paralympic",
                     propertyName: "staette,art,piktogramm,geom",
                     filter: "paralympia"
                 },
                 {
-                    url: "/wscd0096/fachdaten_public/services/wfs_hh_olympiastandorte?service=WFS&request=GetFeature&version=2.0.0",
+                    url: "/geofos/fachdaten_public/services/wfs_hh_olympiastandorte?service=WFS&request=GetFeature&version=2.0.0",
                     typeName: "olympia_sportarten",
                     propertyName: "staette,art,piktogramm,geom",
                     filter: "olympia"
