14557fa961c2160b58c0dfdfe111fdfa2c7094a3
schmity <sebastian.schmitz@gv.hamburg.de>
2015-09-16 11:42:06 +0200

    update ignore und mergelayerIDs angepasst

--- portale/fhh-atlas/config.js
+++ portale/fhh-atlas/config.js
@@ -25,10 +25,10 @@ define(function () {
             metaIDsForMerge: [
                 "38575F13-7FA2-4F26-973F-EDED24D937E5", // Landesgrundbesitzverzeichnis
                 "CD1AA43B-9C7E-4E85-B0A3-8619101D0517", // Landesgrundbesitzverzeichnis FHHNet
-                "757A328B-415C-4E5A-A696-353ABDC80419", // ParkraumGIS
-                "335B680C-CA3E-4FE9-BC05-641BA565E366", // Lärmminderungsplanung, Fluglärm
+                // "757A328B-415C-4E5A-A696-353ABDC80419", // ParkraumGIS
+                "4AC1B569-65AA-4FAE-A5FC-E477DFE5D303", // Großraum- und Schwertransport-Routen in Hamburg
                 "3EE8938B-FF9E-467B-AAA2-8534BB505580", // Bauschutzbereich § 12 LuftVG Hamburg
-                "19A39B3A-2D9E-4805-A5E6-56A5CA3EC8CB", // HH-SIB
+                "19A39B3A-2D9E-4805-A5E6-56A5CA3EC8CB", // Straßen HH-SIB
                 "F691CFB0-D38F-4308-B12F-1671166FF181", // Flurstücke gelb
                 "FE4DAF57-2AF6-434D-85E3-220A20B8C0F1" // Flurstücke schwarz
             ],
@@ -37,7 +37,11 @@ define(function () {
                 "51656D3F-E801-497C-952C-4F1F605843DD", // MRH Metrokarte
                 "AD579C62-0471-4FA5-8C9A-38B3DCB5B2CB", // MRH Übersichtskarte-blau
                 "14E3AFAE-99BE-4F1D-A3A6-6A68A1CDAC7B", // MRH Übersichtskarte-grün
-                "56110E55-72C7-41F2-9F92-1C598E4E0A02" // Digitale Karte Metropolregion
+                "56110E55-72C7-41F2-9F92-1C598E4E0A02", // Digitale Karte Metropolregion
+                "88A22736-FE87-46F7-8A38-84F9E0E945F7", // TN für Olympia
+                "DDB01922-D7B5-4323-9DDF-B68A42C559E6", // Olympiastandorte
+                "AA06AD76-6110-4718-89E1-F1EDDA1DF4CF", // Regionales Raumordnungsprogramm Stade+Rotenburg
+                "1C8086F7-059F-4ACF-96C5-7AFEB8F8B751" // Fachdaten der Metropolregion
             ]
         },
         baseLayer: [
@@ -78,9 +82,7 @@ define(function () {
         startUpModul: "",
         searchBar: {
             placeholder: "Suche Adresse, Stadtteil, Themen, Flurstück",
-            gazetteerURL: function () {
-                return "/geofos/dog_hh/services/wfs?service=WFS&request=GetFeature&version=2.0.0";
-            }
+            gazetteerURL: "/geofos/dog_hh/services/wfs?service=WFS&request=GetFeature&version=2.0.0"
         },
         tools: {
             gfi: true,
