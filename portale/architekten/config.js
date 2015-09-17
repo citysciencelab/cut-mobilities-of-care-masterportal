fa1e0934a54da3d01a7d7d6c8fcbe5c827ff2c9d
Angela Bialluch <angela.bialluch@gv.hamburg.de>
2015-09-17 15:35:50 +0200

    update portale/architekten/config

--- portale/architekten/config.js
+++ portale/architekten/config.js
@@ -7,18 +7,12 @@ define(function () {
             custom: true,
             filter: false,
             orderBy: "architekten",
-            customConfig: "../components/lgv-config/tree-config/architekten.json"
+            customConfig: "../components/lgv-config/tree-config/architekten.json",
             // groupLayerByID: ["DFDA2969-A041-433B-BD65-4CDA9F830A55","38575F13-7FA2-4F26-973F-EDED24D937E5", "757A328B-415C-4E5A-A696-353ABDC80419", "335B680C-CA3E-4FE9-BC05-641BA565E366", "DC71F8A1-7A8C-488C-AC99-23776FA7775E", "3EE8938B-FF9E-467B-AAA2-8534BB505580","19A39B3A-2D9E-4805-A5E6-56A5CA3EC8CB"]
+          
+            
         },
-        baseLayer: [
-            {id: "453", visibility: true, minScale: "1000"},
-            {id: "8", visibility: false},
-            {id: "717", visibility: true, minScale: "1000", name: "Geobasiskarten (farbig)"},
-            {id: "713", visibility: false, minScale: "1000", name: "Geobasiskarten (schwarz-weiß)"},
-            {id: "1043", visibility: false, minScale: "1000", name: "Geobasiskarten (grau-blau)"},
-            {id: "368", visibility: false, name: "ALKIS farbig", maxScale: "10000"},
-            {id: "149", visibility: false, name: "ALKIS grau-blau", maxScale: "10000"}
-        ],
+        
         view: {
             center: [565874, 5934140] // Rathausmarkt
         },
@@ -27,7 +21,7 @@ define(function () {
         styleConf: "../components/lgv-config/style.json",
         print: {
             printID: "99999",
-            title: "Olympia-Portal",
+            title: "Architekten-Portal",
             gfi: false
         },
         proxyURL: "/cgi-bin/proxy.cgi",
