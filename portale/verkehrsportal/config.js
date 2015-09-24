dbac68d511efd22c20897c90e2184195b9d544d8
Michael Bieler <michael.bieler@gv.hamburg.de>
2015-09-24 06:40:15 +0200

    add reisezeiten-theme to reisezeiten

--- portale/verkehrsportal/config.js
+++ portale/verkehrsportal/config.js
@@ -6,71 +6,71 @@ define(function () {
             center: [561210, 5932600],
             extent: [454591, 5809000, 700000, 6075769]
         },
-        layerConf: '../components/lgv-config/services-fhhnet.json',
-        categoryConf: '../components/lgv-config/category.json',
-        styleConf: '../components/lgv-config/style.json',
-        proxyURL: '/cgi-bin/proxy.cgi',
+        layerConf: "../components/lgv-config/services-fhhnet.json",
+        categoryConf: "../components/lgv-config/category.json",
+        styleConf: "../components/lgv-config/style.json",
+        proxyURL: "/cgi-bin/proxy.cgi",
         customModules: ["../portale/verkehrsportal/verkehrsfunctions"],
         layerIDs: [
-            {id: '453', visible: true},
-            {id: '452', visible: false},
-            {id: '2092', visible: false},
+            {id: "453", visible: true},
+            {id: "452", visible: false},
+            {id: "2092", visible: false},
             {id:
              [
                  {
-                     id: '946',
+                     id: "946",
                      attribution:
                      {
-                         eventname: 'aktualisiereverkehrsnetz',
+                         eventname: "aktualisiereverkehrsnetz",
                          timeout: (10 * 60000)
                      }
                  },
                  {
-                     id: '947'
+                     id: "947"
                  }
              ],
-             name: 'Verkehrslage auf Autobahnen', visible: false
+             name: "Verkehrslage auf Autobahnen", visible: false
             },
-            {id: '1935', visible: false, styles: ['geofox_Faehre', 'geofox-bahn'], name: ["HVV Fährverbindungen", "HVV Bahnlinien"]},
+            {id: "1935", visible: false, styles: ["geofox_Faehre", "geofox-bahn"], name: ["HVV Fährverbindungen", "HVV Bahnlinien"]},
             {id:
              [
                 {
-                    id: '1935',
+                    id: "1935",
                     name: "Bus1"
                 },
                 {
-                    id: '1935',
+                    id: "1935",
                     name: "Bus2"
                 }
              ],
-             visible: false, name: 'HVV Buslinien', styles: ['geofox-bus', 'geofox_BusName']
+             visible: false, name: "HVV Buslinien", styles: ["geofox-bus", "geofox_BusName"]
             },
-            {id: '1933', visible: false, styles: 'geofox_stations', name: "HVV Haltestellen"},
-            {id: '46', visible: false, style: '46', clusterDistance: 60, searchField: '', mouseHoverField: '', filterOptions: [], routable: true},
-            {id: '48', visible: false, style: '48', clusterDistance: 40, searchField: '', mouseHoverField: '', filterOptions: [], styleLabelField: '', routable: true},
-            {id: '50', visible: false, style: '50', clusterDistance: 40, searchField: '', mouseHoverField: '', filterOptions: [], styleLabelField: '', routable: true},
-            {id: '53', visible: false, style: '53', clusterDistance: 40, searchField: '', mouseHoverField: '', filterOptions: [], styleLabelField: '', routable: true},
-            {id: '2404', visible: false, style: '45', clusterDistance: 40, searchField: '', mouseHoverField: '', filterOptions: [], styleLabelField: '', routable: true},
-            {id: '2403', visible: false, style: '51', clusterDistance: 40, searchField: '', mouseHoverField: '', filterOptions: [], styleLabelField: '', routable: true},
-            {id: '52', visible: false, style: '52', clusterDistance: 30, searchField: '', mouseHoverField: '', filterOptions: [], styleLabelField: '', styleField: 'situation', routable: true},
-            {id: '2128', visible: false, style: '2128', clusterDistance: 0, searchField: '', mouseHoverField: '', filterOptions: [], styleLabelField: ''},
-            {id: '47', visible: false, style: '47', clusterDistance: 0, searchField: '', mouseHoverField: '', filterOptions: [], styleLabelField: 'id_kost'},
-            {id: '2156', visible: true, style: '2156', clusterDistance: 0, searchField: '', mouseHoverField: '', filterOptions: [], styleLabelField: '', styleField: 'name', routable: false},
-            {id: '2119', visible: false, style: '2119', clusterDistance: 0, searchField: '', mouseHoverField: '', filterOptions: [], styleLabelField: ''},
-            {id: '2132', visible: false, style: '2132', clusterDistance: 0, searchField: '', mouseHoverField: '', filterOptions: [], styleLabelField: ''}
+            {id: "1933", visible: false, styles: "geofox_stations", name: "HVV Haltestellen"},
+            {id: "46", visible: false, style: "46", clusterDistance: 60, searchField: "", mouseHoverField: "", filterOptions: [], routable: true},
+            {id: "48", visible: false, style: "48", clusterDistance: 40, searchField: "", mouseHoverField: "", filterOptions: [], styleLabelField: "", routable: true},
+            {id: "50", visible: false, style: "50", clusterDistance: 40, searchField: "", mouseHoverField: "", filterOptions: [], styleLabelField: "", routable: true},
+            {id: "53", visible: false, style: "53", clusterDistance: 40, searchField: "", mouseHoverField: "", filterOptions: [], styleLabelField: "", routable: true},
+            {id: "2404", visible: false, style: "45", clusterDistance: 40, searchField: "", mouseHoverField: "", filterOptions: [], styleLabelField: "", routable: true},
+            {id: "2403", visible: false, style: "51", clusterDistance: 40, searchField: "", mouseHoverField: "", filterOptions: [], styleLabelField: "", routable: true},
+            {id: "52", visible: false, style: "52", clusterDistance: 30, searchField: "", mouseHoverField: "", filterOptions: [], styleLabelField: "", styleField: "situation", routable: true},
+            {id: "2128", visible: false, style: "2128", clusterDistance: 0, searchField: "", mouseHoverField: "", filterOptions: [], styleLabelField: ""},
+            {id: "47", visible: false, style: "47", clusterDistance: 0, searchField: "", mouseHoverField: "", filterOptions: [], styleLabelField: "id_kost"},
+            {id: "2156", visible: true, style: "2156", clusterDistance: 0, searchField: "", mouseHoverField: "", filterOptions: [], styleLabelField: "", styleField: "name", routable: false},
+            {id: "2119", gfiTheme: "reisezeiten", visible: false, style: "2119", clusterDistance: 0, searchField: "", mouseHoverField: "", filterOptions: [], styleLabelField: ""},
+            {id: "2132", visible: false, style: "2132", clusterDistance: 0, searchField: "", mouseHoverField: "", filterOptions: [], styleLabelField: ""}
         ],
         attributions: true,
         clickCounter: {
-            version: '',
-            desktop: 'http://static.hamburg.de/countframes/verkehrskarte_count.html',
-            mobil : 'http://static.hamburg.de/countframes/verkehrskarte-mobil_count.html'
+            version: "",
+            desktop: "http://static.hamburg.de/countframes/verkehrskarte_count.html",
+            mobil : "http://static.hamburg.de/countframes/verkehrskarte-mobil_count.html"
         },
         menubar: true,
         scaleLine: true,
         mouseHover: false,
         isMenubarVisible: true,
         menu: {
-            viewerName: 'GeoViewer',
+            viewerName: "GeoViewer",
             searchBar: true,
             layerTree: true,
             helpButton: false,
@@ -81,7 +81,7 @@ define(function () {
             legend: false,
             routing: true
         },
-        startUpModul: '',
+        startUpModul: "",
         searchBar: {
             placeholder: "Adresssuche",
             gazetteerURL: function () {
@@ -94,7 +94,7 @@ define(function () {
             print: false,
             draw: false,
             coord: true,
-            active: 'gfi'
+            active: "gfi"
         },
         gfiImgReloadTime: 20000,
         orientation: true,
