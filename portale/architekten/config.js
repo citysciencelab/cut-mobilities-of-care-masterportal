9e9df62a8fcf4b489c41c202ac4371957a1a7225
sebadu <sebastian.duden@gv.hamburg.de>
2015-10-15 14:02:18 +0200

    udpate Architekten-Portal.

--- portale/architekten/config.js
+++ portale/architekten/config.js
@@ -7,32 +7,21 @@ define(function () {
             custom: true,
             filter: false,
             orderBy: "architekten",
-            customConfig: "../components/lgv-config/tree-config/architekten.json",
-            // groupLayerByID: ["DFDA2969-A041-433B-BD65-4CDA9F830A55","38575F13-7FA2-4F26-973F-EDED24D937E5", "757A328B-415C-4E5A-A696-353ABDC80419", "335B680C-CA3E-4FE9-BC05-641BA565E366", "DC71F8A1-7A8C-488C-AC99-23776FA7775E", "3EE8938B-FF9E-467B-AAA2-8534BB505580","19A39B3A-2D9E-4805-A5E6-56A5CA3EC8CB"]
-
-
+            customConfig: "../components/lgv-config/tree-config/architekten.json"
         },
-          baseLayerIDs: [
-            {id: "453", visible: true},
-            {id: "452", visible: false},
-              {id: "1933", visible: false},   //Haltestellen
-            {id:
-                [
-                {id: "1935",name: "Bus1"},
-                {id: "1935",name: "Bus2"}
-                ],
-                visible: false, name: "HVV Buslinien", styles: ["geofox-bus", "geofox_BusName"]
-            },
-
-            {id: "1935", visible: false, styles: ["geofox_Faehre", "geofox-bahn"], name: ["HVV Fährverbindungen", "HVV Bahnlinien"]},
-
-            {id: "1933", visible: false, styles: "geofox_stations", name: "HVV Haltestellen"},
+          baseLayer: [
+            {id: "453", visibility: true},
+            {id: "452", visibility: false}
         ],
+        controls: {
+            zoom: true,
+            toggleMenu: true
+        },
         view: {
             center: [565874, 5934140] // Rathausmarkt
         },
-        layerConf: "../components/lgv-config/services-internet-architekten.json",
-        restConf: "../components/lgv-config/rest-services-internet.json",
+        layerConf: "../components/lgv-config/services-internet.json",
+        restConf: "../components/lgv-config/rest-services-fhhnet.json",
         styleConf: "../components/lgv-config/style.json",
         print: {
             printID: "99999",
