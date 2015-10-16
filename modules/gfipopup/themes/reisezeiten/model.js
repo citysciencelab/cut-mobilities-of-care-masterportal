9c8097749800fe57d8f0e02c45c73f98b63faf13
Michael Bieler <michael.bieler@gv.hamburg.de>
2015-10-16 18:43:11 +0200

    refactor nutze alerting statt alert

--- modules/gfipopup/themes/reisezeiten/model.js
+++ modules/gfipopup/themes/reisezeiten/model.js
@@ -145,7 +145,7 @@ define([
                 complete: function (jqXHR) {
                     Util.hideLoader();
                     if (jqXHR.status !== 200 || jqXHR.responseText.indexOf("ExceptionReport") !== -1) {
-                        alert("Dienst antwortet nicht wie erwartet. Bitte versuchen Sie es später wieder.");
+                        EventBus.trigger("alert", "Dienst antwortet nicht wie erwartet. Bitte versuchen Sie es später wieder.");
                     }
                 },
                 success: function (data) {
