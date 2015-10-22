9b67d400fefd2a2c0465ecdb985121fdfee23e1f
Michael Bieler <michael.bieler@gv.hamburg.de>
2015-10-22 08:18:02 +0200

    update mache orientation zum module

---
+++ modules/controls/orientation/view.js
@@ -0,0 +1,64 @@
+define([
+    "backbone",
+    "text!modules/controls/orientation/template.html",
+    "modules/controls/orientation/model",
+    "config",
+    "eventbus"
+], function (Backbone, OrientationTemplate, Orientation, Config, EventBus) {
+
+    var OrientationView = Backbone.View.extend({
+//        model: Orientation,
+        id: "toggleDiv",
+        template: _.template(OrientationTemplate),
+        events: {
+            "click .buttonStandpunkt": "getOrientation",
+            "click .buttonPOI": "getPOI"
+        },
+        initialize: function () {
+            if (!navigator.geolocation) {
+                return;
+            }
+            else {
+                var that = this;
+
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    that.finalize();
+                }, function (error) {
+//                    alert ("Standortbestimmung nicht verfügbar: " + error.message);
+                    return;
+                });
+            }
+        },
+        finalize: function () {
+            EventBus.on("showGeolocationMarker", this.showGeolocationMarker, this);
+            EventBus.on("clearGeolocationMarker", this.clearGeolocationMarker, this);
+            this.model = Orientation;
+            this.render();
+        },
+        showGeolocationMarker: function () {
+            $("#geolocation_marker").addClass("glyphicon glyphicon-map-marker geolocation_marker");
+        },
+        clearGeolocationMarker: function () {
+            $("#geolocation_marker").removeClass("geolocation_marker glyphicon glyphicon-map-marker");
+        },
+        render: function () {
+            // var attr = this.model.toJSON();
+            // this.getOrientation();
+            // $("#toggleRow").append(this.$el.html(this.template(attr)));
+            var attr = Config;
+
+            $("#toggleRow").append(this.$el.html(this.template(attr)));
+        },
+        getOrientation: function () {
+            this.model.setOrientation("stdPkt");
+        },
+        getPOI: function () {
+            $(function () {
+                $("#loader").show();
+            });
+            this.model.setOrientation("poi");
+        }
+    });
+
+    return OrientationView;
+});
