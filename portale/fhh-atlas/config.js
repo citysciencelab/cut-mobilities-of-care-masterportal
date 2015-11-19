14e976bf70f9b7539ef0fb842abacd32626500a9
sebadu <sebastian.duden@gv.hamburg.de>
2015-11-19 16:14:47 +0100

    update FHH-Atlas Konfiguration.

--- portale/fhh-atlas/config.js
+++ portale/fhh-atlas/config.js
@@ -5,6 +5,15 @@ define(function () {
         tree: {
             type: "default",
             orderBy: "opendata",
+            baseLayer: [
+                {id: "453", minScale: "500"},
+                {id: "8"},
+                {id: "717", visibility: true, minScale: "500", name: "Geobasiskarten (farbig)"},
+                {id: "713", minScale: "500", name: "Geobasiskarten (schwarz-weiß)"},
+                {id: "1043", minScale: "500", name: "Geobasiskarten (grau-blau)"},
+                {id: "368", name: "ALKIS farbig", maxScale: "10000"},
+                {id: "149", name: "ALKIS grau-blau", maxScale: "10000"}
+            ],
             layerIDsToMerge: [
                 ["149", "150", "151", "152", "153", "154", "155", "156", "157", "158", "159", "160", "161", "162", "163", "164", "165", "166", "167", "168", "169", "170", "171", "172", "173", "174", "175", "176", "177", "178"],
                 ["368", "369", "370", "371", "372", "373", "374", "375", "376", "377", "378", "379", "380", "381", "382", "383", "384", "385", "386", "387", "388", "389", "390", "391", "392", "393", "394", "395", "396", "397"],
@@ -49,15 +58,6 @@ define(function () {
                 "A46086BA-4A4C-48A4-AC1D-9735DDB4FDDE" // Denkmalkartierung FIS
             ]
         },
-        baseLayer: [
-            {id: "453", minScale: "500"},
-            {id: "8"},
-            {id: "717", visibility: true, minScale: "500", name: "Geobasiskarten (farbig)"},
-            {id: "713", minScale: "500", name: "Geobasiskarten (schwarz-weiß)"},
-            {id: "1043", minScale: "500", name: "Geobasiskarten (grau-blau)"},
-            {id: "368", name: "ALKIS farbig", maxScale: "10000"},
-            {id: "149", name: "ALKIS grau-blau", maxScale: "10000"}
-        ],
         controls: {
             zoom: true,
             toggleMenu: true
