9994dddcd618fff74db155a2698a31a319246781
Angela Bialluch <angela.bialluch@gv.hamburg.de>
2015-11-20 12:57:14 +0100

    update portale/hausboote/

--- portale/hausboote/config.js
+++ portale/hausboote/config.js
@@ -1,65 +1,73 @@
 define(function () {
-
     var config = {
-
-        allowParametricURL: true,
-
-        view: {
-            center: [565874, 5934140],// Rathausmarkt
-            resolution: 15.874991427504629,// 1:60.000
-            scale: 60000, // für print.js benötigt
-            extent: [454591, 5809000, 700000, 6075769]
+        tree: {
+            type: "light",
+            layer: [
+                {id: "453", visible: true, legendUrl: "ignore"},
+                {id: "452", visible: false},
+                {id: "2426", visible: false} // Bezirksgrenzen
+            ]
         },
-
-        footer: true,
+        wfsImgPath: "../components/lgv-config/img/",
+        allowParametricURL: true,
+        view: {
+            center: [565874, 5934140],
+            resolution: 15.874991427504629,// 1:60.000
+            extent: [454591, 5809000, 700000, 6075769],
+            epsg: "EPSG:25832"
+        },
+        controls: {
+            zoom: true,
+            toggleMenu: true,
+            orientation: true,
+            poi: true
+        },
+        footer: false,
         quickHelp: true,
-
         layerConf: "../components/lgv-config/services-fhhnet.json",
+        restConf: "../components/lgv-config/rest-services-fhhnet.json",
         styleConf: "../components/lgv-config/style.json",
         categoryConf: "../components/lgv-config/category.json",
         proxyURL: "/cgi-bin/proxy.cgi",
-
-        layerIDs: [
-            {id: "453", visible: true,legendUrl: "ignore"},     //Karte
-            {id: "452", visible: false},                        //Luftbild
-            {id: "2426", visible: false},            //Bezirksgrenzen
-             ],
-
-
         attributions: true,
         menubar: true,
         scaleLine: true,
         mouseHover: true,
         isMenubarVisible: true,
-
         menu: {
             viewerName: "GeoViewer",
             searchBar: true,
             layerTree: true,
-            helpButton: true,
-            contactButton: true,
+            helpButton: false,
+            contactButton: {on: true, email: "LGVGeoPortal-Hilfe@gv.hamburg.de"},
             tools: true,
             treeFilter: false,
             wfsFeatureFilter: false,
             legend: true,
-            routing: false
+            routing: true,
+            addWMS: false
         },
-
+        startUpModul: "",
         searchBar: {
-            placeholder: "Suche nach Adresse, Stadtteil",
-            gazetteerURL: function () {
-
-                return "/geofos/dog_hh/services/wfs?service=WFS&request=GetFeature&version=2.0.0";
+            gazetteer: {
+                minChars: 3,
+                url: "/geofos/dog_hh/services/wfs?service=WFS&request=GetFeature&version=2.0.0",
+                searchStreets: true,
+                searchHouseNumbers: true,
+                searchDistricts: true,
+                searchParcels: true
             },
-
+            visibleWFS: {
+                minChars: 3
+            },
+            placeholder: "Suche nach Adresse, Stadtteil",
+            geoLocateHit: true
         },
-
         print: {
             printID: "99999",
-            title: "Hausboote",
+            title: "Portal-Liegeflächen für Hausboote",
             gfi: false
         },
-
         tools: {
             gfi: true,
             measure: true,
@@ -67,11 +75,7 @@ define(function () {
             coord: true,
             draw: false,
             active: "gfi"
-        },
-
-        orientation: true,
-
-        poi: true
+        }
     };
 
     return config;
