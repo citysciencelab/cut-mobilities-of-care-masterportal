057ac1cd58282cde422f154c47cda63a1a87b4a4
schmity <sebastian.schmitz@gv.hamburg.de>
2015-11-26 17:23:52 +0100

    ignore zwischenstand

--- portale/geoportal-mrh/servicesGenerator.js
+++ portale/geoportal-mrh/servicesGenerator.js
@@ -1,47 +1,54 @@
 // run this in the console of http://geoportal.metropolregion.hamburg.de/mrhportal/index.html
 // to write services-mrh.json to console
 
-var mrhServicesGenerator = {
+var servicesGenerator = {
 
 layers: [],
-preServicesJson: [],
+mrhTreeConfig: {},
+lgvTreeConfig: [],
+lgvTreeConfigRecord: {},
 
 run: function () {
-	// iterate global treeConfig
-	for (var i = 0; i < treeConfig.length; i++) {
+
+    // iterate global treeConfig
+    for (var i = 0; i < treeConfig.length; i++) {
 		var treeConfigRecord = treeConfig[i],
 			layerStore = treeConfigRecord.layerStore,
 			isBaseLayerStore = (treeConfigRecord.nodeType === "gx_baselayercontainer"),
-			category;
+			parentFolder = treeConfigRecord.text;
 
-		category = treeConfigRecord.text;
+		this.lgvTreeConfigRecord = {node: parentFolder, childnodes: []};
 
-		// console.log(treeConfigRecord);
-		if (layerStore)	{
+        if (layerStore)	{
 			// console.log("  no children");
-			this.addLayers(layerStore, isBaseLayerStore, category);
+			this.addLayers(layerStore, isBaseLayerStore, parentFolder);
 		}
 		else {
 			for (var k = 0; k < treeConfigRecord.children.length; k++) {
 
 				var childLayerStore = treeConfigRecord.children[k].layerStore,
 					layer = treeConfigRecord.children[k].layer,
-					md = {};
+					childFolder = {};
 
-				md.name = treeConfigRecord.children[k].text;
-				md.id = md.name + k;
+				childFolder.name = treeConfigRecord.children[k].text;
+				childFolder.id = childFolder.name + k;
 
 				if (childLayerStore) {
 					// console.log("  " + md.name);
-					this.addLayers(childLayerStore, isBaseLayerStore, category, md);
+					this.addLayers(childLayerStore, isBaseLayerStore, parentFolder, childFolder);
 
 				}
 				else if (layer) {
-					this.addLayer(layer, category, md);
+					this.addLayer(layer, parentFolder, childFolder);
 				}
 			}
 		}
+
+        this.lgvTreeConfig.push(this.lgvTreeConfigRecord);
+
 	}
+
+    console.log(this.lgvTreeConfig);
 
 	this.writeServicesJson();
 	return "copy json from console.";
@@ -55,6 +62,7 @@ translateGfiProps: function (layer) {
 	if (gfiProps) {
 		for (var i = 0; i < gfiProps.length; i++) {
 			var key = gfiProps[i][0];
+
 			gfiAttributes[key] = gfiProps[i][1];
 		}
 		return gfiAttributes;
@@ -68,77 +76,93 @@ translateGfiProps: function (layer) {
 },
 
 // adds a single layer to this.layers
-addLayer: function (layer, category, md) {
-	// console.log("    " +  layer.name);
-	this.layers.push({
+addLayer: function (layer, parentFolder, childFolder) {
+
+    this.layers.push({
 		layer: layer,
-		category: category,
-		md: md
+		parentFolder: parentFolder,
+		childFolder: childFolder
 	});
 },
 
-// read layers from layer store and call addLayers
-addLayers: function (layerStore, isBaseLayerStore, category, md) {
+// read layers from layer store and call addLayer
+addLayers: function (layerStore, isBaseLayerStore, parentFolder, childFolder) {
 
-	// iterate layerStore
-	layerStore.each(function (rec) {
-			if (isBaseLayerStore) {
-				if(rec.data.layer.isBaseLayer) {
-					this.addLayer(rec.data.layer, category, md);
+    // build second Level folders
+    if (childFolder){
+        this.lgvTreeConfigRecord.childnodes.push({node: childFolder.name, layerIDs: []});
+    }
+
+    // console.log(parentFolder)
+    if (childFolder) {
+        console.log("   " + childFolder.name);
+    }
+
+	layerStore.each(function (rec, index) {
+       // console.log(index);
+        	if (isBaseLayerStore) {
+				if (rec.data.layer.isBaseLayer) {
+					this.addLayer(rec.data.layer, parentFolder, childFolder);
 				}
 			}
 			else {
-				this.addLayer(rec.data.layer, category, md);
+				this.addLayer(rec.data.layer, parentFolder, childFolder);
+                if (parentFolder !== "Hintergrundkarten"){
+                    if (childFolder){
+         //               console.log(this.lgvTreeConfigRecord);
+                        //this.lgvTreeConfigRecord.childnodes[index].layerIDs.push(rec.data.layer.id);
+                    }
+            }
 			}
 	}, this);
 
 	return true;
 },
 
-// write servicesJson array
+// write servicesJson array from previously built this.layers
 writeServicesJson: function () {
 	var servicesJson = [];
-
+    //console.log(this.layers);
 	for (var i = 0; i < this.layers.length; i++) {
 
 		var layer = this.layers[i].layer,
-			category = this.layers[i].category,
-			md = this.layers[i].md;
+			parentFolder = this.layers[i].parentFolder,
+			childFolder = this.layers[i].childFolder,
+            secondLevel = childFolder ? childFolder.name : "layer" ;
 
-            console.log(layer);
-		servicesJson.push({
-			"id" : i.toString(),
-		    "name" : layer.name,
-		    "url" : layer.url,
-		    "typ" : "WMS",
-		    "layers" : layer.params.LAYERS,
-		    "format" : layer.params.FORMAT,
-		    "version" : layer.params.VERSION,
-		    "singleTile" : layer.singleTile || false,
-		    "transparent" : layer.params.TRANSPARENT ||false,
-		    "tilesize" : layer.tileSize.h,
-		    "gutter" : layer.options.gutter || 0,
-		    "minScale" : 0,
-		    "maxScale" : 1000000,
-		    "isbaselayer" : layer.isBaseLayer,
-		    "gfiAttributes" : this.translateGfiProps(layer),
-		    "layerAttribution" : layer.attribution || "nicht vorhanden",
-		    "cache" : false,
-		    "datasets" : [
-		       {
-		          "md_id" : md ? md.id : i.toString(),
-		          "md_name" : md ? md.name : layer.name,
-		          "bbox" : "nicht vorhanden",
-		          "kategorie_opendata" : [category],
-		          "kategorie_inspire": "Kein INSPIRE-Thema"
-		       }
-		    ]
+        servicesJson.push({
+            "id": i.toString(),
+            "name": layer.name,
+            "url": layer.url,
+            "typ": "WMS",
+            "layers": layer.params.LAYERS,
+            "format": layer.params.FORMAT,
+            "version": layer.params.VERSION,
+            "singleTile": layer.singleTile || false,
+            "transparent": layer.params.TRANSPARENT || false,
+            "tilesize": layer.tileSize.h,
+            "gutter": layer.options.gutter || 0,
+            "minScale": 0,
+            "maxScale": 1000000,
+            "isbaselayer": layer.isBaseLayer,
+            "gfiAttributes": this.translateGfiProps(layer),
+            "layerAttribution": layer.attribution || "nicht vorhanden",
+            "cache": false,
+            "datasets": [
+               /*{
+                    "md_id": childFolder ? childFolder.id : i.toString(),
+                    "md_name": childFolder ? childFolder.name : layer.name,
+                    "bbox": "nicht vorhanden",
+                    "kategorie_opendata": [parentFolder],
+                    "kategorie_inspire": "Kein INSPIRE-Thema"
+                }*/
+            ]
 		});
-	}
 
-	console.log(JSON.stringify(servicesJson));
+	}
+	// console.log(JSON.stringify(servicesJson));
 }
 
 };
 
-mrhServicesGenerator.run();
+servicesGenerator.run();
