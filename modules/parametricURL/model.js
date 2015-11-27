41c6f8d7613baebdf3f8d4486f35f5d951aa3f5c
Michael Bieler <michael.bieler@gv.hamburg.de>
2015-11-27 12:30:45 +0100

    fix parametric URL layerids berücksichtigt keine Gruppenlayer

--- modules/parametricURL/model.js
+++ modules/parametricURL/model.js
@@ -1,7 +1,7 @@
 define([
-    'underscore',
-    'config',
-    'backbone'
+    "underscore",
+    "config",
+    "backbone"
 ], function (_, Config, Backbone) {
 
     var ParametricURL = Backbone.Model.extend({
@@ -10,16 +10,19 @@ define([
             // Parsen des parametrisierten Aufruf --> http://wscd0096/libs/lgv/portale/master?layerIDs=453,1346&center=555874,5934140&zoomLevel=4&isMenubarVisible=false
             var query = location.search.substr(1).toUpperCase(), // URL --> alles nach ? wenn vorhanden
                 result = {};
+
             query.split("&").forEach(function (keyValue) {
                 var item = keyValue.split("=");
+
                 result[item[0]] = decodeURIComponent(item[1]); // item[0] = key; item[1] = value;
             });
             /**
              * Gibt die initiale Zentrumskoordinate zurück.
              * Ist der Parameter 'center' vorhanden wird dessen Wert zurückgegeben, ansonsten der Standardwert.
              */
-            if (_.has(result, 'CENTER')) {
-                var values = _.values(_.pick(result, 'CENTER'))[0].split(',');
+            if (_.has(result, "CENTER")) {
+                var values = _.values(_.pick(result, "CENTER"))[0].split(",");
+
                 _.each(values, function (value, index, list) {
                     value = parseInt(value);
                     values[index] = value;
@@ -31,8 +34,8 @@ define([
              * Gibt die LayerIDs für die Layer zurück, die initial sichtbar sein sollen.
              * Ist der Parameter 'layerIDs' vorhanden werden dessen IDs zurückgegeben, ansonsten die konfigurierten IDs.
              */
-            if (_.has(result, 'LAYERIDS')) {
-                var valuesString = _.values(_.pick(result, 'LAYERIDS'))[0],
+            if (_.has(result, "LAYERIDS")) {
+                var valuesString = _.values(_.pick(result, "LAYERIDS"))[0],
                     visibilityListString = _.values(_.pick(result, "VISIBILITY"))[0],
                     values = [],
                     visibilityList = [],
@@ -44,7 +47,7 @@ define([
                 else {
                     values.push(valuesString);
                 }
-                if (_.has(result, 'VISIBILITY')) {
+                if (_.has(result, "VISIBILITY")) {
                     if (visibilityListString && visibilityListString.indexOf(",") !== -1) {
                         visibilityList = visibilityListString.split(",");
                     }
@@ -55,21 +58,23 @@ define([
                 if (Config.tree.type === "light" || Config.tree.type === "custom") {
                     var params = [],
                         visibilitycheck = false;
+
                     if (visibilityListString) {
-                         //für alle Layerid-parameter visible-parameter enthalten?
+                        // für alle Layerid-parameter visible-parameter enthalten?
                         visibilitycheck = (visibilityList.length === values.length);
                     }
-                    //Layer-ID-Objekt aus Url-Params erstellen
-                    _.each(values, function (k,i){
-                        //wenn für alle Layerid-parameter visible-parameter enthalten sind.
+                    // Layer-ID-Objekt aus Url-Params erstellen
+                    _.each(values, function (k, i) {
+                        // wenn für alle Layerid-parameter visible-parameter enthalten sind.
                         if (visibilitycheck === true) {
-                            var visibleParam = (visibilityList[i] == 'TRUE');
+                            var visibleParam = (visibilityList[i] === "TRUE");
+
                             params.push({
                                 id: values[i],
                                 visible: visibleParam
                             });
                         }
-                        //wenn nicht, alle defaultmäßig 'true'setzen
+                        // wenn nicht, alle defaultmäßig 'true'setzen
                         else {
                             params.push({
                                 id: values[i],
@@ -78,9 +83,21 @@ define([
                             }
                     });
                     // Wenn Layer nicht im tree.layer enthalten ist, diesen hinzufügen
-                    _.each(params,function (param) {
+                    _.each(params, function (param) {
                         var layer = _.find(Config.tree.layer, function (layer) {
-                            return layer.id === param.id;
+                            var layerid = "";
+                            // Gruppenlayer
+                            if (_.isObject(layer.id)) {
+                                _.each (layer.id, function (obj) {
+                                    layerid += obj.id + "_";
+                                });
+                                layerid = layerid.substring(0, layerid.length - 1);
+                            }
+                            // Singlelayer
+                            else {
+                                layerid = layer.id;
+                            }
+                            return layerid === param.id;
                         });
                         if (!layer) {
                             Config.tree.layer.push({
@@ -91,9 +108,23 @@ define([
                     });
                     // Layersichtbarkeit schalten
                     _.each(Config.tree.layer, function (layer) {
+                        var layerid = "";
+
+                        // Gruppenlayer
+                        if (_.isObject(layer.id)) {
+                            _.each (layer.id, function (obj) {
+                                layerid += obj.id + "_";
+                            });
+                            layerid = layerid.substring(0, layerid.length - 1);
+                        }
+                        // Singlelayer
+                        else {
+                            layerid = layer.id;
+                        }
                         var param = _.find(params, function (par) {
-                            return par.id === layer.id;
+                            return par.id === layerid;
                         });
+
                         if (param && param.visible === true) {
                             layer.visible = true;
                         }
@@ -130,9 +161,10 @@ define([
             * Ist der Parameter 'isMenubarVisible' vorhanden, wird dieser zurückgegeben, ansonsten der Standardwert.
             *
             */
-            if (_.has(result, 'ISMENUBARVISIBLE')) {
-                var value = _.values(_.pick(result, 'ISMENUBARVISIBLE'))[0].toUpperCase();
-                if (value ==  'TRUE') {
+            if (_.has(result, "ISMENUBARVISIBLE")) {
+                var value = _.values(_.pick(result, "ISMENUBARVISIBLE"))[0].toUpperCase();
+
+                if (value === "TRUE") {
                     Config.isMenubarVisible = true;
                 }
                 else {
@@ -145,16 +177,18 @@ define([
             * Ist der Parameter 'isMenubarVisible' vorhanden, wird dieser zurückgegeben, ansonsten der Standardwert.
             *
             */
-            if (_.has(result, 'STARTUPMODUL')) {
-                var value = _.values(_.pick(result, 'STARTUPMODUL'))[0].toUpperCase();
+            if (_.has(result, "STARTUPMODUL")) {
+                var value = _.values(_.pick(result, "STARTUPMODUL"))[0].toUpperCase();
+
                 Config.startUpModul = value;
             }
 
             /**
             *
             */
-            if (_.has(result, 'QUERY')) {
-                var value = _.values(_.pick(result, 'QUERY'))[0];
+            if (_.has(result, "QUERY")) {
+                var value = _.values(_.pick(result, "QUERY"))[0];
+
                 value = value.substring(0, 1) + value.substring(1).toLowerCase();
                 Config.searchBar.initString = value;
             }
@@ -163,8 +197,9 @@ define([
             * Gibt den Wert für die config-Option clickCounter.enabled zurück.
             *
             */
-            if (_.has(result, 'CLICKCOUNTER')) {
-                var value = _.values(_.pick(result, 'CLICKCOUNTER'))[0].toUpperCase();
+            if (_.has(result, "CLICKCOUNTER")) {
+                var value = _.values(_.pick(result, "CLICKCOUNTER"))[0].toUpperCase();
+
                 Config.clickCounter.version = value;
             }
         }
