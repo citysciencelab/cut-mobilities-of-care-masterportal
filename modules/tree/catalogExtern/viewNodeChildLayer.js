f8ac55b3af97c8623a1aca3de8b7628aee94c7be
sebadu <sebastian.duden@gv.hamburg.de>
2015-12-09 15:49:25 +0100

    udpate css style.

--- modules/tree/catalogExtern/viewNodeChildLayer.js
+++ modules/tree/catalogExtern/viewNodeChildLayer.js
@@ -50,10 +50,11 @@ define([
                     this.$el.css("color", "#fc8d62");
                 }
                 else {
-                    this.$el.css("color", "rgb(150, 150, 150)");
+                    this.$el.css("color", "rgb(153, 153, 153)");
                 }
                 this.model.get("parentView").toggleStyle();
             }
         });
+
         return NodeChildLayerView;
     });
