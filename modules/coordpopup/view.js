510f4e30df566db8f4151a2c6ba02d8c953f43ba
sebadu <sebastian.duden@gv.hamburg.de>
2015-12-28 10:19:50 +0100

    update style Koordinatenabfrage

--- modules/coordpopup/view.js
+++ modules/coordpopup/view.js
@@ -6,9 +6,10 @@ define([
 
     var CoordPopupView = Backbone.View.extend({
         model: CoordPopup,
+        id: "coord-popup",
         template: _.template(CoordPopupTemplate),
         events: {
-            "click .coord-close": "destroy"
+            "click .glyphicon-remove": "destroy"
         },
         initialize: function () {
             this.listenTo(this.model, "change:coordinateGeo", this.render);
@@ -19,7 +20,7 @@ define([
             this.$el.html(this.template(attr));
             $(this.model.get("element")).popover({
                 placement: function () {
-                    if (this.getPosition().y > window.innerWidth / 2) {
+                    if (this.getPosition().top > window.innerWidth / 2) {
                         return "top";
                     }
                     else {
@@ -27,8 +28,7 @@ define([
                     }
                 },
                 html: true,
-                content: this.$el,
-                viewport: "canvas"
+                content: this.$el
             });
             this.model.showPopup();
         },
