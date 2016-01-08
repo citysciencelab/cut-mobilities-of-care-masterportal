6e8e24ffd6637529c1fea0b0ac98201b17b236e9
Angela Bialluch <angela.bialluch@gv.hamburg.de>
2016-01-08 13:11:53 +0100

    update geoportal-mrh/config, werkzeuge

--- portale/geoportal-mrh/config.js
+++ portale/geoportal-mrh/config.js
@@ -143,13 +143,31 @@ define(function () {
             geoLocateHit: true
         },
         tools: {
-            gfi: true,
-            measure: true,
-            print: true,
-            coord: true,
-            draw: true,
-            record: false,
-            active: "gfi"
+            gfi: {
+                title: "Informationen abfragen",
+                glyphicon: "glyphicon-info-sign",
+                isActive: true
+            },
+            print: {
+                title: "Karte drucken",
+                glyphicon: "glyphicon-print"
+            },
+            coord: {
+                title: "Koordinate abfragen",
+                glyphicon: "glyphicon-screenshot"
+            },
+            measure: {
+                title: "Strecke / Fläche messen",
+                glyphicon: "glyphicon-resize-full"
+            },
+            draw: {
+                title: "Zeichnen",
+                glyphicon: "glyphicon-pencil"
+            },
+            searchByCoord: {
+                title: "Koordinatensuche",
+                glyphicon: "glyphicon-search"
+            }
         },
         print: {
             printID: "99999",
