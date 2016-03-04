07957a636f98e5d73e7d92d5ad2d73d978fa50f1
sebadu <sebastian.duden@gv.hamburg.de>
2016-03-04 14:44:28 +0100

    update catalogBaseLayer - add Listener MenuBar/switchedMenu

--- modules/tree/catalogBaseLayer/listView.js
+++ modules/tree/catalogBaseLayer/listView.js
@@ -1,27 +1,36 @@
 define([
     "backbone",
+    "backbone.radio",
     "modules/tree/catalogBaseLayer/list",
     "modules/tree/catalogBaseLayer/view"
-], function (Backbone, BaseLayerList, BaseLayerView) {
+], function (Backbone, Radio, BaseLayerList, BaseLayerView) {
 
     var listView = Backbone.View.extend({
         collection: BaseLayerList,
         tagName: "ul",
         className: "list-group base-layer-list",
         initialize: function () {
+            this.listenTo(Radio.channel("MenuBar"), {
+                "switchedMenu": this.render
+            });
+
             this.collection.on("reset", this.render, this);
             this.render();
         },
         render: function () {
-            $(".base-layer-catalog > .content").append(this.$el.html(""));
-            this.collection.forEach(this.addBaseLayer, this);
+            var isMobile = Radio.request("MenuBar", "isMobile");
+
+            if (isMobile === false) {
+                $(".base-layer-catalog > .content").append(this.$el.html(""));
+                this.collection.forEach(this.addBaseLayer, this);
+            }
         },
         addBaseLayer: function (baselayer) {
             var nodeView;
 
             nodeView = new BaseLayerView({model: baselayer});
             this.$el.append(nodeView.render().el);
-        },
+        }
     });
 
     return listView;
