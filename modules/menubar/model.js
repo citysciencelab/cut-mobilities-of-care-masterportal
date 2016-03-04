e846de8918a571db9e37ae66e0b6d4ef70782462
sebadu <sebastian.duden@gv.hamburg.de>
2016-03-04 13:16:51 +0100

    add mobiles menu temp

--- modules/menubar/model.js
+++ modules/menubar/model.js
@@ -1,15 +1,37 @@
 define([
     "backbone",
     "config",
+    "backbone.radio",
     "bootstrap/dropdown",
     "bootstrap/collapse"
-], function (Backbone, Config) {
+], function () {
 
-    var Menubar = Backbone.Model.extend({
+    var Backbone = require("backbone"),
+        Radio = require("backbone.radio"),
+        Config = require("config"),
+        Menubar;
+
+    Menubar = Backbone.Model.extend({
+        defaults: {
+            // true wenn Fensterbreite < 768px
+            isMobile: false,
+            // true wenn nur ein Tool konfiguriert ist
+            onlyOneTool: false,
+            // Baumtyp - default/custom/light
+            treeType: ""
+        },
         initialize: function () {
-            this.set("platform", navigator.platform);
-            this.set("cookieEnabled", navigator.cookieEnabled);
-            this.set("userAgent", navigator.userAgent);
+            var channel = Radio.channel("MenuBar");
+
+            channel.reply({
+                "isMobile": this.getIsMobile
+            }, this);
+
+            $(window).on("resize", _.bind(this.setIsMobile, this));
+
+            // this.listenTo(this, {
+            //     "change:isMobile"
+            // })
             _.each(Config.menu, this.setAttributes, this);
             // Wenn nur ein Tool aktiviert ist, wird der Menüeintrag Werkzeuge nicht erzeugt. --> Abfrage im template
             if (_.toArray(Config.tools).length === 1) {
@@ -18,11 +40,30 @@ define([
             else {
                 this.set("oneTool", false);
             }
+            this.setIsMobile();
+            this.setTreeType(Config.tree.type);
         },
         setAttributes: function (value, key) {
             this.set(key, value);
+        },
+        setIsMobile: function () {
+            if ($(window).width() >= 768) {
+                this.set("isMobile", false);
+            }
+            else {
+                this.set("isMobile", true);
+            }
+        },
+        getIsMobile: function () {
+            return this.get("isMobile");
+        },
+        setTreeType: function (value) {
+            this.set("treeType", value);
+        },
+        getTreeType: function () {
+            return this.get("treeType");
         }
     });
 
-    return new Menubar();
+    return Menubar;
 });
--- modules/menubar/view.js
+++ modules/menubar/view.js
@@ -1,27 +1,24 @@
 define([
-    "backbone",
     "text!modules/menubar/template.html",
-    "modules/treeLight/listView",
+    "text!modules/menubar/templateDesktop.html",
     "modules/menubar/model",
-    "config",
+    "modules/tree/view",
+    "modules/treeLight/listView",
     "eventbus"
-], function (Backbone, MenubarTemplate, TreeLightView, Menubar, Config, EventBus) {
+], function (MenubarTemplate, DesktopMenubarTemplate, Menubar, EventBus) {
 
-    var MenubarView = Backbone.View.extend({
-        model: Menubar,
+    var Backbone = require("backbone"),
+        Radio = require("backbone.radio"),
+        Config = require("config"),
+        MenubarView;
+
+    MenubarView = Backbone.View.extend({
+        model: new Menubar(),
         tagName: "nav",
         className: "navbar navbar-default navbar-fixed-top",
         attributes: {role: "navigation"},
         template: _.template(MenubarTemplate),
-        initialize: function () {
-            EventBus.on("appendItemToMenubar", this.appendItemToMenubar, this);
-            this.render();
-            $(".dropdown-tree").on({
-                click: function (e) {
-                    e.stopPropagation();
-                }
-            });
-        },
+        templateDesktop: _.template(DesktopMenubarTemplate),
         events: {
             "click .filterTree": "activateFilterTree",
             "click .filterWfsFeature": "activateWfsFilter",
@@ -30,28 +27,68 @@ define([
             "click .addWMS": "activateAddWMSModul",
             "click .wfsFeatureFilter": "activateWfsFeatureFilter"
         },
+
+        /**
+         *
+         */
+        initialize: function () {
+            this.listenTo(this.model, {
+                "change:isMobile": this.renderDesktopMenu
+            });
+
+            this.listenTo(EventBus, {
+                "appendItemToMenubar": this.appendItemToMenubar
+            });
+
+            this.loadTrees();
+            this.render();
+        },
+
         render: function () {
             var attr = this.model.toJSON();
 
             $("body").append(this.$el.append(this.template(attr)));
 
+            this.renderDesktopMenu();
+            // this.loadTrees();
+            // Radio.trigger("MenuBar", "switchedMenu");
+
             if (Config.isMenubarVisible === false) {
                 $("#navbarRow").toggle();
-            }
-            if (_.has(Config, "tree") && Config.tree.type !== "light") {
-                require(["modules/tree/view"], function (TreeView) {
-                    new TreeView();
-                });
-            }
-            else {
-                new TreeLightView();
             }
             if (_.has(Config, "title") === true) {
                 require(["modules/title/view"], function (TitleView) {
                     new TitleView();
                 });
             }
-            // new OpenDataTreeList();
+        },
+        renderDesktopMenu: function () {
+            $(".navbar-collapse").empty();
+
+            if (this.model.getIsMobile() === false) {
+                var attr = this.model.toJSON();
+
+                $(".navbar-collapse").append(this.templateDesktop(attr));
+                Radio.trigger("MenuBar", "switchedMenu");
+            }
+            else {
+                this.renderTreeMobile();
+            }
+        },
+        loadTrees: function () {
+            if (this.model.getTreeType() !== "light") {
+                var TreeView = require("modules/tree/view");
+
+                new TreeView();
+            }
+            else {
+                var TreeLightView = require("modules/treeLight/listView");
+
+                new TreeLightView();
+            }
+        },
+        renderTreeMobile: function () {
+            console.log(54);
         },
         appendItemToMenubar: function (obj) {
             var html = "<li>";
