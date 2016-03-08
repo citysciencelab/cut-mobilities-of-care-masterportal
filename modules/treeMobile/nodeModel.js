80f14355539efbec387184190522ce7c34c9b4b3
sebadu <sebastian.duden@gv.hamburg.de>
2016-03-08 17:10:25 +0100

    update nodeModel

--- modules/treeMobile/nodeModel.js
+++ modules/treeMobile/nodeModel.js
@@ -6,30 +6,23 @@ define([
         Node;
 
     Node = Backbone.Model.extend({
-        defaults: {
-            // true wenn die Node sichtbar
-            isVisible: false,
-            // true wenn die Node zur ersten Ebene gehört
-            isRoot: false,
-            // welcher Node-Type - folder/layer/item
-            type: "",
-            // die ID der Parent-Node
-            parentID: 0
-        },
         setIsVisible: function (value) {
             this.set("isVisible", value);
         },
         getIsVisible: function () {
             return this.get("isVisible");
         },
-        getType: function () {
-            return this.get("type");
-        },
         setParentID: function (value) {
             this.set("parentID", value);
         },
         getParentID: function () {
             return this.get("parentID");
+        },
+        getType: function () {
+            return this.get("type");
+        },
+        getTargetElement: function () {
+            return this.get("targetElement");
         }
     });
 
