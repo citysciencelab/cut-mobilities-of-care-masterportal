6da7ed82b2dfac72a94823e6e1a27d5ef09e9df1
Michael Bieler <michael.bieler@gv.hamburg.de>
2016-04-08 15:43:39 +0200

    update neuer WPS-Server im RZ2 im  Einsatz

--- portale/ida/config.js
+++ portale/ida/config.js
@@ -4,7 +4,7 @@ define(function () {
     var config = {
         restConf: "../../components/lgv-config/rest-services-fhhnet.json",
         proxyURL: "/cgi-bin/proxy.cgi",
-        wpsID: "99998",
+        wpsID: "1001",
         minJahr: 1973,
         maxJahr: 2013,
         searchbar: {
