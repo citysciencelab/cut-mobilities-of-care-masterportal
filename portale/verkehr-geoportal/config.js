bb6f7ff1a67cf8da8e2bac1421a30fcd31739a6d
Angela Bialluch <angela.bialluch@gv.hamburg.de>
2016-04-21 15:04:35 +0200

    update portale/verkehr-geoportal/config portale/verkehr-geoportal/verkehrsfunctions

--- portale/verkehr-geoportal/config.js
+++ portale/verkehr-geoportal/config.js
@@ -1,55 +1,39 @@
 define(function () {
 
     var config = {
-        wfsImgPath: "..components/lgv-config/img",
+        wfsImgPath: "../components/lgv-config/img/",
         allowParametricURL: true,
         tree: {
             type: "custom",
             layerIDsToMerge: [
-                ["149", "150", "151", "152", "153", "154", "155", "156", "157", "158", "159", "160", "161", "162", "163", "164", "165", "166", "167", "168", "169", "170", "171", "172", "173", "174", "175", "176", "177", "178"],
+               ["149", "150", "151", "152", "153", "154", "155", "156", "157", "158", "159", "160", "161", "162", "163", "164", "165", "166", "167", "168", "169", "170", "171", "172", "173", "174", "175", "176", "177", "178"],
                 ["368", "369", "370", "371", "372", "373", "374", "375", "376", "377", "378", "379", "380", "381", "382", "383", "384", "385", "386", "387", "388", "389", "390", "391", "392", "393", "394", "395", "396", "397"],
                 ["717", "718", "719", "720"],
-                ["713", "714", "715", "716"],
                 ["1043", "1044", "1045", "1046"]
-            ],
-            layerIDsToStyle: [
+              ],
+            layerIDsToStyle: [                
+                {
+                    "id": "1935",
+                    "styles": ["geofox-bahn", "geofox-bus", "geofox_BusName", "geofox_Faehre"],
+                    "name": ["USAR-Bahnlinien", "Buslinien", "Busliniennummern", "Fährlinien"],
+                    "legendURL": ["http://87.106.16.168/legende_mrh/hvv-bahn.png",
+                        "http://87.106.16.168/legende_mrh/hvv-bus.png", "http://87.106.16.168/legende_mrh/hvv-bus.png", "http://87.106.16.168/legende_mrh/hvv-faehre.png"]
+                },
                 {
                     "id": "1933",
                     "styles": "geofox_stations",
-                    "name": "Haltestellen",
-                    "legendURL": "http://87.106.16.168/legende_mrh/hvv-bus.png"
-                },
-                {
-                    "id": "1935",
-                    "styles": ["geofox_Faehre", "geofox-bahn", "geofox-bus", "geofox_BusName"],
-                    "name": ["Fährverbindungen", "Bahnlinien", "Buslinien", "Busliniennummern"],
-                    "legendURL": ["http://87.106.16.168/legende_mrh/hvv-faehre.png",          "http://87.106.16.168/legende_mrh/hvv-bahn.png",
-                        "http://87.106.16.168/legende_mrh/hvv-bus.png", "http://87.106.16.168/legende_mrh/hvv-bus.png"]
+                    "name": "USAR-Stationen, Bus-Haltestellen, Fähr-Stationen",
+                    "legendURL": ["http://87.106.16.168/legende_mrh/hvv-bahn.png", "http://87.106.16.168/legende_mrh/hvv-bus.png", "http://87.106.16.168/legende_mrh/hvv-faehre.png"]
                 }
             ],
             baseLayer: [
                 {id: "452", visibility: false}, // luftbilder 
                 {id: "453", visibility: false}, // stadtplan farbig
-                {id: "713", visibility: false}, // stadtplan s-w
-                {id:
-                    [
-                        {
-                            id: "946",
-                            attribution:
-                                {
-                                    eventname: "aktualisiereverkehrsnetz",
-                                    timeout: (10 * 60000)
-                                }
-                        },
-                        {
-                            id: "947"
-                        }
-                    ],
-                    name: "Verkehrslage auf Autobahnen", visibility: false
-                        }
+                {id: "713", visibility: false}
+               
             ],
             customConfig: "../components/lgv-config/tree-config/verkehr-geoportal.json"
-        },
+            },
         controls: {
             zoom: true,
             toggleMenu: true,
@@ -68,7 +52,7 @@ define(function () {
             //resolution: 26.458319045841044,
             epsg: "EPSG:25832"
         },
-        //customModules: ["../portale/verkehr-geoportal/verkehrsfunctions"],
+        customModules: ["../portale/verkehr-geoportal/verkehrsfunctions"],
         footer: false,
         quickHelp: true,
         layerConf: "../components/lgv-config/services-fhhnet.json",
