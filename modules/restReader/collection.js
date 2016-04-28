63de4190bc3a634c42868ccfc33b5ef179cd08ac
sebadu <sebastian.duden@gv.hamburg.de>
2016-04-28 09:22:05 +0200

    remove restReader/model.js ist ueberfluessig

--- modules/restReader/collection.js
+++ modules/restReader/collection.js
@@ -2,28 +2,23 @@ define([
     "backbone",
     "config",
     "eventbus",
-    "modules/core/util",
-    "modules/restReader/model",
-    ], function (Backbone, Config, EventBus, Util, Model) {
+    "modules/core/util"
+    ], function (Backbone, Config, EventBus, Util) {
     "use strict";
     var RestList = Backbone.Collection.extend({
         url: Util.getPath(Config.restConf),
-        model: Model,
         initialize: function () {
             this.fetch({
                 cache: false,
-                async: false,
                 error: function () {
                     EventBus.trigger("alert", {
                         text: "Fehler beim Laden von: " + Util.getPath(Config.restConf),
                         kategorie: "alert-warning"
                     });
-                },
-                success: function (collection) {
                 }
             });
         },
-        getAllServices: function () {
+        getAllServices: function () {console.log(this.models);
             return this.models;
         },
         getServiceById: function (id) {
