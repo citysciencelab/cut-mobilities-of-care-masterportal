09d7002518c65c866255bb2799d5262490e7bc58
Angela Bialluch <angela.bialluch@gv.hamburg.de>
2016-05-03 15:11:55 +0200

    update portale/hundert-jahre-stadtgruen/config

--- portale/hundert-jahre-stadtgruen/config.js
+++ portale/hundert-jahre-stadtgruen/config.js
@@ -30,30 +30,48 @@ define(function () {
             poi: true,
             fullScreen: true
         },
+        customModules: [],
         footer: false,
         quickHelp: true,
         layerConf: "../components/lgv-config/services-fhhnet.json",
         restConf: "../components/lgv-config/rest-services-fhhnet.json",
         styleConf: "../components/lgv-config/style.json",
         proxyURL: "/cgi-bin/proxy.cgi",
+        
         attributions: true,
         menubar: true,
         scaleLine: true,
         mouseHover: true,
         isMenubarVisible: true,
+         menuItems: {
+            tree: {
+                title: "Themen",
+                glyphicon: "glyphicon-list"
+            },
+            tools: {
+                title: "Werkzeuge",
+                glyphicon: "glyphicon-wrench"
+            },
+            legend: {
+                title: "Legende",
+                glyphicon: "glyphicon-book"
+            },
+             contact: {
+                title: "Kontakt",
+                glyphicon: "glyphicon-envelope",
+                email: "LGVGeoPortal-Hilfe@gv.hamburg.de"
+            },            
+            routing: {
+                title: "Routenplaner",
+                glyphicon: "glyphicon-road"
+            }
+        },
+        
         menu: {
             helpButton: false,
             searchBar: true,
             layerTree: true,
-            tools: true,
-            featureLister: false,
-            treeFilter: false,
-            wfsFeatureFilter: true,
-            legend: true,
-            routing: true,
-            addWMS: false,
-            formular: {},
-            contact: {
+             contact: {
                 serviceID: "80001",
                 from: [{
                     email: "lgvgeoportal-hilfe@gv.hamburg.de",
@@ -69,7 +87,13 @@ define(function () {
                 subject: "",
                 textPlaceholder: "",
                 includeSystemInfo: true
-            }
+            },
+            tools: true,
+            featureLister: false,
+            treeFilter: false,
+            wfsFeatureFilter: true,
+            
+            
         },
         startUpModul: "",
         searchBar: {
