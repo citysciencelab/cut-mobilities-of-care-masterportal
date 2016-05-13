5b9d6a54d0b801ec11a7cece3a96c945f002bec3
sebadu <sebastian.duden@gv.hamburg.de>
2016-05-13 09:43:23 +0200

    update DefaultTreeParser

--- modules/core/parser/defaultTree.js
+++ modules/core/parser/defaultTree.js
@@ -1,4 +1,4 @@
-define("Parser", [
+define([
     "backbone.radio",
     "modules/core/parser/portalConfig"
 ], function () {
@@ -9,17 +9,29 @@ define("Parser", [
 
     DefaultTreeParser = Parser.extend({
         initialize: function () {
-            var rawLayerList = Radio.request("RawLayerList", "getLayerAttributesList");
+            var layerList = Radio.request("RawLayerList", "getLayerAttributesList");
 
-            console.log("default");
-            this.parseTree(rawLayerList);
+            this.parseTree(layerList);
         },
 
         /**
          *
          */
-        parseTree: function (rawLayerList) {
+        parseTree: function (layerList) {
+            // Im Default-Tree(FHH-Atlas / GeoOnline) werden nur WMS angezeigt
+            // Und nur Layer die einem Metadatensatz zugeordnet sind
+            layerList = this.filterList(layerList);
+        },
 
+        /**
+         * Filtert alle Objekte aus der layerList, die kein WMS sind und min. einem Datensatz zugordnet sind
+         * @param  {Object[]} layerList - Objekte aus der services.json
+         * @return {Object[]} layerList - Objekte aus der services.json
+         */
+        filterList: function (response) {
+            return _.filter(response, function (element) {
+                return (element.datasets.length > 0 && element.typ === "WMS") ;
+            });
         }
     });
 
