ace5b7ffcaa811cb601d950c85ed3870057f0f2b
sebadu <sebastian.duden@gv.hamburg.de>
2016-05-19 15:42:37 +0200

    update menu

--- modules/menu/view.js
+++ modules/menu/view.js
@@ -10,21 +10,27 @@ define([
     "modules/menu/desktop/tool/view",
     "modules/menu/mobile/folder/view",
     "modules/menu/mobile/layer/view",
-    "modules/menu/mobile/tool/view"
+    "modules/menu/mobile/tool/view",
+    "bootstrap/dropdown",
+    "bootstrap/collapse"
 ], function () {
+
     var Backbone = require("backbone"),
-    Radio = require("backbone.radio"),
-    MenuTemplate = require("text!modules/menu/template.html"),
-    DesktopComplexTemplate = require("text!modules/menu/desktop/template.html"),
-    DesktopLightTemplate = require("text!modules/menu/desktop/templateLight.html"),
-    MobileTemplate = require("text!modules/menu/mobile/template.html"),
-    DesktopFolderView = require("modules/menu/desktop/folder/view"),
-    DesktopLayerView = require("modules/menu/desktop/layer/view"),
-    DesktopToolView = require("modules/menu/desktop/tool/view"),
-    MobileFolderView = require("modules/menu/mobile/folder/view"),
-    MobileLayerView = require("modules/menu/mobile/layer/view"),
-    MobileToolView = require("modules/menu/mobile/tool/view"),
+        Radio = require("backbone.radio"),
+        MenuTemplate = require("text!modules/menu/template.html"),
+        DesktopComplexTemplate = require("text!modules/menu/desktop/template.html"),
+        DesktopLightTemplate = require("text!modules/menu/desktop/templateLight.html"),
+        MobileTemplate = require("text!modules/menu/mobile/template.html"),
+        DesktopFolderView = require("modules/menu/desktop/folder/view"),
+        DesktopLayerView = require("modules/menu/desktop/layer/view"),
+        DesktopToolView = require("modules/menu/desktop/tool/view"),
+        MobileFolderView = require("modules/menu/mobile/folder/view"),
+        MobileLayerView = require("modules/menu/mobile/layer/view"),
+        MobileToolView = require("modules/menu/mobile/tool/view"),
+        Menu;
+
     Menu = Backbone.View.extend({
+        collection: Radio.request("ModelList", "getCollection"),
         tagName: "nav",
         className: "navbar navbar-default navbar-fixed-top",
         attributes: {role: "navigation"},
@@ -32,40 +38,55 @@ define([
         desktopComplexTemplate: _.template(DesktopComplexTemplate),
         desktopLightTemplate: _.template(DesktopLightTemplate),
         mobileTemplate: _.template(MobileTemplate),
-        targetElement: ".menubarlgv.nav.navbar-nav",
-        collection: {},
+
         initialize: function () {
-            this.collection = Radio.request("ModelList", "getCollection");
+            this.listenTo(this.collection, {
+                "change": function () {
+                    console.log(42);
+                }
+            });
+            // console.log(this.collection);
+            // this.collection = Radio.request("ModelList", "getCollection");
             this.render();
         },
+
         renderTopMenu: function (isMobile) {
             var rootModels = this.collection.where({parentId: "root"});
                 _.each(rootModels, function (model) {
                     this.addViews(model, isMobile);
                 }, this);
         },
+
         render: function () {
             var isMobile = Radio.request("Util", "isViewMobile");
-            $("body").append(this.$el.append(this.template));
-            this.renderTopMenu(isMobile);
 
+            $("body").append(this.$el.html(this.template()));
 
             if (isMobile) {
-                //Mobile
-                $("body").append(this.$el.append(this.mobileTemplate()));
+                $("div.collapse.navbar-collapse ul").removeClass("menubarlgv nav navbar-nav");
+                $("div.collapse.navbar-collapse ul").addClass("list-group tree-mobile");
             }
             else {
-                var treeType = Radio.request("Parser", "getPortalConfig").Baumtyp;
-
-                if (treeType === "light") {
-                    //Use light Template
-                    $("body").append(this.$el.append(this.desktopLightTemplate()));
-                }
-                else {
-                    //Use complex Template
-                    $("body").append(this.$el.append(this.desktopComplexTemplate()));
-                }
+                $("div.collapse.navbar-collapse ul").addClass("menubarlgv nav navbar-nav");
+                $("div.collapse.navbar-collapse ul").removeClass("list-group tree-mobile");
             }
+            this.renderTopMenu(isMobile);
+
+            // if (isMobile) {
+            //     $("body").append(this.$el.append(this.mobileTemplate()));
+            // }
+            // else {
+            //     var treeType = Radio.request("Parser", "getPortalConfig").Baumtyp;
+            //
+            //     if (treeType === "light") {
+            //         // Use light Template
+            //         $("body").append(this.$el.append(this.desktopLightTemplate()));
+            //     }
+            //     else {
+            //         // Use complex Template
+            //         $("body").append(this.$el.append(this.desktopComplexTemplate()));
+            //     }
+            // }
             Radio.trigger("MenuBar", "switchedMenu");
         },
         /**
@@ -92,7 +113,7 @@ define([
                     break;
                 }
             }
-            $(this.targetElement).append(nodeView.render().el);
+            $("div.collapse.navbar-collapse ul").append(nodeView.render().el);
         }
     });
 
