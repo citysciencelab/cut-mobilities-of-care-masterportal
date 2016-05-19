57a063453f1e8d1d03834255125f2a5a26beb69e
sebadu <sebastian.duden@gv.hamburg.de>
2016-05-19 11:55:12 +0200

    update rawlayerlist hvv-funktionen aus der layerlist uebernommen

--- modules/core/rawLayerList.js
+++ modules/core/rawLayerList.js
@@ -50,7 +50,12 @@ define([
             if (_.has(Config.tree, "metaIDsToMerge")) {
                 response = this.mergeLayersByMetaIds(response, Config.tree.metaIDsToMerge);
             }
-
+            // Die HVV Layer bekommen Ihre Styles zugeordnet
+            // Pro Style wird eine neuer Layer erzeugt
+            if (_.has(Config.tree, "layerIDsToStyle")) {
+                this.setStyleForHVVLayer(response);
+                response = this.cloneByStyle(response);
+            }
             return response;
         },
 
@@ -129,7 +134,7 @@ define([
             _.each(metaIds, function (metaID) {
                 // Objekte mit derselben Metadaten-Id
                 objectsById = _.filter(response, function (layer) {
-                    return layer.datasets[0].md_id === metaID;
+                    return layer.datasets.length > 0 && layer.datasets[0].md_id === metaID;
                 });
                 // Das erste Objekt wird kopiert
                 newObject = _.clone(objectsById[0]);
@@ -150,6 +155,59 @@ define([
                 // Fügt das kopierte (gruppierte) Objekt der response hinzu
                 response.push(newObject);
             });
+
+            return response;
+        },
+
+        /**
+         * Holt sich die HVV-Objekte aus der services.json
+         * Fügt den Objekten konfigurierte Attribute aus der config.js über die Id hinzu
+         * @param {Object[]} response - Objekte aus der services.json
+         */
+        setStyleForHVVLayer: function (response) {
+            var styleLayerIDs = _.pluck(Config.tree.layerIDsToStyle, "id"),
+                layersByID;
+
+            layersByID = _.filter(response, function (layer) {
+                return _.contains(styleLayerIDs, layer.id);
+            });
+            _.each(layersByID, function (layer) {
+                var styleLayer = _.findWhere(Config.tree.layerIDsToStyle, {"id": layer.id});
+
+                layer = _.extend(layer, styleLayer);
+            });
+        },
+
+        /**
+         * Aus Objekten mit mehreren Styles, wird pro Style ein neues Objekt erzeugt
+         * Das "alte" Objekt wird aus der respnse entfernt
+         * @param {Object[]} response - Objekte aus der services.json
+         * @return {Object[]} response - Objekte aus der services.json
+         */
+        cloneByStyle: function (response) {
+            // Layer die mehrere Styles haben
+            var objectsByStyle = _.filter(response, function (model) {
+                return typeof model.styles === "object" && model.typ === "WMS";
+            });
+
+            // Iteriert über die Objekte
+            _.each(objectsByStyle, function (obj) {
+                // Iteriert über die Styles
+                _.each(obj.styles, function (style, index) {
+                    // Objekt wird kopiert
+                    var cloneObj = _.clone(obj);
+                    // Die Attribute name, Id, etc. werden für das kopierte Objekt gesetzt
+                    cloneObj.style = style;
+                    cloneObj.legendURL = obj.legendURL[index];
+                    cloneObj.name = obj.name[index];
+                    cloneObj.id = obj.id + obj.styles[index].toLowerCase();
+                    cloneObj.styles = obj.styles[index];
+                    // Objekt wird der Response hinzugefügt
+                    response.splice(_.indexOf(response, obj), 0, cloneObj);
+                }, this);
+                // Das ursprüngliche Objekt wird gelöscht
+                response = _.without(response, obj);
+            }, this);
 
             return response;
         },
