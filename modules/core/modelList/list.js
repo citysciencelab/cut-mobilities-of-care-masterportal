7915f7a329ce2faa641c24a58b847498fd042cce
coderwelsch <LuckeyRo@LGV-W-D614726.fhhnet.stadt.hamburg.de>
2016-05-27 11:56:29 +0200

    update:  Kommentare etc.

--- modules/core/modelList/list.js
+++ modules/core/modelList/list.js
@@ -20,7 +20,7 @@ define([
                channel.reply({
                    "getCollection": this,
                    "getModelsByAttributes": function (attributes) {
-                       return this.where(attributes)
+                       return this.where(attributes);
                    }
                }, this);
 
@@ -41,7 +41,7 @@ define([
             },
 
             model: function (attrs, options) {
-                if (attrs.type === "layer") {console.log(attrs);
+                if (attrs.type === "layer") {
                     if (attrs.typ === "WMS") {
                         return new WMSLayer(attrs, options);
                     }
@@ -49,7 +49,7 @@ define([
                         return new WFSLayer(attrs, options);
                     }
                     else if (attrs.typ === "WFS") {
-                        // muss noch gemacht werden
+                        // TODO muss noch gemacht werden
                     }
                 }
                 else if (attrs.type === "folder") {
