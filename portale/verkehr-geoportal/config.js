6415383cbc406f7d711cf83d5b8b6601a7593d43
Angela Bialluch <angela.bialluch@gv.hamburg.de>
2016-06-01 14:11:51 +0200

    update portale/verkehr-geoportal/config.js portale/verkehr-geoportal/tree-config.json

--- portale/verkehr-geoportal/config.js
+++ portale/verkehr-geoportal/config.js
@@ -153,7 +153,7 @@ define(function () {
         searchBar: {
             placeholder: "Suche Adresse, Stadtteil, Thema",
             gazetteer: {
-                url: "/geodienste-hamburg/HH_WFS_DOG?service=WFS&request=GetFeature&version=2.0.0",
+                url: "/geodienste_hamburg_de/HH_WFS_DOG?service=WFS&request=GetFeature&version=2.0.0",
                 searchStreets: true,
                 searchHouseNumbers: true,
                 searchDistricts: true,
