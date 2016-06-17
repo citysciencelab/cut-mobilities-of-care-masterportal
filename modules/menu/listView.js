a6e191ee383475f4a01ebb5b1dd51ddce8cdb7cf
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2016-06-17 14:02:06 +0200

    update listView add desktop class

--- modules/menu/listView.js
+++ modules/menu/listView.js
@@ -161,11 +161,11 @@ define([
             var isMobile = Radio.request("Util", "isViewMobile");
 
             if (isMobile) {
-                $("div.collapse.navbar-collapse ul.nav-menu").removeClass("nav navbar-nav");
+                $("div.collapse.navbar-collapse ul.nav-menu").removeClass("nav navbar-nav desktop");
                 $("div.collapse.navbar-collapse ul.nav-menu").addClass("list-group mobile");
             }
             else {
-                $("div.collapse.navbar-collapse ul.nav-menu").addClass("nav navbar-nav");
+                $("div.collapse.navbar-collapse ul.nav-menu").addClass("nav navbar-nav desktop");
                 $("div.collapse.navbar-collapse ul.nav-menu").removeClass("list-group mobile");
             }
             this.renderTopMenu(isMobile);
