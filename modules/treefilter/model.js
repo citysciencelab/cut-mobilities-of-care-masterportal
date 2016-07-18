a2690ef46e08cbe8966f0d180be179ddbf05722b
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2016-07-18 09:21:20 +0200

    update url im treefilter angepasst

--- modules/treefilter/model.js
+++ modules/treefilter/model.js
@@ -343,7 +343,7 @@ define([
         getFilterHits: function () {
             $("#loader").show();
             $.ajax({
-                url: "/geodienste-hamburg/HH_WFS_Strassenbaumkataster",
+                url: "/geodienste_hamburg_de/HH_WFS_Strassenbaumkataster",
                 data: "<?xml version='1.0' encoding='UTF-8'?><wfs:GetFeature service='WFS' version='1.1.0' resultType='hits' xmlns:app='http://www.deegree.org/app' xmlns:wfs='http://www.opengis.net/wfs' xmlns:gml='http://www.opengis.net/gml' xmlns:ogc='http://www.opengis.net/ogc' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd'><wfs:Query typeName='app:strassenbaumkataster'>" + this.get("filter") + "</wfs:Query></wfs:GetFeature>",
                 type: "POST",
                 context: this, // model
