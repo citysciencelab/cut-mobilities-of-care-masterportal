19e3848b4158190c984130368e462262db3cb7e7
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2016-08-03 08:04:48 +0000

    update FHH-Atlas config.js online with Bitbucket

--- portale/fhh-atlas/config.js
+++ portale/fhh-atlas/config.js
@@ -82,7 +82,7 @@ define(function () {
                 {
                     "bezeichnung": "Kartographie und Gestaltung: ",
                     "url": "http://www.geoinfo.hamburg.de/",
-                    "alias": "Landesbetrieb Geoniformation und Vermessung",
+                    "alias": "Landesbetrieb Geoinformation und Vermessung",
                     "alias_mobil": "LGV Hamburg"
                 },
                 {
