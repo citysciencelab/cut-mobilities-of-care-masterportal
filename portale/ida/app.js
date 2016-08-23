df4f2b787ddc5cc7ba4734eda5dcdca7c0af4345
Michael Bieler <michael.bieler@gv.hamburg.de>
2016-08-23 07:03:20 +0200

    fix nach git pull wurde restreader nicht mehr geladen

--- portale/ida/app.js
+++ portale/ida/app.js
@@ -1,6 +1,7 @@
 define("app", ["jquery", "backbone.radio", "modules/restReader/collection"], function ($, Radio, RestReaderList) {
     "use strict";
     var channel = Radio.channel("RestReader");
+    new RestReaderList();
 
     channel.on({
         "isReady": function () {
