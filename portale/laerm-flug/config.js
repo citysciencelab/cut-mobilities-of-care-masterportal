8d4c1e138b962c504112ba08524ab41086193cb7
Angela Bialluch <angela.bialluch@gv.hamburg.de>
2016-08-23 14:16:28 +0200

    update portale/laerm-flug/config.js portale/laerm-flug/config.json portale/laerm-flug/index.html

---
+++ portale/laerm-flug/config.js
@@ -0,0 +1,87 @@
+define(function () {
+    var config = {
+        title: "Lärmschutzbereiche Flughafen Hamburg",
+        simpleMap: false,
+        wfsImgPath: "../components/lgv-config/img/",
+        allowParametricURL: true,
+        zoomtofeature: {
+            url: "http://geodienste.hamburg.de/Test_HH_WFST_Eventlotse",
+            version: "2.0.0",
+            typename: "app:hamburgconvention",
+            valuereference: "app:flaechenid",
+            imglink: "../img/location_eventlotse.svg",
+            layerid: "4426"
+        },
+        view: {
+            background: "white",
+             center: [565686, 5942986] //Flughafen
+           /* center: [565874, 5934140],
+            extent: [454591, 5809000, 700000, 6075769],
+            epsg: "EPSG:25832"
+            */
+        },
+        //customModules: ["../portale/master/verkehrsfunctions"],
+        footer: {
+            visibility: true,
+            urls: [
+                {
+                    "bezeichnung": "Kartographie und Gestaltung: ",
+                    "url": "http://www.geoinfo.hamburg.de/",
+                    "alias": "Landesbetrieb Geoniformation und Vermessung",
+                    "alias_mobil": "LGV"
+                },
+                {
+                    "bezeichnung": "",
+                    "url": "http://geofos.fhhnet.stadt.hamburg.de/sdp-daten-download/index.php",
+                    "alias": "SDP Download",
+                    "alias_mobil": "SDP"
+                },
+                {
+                    "bezeichnung": "",
+                    "url": "http://www.hamburg.de/bsu/timonline",
+                    "alias": "Kartenunstimmigkeit"
+                }
+            ]
+        },
+        quickHelp: true,
+        layerConf: "../components/lgv-config/services-fhhnet-ALL.json",
+        restConf: "../components/lgv-config/rest-services-fhhnet.json",
+        styleConf: "../components/lgv-config/style.json",
+        proxyURL: "/cgi-bin/proxy.cgi",
+        
+        attributions: true,
+        scaleLine: true,
+        mouseHover: true,
+        isMenubarVisible: true,
+        startUpModul: "",
+        
+        searchBar: {
+            minChars: 3,
+            gazetteer: {
+                minChars: 3,
+                url: "/geodienste_hamburg_de/HH_WFS_DOG?service=WFS&request=GetFeature&version=2.0.0",
+                searchStreets: true,
+                searchHouseNumbers: true,
+                searchDistricts: true,
+                searchParcels: true
+            },
+            visibleWFS: {
+                minChars: 3
+            },
+            // layer: {
+            //     minChar: 3
+            // },
+            placeholder: "Suche nach Adresse/Stadtteil"
+        },
+        print: {
+            printID: "99999",
+            title: "Master",
+            gfi: false
+        },
+        geoAPI: false,
+        clickCounter: {},
+        gemarkungen: "../components/lgv-config/gemarkung.json"
+    };
+
+    return config;
+});
