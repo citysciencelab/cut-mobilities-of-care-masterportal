ce244c13f61a1adf5e766164631adacaa762f233
Angela Bialluch <angela.bialluch@gv.hamburg.de>
2016-08-30 09:49:40 +0200

    update portale/verkehr-planer/config.js portale/verkehr-planer/config.json

--- portale/verkehr-planer/config.js
+++ portale/verkehr-planer/config.js
@@ -1,43 +1,41 @@
 define(function () {
 
     var config = {
-            title: "Planerportal-Verkehr",
-            logo: "../img/hh-logo.png",
-            wfsImgPath: "..components/lgv-config/img",
-            allowParametricURL: true,
-            view: {
-                center: [565874, 5934140] // Rathausmarkt
-            },
-            layerConf: "../components/lgv-config/services-fhhnet-ALL.json",
-            restConf: "../components/lgv-config/rest-services-fhhnet.json",
-            styleConf: "../components/lgv-config/style.json",
-            print: {
-                printID: "99999",
-                title: "Geoportal-Verkehr",
-                gfi: false
-            },
-            proxyURL: "/cgi-bin/proxy.cgi",
-            mouseHover: true,
-            scaleLine: true,
-            startUpModul: "",
-            searchBar: {
-                minChars: 3,
-                gazetteer: {
-                    minChars: 3,
-                    url: "/geodienste_hamburg_de/HH_WFS_DOG?service=WFS&request=GetFeature&version=2.0.0",
-                    searchStreets: true,
-                    searchHouseNumbers: true,
-                    searchDistricts: true,
-                    searchParcels: true
-                },
-          
-                specialWFS: {
-                minChar: 3,
-                    definitions: [
-                        {
-                            url: "/geodienste_hamburg_de/HH_WFS_Bebauungsplaene?service=WFS&request=GetFeature&version=2.0.0",
-                            data: "typeNames=prosin_festgestellt&propertyName=planrecht",
-                            name: "bplan"
+       wfsImgPath: "..components/lgv-config/img",
+       allowParametricURL: true,
+       view: {
+           center: [565874, 5934140] // Rathausmarkt
+       },
+       layerConf: "../components/lgv-config/services-fhhnet-ALL.json",
+       restConf: "../components/lgv-config/rest-services-fhhnet.json",
+       styleConf: "../components/lgv-config/style.json",
+       print: {
+           printID: "99999",
+           title: "Planerportal-Verkehr",
+           gfi: false
+       },
+       proxyURL: "/cgi-bin/proxy.cgi",
+       mouseHover: true,
+       scaleLine: true,
+       startUpModul: "",
+       searchBar: {
+            minChars: 3,
+            gazetteer: {
+               minChars: 3,
+               url: "/geodienste_hamburg_de/HH_WFS_DOG?service=WFS&request=GetFeature&version=2.0.0",
+               searchStreets: true,
+               searchHouseNumbers: true,
+               searchDistricts: true,
+               searchParcels: true
+           },
+           
+           specialWFS: {
+               minChar: 3,
+               definitions: [
+                   {
+                       url: "/geodienste_hamburg_de/HH_WFS_Bebauungsplaene?service=WFS&request=GetFeature&version=2.0.0",
+                       data: "typeNames=prosin_festgestellt&propertyName=planrecht",
+                       name: "bplan"
                    },
                    {
                        url: "/geodienste_hamburg_de/HH_WFS_Bebauungsplaene?service=WFS&request=GetFeature&version=2.0.0",
