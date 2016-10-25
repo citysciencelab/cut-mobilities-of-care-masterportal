07ab8074bb1d3b3fd7a6c42fd2c7da8b9f71238c
Angela Bialluch <angela.bialluch@gv.hamburg.de>
2016-10-25 08:16:43 +0200

    update portale/solaratlas/config.js portale/solaratlas/config.json

--- portale/solaratlas/config.js
+++ portale/solaratlas/config.js
@@ -1,9 +1,6 @@
 define(function () {
     var config = {
         ignoredKeys: ["BOUNDEDBY", "SHAPE", "SHAPE_LENGTH", "SHAPE_AREA", "OBJECTID", "GLOBALID", "GEOMETRY", "SHP", "SHP_AREA", "SHP_LENGTH","GEOM"],
-
-        logoLink: "https://www.hamburgenergie.de/",
-        logoTooltip: "Hamburg Energie",
         simpleMap: false,
         wfsImgPath: "../components/lgv-config/img/",
         allowParametricURL: true,
