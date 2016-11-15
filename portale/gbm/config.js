c560688eaf220f5867c39bc0a3fe150b830bee15
Angela Bialluch <angela.bialluch@gv.hamburg.de>
2016-11-15 08:17:11 +0100

    update der config custom, neue layer

--- portale/gbm/config.js
+++ portale/gbm/config.js
@@ -7,6 +7,11 @@ define(function () {
        view: {
            center: [565874, 5934140] // Rathausmarkt
        },
+        
+        namedProjections: [
+        // ETRS89 UTM
+            ["EPSG:25832", "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
+        ],
        layerConf: "../components/lgv-config/services-fhhnet-ALL.json",
        restConf: "../components/lgv-config/rest-services-fhhnet.json",
        styleConf: "../components/lgv-config/style.json",
