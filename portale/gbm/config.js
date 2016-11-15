13cfc66e7e5dd75f59f7e06de8cae34d9940410e
Angela Bialluch <angela.bialluch@gv.hamburg.de>
2016-11-15 13:23:18 +0100

    update config und themenbaum

--- portale/gbm/config.js
+++ portale/gbm/config.js
@@ -1,17 +1,15 @@
 define(function () {
 
     var config = {
-        ignoredKeys: ["BOUNDEDBY", "SHAPE", "SHAPE_LENGTH", "SHAPE_AREA", "OBJECTID", "GLOBALID", "GEOMETRY", "SHP", "SHP_AREA", "SHP_LENGTH","GEOM"],
        wfsImgPath: "..components/lgv-config/img",
        allowParametricURL: true,
        view: {
            center: [565874, 5934140] // Rathausmarkt
        },
-        
         namedProjections: [
-        // ETRS89 UTM
-            ["EPSG:25832", "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
-        ],
+            // ETRS89 UTM
+                ["EPSG:25832", "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
+            ],
        layerConf: "../components/lgv-config/services-fhhnet-ALL.json",
        restConf: "../components/lgv-config/rest-services-fhhnet.json",
        styleConf: "../components/lgv-config/style.json",
