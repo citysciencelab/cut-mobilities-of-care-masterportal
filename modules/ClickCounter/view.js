2a219d64023ad3fc270509eb83f2ee39528bc780
Michael Bieler <michael.bieler@gv.hamburg.de>
2016-11-24 10:43:10 +0100

    refactor jsLint conventions umgesetzt

--- modules/ClickCounter/view.js
+++ modules/ClickCounter/view.js
@@ -1,21 +1,21 @@
 define([
-    'jquery',
-    'backbone',
-    'eventbus',
-    'modules/ClickCounter/model'
+    "jquery",
+    "backbone",
+    "eventbus",
+    "modules/ClickCounter/model"
 ], function ($, Backbone, EventBus, ClickCounter) {
 
     var ClickCounterView = Backbone.View.extend({
         model: ClickCounter,
         initialize: function () {
-            if (this.model.get('srcUrl') !== '') {
+            if (this.model.get("srcUrl") !== "") {
                 // Warte auf Zufügen von Layern in Layertree
-                EventBus.on('registerLayerTreeInClickCounter', this.registerLayerEvent, this);
-                EventBus.on('registerToolsClickInClickCounter', this.registerToolsClickEvent, this);
-                EventBus.on('registerRoutingClickInClickCounter', this.registerRoutingClickEvent, this);
-                EventBus.on('registerZoomButtonsInClickCounter', this.registerZoomButtonsClickEvent, this);
+                EventBus.on("registerLayerTreeInClickCounter", this.registerLayerEvent, this);
+                EventBus.on("registerToolsClickInClickCounter", this.registerToolsClickEvent, this);
+                EventBus.on("registerRoutingClickInClickCounter", this.registerRoutingClickEvent, this);
+                EventBus.on("registerZoomButtonsInClickCounter", this.registerZoomButtonsClickEvent, this);
                 // Erzeuge iFrame
-                $('<iframe " src="' + this.model.get('srcUrl') + '" id="' + this.model.get('countframeid') + '" width="0" height="0" frameborder="0"/>').appendTo('body');
+                $("<iframe ' src='" + this.model.get("srcUrl") + "' id='" + this.model.get("countframeid") + "' width='0' height='0' frameborder='0'/>").appendTo("body");
                 this.registerMap();
                 // fired beim Öffnen der Seite
                 this.registerClick();
@@ -24,7 +24,7 @@ define([
         registerZoomButtonsClickEvent: function (zoomButtons) {
             // fired beim Zoomen über zoombuttons
             if (zoomButtons.length > 0) {
-                zoomButtons.click(function() {
+                zoomButtons.click(function () {
                   this.registerClick();
                 }.bind(this));
             }
@@ -32,7 +32,7 @@ define([
         registerRoutingClickEvent: function (routingTool) {
             // fired beim Klicken in der Routenberechnung
             if (routingTool.length > 0) {
-                routingTool.click(function() {
+                routingTool.click(function () {
                   this.registerClick();
                 }.bind(this));
             }
@@ -40,7 +40,7 @@ define([
         registerLayerEvent: function (layertree) {
             // fired beim LayerChange, Info-Button, Einstellungen auf dem Layertree
             if (layertree.length > 0) {
-                layertree.click(function() {
+                layertree.click(function () {
                      this.registerClick();
                 }.bind(this));
             }
@@ -48,14 +48,14 @@ define([
         registerToolsClickEvent: function (toolsmenu) {
             // fired beim ToolChange
             if (toolsmenu.length > 0) {
-                toolsmenu.click(function() {
+                toolsmenu.click(function () {
                   this.registerClick();
                 }.bind(this));
             }
         },
         registerMap: function () {
             // fired beim Ausschnitt-Move und Klickabfragen auf Features
-            $('#map').click(function() {
+            $("#map").click(function () {
               this.registerClick();
             }.bind(this));
         },
