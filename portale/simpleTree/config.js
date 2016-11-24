0e05be4d4a6e8042f9315159a53504c1f4a5d5fc
Christopher Vornfeld <christopher.vornfeld@gv.hamburg.de>
2016-11-24 13:09:54 +0100

    config simple und simpleTree

--- portale/simpleTree/config.js
+++ portale/simpleTree/config.js
@@ -25,42 +25,42 @@ define(function() {
             options: [
                 {
                     resolution: 76.43718115953851,
-                    scale: "288896",
+                    scale: 288896,
                     zoomLevel: 0
                 },
                 {
                     resolution: 38.21859057976939,
-                    scale: "144448",
+                    scale: 144448,
                     zoomLevel: 1
                 },
                 {
                     resolution: 19.109295289884642,
-                    scale: "72223",
+                    scale: 72223,
                     zoomLevel: 2
                 },
                 {
                     resolution: 9.554647644942321,
-                    scale: "36112",
+                    scale: 36112,
                     zoomLevel: 3
                 },
                 {
                     resolution: 4.7773238224711605,
-                    scale: "18056",
+                    scale: 18056,
                     zoomLevel: 4
                 },
                 {
                     resolution: 2.3886619112355802,
-                    scale: "9028",
+                    scale: 9028,
                     zoomLevel: 5
                 },
                 {
                     resolution: 1.1943309556178034,
-                    scale: "4514",
+                    scale: 4514,
                     zoomLevel: 6
                 },
                 {
                     resolution: 0.5971654778089017,
-                    scale: "2257",
+                    scale: 2257,
                     zoomLevel: 7
                 }
             ]
