52f227a4cc69cc452a224b0f1c9266cab93717fe
Michael Bieler <michael.bieler@gv.hamburg.de>
2016-11-25 13:58:48 +0100

    update ClickCounter Event Zoom In Out mit Radio

--- modules/ClickCounter/view.js
+++ modules/ClickCounter/view.js
@@ -14,24 +14,16 @@ define([
 
             channel.on({
                 "toolChanged": this.registerClick,
-                "calcRoute": this.registerClick
+                "calcRoute": this.registerClick,
+                "zoomChanged": this.registerClick
             }, this);
 
             // Warte auf Zufügen von Layern in Layertree
             EventBus.on("registerLayerTreeInClickCounter", this.registerLayerEvent, this);
-            EventBus.on("registerZoomButtonsInClickCounter", this.registerZoomButtonsClickEvent, this);
 
             this.registerMap();
             // fired beim Öffnen der Seite
             this.registerClick();
-        },
-        registerZoomButtonsClickEvent: function (zoomButtons) {
-            // fired beim Zoomen über zoombuttons
-            if (zoomButtons.length > 0) {
-                zoomButtons.click(function () {
-                  this.registerClick();
-                }.bind(this));
-            }
         },
         registerLayerEvent: function (layertree) {
             // fired beim LayerChange, Info-Button, Einstellungen auf dem Layertree
