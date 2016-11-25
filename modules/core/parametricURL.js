274aae5bfb18bcc7eb36486d2de9e77f356ad744
Michael Bieler <michael.bieler@gv.hamburg.de>
2016-11-25 15:26:33 +0100

    hotfix Parametrisierter Aufruf mit layerid ohne visibility / transparency führte zu Fehler

--- modules/core/parametricURL.js
+++ modules/core/parametricURL.js
@@ -1,12 +1,15 @@
 define([
     "backbone",
     "backbone.radio",
+    "underscore.string",
     "config",
     "modules/core/configLoader/preparser"
-], function (Backbone, Radio, Config, Preparser) {
+], function (Backbone, Radio, _String, Config, Preparser) {
 
     var ParametricURL = Backbone.Model.extend({
-
+        defaults: {
+            layerParams: []
+        },
         initialize: function () {
             var channel = Radio.channel("ParametricURL");
 
@@ -50,40 +53,36 @@ define([
         },
         createLayerParams: function () {
             var layerIdString = _.values(_.pick(this.getResult(), "LAYERIDS"))[0],
-                visibilityListString = "",
-                transparencyListString = "",
-                layerIdList = [],
-                visibilityList = [],
-                transparencyList = [],
+                visibilityListString = _.has(this.getResult(), "VISIBILITY") ? _.values(_.pick(this.getResult(), "VISIBILITY"))[0] : "",
+                transparencyListString = _.has(this.getResult(), "TRANSPARENCY") ? _.values(_.pick(this.getResult(), "TRANSPARENCY"))[0] : "",
+                layerIdList = layerIdString.indexOf(",") !== -1 ? layerIdString.split(",") : new Array(layerIdString),
+                visibilityList = visibilityListString === "" ? _.map(layerIdList, function () {
+                    return true;
+                }) : visibilityListString.indexOf(",") > -1 ? _.map(visibilityListString.split(","), function (val) {
+                    return _String.toBoolean(val);
+                }) : new Array(_String.toBoolean(visibilityListString)),
+                transparencyList = transparencyListString === "" ? _.map(layerIdList, function () {
+                    return 0;
+                }) : transparencyListString.indexOf(",") > -1 ? _.map(transparencyListString.split(","), function (val) {
+                    return _String.toNumber(val);
+                }) : new Array(_String.toNumber(transparencyListString)),
                 layerParams = [];
 
-                if (_.has(this.getResult(), "VISIBILITY")) {
-                    visibilityListString = _.values(_.pick(this.getResult(), "VISIBILITY"))[0];
-                }
-                if (_.has(this.getResult(), "TRANSPARENCY")) {
-                     transparencyListString = _.values(_.pick(this.getResult(), "TRANSPARENCY"))[0];
-                }
-
-           if (layerIdString.indexOf(",") !== -1) {
-               layerIdList = layerIdString.split(",");
-               visibilityList = visibilityListString.split(",");
-               transparencyList = transparencyListString.split(",");
-           }
-           else {
-               layerIdList.push(layerIdString);
-               visibilityList.push(visibilityListString);
-               transparencyList.push(transparencyListString);
-           }
-           _.each(layerIdList, function (val, index) {
-               layerParams.push({id: val, visibility: visibilityList[index], transparency: transparencyList[index]});
-           });
-           this.setLayerParams(layerParams);
+            if (layerIdList.length !== visibilityList.length || visibilityList.length !== transparencyList.length) {
+                Radio.trigger("Alert", "alert", {text: "<strong>Parametrisierter Aufruf fehlerhaft!</strong> Die Angaben zu LAYERIDS passen nicht zu VISIBILITY bzw. TRANSPARENCY. Die Parameter werden ignoriert.", kategorie: "alert-warning"});
+            }
+            else {
+                _.each(layerIdList, function (val, index) {
+                    layerParams.push({id: val, visibility: visibilityList[index], transparency: transparencyList[index]});
+                });
+                this.setLayerParams(layerParams);
+            }
         },
         createLayerParamsUsingMetaId: function (metaIds) {
             var layers = [],
-            layerParams = [];
+                layerParams = [],
+                hintergrundKarte = Radio.request("Parser", "getItemByAttributes", {id: "453"});
 
-            var hintergrundKarte = Radio.request("Parser", "getItemByAttributes", {id: "453"});
             layers.push(hintergrundKarte);
 
             _.each(metaIds, function (metaId) {
@@ -93,7 +92,7 @@ define([
                 });
             });
             _.each(layers, function (layer) {
-                layerParams.push({id: layer.id, visibility: "TRUE", transparency: "0"});
+                layerParams.push({id: layer.id, visibility: true, transparency: 0});
             });
             this.setLayerParams(layerParams);
         },
