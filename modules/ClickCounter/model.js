4e666334ab89584a724f5f51789d6ff25de591be
Michael Bieler <michael.bieler@gv.hamburg.de>
2016-11-25 13:45:31 +0100

    refactor jsLint conventions umgesetzt

--- modules/ClickCounter/model.js
+++ modules/ClickCounter/model.js
@@ -1,26 +1,29 @@
 define([
-    'backbone',
-    'config',
+    "backbone",
+    "config"
 ], function (Backbone, Config) {
 
     var ClickCounter = Backbone.Model.extend({
         defaults: {
-            countframeid: _.uniqueId('countframe'),
-    		srcUrl: ''
+            countframeid: _.uniqueId("countframe"),
+            srcUrl: ""
         },
-    	initialize: function () {
-            var srcurl = '';
+        initialize: function () {
+            var srcurl = "";
+
             _.each(Config.clickCounter, function (value, key) {
                 if (Config.clickCounter.version.toUpperCase() === key.toUpperCase()) {
                     srcurl = value;
                 }
             });
-            if (srcurl != '') {
-                this.set('srcUrl', srcurl);
+            if (srcurl != "") {
+                this.set("srcUrl", srcurl);
             }
         },
         refreshIframe: function () {
-            $('#' + this.get('countframeid')).attr( 'src', function ( i, val ) { return val; });
+            $("#" + this.get("countframeid")).attr("src", function (i, val) {
+                return val;
+            });
         }
     });
     return new ClickCounter();
