076c1d294aa17efbcc601e9b3d1c7d4bc60afd37
Michael Bieler <michael.bieler@gv.hamburg.de>
2016-12-08 09:28:30 +0100

    fix verkehrsfunction Util-Fehler durch Radio abgelöst

--- portal/master/config.js
+++ portal/master/config.js
@@ -156,7 +156,7 @@ define(function () {
         * @example customModules: ["../url", "../url"]
         * @default []
         */
-        customModules: ["../portale/master/verkehrsfunctions"],
+        customModules: ["../portal/master/verkehrsfunctions"],
         /**
         * @memberof config
         * @type {Object}
