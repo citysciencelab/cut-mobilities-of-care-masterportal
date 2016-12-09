1825b83ba913ed1a23bd0560cc2c258b1f381b69
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2016-12-09 14:10:36 +0100

    fix lighttree sortierung

--- modules/mapMarker/view.js
+++ modules/mapMarker/view.js
@@ -9,6 +9,7 @@ define([
 
     var searchVector = new ol.layer.Vector({
         source: new ol.source.Vector(),
+        alwaysOnTop: true,
         style: new ol.style.Style({
             stroke: new ol.style.Stroke({
                 color: "#08775f",
@@ -21,7 +22,7 @@ define([
         })
     });
 
-    Radio.trigger("Map", "addLayer", searchVector);
+    Radio.trigger("Map", "addLayerToIndex", [searchVector, Radio.request("Map", "getLayers").getArray().length]);
 
     return Backbone.View.extend({
         model: MapHandlerModel,
