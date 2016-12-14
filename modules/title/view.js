bbaee03522a5bd2fcbbeabfe899a741bb3dd1882
Weiter <WeiterJo@FHHNET.Stadt.Hamburg.de>
2016-12-14 09:29:43 +0100

    update width wird in der view.js gesetzt

--- modules/title/view.js
+++ modules/title/view.js
@@ -6,11 +6,22 @@ define([
 
     var TitleView = Backbone.View.extend({
         className: "visible-lg-block portal-title",
+        id: "portalTitle",
         template: _.template(TitleTemplate),
         initialize: function (title) {
+            $(window).on("resize", this.setSize);
             this.setLogo();
             this.render(title);
         },
+
+        setSize: function () {
+            var rootWidth = $("#root").width(),
+                searchbarWidth = $("#searchbar").width(),
+                width = window.innerWidth - rootWidth - searchbarWidth - 30;
+
+            $("#portalTitle").width(width);
+        },
+
         render: function (portalTitle) {
             this.$el.html(this.template({
                 title: portalTitle,
@@ -18,7 +29,14 @@ define([
                 logoLink: Radio.request("Parser", "getPortalConfig").LogoLink || "http://geoinfo.hamburg.de",
                 logoTooltip: Radio.request("Parser", "getPortalConfig").LogoToolTip || "Landesbetrieb Geoinformation und Vermessung"
             }));
+
             $(".navbar-collapse").append(this.$el);
+
+            // maximaler platz den der Titel haben kann, Fensterbreite - root - searchbar - 10 (als toleranz)
+            var width = window.innerWidth - 678 - 407 - 10;
+
+            $("#portalTitle").width(width);
+
         },
         setLogo: function () {
             var logo = Radio.request("Parser", "getPortalConfig").PortalLogo,
