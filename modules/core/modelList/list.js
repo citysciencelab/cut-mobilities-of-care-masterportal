3d33131d898d32c4513f0d6945d5a6ebceba3482
Christopher Reder <christopher.reder@gv.hamburg.de>
2017-05-12 14:51:17 +0200

    update if Anweisung entfernt

--- modules/core/modelList/list.js
+++ modules/core/modelList/list.js
@@ -351,11 +351,9 @@ define([
             }
             // Parametrisierter Aufruf
             else if (paramLayers.length > 0) {
-                _.each(Radio.request("Parser", "getItemsByAttributes", {type: "layer"}), function (layer) {
-                    if (layer.isVisibleInMap === true) {
-                        layer.isVisibleInMap = false;
-                        layer.isSelected = false;
-                    }
+                _.each(Radio.request("Parser", "getItemsByAttributes", {isVisibleInMap: true}), function (layer) {
+                    layer.isVisibleInMap = false;
+                    layer.isSelected = false;
                 });
                 _.each(paramLayers, function (paramLayer) {
                     var lightModel = Radio.request("Parser", "getItemByAttributes", {id: paramLayer.id});
