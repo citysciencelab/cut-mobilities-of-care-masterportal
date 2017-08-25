4c6c143272b6c1163893becb76e9de50014f5000
FHHNET\WeiterJo <jonas.weiter@gv.hamburg.de>
2017-08-25 10:29:55 +0200

    update wfs parsed attributwerte mit pipe

--- modules/tools/filter/query/source/wfs.js
+++ modules/tools/filter/query/source/wfs.js
@@ -61,13 +61,38 @@ define(function (require) {
                         values.push(parseInt(feature.get(featureAttribute.name), 10));
                     }
                     else {
-                        values.push(feature.get(featureAttribute.name));
+                        var stringValues = this.parseStringType(feature, featureAttribute);
+
+                        values = _.union(values, stringValues);
                     }
-                });
+                }, this);
                 featureAttribute.values = _.unique(values);
             }, this);
 
             return featureAttributesMap;
+        },
+        /**
+         * parsed attributwerte mit einem Pipe-Zeichen ("|") und returned ein Array mit den einzelnen Werten
+         * @param  {[type]} feature          [description]
+         * @param  {[type]} featureAttribute [description]
+         * @return {[type]}                  [description]
+         */
+        parseStringType: function (feature, featureAttribute) {
+            var values = [];
+
+            if (!_.isUndefined(feature.get(featureAttribute.name))) {
+                if (feature.get(featureAttribute.name).indexOf("|") !== -1) {
+                    var featureValues = feature.get(featureAttribute.name).split("|");
+
+                    _.each(featureValues, function (value) {
+                        values.push(value);
+                    });
+                }
+                else {
+                    values.push(feature.get(featureAttribute.name));
+                }
+            }
+            return values;
         }
     });
 
