cd4f2fb254fdc814657d8e61670c7dd4305fa4dd
coderwelsch <LuckeyRo@LGV-W-D614726.fhhnet.stadt.hamburg.de>
2017-08-25 15:36:46 +0200

    add sidebar model

---
+++ modules/sidebar/view.js
@@ -0,0 +1,39 @@
+define(function (require) {
+
+    var Model = require("modules/sidebar/model"),
+        SidebarView;
+
+    SidebarView = Backbone.View.extend({
+        model: new Model(),
+        className: "sidebar",
+        initialize: function () {
+            this.listenTo(this.model, {
+                "change:isOpen": this.toggle
+            });
+        },
+        toggle: function (model, isOpen) {
+            console.log(isOpen);
+            if (isOpen) {
+                this.render();
+            }
+            else {
+                this.removeView();
+            }
+        },
+        removeView: function () {
+            this.$el.remove();
+            $("#map").css("width", "100%");
+        },
+        render: function () {
+            console.log("render");
+            $("#map").css("width", "70%");
+
+            $("#map").after(this.$el);
+        },
+        closeSidebar: function () {
+            console.log("closeSidebar");
+            this.model.setIsOpen(false);
+        }
+    });
+    return SidebarView;
+});
