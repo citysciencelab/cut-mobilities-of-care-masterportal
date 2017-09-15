7966cd54f781864da279f47e28c4f336a964e03c
mbgvhh <markus.besenfelder@gv.hamburg.de>
2017-09-15 13:07:36 +0200

    update buttonTests umgesetzt

--- test/end2end/chrome.js
+++ test/end2end/chrome.js
@@ -1,41 +1,78 @@
-// var assert = require("chai").assert,
-//     expect = require("chai").expect,
-//     webdriver = require("selenium-webdriver"),
-//     driver = new webdriver.Builder().
-//     withCapabilities(webdriver.Capabilities.chrome()).
-//     build();
-
-// describe("Google Search", function () {
-//   it("should work", function () {
-
-
-//   driver.get("http://www.google.com");
-
-//   });
-// });
-
-var  assert = require("chai").assert,
+var assert = require("chai").assert,
     expect = require("chai").expect,
     test = require("selenium-webdriver/testing"),
     webdriver = require("selenium-webdriver"),
+    until = webdriver.until,
     driver,
-    until = webdriver.until;
+    loader;
 
-test.describe("Google Search", function () {
+test.describe("Master in Chrome", function () {
   this.timeout(15000);
   test.before(function () {
                 /* runs before the first it() is executed */
                 driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
-
+                driver.get("https://localhost:9001/portal/master");
+                loader = driver.findElement(webdriver.By.id("loader"));
             });
 
-  test.it("should work", function () {
-                driver.get("http://www.google.com");
-                var searchBox = driver.findElement(webdriver.By.name('q'));
-                searchBox.sendKeys('simple programmer');
-                searchBox.getAttribute('value').then(function(value) {
-                   expect(value.to.be('simple programmer'));
+  test.it("should have plusbutton", function () {
+                driver.wait(until.elementIsNotVisible(loader), 50000, "Loader nach timeout noch sichtbar");
+                driver.wait(webdriver.until.elementLocated(webdriver.By.css("span.glyphicon.glyphicon-plus")), 9000);
+                var plus = driver.findElement(webdriver.By.css("span.glyphicon.glyphicon-plus"));
+
+                expect(plus).to.exist;
+            });
+
+  test.it("should zoom in after click plusbutton", function () {
+                driver.wait(until.elementIsNotVisible(loader), 50000, "Loader nach timeout noch sichtbar");
+                var plus = driver.findElement(webdriver.By.css("span.glyphicon.glyphicon-plus")),
+                    resolution;
+
+                function getResolution () {
+                  resolution = Backbone.Radio.request("MapView", "getResolution").resolution;
+                  return resolution;
+                };
+
+                driver.executeScript(getResolution).then(function (resolution) {
+                    this.resolution = resolution;
+                    plus.click();
                 });
-               driver.quit();
+
+                driver.executeScript(getResolution).then(function (resolution) {
+                   expect(this.resolution).to.be.above(resolution);
+                });
+
             });
+
+  test.it("should have minusbutton", function () {
+                driver.wait(until.elementIsNotVisible(loader), 50000, "Loader nach timeout noch sichtbar");
+                var minus = driver.findElement(webdriver.By.css("span.glyphicon.glyphicon-minus"));
+
+                expect(minus).to.exist;
+            });
+
+    test.it("should zoom out after click minusbutton", function () {
+                driver.wait(until.elementIsNotVisible(loader), 50000, "Loader nach timeout noch sichtbar");
+                var minus = driver.findElement(webdriver.By.css("span.glyphicon.glyphicon-minus")),
+                    resolution;
+
+                function getResolution () {
+                  resolution = Backbone.Radio.request("MapView", "getResolution").resolution;
+                  return resolution;
+                };
+
+                driver.executeScript(getResolution).then(function (resolution) {
+                    this.resolution = resolution;
+                    minus.click();
+                });
+
+                driver.executeScript(getResolution).then(function (resolution) {
+                   expect(this.resolution).to.be.below(resolution);
+                });
+
+            });
+
+  test.after(function () {
+          // driver.quit();
+        });
   });
