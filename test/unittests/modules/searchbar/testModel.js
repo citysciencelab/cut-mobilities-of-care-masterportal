85317a140dc81abefbfb51db0c0f4cd1c95ad533
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2017-09-20 09:47:25 +0200

    fix searchbar test

--- test/unittests/modules/searchbar/testModel.js
+++ test/unittests/modules/searchbar/testModel.js
@@ -21,16 +21,16 @@ define(function(require) {
         });
         describe("shortenNames", function () {
             it("should return name if name is shorter then length", function () {
-                expect(model.shortenNames("test", 5)).to.be.an("string").that.equals("test");
+                expect(model.shortenString("test", 5)).to.be.an("string").that.equals("test");
             });
             it("should return name if name length is euqal to length", function () {
-                expect(model.shortenNames("test", 4)).to.be.an("string").that.equals("test");
+                expect(model.shortenString("test", 4)).to.be.an("string").that.equals("test");
             });
             it("should return name croped to length extended with '...' if name is longer then length", function () {
-                expect(model.shortenNames("test", 3)).to.be.an("string").that.equals("tes...");
+                expect(model.shortenString("test", 3)).to.be.an("string").that.equals("tes..");
             });
-            it("should not throw error if name is undefined", function () {
-                expect(model.shortenNames(undefined, 1)).to.be.undefined;
+            it("should return undefined if name is undefined", function () {
+                expect(model.shortenString(undefined, 1)).to.be.undefined;
             });
         });
     });
