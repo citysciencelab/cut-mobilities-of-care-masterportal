dd3e810f045ea7de91a1c66c6391b4dfcccf828a
jonas_weiter <jonas.weiter@gv.hamburg.de>
2018-01-31 09:41:15 +0100

    update postMessage url auch in filter und gfi

--- modules/tools/gfi/themes/itgbm/model.js
+++ modules/tools/gfi/themes/itgbm/model.js
@@ -5,16 +5,24 @@ define(function (require) {
         ImgView = require("modules/tools/gfi/objects/image/view"),
         VideoView = require("modules/tools/gfi/objects/video/view"),
         RoutableView = require("modules/tools/gfi/objects/routingButton/view"),
+        Config = require("config"),
         ItGbmTheme;
 
     ItGbmTheme = Theme.extend({
-
+        defaults: _.extend({},Theme.prototype.defaults,
+            {postMessageUrl: "http://localhost:8080"}
+        ),
         initialize: function () {
             this.listenTo(this, {
                 "change:isReady": this.parseGfiContent
             });
+            this.setParams(Config);
         },
-
+        setParams: function (config) {
+            if (_.has(config, "postMessageUrl") && config.postMessageUrl.length > 0) {
+                this.setPostMessageUrl(config.postMessageUrl);
+            }
+        },
         /**
          * sets title and gfiContent attributes
          */
@@ -29,7 +37,10 @@ define(function (require) {
         postMessageToItGbm: function () {
             var featureProperties = _.omit(this.get("feature").getProperties(), ["geometry", "geometry_EPSG_25832", "geometry_EPSG_4326"]);
 
-            parent.postMessage({"featureToDetail": JSON.stringify(featureProperties)}, "http://localhost:8080");
+            parent.postMessage({"featureToDetail": JSON.stringify(featureProperties)}, this.get("postMessageUrl"));
+        },
+        setPostMessageUrl: function (value) {
+            this.set("postMessageUrl", value);
         }
     });
 
