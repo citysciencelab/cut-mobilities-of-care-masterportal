ba2a6a0bbdeedd2104ec7df022dcdb13ee5c3f12
Christopher Reder <christopher.reder@gv.hamburg.de>
2018-02-23 09:29:27 +0100

    fix linter fehler behoben

--- test/unittests/SpecRunner.js
+++ test/unittests/SpecRunner.js
@@ -12,7 +12,7 @@ require.config({
     util: "util",
     config: "testConfig",
     moment: "../../node_modules/moment/min/moment.min",
-    bootstrap: "../../node_modules/bootstrap/js",
+    bootstrap: "../../node_modules/bootstrap/js"
   },
   shim: {
         bootstrap: {
@@ -27,7 +27,7 @@ require.config({
     }
 });
 
-define(function(require) {
+define(function (require) {
     require("mocha");
     require("jquery");
 
@@ -47,7 +47,7 @@ define(function(require) {
     "modules/tools/gfi/themes/schulinfo/testModel.js",
     "modules/core/testMap.js",
     "modules/core/modelList/testList.js"
-    ], function (require) {
+    ], function () {
         Radio = Backbone.Radio;
         mocha.run();
     });
