b64ef7b9fb305239841898a6164b0ff267b960c9
jonas_weiter <jonas.weiter@gv.hamburg.de>
2018-02-26 08:43:09 +0100

    update itgbm gfi add units

--- modules/tools/gfi/themes/itgbm/model.js
+++ modules/tools/gfi/themes/itgbm/model.js
@@ -16,15 +16,33 @@ define(function (require) {
         parseGfiContent: function () {
             this.set("title", this.get("gfiContent")[0].Name);
             this.set("gfiContent", _.omit(this.get("gfiContent")[0], "Name"));
+            this.set("gfiContent", this.addUnits(this.get("gfiContent"), ["Größe"]));
         },
+        /**
+        * adds " ha" on Gewerbliche Standorte and " m²" on Flurstücke where key is inside attrArray
+        */
+        addUnits: function (gfiContent, attrArray) {
+            _.each(gfiContent, function (value, key) {
+                // Gewerbliche Standorte
+                if (this.get("id") === "10319" && _.contains(attrArray, key)) {
+                    gfiContent[key] = String(value) + " ha";
+                }
+                // Flurstücke
+                if (this.get("id") === "10320" && _.contains(attrArray, key)) {
+                    gfiContent[key] = String(value) + " m²";
+                }
+            }, this);
 
+            return gfiContent;
+        },
         /**
          * triggers feature properties via postMessage
          */
         postMessageToItGbm: function () {
             var featureProperties = _.omit(this.get("feature").getProperties(), ["geometry", "geometry_EPSG_25832", "geometry_EPSG_4326"]);
+
             featureProperties.extent = this.get("feature").getGeometry().getExtent();
-            Radio.trigger("RemoteInterface", "postMessage", {"featureToDetail": JSON.stringify(featureProperties),  "layerId": this.get("id"), "layerName": this.get("name")});
+            Radio.trigger("RemoteInterface", "postMessage", {"featureToDetail": JSON.stringify(featureProperties), "layerId": this.get("id"), "layerName": this.get("name")});
         }
     });
 
