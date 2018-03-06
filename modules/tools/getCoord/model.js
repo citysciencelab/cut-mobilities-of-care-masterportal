d37bd9a58d018a044ec44de434c514b1b618500a
elbphilharmonie <michael.bieler@gv.hamburg.de>
2018-03-06 23:44:38 +0100

    update Anpassung Design von Koordinate abfragen an window

--- modules/tools/getCoord/model.js
+++ modules/tools/getCoord/model.js
@@ -2,32 +2,26 @@ define([
     "backbone",
     "backbone.radio",
     "openlayers",
-    "proj4",
-    "config",
-    "bootstrap/popover"
-], function (Backbone, Radio, ol, proj4, Config) {
+    "config"
+], function (Backbone, Radio, ol, Config) {
 
     var CoordPopup = Backbone.Model.extend({
          defaults: {
-            element: $("#popup"),
-            coordOverlay: {},
-            coordinateUTM: {},
-            coordinateGeo: {}
+            selectPointerMove: null,
+            namedProjections: null,
+            mapProjection: null,
+            positionMapProjection: [],
+            updatePosition: true
         },
         initialize: function () {
             this.listenTo(Radio.channel("Window"), {
                 "winParams": this.setStatus
             });
 
-            // this.listenTo(Radio.channel("Tool"), {
-            //     "activatedTool": this.checkTool
-            // });
-
-            // this.setCoordOverlay(new ol.Overlay({
-            //     element: this.getElement()[0]
-            // }));
-            // Radio.trigger("Map", "addOverlay", this.getCoordOverlay());
+            this.setNamedProjections(Radio.request("CRS", "getNamedProjections"));
+            this.setMapProjection(Radio.request("MapView", "getProjection"));
         },
+
         setStatus: function (args) { // Fenstermanagement
             if (args[2].getId() === "coord") {
                 this.set("isCollapsed", args[1]);
@@ -39,48 +33,86 @@ define([
                 this.formats = {};
             }
         },
-        checkTool: function (name) {
-            if (name === "coord") {
-                Radio.trigger("Map", "registerListener", "click", this.setPosition, this);
+
+        createInteraction: function () {
+            this.setSelectPointerMove(new ol.interaction.Pointer({
+                handleMoveEvent: function (evt) {
+                    console.log(evt);
+                    this.checkPosition(evt.coordinate);
+                }.bind(this),
+                handleDownEvent: function (evt) {
+                    this.positionClicked(evt.coordinate);
+                }.bind(this)
+            }, this));
+
+            Radio.trigger("Map", "addInteraction", this.getSelectPointerMove());
+        },
+
+        removeInteraction: function () {
+            Radio.trigger("Map", "removeInteraction", this.getSelectPointerMove());
+            this.set("selectPointerMove", null);
+        },
+
+        checkPosition: function (position) {
+            if (this.getUpdatePosition() === true) {
+                this.setPositionMapProjection(position);
             }
-            else {
-                Radio.trigger("Map", "unregisterListener", "click", this.setPosition, this);
-            }
         },
-        getCoordOverlay: function () {
-            return this.get("coordOverlay");
+
+        positionClicked: function (position) {
+            this.setUpdatePosition(!this.getUpdatePosition());
         },
-        setCoordOverlay: function (coordOverlay) {
-            this.set("coordOverlay", coordOverlay);
+
+        transformPosition: function (targetProjection) {
+            var positionMapProjection = this.getPositionMapProjection(),
+                positionTargetProjection = Radio.request("CRS", "transformFromMapProjection", targetProjection, positionMapProjection);
+
+            return positionTargetProjection;
         },
-        getCoordinateUTM: function () {
-            return this.get("coordinateUTM");
+
+        // getter for selectPointerMove
+        getSelectPointerMove: function () {
+            return this.get("selectPointerMove");
         },
-        setCoordinateUTM: function (coordinateUTM) {
-            this.set("coordinateUTM", coordinateUTM);
+        // setter for selectPointerMove
+        setSelectPointerMove: function (value) {
+            this.set("selectPointerMove", value);
         },
-        getCoordinateGeo: function () {
-            return this.get("coordinateGeo");
+
+        // getter for mapProjection
+        getMapProjection: function () {
+            return this.get("mapProjection");
         },
-        setCoordinateGeo: function (coordinateGeo) {
-            this.set("coordinateGeo", coordinateGeo);
+        // setter for mapProjection
+        setMapProjection: function (value) {
+            this.set("mapProjection", value);
         },
-        getElement: function () {
-            return this.get("element");
+
+        // getter for namedProjections
+        getNamedProjections: function () {
+            return this.get("namedProjections");
         },
-        setElement: function (element) {
-            this.set("element", element);
+        // setter for namedProjections
+        setNamedProjections: function (value) {
+            this.set("namedProjections", value);
         },
-        destroyPopup: function () {
-            this.getElement().popover("destroy");
+
+        // getter for positionMapProjection
+        getPositionMapProjection: function () {
+            return this.get("positionMapProjection");
         },
-        showPopup: function () {
-            this.getElement().popover("show");
+        // setter for positionMapProjection
+        setPositionMapProjection: function (value) {
+            this.set("positionMapProjection", value);
         },
-        setPosition: function (evt) {
-            this.getCoordOverlay().setPosition(evt.coordinate);
-            this.setCoordinateUTM(evt.coordinate);
-            this.setCoordinateGeo(ol.coordinate.toStringHDMS(proj4(proj4(Config.view.epsg), proj4("EPSG:4326"), this.getCoordinateUTM())));
+
+        // getter for updatePosition
+        getUpdatePosition: function () {
+            return this.get("updatePosition");
+        },
+        // setter for updatePosition
+        setUpdatePosition: function (value) {
+            this.set("updatePosition", value);
         }
     });
 
--- modules/tools/getCoord/view.js
+++ modules/tools/getCoord/view.js
@@ -13,20 +13,36 @@ define(function (require) {
         },
         initialize: function () {
             this.listenTo(this.model, {
-                "change:isCollapsed change:isCurrentWin change:url": this.render
+                "change:isCollapsed change:isCurrentWin change:url": this.render,
+                "change:positionMapProjection": this.changedPosition
             });
             this.listenTo(this.model, "change:coordinateGeo", this.render);
         },
+
         render: function () {
             if (this.model.get("isCurrentWin") === true && this.model.get("isCollapsed") === false) {
                 var attr = this.model.toJSON();
 
                 this.$el.html("");
                 $(".win-heading").after(this.$el.html(this.template(attr)));
+                this.model.createInteraction();
                 this.delegateEvents();
             }
             else {
+                this.model.removeInteraction();
                 this.undelegateEvents();
+            }
+        },
+
+        changedPosition: function () {
+            var targetProjection = $("#coordSystemField option:selected").text(),
+                position = this.model.transformPosition(targetProjection),
+                easting = (Math.round(position[0] * 100) / 100).toFixed(2);
+                northing = (Math.round(position[1] * 100) / 100).toFixed(2);
+
+            if (position) {
+                $("#coordinatesEastingField").val(easting);
+                $("#coordinatesNorthingField").val(northing);
             }
         }
     });
