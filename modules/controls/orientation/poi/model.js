58b01fb41ed96485afa7a3f7b250c4b925674461
elbphilharmonie <michael.bieler@gv.hamburg.de>
2018-03-19 17:31:08 +0100

    update POI findet nun auch Polygone und Line und Circle

--- modules/controls/orientation/poi/model.js
+++ modules/controls/orientation/poi/model.js
@@ -90,40 +90,61 @@ define(function (require) {
          * @return {string}      imgPath
          */
         getImgPath: function (feat) {
-            var style = Radio.request("StyleList", "returnModelById", feat.styleId),
-                styleField = style.get("styleField") && style.get("styleField") !== "" ? style.get("styleField") : null,
-                styleFieldValues = style.get("styleFieldValues") && style.get("styleFieldValues").length > 0 ? style.get("styleFieldValues") : null,
-                imageName = style.get("imageName") && style.get("imageName") !== "" ? style.get("imageName") : null,
-                imagePath = style.get("imagePath") && style.get("imagePath") !== "" ? style.get("imagePath") : null;
+            var imagePath = "",
+                style = Radio.request("StyleList", "returnModelById", feat.styleId),
+                styleClass = style.get("class"),
+                styleSubClass = style.get("subClass"),
+                styleFieldValues = style.get("styleFieldValues");
 
-            if (styleField && styleFieldValues && imagePath) {
-                return imagePath + this.getStyleFieldImageName(feat, styleField, styleFieldValues);
+            if (styleClass === "POINT") {
+                // Custom Point Styles
+                if (styleSubClass === "CUSTOM") {
+                    imagePath = style.get("imagePath") + this.createStyleFieldImageName(feat, style);
+                }
+                // Circle Point Style
+                if (styleSubClass === "CIRCLE") {
+                    imagePath = this.createCircleSVG(style);
+                }
+                else {
+                    if (style.get("imageName") !== "blank.png") {
+                        imagePath = style.get("imagePath") + style.get("imageName");
+                    }
+                }
             }
-            else if (imageName && imagePath) {
-                return imagePath + imageName;
+            // Simple Line Style
+            if (styleClass === "LINE") {
+                imagePath = this.createLineSVG(style);
             }
-            else {
-                return "";
+            // Simple Polygon Style
+            if (styleClass === "POLYGON") {
+                imagePath = this.createPolygonSVG(style);
             }
+
+            return imagePath;
         },
 
         /**
          * Sucht nach dem ImageName bei styleField-Angaben im Style
          * @param  {ol.feature} feature      Feature mit allen Angaben
-         * @param  {string} styleField       Name des StyleFields
-         * @param  {[Object]} StyleFields    Array aus Objekten mit styleFieldValues
+         * @param  {object} styleField       Style des Features
          * @return {string}                  Name des Bildes
          */
-        getStyleFieldImageName: function (feature, styleField, styleFields) {
-            var value = feature.get(styleField),
+        createStyleFieldImageName: function (feature, style) {
+            var styleField = style.get("styleField"),
+                styleFields = style.get("styleFieldValues"),
+                value = feature.get(styleField),
                 image = _.find(styleFields, function (style) {
                     return style.styleFieldValue === value;
                 }),
                 value = image.imageName;
 
-                return value;
+            return value;
         },
 
+        /**
+         * Triggert das Zommen auf das geklickte Feature
+         * @param  {string} id featureId
+         */
         zoomFeature: function (id) {
             var poiFeatures = this.getPoiFeatures(),
                 activeCategory = this.getActiveCategory(),
@@ -139,6 +160,90 @@ define(function (require) {
                 type: "POI",
                 coordinate: extent
             });
+        },
+
+        /**
+         * Erzeugt ein SVG eines Kreises
+         * @param  {object} style Style des Features
+         * @return {string}       SVG
+         */
+        createCircleSVG: function (style) {
+            var svg = "",
+                circleStrokeColor = style.returnColor(style.get("circleStrokeColor"), "hex"),
+                circleStrokeOpacity = style.get("circleStrokeColor")[3].toString() || 0,
+                circleStrokeWidth = style.get("circleStrokeWidth"),
+                circleFillColor = style.returnColor(style.get("circleFillColor"), "hex"),
+                circleFillOpacity = style.get("circleFillColor")[3].toString() || 0;
+
+            svg += "<svg height='35' width='35'>";
+            svg += "<circle cx='17.5' cy='17.5' r='15' stroke='";
+            svg += circleStrokeColor;
+            svg += "' stroke-opacity='";
+            svg += circleStrokeOpacity;
+            svg += "' stroke-width='";
+            svg += circleStrokeWidth;
+            svg += "' fill='";
+            svg += circleFillColor;
+            svg += "' fill-opacity='";
+            svg += circleFillOpacity;
+            svg += "'/>";
+            svg += "</svg>";
+
+            return svg;
+        },
+
+        /**
+         * Erzeugt ein SVG einer Line
+         * @param  {object} style Style des Features
+         * @return {string}       SVG
+         */
+        createLineSVG: function (style) {
+            var svg = "",
+                strokeColor = style.returnColor(style.get("lineStrokeColor"), "hex"),
+                strokeWidth = parseInt(style.get("lineStrokeWidth"), 10),
+                strokeOpacity = style.get("lineStrokeColor")[3].toString() || 0;
+
+            svg += "<svg height='35' width='35'>";
+            svg += "<path d='M 05 30 L 30 05' stroke='";
+            svg += strokeColor;
+            svg += "' stroke-opacity='";
+            svg += strokeOpacity;
+            svg += "' stroke-width='";
+            svg += strokeWidth;
+            svg += "' fill='none'/>";
+            svg += "</svg>";
+
+            return svg;
+        },
+
+        /**
+         * Erzeugt ein SVG eines Polygons
+         * @param  {object} style Style des Features
+         * @return {string}       SVG
+         */
+        createPolygonSVG: function (style) {
+            var svg = "",
+                fillColor = style.returnColor(style.get("polygonFillColor"), "hex"),
+                strokeColor = style.returnColor(style.get("polygonStrokeColor"), "hex"),
+                strokeWidth = parseInt(style.get("polygonStrokeWidth"), 10),
+                fillOpacity = style.get("polygonFillColor")[3].toString() || 0,
+                strokeOpacity = style.get("polygonStrokeColor")[3].toString() || 0;
+
+            svg += "<svg height='35' width='35'>";
+            svg += "<polygon points='5,5 30,5 30,30 5,30' style='fill:";
+            svg += fillColor;
+            svg += ";fill-opacity:";
+            svg += fillOpacity;
+            svg += ";stroke:";
+            svg += strokeColor;
+            svg += ";stroke-opacity:";
+            svg += strokeOpacity;
+            svg += ";stroke-width:";
+            svg += strokeWidth;
+            svg += ";'/>";
+            svg += "</svg>";
+
+            return svg;
         },
 
         // getter for poiDistances
