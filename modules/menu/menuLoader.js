b3d73107219efce8aa90a23131ca28ac38a74e97
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-04-13 13:10:10 +0200

    update switch on navbar only if not table or simple

--- modules/menu/menuLoader.js
+++ modules/menu/menuLoader.js
@@ -1,9 +1,10 @@
 define([
+    "config",
     "backbone.radio",
     "modules/menu/desktop/listViewLight",
     "modules/menu/desktop/listView",
     "modules/menu/mobile/listView"
-], function () {
+], function (Config) {
     var Radio = require("backbone.radio"),
         MenuLoader;
 
@@ -11,27 +12,38 @@ define([
         var channel = Radio.channel("MenuLoader");
 
         this.treeType = Radio.request("Parser", "getTreeType");
-        this.loadMenu = function (caller) {
-            var isMobile = Radio.request("Util", "isViewMobile");
 
-            if (isMobile) {
-                require(["modules/menu/mobile/listView"], function (Menu) {
-                    caller.currentMenu = new Menu();
-                    channel.trigger("ready");
-                });
+        this.loadMenu = function (caller) {
+            var isMobile = Radio.request("Util", "isViewMobile"),
+                style = Radio.request("ParametricURL", "getStyle"),
+                isTable = Config.uiMode;
+
+            if (isTable) {
+                alert("is table!");
             }
-            else {
-                if (this.treeType === "light") {
-                    require(["modules/menu/desktop/listViewLight"], function (Menu) {
+            else if (!style || style !== "SIMPLE") {
+                    $("#map").css("height", "calc(100% - 50px)");
+                    $("#main-nav").show();
+
+                if (isMobile) {
+                    require(["modules/menu/mobile/listView"], function (Menu) {
                         caller.currentMenu = new Menu();
                         channel.trigger("ready");
                     });
                 }
                 else {
-                    require(["modules/menu/desktop/listView"], function (Menu) {
-                        caller.currentMenu = new Menu();
-                        channel.trigger("ready");
-                    });
+                    if (this.treeType === "light") {
+                        require(["modules/menu/desktop/listViewLight"], function (Menu) {
+                            caller.currentMenu = new Menu();
+                            channel.trigger("ready");
+                        });
+                    }
+                    else {
+                        require(["modules/menu/desktop/listView"], function (Menu) {
+                            caller.currentMenu = new Menu();
+                            channel.trigger("ready");
+                        });
+                    }
                 }
             }
         };
