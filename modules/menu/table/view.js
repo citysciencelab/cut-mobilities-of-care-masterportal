0713867b400e0b60399486ace32f2c0c60c97965
robertdietzel <robert.dietzel@gv.hamburg.de>
2018-04-23 11:07:59 +0200

    change:: burgerbutton as layer with view.js, template.html

--- modules/menu/table/view.js
+++ modules/menu/table/view.js
@@ -3,6 +3,7 @@ define(function (require) {
         _ = require("underscore"),
         MainTemplate = require("text!modules/menu/table/main/template.html"),
         $ = require("jquery"),
+        LayerView = require("modules/menu/table/layer/view"),
         Menu;
 
   Menu = Backbone.View.extend({
@@ -12,10 +13,14 @@ define(function (require) {
       template: _.template(MainTemplate),
       initialize: function () {
           this.render();
+          this.renderLayer();
       },
       render: function () {
           $(this.el).html(this.template());
           $(".lgv-container").append(this.$el);
+      },
+            renderLayer: function() {
+          this.$el.append(new LayerView().render());
       }
   });
   return Menu;
