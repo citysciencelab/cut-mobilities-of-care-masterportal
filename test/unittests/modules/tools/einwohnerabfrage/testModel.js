a95563246f533a12c0f74d0be648fdfc7ed309e2
jonas_weiter <jonas.weiter@gv.hamburg.de>
2018-04-26 14:34:57 +0200

    update prepared unit test

---
+++ test/unittests/modules/tools/einwohnerabfrage/testModel.js
@@ -0,0 +1,12 @@
+define(function(require) {
+    var assert = require("chai").assert,
+        Model = require("../../../../../modules/tools/einwohnerabfrage/model.js");
+
+    describe("tools/einwohnerabfrageModel", function () {
+        describe("Test", function () {
+            it("testfunction", function () {
+                assert.isTrue(true);
+            });
+        });
+    });
+});
