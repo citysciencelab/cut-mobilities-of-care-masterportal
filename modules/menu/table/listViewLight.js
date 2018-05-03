14531a42c1dd3e0712c5bb41a97b6b519c7a2294
robertdietzel <robert.dietzel@gv.hamburg.de>
2018-05-03 11:29:20 +0200

    update auf alten Commit zurücksetzen

---
+++ modules/menu/table/listViewLight.js
@@ -0,0 +1,50 @@
+define(function (require) {
+
+    var listView = require("modules/menu/desktop/listViewMain"),
+        DesktopLayerViewLight = require("modules/menu/table/layer/viewLight"),
+        Radio = require("backbone.radio"),
+        Menu;
+
+    Menu = listView.extend({
+        initialize: function () {
+            this.collection = Radio.request("ModelList", "getCollection");
+            Radio.on("Autostart", "startModul", this.startModul, this);
+            this.listenTo(this.collection, {
+                "updateLightTree": function () {
+                    this.render();
+                }
+            });
+            this.renderMain();
+            this.render();
+            Radio.trigger("Autostart", "initializedModul", "tree-table");
+        },
+        render: function () {
+            $("#" + "tree-table").html("");
+            var models = this.collection.where({type: "layer"});
+
+            models = _.sortBy(models, function (model) {
+                return model.getSelectionIDX();
+            });
+
+            this.addViews(models);
+            Radio.trigger("Title", "setSize");
+            $("ul#tree-table.light").css("max-height", $("#map").height() - 160);
+        },
+        addViews: function (models) {
+            _.each(models, function (model) {
+                 new DesktopLayerViewLight({model: model});
+            }, this);
+        },
+        startModul: function (modulId) {
+            var modul = _.findWhere(this.collection.models, {id: modulId});
+
+            if (modul.attributes.type === "tool") {
+                modul.setIsActive(true);
+            }
+            else {
+                $("#" + modulId).parent().addClass("open");
+            }
+        }
+    });
+    return Menu;
+});
--- modules/menu/table/view.js
+++ modules/menu/table/view.js
@@ -5,7 +5,7 @@ define(function (require) {
         $ = require("jquery"),
         LayerView = require("modules/menu/table/layer/view"),
         ToolView = require("modules/menu/table/tool/view"),
-        ListView = require("modules/menu/table/layerList"),
+        ListView = require("modules/menu/table/listViewLight"),
         Menu;
 
     Menu = Backbone.View.extend({
@@ -27,11 +27,9 @@ define(function (require) {
         renderLayer: function () {
             this.$el.find("#table-nav-main").append(new LayerView().render());
         },
-
          renderList: function () {
             new ListView().render();
         },
-
         renderTool: function () {
             this.$el.append(new ToolView().render());
         }
