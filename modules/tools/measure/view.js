f30662126d1db4543afea0d8c7d82a2db07893b7
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-05-16 16:36:41 +0200

    fix delete keeps overlay

--- modules/tools/measure/view.js
+++ modules/tools/measure/view.js
@@ -1,24 +1,58 @@
 define([
+    "backbone",
     "backbone.radio",
-    "modules/tools/measure/default/view",
-    "modules/tools/measure/table/view"
-], function () {
-    var Radio = require("backbone.radio"),
-        MeasureView;
+    "text!modules/tools/measure/default/template.html",
+    "text!modules/tools/measure/table/template.html",
+    "modules/tools/measure/model"
+], function (Backbone, Radio, DefaultTemplate, TableTemplate, Measure) {
 
-    MeasureView = function () {
-            var menuStyle = Radio.request("Util", "getUiStyle");
+    var MeasureView = Backbone.View.extend({
+        model: new Measure(),
+        className: "win-body",
+        // template: _.template(DefaultTemplate),
+        events: {
+            "change select#geomField": "setGeometryType",
+            "change select#unitField": "setUnit",
+            "click button": "deleteFeatures",
+            "click .form-horizontal > .form-group-sm > .col-sm-12 > .glyphicon-question-sign": function () {
+                Radio.trigger("Quickhelp", "showWindowHelp", "measure");
+            }
+        },
 
-            if (menuStyle === "TABLE") {
-                require(["modules/tools/measure/table/view"], function (MeasureView) {
-                    new MeasureView();
-                });
+        initialize: function () {
+            debugger;
+            this.listenTo(this.model, {
+                "change:isCollapsed change:isCurrentWin change:type": this.render
+            });
+        },
+
+        render: function () {
+            if (this.model.get("isCurrentWin") === true && this.model.get("isCollapsed") === false) {
+                var attr = this.model.toJSON(),
+                    template = Radio.request("Util", "getUiStyle") === "TABLE" ? _.template(TableTemplate) : _.template(DefaultTemplate);
+
+                this.$el.html("");
+                $(".win-heading").after(this.$el.html(template(attr)));
+                this.delegateEvents();
             }
             else {
-                require(["modules/tools/measure/default/view"], function (MeasureView) {
-                    new MeasureView();
-                });
+                this.undelegateEvents();
             }
-    };
+        },
+
+        setGeometryType: function (evt) {
+            this.model.setGeometryType(evt.target.value);
+            Radio.trigger("Map", "activateClick", "measure");
+        },
+
+        setUnit: function (evt) {
+            this.model.setUnit(evt.target.value);
+        },
+
+        deleteFeatures: function () {
+            this.model.deleteFeatures();
+        }
+    });
+
     return MeasureView;
 });
