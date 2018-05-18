acd7fff85bf102325383088029b56038dc1e0ed8
Duden <DudenSe-Admin@fhhnet.stadt.hamburg.de>
2018-05-18 17:22:24 +0200

    fix - 3d orientation north down

--- modules/controls/orientation3d/view.js
+++ modules/controls/orientation3d/view.js
@@ -1,10 +1,9 @@
-define([
-    "backbone",
-    "backbone.radio",
-    "text!modules/controls/orientation3d/template.html"
-], function (Backbone, Radio, Orientation3DTemplate) {
+define(function (require) {
+    var Orientation3DTemplate = require("text!modules/controls/orientation3d/template.html"),
+        Cesium = require("cesium"),
+        Orientation3DView;
 
-    var Orientation3DView = Backbone.View.extend({
+    Orientation3DView = Backbone.View.extend({
         className: "row",
         template: _.template(Orientation3DTemplate),
         events: {
@@ -15,7 +14,7 @@ define([
         initialize: function () {
             var channel = Radio.channel("Map");
             this.render();
-            if(Radio.request("Map", "isMap3d")){
+            if (Radio.request("Map", "isMap3d")) {
                 this.show();
             }
             channel.on({
@@ -28,103 +27,106 @@ define([
             this.$el.html(this.template);
             this.$el.hide();
         },
-        hide: function() {
+        hide: function () {
             if (this.unlisten) {
                 this.unlisten();
                 this.unlisten = null;
             }
             this.$el.hide();
         },
-        show: function() {
+        show: function () {
+            var scene = Radio.request("Map", "getMap3d").getCesiumScene(),
+                camera = scene.camera;
+
             this.$el.show();
-            var scene = Radio.request("Map", "getMap3d").getCesiumScene();
             if (this.unlisten) {
                 this.unlisten();
                 this.unlisten = null;
             }
-            var camera = scene.camera;
-            this.unlisten = scene.postRender.addEventListener(function() {
-                $("#north-pointer").css({ transform: 'rotate(' + camera.heading + 'rad)' });
-            });
-            this.$el.show(); },
-        move: function(event) {
-            var scene = Radio.request("Map", "getMap3d").getCesiumScene();
-            var camera = scene.camera;
-
-            var distance = camera.positionCartographic.height / 2;
-            var angle = 0.15;
-            var down = camera.pitch + angle > 0 ? 0 : angle;
-            var up = camera.pitch - angle < Math.PI / -2 ? 0 : angle;
-            var directions = {
-                'compass_north': camera.moveUp.bind(camera, distance),
-                'compass_south': camera.moveDown.bind(camera, distance),
-                'compass_east': camera.moveRight.bind(camera, distance),
-                'compass_west': camera.moveLeft.bind(camera, distance),
-                'tilt-down': camera.lookUp.bind(camera, down),
-                'tilt-up': camera.lookDown.bind(camera, up),
-                'zoom-in': camera.zoomIn.bind(camera, distance),
-                'zoom-out': camera.zoomOut.bind(camera, distance)
-            };
+            this.unlisten = scene.postRender.addEventListener(function () {
+                this.$el.find("#north-pointer").css({transform: "rotate(" + camera.heading + "rad)"});
+            }.bind(this));
+            this.$el.show();
+        },
+        move: function (event) {
+            var scene = Radio.request("Map", "getMap3d").getCesiumScene(),
+                camera = scene.camera,
+                distance = camera.positionCartographic.height / 2,
+                angle = 0.15,
+                down = camera.pitch + angle > 0 ? 0 : angle,
+                up = camera.pitch - angle < Math.PI / -2 ? 0 : angle,
+                directions = {
+                    "compass_north": camera.moveUp.bind(camera, distance),
+                    "compass_south": camera.moveDown.bind(camera, distance),
+                    "compass_east": camera.moveRight.bind(camera, distance),
+                    "compass_west": camera.moveLeft.bind(camera, distance),
+                    "tilt-down": camera.lookUp.bind(camera, down),
+                    "tilt-up": camera.lookDown.bind(camera, up),
+                    "zoom-in": camera.zoomIn.bind(camera, distance),
+                    "zoom-out": camera.zoomOut.bind(camera, distance)
+                };
             directions[event.target.id]();
         },
-        northDown: function(event) {
+        northDown: function (event) {
+            var offsetRect = this.$el.find("#north-pointer").get(0).getBoundingClientRect(),
+                scene = Radio.request("Map", "getMap3d").getCesiumScene(),
+                camera = scene.camera,
+                ray = new Cesium.Ray(camera.position, camera.direction),
+                groundPositionCartesian = scene.globe.pick(ray, scene);
+
             this.startTime = new Date().getTime();
-            this.cursorPosition = { x: event.clientX, y: event.clientY };
-            var offsetRect = $("#north-pointer").get(0).getBoundingClientRect();
+            this.cursorPosition = {x: event.clientX, y: event.clientY};
             this.correction = {
-                y: (offsetRect.top + (offsetRect.height / 2)) - this.cursorPosition.y,
-                x: (offsetRect.left + (offsetRect.width / 2)) - this.cursorPosition.x,
+                y: offsetRect.top + offsetRect.height / 2 - this.cursorPosition.y,
+                x: offsetRect.left + offsetRect.width / 2 - this.cursorPosition.x
             };
 
-            var scene = Radio.request("Map", "getMap3d").getCesiumScene();
-            var camera = scene.camera;
-            var ray = new Cesium.Ray(camera.position, camera.direction);
-            var groundPositionCartesian = scene.globe.pick(ray, scene);
             if (groundPositionCartesian) {
                 this.destination = groundPositionCartesian;
                 this.distance = Cesium.Cartesian3.distance(camera.position, groundPositionCartesian);
             }
 
             this.mouseDraggedBound = this.mouseDragged.bind(this);
-            window.addEventListener('mouseup', this.northUp.bind(this), { once: true });
-            window.addEventListener('mousemove', this.mouseDraggedBound);
+            window.addEventListener("mouseup", this.northUp.bind(this), {once: true});
+            this.$el.on("mousemove", this.mouseDraggedBound);
         },
-        northUp: function() {
+        northUp: function () {
             var endTime = new Date().getTime();
-            if ((endTime - this.startTime) < 200) {
+            if (endTime - this.startTime < 200) {
                 this.setHeading(0);
             }
-            window.removeEventListener('mousemove', this.mouseDraggedBound);
+            this.$el.off("mousemove", this.mouseDraggedBound);
         },
-        mouseDragged: function(event) {
+        mouseDragged: function (event) {
+            var offsetRect = this.$el.find(".compass").get(0).getBoundingClientRect(),
+                top = offsetRect.top + offsetRect.height / 2,
+                left = offsetRect.left + offsetRect.width / 2,
+                y = event.clientY - top + this.correction.y,
+                x = event.clientX - left + this.correction.x,
+                rads = Math.atan2(y, x) + Math.PI / 2;
+
             event.preventDefault();
-            var offsetRect = $(".compass").get(0).getBoundingClientRect();
-            var top = offsetRect.top + (offsetRect.height / 2);
-            var left = offsetRect.left + (offsetRect.width / 2);
-            var y = (event.clientY - top) + this.correction.y;
-            var x = (event.clientX - left) + this.correction.x;
-            var rads = Math.atan2(y, x) + (Math.PI / 2);
-            rads = rads > 0 ? rads : rads + (2 * Math.PI);
+            rads = rads > 0 ? rads : rads + 2 * Math.PI;
 
             this.setHeading(rads);
         },
-        setHeading: function(heading) {
-            var scene = Radio.request("Map", "getMap3d").getCesiumScene();
-            var camera = scene.camera;
-            var options = {
-                orientation: {
-                    pitch: camera.pitch,
-                    roll: camera.roll,
-                    heading: heading
-                }
-            };
+        setHeading: function (heading) {
+            var scene = Radio.request("Map", "getMap3d").getCesiumScene(),
+                camera = scene.camera,
+                options = {
+                    orientation: {
+                        pitch: camera.pitch,
+                        roll: camera.roll,
+                        heading: heading
+                    }
+                };
 
             if (this.destination) {
                 options.destination = this.destination;
             }
 
             camera.setView(options);
-            if (this.distance != null) {
+            if (this.distance !== null) {
                 camera.moveBackward(this.distance);
             }
         }
