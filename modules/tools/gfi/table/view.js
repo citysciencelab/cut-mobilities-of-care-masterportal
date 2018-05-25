c31c05f868a47b685b5290eceb361f11c3e7a444
robertdietzel <robert.dietzel@gv.hamburg.de>
2018-05-25 11:30:55 +0200

    update gfi wird während des draggens unter dem Finger zentriert

--- modules/tools/gfi/table/view.js
+++ modules/tools/gfi/table/view.js
@@ -16,33 +16,25 @@ define(function (require) {
             var attr = this.model.toJSON();
 
             $("#map").append(this.$el.html(this.template(attr)));
-            // this.$el.draggable({
-            //     containment: "#map",
-            //     handle: ".gfi-header",
-            //     stop: function (evt, ui) {
-            //         // helper, so that "left" is never 0. needed for gfi/themes/view.js adjustGfiWindow()
-            //         $(".gfi").css("left", (ui.position.left + 1) + "px");
-            //         // $(".gfi").css("top", (ui.position.top - 50) + "px");
-            //     }
-            // });
+            this.$el.draggable({
+                containment: "#map",
+                handle: ".gfi-header",
+                stop: function (evt, ui) {
+                    // helper, so that "left" is never 0. needed for gfi/themes/view.js adjustGfiWindow()
+                    $(".gfi").css("left", (ui.position.left + 1) + "px");
+                    // $(".gfi").css("top", (ui.position.top - 50) + "px");
+                }
+            });
             this.$el.on("touchmove", function(evt) {
                 var touch = evt.originalEvent.touches[0];
-                var x = touch.clientX;
-                var y = touch.clientY;
-                console.log("x: " + x + "y: " + y);
-                console.log(this);
-                console.log(JSON.stringify($(this).position()));
-                
-                
-                
+                var width = $(this). width() /2;
+                var height = $(this). height() /2;
+                var x = touch.clientX - width;
+                var y = touch.clientY - height;
                 $(this).css({
-                    //"-webkit-transform": "translate3d("+ x +"px, "+ y +"px, 0)"
                     "left": x + "px",
                     "top": y + "px"
                 });
-
-                //console.log(x);
-
             });
         },
 
