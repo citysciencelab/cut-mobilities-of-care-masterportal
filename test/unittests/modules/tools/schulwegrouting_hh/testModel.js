69de7a854d28961e403c1f2c0147ae13198ded45
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2018-06-06 09:00:52 +0200

    update - schule aus liste auswählen

--- test/unittests/modules/tools/schulwegrouting_hh/testModel.js
+++ test/unittests/modules/tools/schulwegrouting_hh/testModel.js
@@ -1,41 +1,46 @@
 define(function (require) {
     var expect = require("chai").expect,
+        Util = require("util"),
         Model = require("../../../../../modules/tools/schulwegrouting_hh/model.js");
 
     describe("tools/schulwegrouting_hh", function () {
         var model,
+            utilModel,
+            schoolFeatures,
             addressList = [
                 {
                     affix: "a",
                     joinAddress: "NeuenfelderStraße13a",
                     number: "13",
-                    position: "566326.134 5928222.917",
+                    position: [566326.134, 5928222.917],
                     street: "Neuenfelder Straße"
                 },
                 {
                     affix: "a",
                     joinAddress: "NeuenfelderStraße15a",
                     number: "15",
-                    position: "566376.183 5928211.680",
+                    position: [566376.183, 5928211.680],
                     street: "Neuenfelder Straße"
                 },
                 {
                     affix: "b",
                     joinAddress: "NeuenfelderStraße84b",
                     number: "84",
-                    position: "567147.825 5927969.049",
+                    position: [567147.825, 5927969.049],
                     street: "Neuenfelder Straße"
                 },
                 {
                     affix: "a",
                     joinAddress: "KielerStraße160a",
                     number: "160",
-                    position: "562202.187 5936669.860",
+                    position: [562202.187, 5936669.860],
                     street: "Kieler Straße"
                 }
             ];
 
         before(function () {
+            utilModel = new Util();
+            schoolFeatures = utilModel.createTestFeatures("resources/testFeaturesSchulen.xml");
             model = new Model();
             model.setLayer(Radio.request("Map", "createLayerIfNotExists", "school_route_layer"));
             model.addRouteFeatures(model.get("layer").getSource());
@@ -67,21 +72,42 @@ define(function (require) {
             });
         });
 
+        describe("sortSchoolsByName", function () {
+            it("should have a school with the id 'APP_STAATLICHE_SCHULEN_172968'", function () {
+                var schoolList = model.sortSchoolsByName(schoolFeatures);
+
+                expect(schoolList[0].get("schulname")).to.equal("Adolph-Schönfelder-Schule");
+            });
+        });
+
+        describe("filterSchoolById", function () {
+            it("should have a school with the id 'APP_STAATLICHE_SCHULEN_172968'", function () {
+                var schoolFeature = model.filterSchoolById(schoolFeatures, "APP_STAATLICHE_SCHULEN_172968");
+
+                expect(schoolFeature.getId()).to.equal("APP_STAATLICHE_SCHULEN_172968");
+            });
+            it("should be undefined for a school with the id 'AP_STTCHE_SHLEN_268'", function () {
+                var schoolFeature = model.filterSchoolById(schoolFeatures, "AP_STTCHE_SHLEN_268");
+
+                expect(schoolFeature).to.be.undefined;
+            });
+        });
+
         describe("setRoutePositionById", function () {
             it("should have the coordinates '567147.825 5927969.049' for the startpoint", function () {
                 var feature;
 
-                model.setRoutePositionById("startPoint", model.get("layer").getSource(), "567147.825 5927969.049");
+                model.setRoutePositionById("startPoint", model.get("layer").getSource(), [567147.825, 5927969.049]);
                 feature = model.get("layer").getSource().getFeatureById("startPoint");
-                expect(feature.getGeometry().getCoordinates()).to.deep.equal([567147, 5927969]);
+                expect(feature.getGeometry().getCoordinates()).to.deep.equal([567147.825, 5927969.049]);
             });
 
             it("should have the coordinates '566326.134 5928222.917' for the endpoint", function () {
                 var feature;
 
-                model.setRoutePositionById("endPoint", model.get("layer").getSource(), "566326.134 5928222.917");
+                model.setRoutePositionById("endPoint", model.get("layer").getSource(), [566326.134, 5928222.917]);
                 feature = model.get("layer").getSource().getFeatureById("endPoint");
-                expect(feature.getGeometry().getCoordinates()).to.deep.equal([566326, 5928222]);
+                expect(feature.getGeometry().getCoordinates()).to.deep.equal([566326.134, 5928222.917]);
             });
         });
 
