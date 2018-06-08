83a95b982f02076122035a37de9840e3130e91dc
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2018-06-08 16:38:29 +0200

    fix - sidebar in mobile mode

--- modules/sidebar/model.js
+++ modules/sidebar/model.js
@@ -5,47 +5,36 @@ define(function () {
             // true if sidebar is visible
             isVisible: false,
             // true if viewport width < 768px
-            isMobile: false,
-            renderElement: null,
-            toolName: ""
+            isMobile: false
         },
         initialize: function () {
             var channel = Radio.channel("Sidebar");
 
             this.listenTo(channel, {
                 "toggle": this.setIsVisible,
-                "append": this.append
+                "append": this.addContent
             });
             this.listenTo(Radio.channel("Util"), {
                 "isViewMobileChanged": this.setIsMobile
             });
             this.setIsMobile(Radio.request("Util", "isViewMobile"));
         },
-        append: function (name, element) {
-            if (this.getToolName() !== name) {
-                this.setRenderElement(element);
-                this.setToolName(name);
-                this.trigger("render");
-            }
+
+        /**
+         * passes a DOM element to the view
+         * @param {DOM} element - from a tool view
+         * @returns {void}
+         */
+        addContent: function (element) {
+            this.trigger("addContent", element);
         },
 
         setIsMobile: function (value) {
             this.set("isMobile", value);
         },
+
         setIsVisible: function (value) {
             this.set("isVisible", value);
-        },
-        getRenderElement: function () {
-            return this.get("renderElement");
-        },
-        setRenderElement: function (value) {
-            this.set("renderElement", value);
-        },
-        getToolName: function () {
-            return this.get("toolName");
-        },
-        setToolName: function (value) {
-            this.set("toolName", value);
         }
     });
 
--- modules/sidebar/view.js
+++ modules/sidebar/view.js
@@ -15,65 +15,80 @@ define(function (require) {
         initialize: function () {
             this.listenTo(this.model, {
                 "change:isVisible": this.toggle,
-                "change:isMobile": this.toggleCssClass,
-                "render": this.render
+                "change:isMobile": this.toggleClass,
+                "addContent": this.addContent
             });
-        },
-
-        render: function () {
-            this.$el.html("");
             $("#map").after(this.$el);
-            if (!this.model.get("isMobile")) {
-                this.$el.css("height", $("#map").height());
-                $("#map").css("width", "70%");
-                Radio.trigger("Map", "updateSize");
-                this.$el.append(this.model.getRenderElement());
-                this.removeBackdrop();
-            }
-            else {
-                this.$el.css("height", "");
-                $("#map").css("width", "100%");
-                this.addBackdrop();
-            }
         },
 
+        /**
+         * add HTML content to this sidebar
+         * @param {DOM} element - from a tool view
+         * @returns {void}
+         */
+        addContent: function (element) {
+            this.$el.html(element);
+        },
+
+        /**
+         * shows or hides this view
+         * @param {Backbone.Model} model - this.model
+         * @param {boolean} isVisible - is the sidebar visible
+         * @return {void}
+         */
         toggle: function (model, isVisible) {
             if (isVisible) {
-                this.render();
+                this.$el.show();
             }
             else {
-                this.removeView();
-                this.removeBackdrop();
+                this.$el.hide();
             }
-        },
-
-        toggleCssClass: function (model, value) {
-            if (value) {
-                this.$el.removeClass("sidebar");
-                this.$el.addClass("sidebar-mobile");
-            }
-            else {
-                this.$el.removeClass("sidebar-mobile");
-                this.$el.addClass("sidebar");
-            }
-            if (this.model.get("isVisible")) {
-                this.render();
-            }
-        },
-
-        removeView: function () {
-            this.$el.remove();
-            $("#map").css("width", "100%");
-
+            this.toggleBackdrop(this.model.get("isMobile"), isVisible);
+            this.setMapWidth(this.model.get("isMobile"), isVisible);
             Radio.trigger("Map", "updateSize");
         },
 
-        addBackdrop: function () {
-            $(".lgv-container").append("<div class='backdrop'></div>");
+        /**
+         * toggles the css class for this view
+         * @param {Backbone.Model} model - this.model
+         * @param {boolean} isMobile -
+         * @return {void}
+         */
+        toggleClass: function (model, isMobile) {
+            this.$el.toggleClass("sidebar sidebar-mobile");
+            this.toggleBackdrop(isMobile, this.model.get("isVisible"));
+            this.setMapWidth(isMobile, this.model.get("isVisible"));
         },
 
-        removeBackdrop: function () {
-            $(".backdrop").remove();
+        /**
+         * sets the width of the map
+         * @param {boolean} isMobile -
+         * @param {boolean} isVisible - is the sidebar visible
+         * @return {void}
+         */
+        setMapWidth: function (isMobile, isVisible) {
+            if (!isMobile && isVisible) {
+                $("#map").css("width", "70%");
+            }
+            else {
+                $("#map").css("width", "100%");
+            }
+        },
+
+        /**
+         * toggles the backdrop
+         * needed for the mobile mode
+         * @param {boolean} isMobile -
+         * @param {boolean} isVisible - is the sidebar visible
+         * @return {void}
+         */
+        toggleBackdrop: function (isMobile, isVisible) {
+            if (isMobile && isVisible) {
+                $(".lgv-container").append("<div class='backdrop'></div>");
+            }
+            else {
+                $(".backdrop").remove();
+            }
         }
     });
     return SidebarView;
