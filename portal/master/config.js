90b8c7129de22c6484fd87497b4d6e4fdaf18c2b
Maria <maria.dauwalter@hcu-hamburg.de>
2018-06-14 12:29:34 +0200

    update config.js

---
+++ portal/master/config.js
@@ -0,0 +1,86 @@
+define(function () {
+    var config = {
+        animation: {
+            steps: 30,
+            url: "http://geodienste.hamburg.de/Test_MRH_WFS_Pendlerverflechtung",
+            params: {
+                REQUEST: "GetFeature",
+                SERVICE: "WFS",
+                TYPENAME: "app:mrh_kreise",
+                VERSION: "1.1.0",
+                maxFeatures: "10000"
+            },
+            featureType: "mrh_einpendler_gemeinde",
+            attrAnzahl: "anzahl_einpendler",
+            attrKreis: "wohnort_kreis",
+            minPx: 5,
+            maxPx: 30,
+            num_kreise_to_style: 4,
+            zoomlevel: 1,
+            colors: ["rgba(255,0,0,0.5)", "rgba(0,255,0,0.5)", "rgba(0,0,255,0.5)", "rgba(0,255,255,0.5)"]
+        },
+        ignoredKeys: ["BOUNDEDBY", "SHAPE", "SHAPE_LENGTH", "SHAPE_AREA", "OBJECTID", "GLOBALID", "GEOMETRY", "SHP", "SHP_AREA", "SHP_LENGTH","GEOM"],
+        gfiWindow: "attached",
+        simpleMap: false,
+        wfsImgPath: "../node_modules/lgv-config/img/",
+        allowParametricURL: true,
+        zoomtofeature: {
+            attribute: "flaechenid",
+            imglink: "../img/location_eventlotse.svg",
+            layerid: "4561",
+            WFSid: "4560"
+        },
+        postMessageUrl: "http://localhost:8080",
+        namedProjections: [
+            // GK DHDN
+            ["EPSG:31461", "+title=Gauß 3° Bessel +proj=tmerc +lat_0=0 +lon_0=3 +k=1 +x_0=1500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs"],
+            ["EPSG:31462", "+title=Gauß 6° Bessel +proj=tmerc +lat_0=0 +lon_0=6 +k=1 +x_0=2500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs"],
+            ["EPSG:31463", "+title=Gauß 9° Bessel +proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs"],
+            ["EPSG:31464", "+title=Gauß 12° Bessel +proj=tmerc +lat_0=0 +lon_0=12 +k=1 +x_0=4500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs"],
+            ["EPSG:31465", "+title=Gauß 15° Bessel +proj=tmerc +lat_0=0 +lon_0=15 +k=1 +x_0=5500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs"],
+            ["EPSG:31466", "+title=Gauß-Krüger 6° Bessel +proj=tmerc +lat_0=0 +lon_0=6 +k=1 +x_0=2500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs"],
+            ["EPSG:31467", "+title=Gauß-Krüger 9° Bessel +proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs"],
+            ["EPSG:31468", "+title=Gauß-Krüger 12° Bessel +proj=tmerc +lat_0=0 +lon_0=12 +k=1 +x_0=4500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs"],
+            // ETRS89 UTM
+            ["EPSG:25831", "+title=ETRS89/UTM 31N +proj=utm +zone=31 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"],
+            ["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"],
+            ["EPSG:25833", "+title=ETRS89/UTM 33N +proj=utm +zone=33 +ellps=WGS84 +towgs84=0,0,0,0,0,0,1 +units=m +no_defs"],
+            // Soldner Berlin
+            ["EPSG:3068", "+title=Soldner Berlin +proj=cass +lat_0=52.41864827777778 +lon_0=13.62720366666667 +x_0=40000 +y_0=10000 +ellps=bessel +datum=potsdam +units=m +no_defs"],
+            // Pulkovo
+            ["EPSG:4178", "+proj=longlat +ellps=krass +towgs84=24,-123,-94,0.02,-0.25,-0.13,1.1 +no_defs"],
+            // Organisations
+            ["SR-ORG:95", "+proj=merc +lon_0=0 +lat_ts=0 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +units=m +no_defs"],
+            // WGS84
+            ["EPSG:4326", "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"]
+        ],
+        footer: {
+            visibility: true,
+            urls: [
+                {
+                    "bezeichnung": "Kartographie und Gestaltung: ",
+                    "url": "http://www.geoinfo.hamburg.de/",
+                    "alias": "Landesbetrieb Geoinformation und Vermessung",
+                    "alias_mobil": "LGV"
+                }
+            ]
+        },
+        quickHelp: true,
+        portalConf: "../../portal/master/",
+        layerConf: "../node_modules/lgv-config/services-fhhnet-ALL.json",
+        restConf: "../node_modules/lgv-config/rest-services-fhhnet.json",
+        styleConf: "../node_modules/lgv-config/style_v2.json",
+        proxyURL: "/cgi-bin/proxy.cgi",
+        attributions: true,
+        scaleLine: true,
+        mouseHover: {
+            numFeaturesToShow: 2,
+            infoText: "(weitere Objekte. Bitte zoomen.)"
+        },
+        isMenubarVisible: true,
+        geoAPI: false,
+        clickCounter: {}
+    };
+
+    return config;
+});
