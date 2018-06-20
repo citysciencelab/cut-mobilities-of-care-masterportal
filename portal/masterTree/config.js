5bf74f28178cd44cf0d1520f9e39513b6cda84b1
Michael Bieler <michael.bieler@gv.hamburg.de>
2018-06-20 15:45:06 +0200

    fix sinnvolle default Koordinatensysteme für Hamburg

--- portal/masterTree/config.js
+++ portal/masterTree/config.js
@@ -40,10 +40,14 @@ define(function () {
             infoText: "(weitere Objekte. Bitte zoomen.)"
         },
         namedProjections: [
+            // GK DHDN
+            ["EPSG:31467", "+title=Bessel/Gauß-Krüger 3 +proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs"],
             // ETRS89 UTM
-            ["EPSG:25832", "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"],
-
-            ["EPSG:4326", "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"]
+            ["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"],
+            // LS 320: zusammen mit Detlef Koch eingepflegt und geprüft
+            ["EPSG:8395", "+title=ETRS89/Gauß-Krüger 3 +proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=GRS80 +datum=GRS80 +units=m +no_defs"],
+            // WGS84
+            ["EPSG:4326", "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"]
         ],
         portalConf: "../../portal/master/",
         postMessageUrl: "http://localhost:8080",
