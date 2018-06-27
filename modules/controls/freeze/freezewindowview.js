22dc14735da58f831c414099291331d0f5db9598
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-06-27 14:04:10 +0200

    fix linterfehler

--- modules/controls/freeze/freezewindowview.js
+++ modules/controls/freeze/freezewindowview.js
@@ -1,40 +1,40 @@
 define(function (require) {
-    var Backbone = require("backbone"),
-        FreezeTemplate = require("text!modules/controls/freeze/template.html"),
+    var FreezeTemplate = require("text!modules/controls/freeze/template.html"),
         $ = require("jquery"),
         FreezeView;
 
     FreezeView = Backbone.View.extend({
-        collection: {},
-        id: "freeze-view",
-        className: "freeze-view freeze-deactivated",
-        template: _.template(FreezeTemplate),
         events: {
             "click .freeze-view-close": "hideFreezeWin"
-
         },
         initialize: function () {
             this.render();
         },
+        collection: {},
+        id: "freeze-view",
+        className: "freeze-view freeze-deactivated",
+        template: _.template(FreezeTemplate),
         render: function () {
             $(this.$el).html(this.template());
             $(".lgv-container").append(this.$el);
+
+            return this;
         },
         showFreezeWin: function () {
-            $("div.freeze-view").removeClass("freeze-deactivated");
-            $("div.freeze-view").addClass("freeze-activated");
+            this.$("div.freeze-view").removeClass("freeze-deactivated");
+            this.$("div.freeze-view").addClass("freeze-activated");
             if ($(".table-nav-main").length === 0) {
-                $("p.freeze-view-close").css("left", "30px");
-                $("p.freeze-view-close").css("top", "30px");
+                this.$("p.freeze-view-close").css("left", "30px");
+                this.$("p.freeze-view-close").css("top", "30px");
             }
             else {
-                $("p.freeze-view-close").css("left", $(".table-nav-main").offset().left);
-                $("p.freeze-view-close").css("top", $(".table-nav-main").offset().top);
+                this.$("p.freeze-view-close").css("left", $(".table-nav-main").offset().left);
+                this.$("p.freeze-view-close").css("top", $(".table-nav-main").offset().top);
             }
         },
         hideFreezeWin: function () {
-            $("div.freeze-view").removeClass("freeze-activated");
-            $("div.freeze-view").addClass("freeze-deactivated");
+            this.$("div.freeze-view").removeClass("freeze-activated");
+            this.$("div.freeze-view").addClass("freeze-deactivated");
         }
     });
     return FreezeView;
