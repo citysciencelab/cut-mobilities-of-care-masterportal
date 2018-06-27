cf445e5f5e10de2aff03ab3c2bcdca04c91d9625
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-06-27 13:58:17 +0200

    fix lintererror

--- modules/controls/freeze/freezecontrolmenuview.js
+++ modules/controls/freeze/freezecontrolmenuview.js
@@ -1,18 +1,17 @@
-define(function (require) {
-    var Backbone = require("backbone"),
-        FreezeControlViewMenu;
+define(function () {
+    var FreezeControlViewMenu;
 
     FreezeControlViewMenu = Backbone.View.extend({
-        collection: {},
-        id: "freeze-view-control",
-        className: "freeze-view-start",
-        template: _.template("<div class='freeze-view-start' title='Ansicht sperren'><span class='glyphicon icon-lock lock-control'></span></div>"),
         events: {
             "click .freeze-view-start": "toggleFreezeWindow"
         },
         initialize: function () {
             this.renderAsControl();
         },
+        collection: {},
+        id: "freeze-view-control",
+        className: "freeze-view-start",
+        template: _.template("<div class='freeze-view-start' title='Ansicht sperren'><span class='glyphicon icon-lock lock-control'></span></div>"),
         renderAsControl: function () {
             this.$el.html(this.template);
         },
