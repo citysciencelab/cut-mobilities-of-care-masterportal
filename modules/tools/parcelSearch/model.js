fcecbac2b30a097eee2b97256cf899ff145b010c
jonas_weiter <jonas.weiter@gv.hamburg.de>
2018-07-31 13:57:25 +0200

    fix download layout and user feedback

--- modules/tools/parcelSearch/model.js
+++ modules/tools/parcelSearch/model.js
@@ -39,7 +39,6 @@ define(function (require) {
          * wird getriggert, wenn ein Tool in der Menüleiste geklickt wird. Übergibt die Konfiguration der parcelSearch aus args an readConfig().
          */
         setStatus: function (args) {
-            console.log(args);
             if (args[2].get("id") === "parcelSearch") {
                 this.setIsCollapsed(args[1]);
                 this.setIsCurrentWin(args[0]);
