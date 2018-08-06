a105cb89579eee71f827fa746f4740be16537d07
jonas_weiter <jonas.weiter@gv.hamburg.de>
2018-08-06 08:07:54 +0200

    update mastertree config animation now in config.json

--- portal/masterTree/config.js
+++ portal/masterTree/config.js
@@ -1,25 +1,6 @@
 define(function () {
 
     var config = {
-        animation: {
-            steps: 30,
-            url: "http://geodienste.hamburg.de/Test_MRH_WFS_Pendlerverflechtung",
-            params: {
-                REQUEST: "GetFeature",
-                SERVICE: "WFS",
-                TYPENAME: "app:mrh_kreise",
-                VERSION: "1.1.0",
-                maxFeatures: "10000"
-            },
-            featureType: "mrh_einpendler_gemeinde",
-            attrAnzahl: "anzahl_einpendler",
-            attrKreis: "wohnort_kreis",
-            minPx: 5,
-            maxPx: 30,
-            num_kreise_to_style: 4,
-            zoomlevel: 1,
-            colors: ["rgba(255,0,0,0.5)", "rgba(0,255,0,0.5)", "rgba(0,0,255,0.5)", "rgba(0,255,255,0.5)"]
-        },
         clickCounter: {},
         footer: {
             visibility: true,
