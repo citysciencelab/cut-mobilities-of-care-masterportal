2e8b1421926fa4c95792a8c48145db9bb53fe4d9
jonas_weiter <jonas.weiter@gv.hamburg.de>
2018-08-08 10:04:46 +0200

    update schulwegrouting if no school found in gages

--- modules/tools/schulwegRouting_hh/view.js
+++ modules/tools/schulwegRouting_hh/view.js
@@ -43,9 +43,11 @@ define(function (require) {
             "click .print-route": "printRoute",
             "click .description button": "toggleRouteDesc",
             "click #regional-school": function () {
-                this.updateSelectedSchool(this.model.get("regionalSchool").get("schul_id"));
-                this.model.selectSchool(this.model.get("schoolList"), this.model.get("regionalSchool").get("schul_id"));
-                this.model.prepareRequest(this.model.get("startAddress"));
+                if (!_.isEmpty(this.model.get("regionalSchool"))) {
+                    this.updateSelectedSchool(this.model.get("regionalSchool").get("schul_id"));
+                    this.model.selectSchool(this.model.get("schoolList"), this.model.get("regionalSchool").get("schul_id"));
+                    this.model.prepareRequest(this.model.get("startAddress"));
+                }
             }
         },
         initialize: function () {
