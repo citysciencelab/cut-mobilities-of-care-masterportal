39752faa3f1752cd584b66ddebd26d25399e03f1
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2018-08-14 14:18:30 +0200

    update - view logic

--- modules/tools/print_/model.js
+++ modules/tools/print_/model.js
@@ -12,6 +12,13 @@ define(function (require) {
             // title for the printout
             title: "",
             layoutList: [],
+            currentLayout: undefined,
+            formatList: [],
+            currentFormat: "pdf",
+            scaleList: [],
+            currentScale: undefined,
+            // does the current layout have a legend
+            isLegendAvailable: true,
             // the id from the rest services json for the plot app
             plotServiceId: undefined,
             deaktivateGFI: false,
@@ -21,14 +28,20 @@ define(function (require) {
         initialize: function () {
             this.superInitialize();
 
+            // listen until the tool is activated for the first time
             this.listenTo(this, {
                 "change:isActive": this.getCapabilites
+            });
+
+            this.listenTo(Radio.channel("MapView"), {
+                "changedOptions": function (options) {
+                    this.setCurrentScale(options.scale);
+                }
             });
         },
 
         /**
          * Gets the capabilities for a specific print configuration
-         * or firing 'render' at the view
          * @param {Backbone.Model} model - this
          * @param {boolean} value - is this tool activated or not
          * @returns {void}
@@ -51,9 +64,54 @@ define(function (require) {
 
         parseMapfishCapabilities: function (response) {
             this.setLayoutList(response.layouts);
+            this.setCurrentLayout(response.layouts[0]);
+            this.setIsLegendAvailable(this.isLegendAvailable(response.layouts[0]));
             this.setFormatList(response.formats);
-            // this.setMapSize(response.layouts[0]);
-            this.trigger("render", this, this.get("isActive"));
+            this.setScaleList(this.createScaleList(Radio.request("MapView", "getScales")));
+            this.setCurrentScale(Radio.request("MapView", "getOptions").scale);
+        },
+
+        /**
+         * checks if the current layout has a legend
+         * @param {object} layout - current print layout
+         * @returns {boolean} true if the current layout has a legend otherwise false
+         */
+        isLegendAvailable: function (layout) {
+            return layout.attributes.some(function (attribute) {
+                return attribute.name === "legend";
+            });
+        },
+
+        /**
+         * creates the scale objects with display name and value
+         * is needed for the template
+         * @param {number[]} scales - scales from the map view
+         * @returns {object[]} scaleList
+         */
+        createScaleList: function (scales) {
+            var scaleList = [];
+
+            scales.forEach(function (scale) {
+                var scaleText = scale.toString();
+
+                if (scale >= 10000) {
+                    scaleText = scaleText.substring(0, scaleText.length - 3) + " " + scaleText.substring(scaleText.length - 3);
+                }
+                scaleList.push({value: scale, displayText: scaleText});
+            });
+            return scaleList;
+        },
+
+        /**
+         * returns the layout for the given layout name
+         * @param {object[]} layoutList - available layouts of the specified print configuration
+         * @param {string} layoutName - name for the layout to be found
+         * @returns {object} layout
+         */
+        getLayoutByName: function (layoutList, layoutName) {
+            return _.find(layoutList, function (layout) {
+                return layout.name === layoutName;
+            });
         },
 
         /**
@@ -81,11 +139,59 @@ define(function (require) {
         },
 
         /**
-         * @param {object[]} value - available formats of the specified print configuration
+         * @param {object[]} value - current print layout
+         * @returns {void}
+         */
+        setCurrentLayout: function (value) {
+            this.set("currentLayout", value);
+        },
+
+        /**
+         * @param {string[]} value - available formats of the specified print configuration
          * @returns {void}
          */
         setFormatList: function (value) {
             this.set("formatList", value);
+        },
+
+        /**
+         * @param {string} value - current print format
+         * @returns {void}
+         */
+        setCurrentFormat: function (value) {
+            this.set("currentFormat", value);
+        },
+
+        /**
+         * @param {number[]} value - available scales of the map
+         * @returns {void}
+         */
+        setScaleList: function (value) {
+            this.set("scaleList", value);
+        },
+
+        /**
+         * @param {number} value - current map scale
+         * @returns {void}
+         */
+        setCurrentScale: function (value) {
+            this.set("currentScale", value);
+        },
+
+        /**
+         * @param {boolean} value - true if the current layout has a legend
+         * @returns {void}
+         */
+        setIsLegendAvailable: function (value) {
+            this.set("isLegendAvailable", value);
+        },
+
+        /**
+         * @param {string} value - title for the printout
+         * @returns {void}
+         */
+        setTitle: function (value) {
+            this.set("title", value);
         }
     });
 
--- modules/tools/print_/view.js
+++ modules/tools/print_/view.js
@@ -4,21 +4,22 @@ define(function (require) {
 
     PrintView = Backbone.View.extend({
         events: {
-            "click": function () {
-                console.log(44);
-            }
+            "change #printLayout": "setCurrentLayout",
+            "change #printFormat": "setCurrentFormat",
+            "change #printScale": "setCurrentScale",
+            "keyup input[type='text']": "setTitle"
         },
-
         initialize: function () {
             this.template = _.template(PrintTemplate);
             this.listenTo(this.model, {
                 "change:isActive": this.render,
-                "render": this.render
+                "change:currentScale": this.render,
+                "change:isLegendAvailable": this.render
             });
         },
 
-        render: function (model, value) {
-            if (value) {
+        render: function (model) {
+            if (model.get("isActive")) {
                 this.setElement(document.getElementsByClassName("win-body")[0]);
                 this.$el.html(this.template(model.toJSON()));
                 this.delegateEvents();
@@ -28,6 +29,25 @@ define(function (require) {
             }
 
             return this;
+        },
+
+        setCurrentLayout: function (evt) {
+            var newLayout = this.model.getLayoutByName(this.model.get("layoutList"), evt.target.value);
+
+            this.model.setCurrentLayout(newLayout);
+            this.model.setIsLegendAvailable(this.model.isLegendAvailable(newLayout));
+        },
+
+        setCurrentFormat: function (evt) {
+            this.model.setCurrentFormat(evt.target.value);
+        },
+
+        setCurrentScale: function (evt) {
+            this.model.setCurrentScale(parseInt(evt.target.value, 10));
+        },
+
+        setTitle: function (evt) {
+            this.model.setTitle(evt.target.value);
         }
     });
 
