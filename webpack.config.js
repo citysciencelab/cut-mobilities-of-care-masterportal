c3917c6da7032e4f33372977c6243719c745d45e
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-08-28 12:40:20 +0200

    update introduce babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,19 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "build"),
         filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
     }
 };
