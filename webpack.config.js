d33dfb189344a8c76bb8b097fb7bbf47356f8e65
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-08-28 09:37:33 +0200

    update buildpath

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
-var webpack = require("webpack");
+var webpack = require("webpack"),
+    path = require("path");
 
 module.exports = {
     entry: "./js/main.js",
+    mode: "development",
     output: {
-        path: "build",
+        path: path.resolve(__dirname, "build"),
         filename: "bundle.js"
     }
 };
