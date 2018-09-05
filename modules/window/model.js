41f39751ee08deed995e46a5661dceeac24b464a
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-09-05 10:01:01 +0200

    update convert window to es6

--- modules/window/model.js
+++ modules/window/model.js
@@ -1,55 +1,53 @@
-define(function () {
-    var Window = Backbone.Model.extend({
-        defaults: {
-            isCollapsed: false,
-            isVisible: false,
-            maxPosLeft: "",
-            maxPosTop: "50px"
-        },
-        initialize: function () {
-            var channel = Radio.channel("Window");
+const Window = Backbone.Model.extend({
+    defaults: {
+        isCollapsed: false,
+        isVisible: false,
+        maxPosLeft: "",
+        maxPosTop: "50px"
+    },
+    initialize: function () {
+        var channel = Radio.channel("Window");
 
-            channel.on({
-                "toggleWin": this.setParams,
-                "closeWin": this.setIsVisible,
-                "collapseWin": this.collapseWindow,
-                "uncollapseWin": this.uncollapseWindow
-            }, this);
-        },
-        collapseWindow: function () {
-            this.setCollapse(true);
-        },
-        uncollapseWindow: function () {
-            this.setCollapse(false);
-        },
-        setCollapse: function (value) {
-            this.set("isCollapsed", value);
-        },
-        setIsVisible: function (value) {
-            this.set("isVisible", value);
-            if (!value) {
-                this.setCollapse(value);
-            }
-        },
-        setParams: function (value) {
-            this.setTool(value);
-            this.set("title", value.get("name"));
-            this.set("icon", value.get("glyphicon"));
-            this.set("winType", value.get("id"));
-            if (value.get("id") === "gfi") {
-                this.set("isVisible", false);
-            }
-            else {
-                this.set("isVisible", true);
-            }
-        },
-        sendParamsToWinCotent: function () {
-            Radio.trigger("Window", "winParams", [this.get("isVisible"), this.get("isCollapsed"), this.get("tool")]);
-        },
-        setTool: function (value) {
-            this.set("tool", value);
+        channel.on({
+            "toggleWin": this.setParams,
+            "closeWin": this.setIsVisible,
+            "collapseWin": this.collapseWindow,
+            "uncollapseWin": this.uncollapseWindow
+        }, this);
+    },
+    collapseWindow: function () {
+        this.setCollapse(true);
+    },
+    uncollapseWindow: function () {
+        this.setCollapse(false);
+    },
+    setCollapse: function (value) {
+        this.set("isCollapsed", value);
+    },
+    setIsVisible: function (value) {
+        this.set("isVisible", value);
+        if (!value) {
+            this.setCollapse(value);
         }
-    });
-
-    return Window;
+    },
+    setParams: function (value) {
+        this.setTool(value);
+        this.set("title", value.get("name"));
+        this.set("icon", value.get("glyphicon"));
+        this.set("winType", value.get("id"));
+        if (value.get("id") === "gfi") {
+            this.set("isVisible", false);
+        }
+        else {
+            this.set("isVisible", true);
+        }
+    },
+    sendParamsToWinCotent: function () {
+        Radio.trigger("Window", "winParams", [this.get("isVisible"), this.get("isCollapsed"), this.get("tool")]);
+    },
+    setTool: function (value) {
+        this.set("tool", value);
+    }
 });
+
+export default Window;
--- modules/window/view.js
+++ modules/window/view.js
@@ -1,93 +1,88 @@
-define(function (require) {
-    var Window = require("modules/window/model"),
-        templateMax = require("text!modules/window/templateMax.html"),
-        templateMin = require("text!modules/window/templateMin.html"),
-        templateTable = require("text!modules/window/templateTable.html"),
-        $ = require("jquery"),
-        WindowView;
+import Window from "./model";
+import templateMax from "text-loader!./templateMax.html";
+import templateMin from "text-loader!./templateMin.html";
+import templateTable from "text-loader!./templateTable.html";
+import "jquery-ui/ui/widgets/draggable";
 
-    require("jqueryui/widgets/draggable");
+const WindowView = Backbone.View.extend({
+    events: {
+        "click .glyphicon-minus": "minimize",
+        "click .header-min > .title": "maximize",
+        "click .glyphicon-remove": "hide"
+    },
+    initialize: function () {
+        this.listenTo(this.model, {
+            "change:isVisible change:isCollapsed change:winType": this.render
+        });
+        this.$el.draggable({
+            containment: "#map",
+            handle: ".move",
+            stop: function (event, ui) {
+                ui.helper.css({"height": "", "width": ""});
+            }
+        });
+        this.$el.css({
+            "max-height": window.innerHeight - 100 // 100 fixer Wert für navbar &co.
+        });
 
-    WindowView = Backbone.View.extend({
-        events: {
-            "click .glyphicon-minus": "minimize",
-            "click .header-min > .title": "maximize",
-            "click .glyphicon-remove": "hide"
-        },
-        initialize: function () {
-            this.listenTo(this.model, {
-                "change:isVisible change:isCollapsed change:winType": this.render
-            });
-            this.$el.draggable({
-                containment: "#map",
-                handle: ".move",
-                stop: function (event, ui) {
-                    ui.helper.css({"height": "", "width": ""});
-                }
-            });
+        $(window).resize($.proxy(function () {
             this.$el.css({
                 "max-height": window.innerHeight - 100 // 100 fixer Wert für navbar &co.
             });
+        }, this));
+    },
+    id: "window",
+    className: "tool-window ui-widget-content",
+    model: new Window(),
+    templateMax: _.template(templateMax),
+    templateMin: _.template(templateMin),
+    templateTable: _.template(templateTable),
+    render: function () {
+        var attr = this.model.toJSON();
 
-            $(window).resize($.proxy(function () {
-                this.$el.css({
-                    "max-height": window.innerHeight - 100 // 100 fixer Wert für navbar &co.
-                });
-            }, this));
-        },
-        id: "window",
-        className: "tool-window ui-widget-content",
-        model: new Window(),
-        templateMax: _.template(templateMax),
-        templateMin: _.template(templateMin),
-        templateTable: _.template(templateTable),
-        render: function () {
-            var attr = this.model.toJSON();
-
-            if (this.model.get("isVisible") === true) {
-                if (Radio.request("Util", "getUiStyle") === "TABLE") {
-                    $(".lgv-container").append(this.$el.html(this.templateTable(attr)));
-                    this.$el.addClass("table-tool-window");
-                }
-                else if (this.model.get("isCollapsed") === true) {
-                    $("body").append(this.$el.html(this.templateMin(attr)));
-                    this.$el.css({"top": "", "bottom": "0", "left": "0", "margin-bottom": "60px"});
-                }
-                else {
-                    $("body").append(this.$el.html(this.templateMax(attr)));
-                    this.$el.css({"top": this.model.get("maxPosTop"), "bottom": "", "left": this.model.get("maxPosLeft"), "margin-bottom": "30px"});
-                }
-                this.model.sendParamsToWinCotent();
-                this.$el.show("slow");
+        if (this.model.get("isVisible") === true) {
+            if (Radio.request("Util", "getUiStyle") === "TABLE") {
+                $(".lgv-container").append(this.$el.html(this.templateTable(attr)));
+                this.$el.addClass("table-tool-window");
+            }
+            else if (this.model.get("isCollapsed") === true) {
+                $("body").append(this.$el.html(this.templateMin(attr)));
+                this.$el.css({"top": "", "bottom": "0", "left": "0", "margin-bottom": "60px"});
             }
             else {
-                this.hide();
+                $("body").append(this.$el.html(this.templateMax(attr)));
+                this.$el.css({"top": this.model.get("maxPosTop"), "bottom": "", "left": this.model.get("maxPosLeft"), "margin-bottom": "30px"});
             }
-            return this;
-        },
-        minimize: function () {
-            this.model.set("maxPosTop", this.$el.css("top"));
-            this.model.set("maxPosLeft", this.$el.css("left"));
-            this.model.setCollapse(true);
-        },
-        maximize: function () {
-            this.model.setCollapse(false);
-        },
-        hide: function () {
-            var toolModel = Radio.request("ModelList", "getModelByAttributes", {id: this.model.get("winType")});
-
-            if (toolModel) {
-                toolModel.setIsActive(false);
-            }
-            if (this.model.get("winType") === "download") {
-                Radio.request("ModelList", "getModelByAttributes", {id: "draw"}).setIsActive(false);
-            }
-            this.$el.hide("slow");
-            this.model.setIsVisible(false);
             this.model.sendParamsToWinCotent();
-            Radio.channel("Tool").trigger("activatedTool", "gfi", false);
+            this.$el.show("slow");
         }
-    });
+        else {
+            this.hide();
+        }
+        return this;
+    },
+    minimize: function () {
+        this.model.set("maxPosTop", this.$el.css("top"));
+        this.model.set("maxPosLeft", this.$el.css("left"));
+        this.model.setCollapse(true);
+    },
+    maximize: function () {
+        this.model.setCollapse(false);
+    },
+    hide: function () {
+        var toolModel = Radio.request("ModelList", "getModelByAttributes", {id: this.model.get("winType")});
 
-    return WindowView;
+        if (toolModel) {
+            toolModel.setIsActive(false);
+        }
+        if (this.model.get("winType") === "download") {
+            Radio.request("ModelList", "getModelByAttributes", {id: "draw"}).setIsActive(false);
+        }
+        this.$el.hide("slow");
+        this.model.setIsVisible(false);
+        this.model.sendParamsToWinCotent();
+        Radio.channel("Tool").trigger("activatedTool", "gfi", false);
+    }
 });
+
+export default WindowView;
