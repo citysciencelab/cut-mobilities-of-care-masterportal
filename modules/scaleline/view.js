4990ae2915ecc4607a6ab3db483ce45fd8776b96
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-09-05 14:38:12 +0200

    update convert scaleline to es6

--- modules/scaleline/view.js
+++ modules/scaleline/view.js
@@ -1,25 +1,22 @@
-define(function (require) {
-    var ScaleLine = require("modules/scaleline/model"),
-        ScaleLineTemplate = require("text!modules/scaleline/template.html"),
-        ScaleLineView;
+import ScaleLine from "./model";
+import ScaleLineTemplate from "text-loader!./template.html";
 
-    ScaleLineView = Backbone.View.extend({
-        initialize: function () {
-            this.listenTo(this.model, "change:scaleLineValue", this.render);
-            this.render();
-        },
-        model: new ScaleLine(),
-        className: "scale-line",
-        template: _.template(ScaleLineTemplate),
-        render: function () {
-            var attr = this.model.toJSON();
+const ScaleLineView = Backbone.View.extend({
+    initialize: function () {
+        this.listenTo(this.model, "change:scaleLineValue", this.render);
+        this.model = new ScaleLine();
+        this.render();
+    },
+    className: "scale-line",
+    template: _.template(ScaleLineTemplate),
+    render: function () {
+        var attr = this.model.toJSON();
 
-            this.$el.html(this.template(attr));
-            document.getElementsByClassName("footer")[0].appendChild(this.el);
-            this.$el.css("right", 0);
-            return this;
-        }
-    });
-
-    return ScaleLineView;
+        this.$el.html(this.template(attr));
+        document.getElementsByClassName("footer")[0].appendChild(this.el);
+        this.$el.css("right", 0);
+        return this;
+    }
 });
+
+export default ScaleLineView;
