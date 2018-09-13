4cc2ade1d86dbde8151c70c2786df99ee2e7632c
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-09-13 11:17:15 +0200

    update convert mousehover to es6

--- modules/mouseHover/view.js
+++ modules/mouseHover/view.js
@@ -1,56 +1,51 @@
-define(function (require) {
-    var MouseHoverPopup = require("modules/mouseHover/model"),
-        $ = require("jquery"),
-        MouseHoverPopupView;
+import MouseHoverPopup from "./model";
+import "bootstrap/js/popover";
 
-    require("bootstrap/popover");
+const MouseHoverPopupView = Backbone.View.extend({
+    initialize: function (attr) {
+        this.model = new MouseHoverPopup(attr);
+        this.listenTo(this.model, {
+            "render": this.render,
+            "destroy": this.destroy
+        });
 
-    MouseHoverPopupView = Backbone.View.extend({
-        initialize: function (attr) {
-            this.model = new MouseHoverPopup(attr);
-            this.listenTo(this.model, {
-                "render": this.render,
-                "destroy": this.destroy
-            });
+        this.createOverlayElement();
+    },
 
-            this.createOverlayElement();
-        },
+    /**
+     * Rendert den Bootstrap-Toolip ins mousehoverpopup.
+     * html: true - Damit <br> ausgewertet wird
+     * trigger: manual - lösst Bug mit verschwindendem Tooltip
+     * @param {string} text -
+     * @returns {void}
+     */
+    render: function (text) {
+        var element = this.model.get("overlay").getElement();
 
-        /**
-         * Rendert den Bootstrap-Toolip ins mousehoverpopup.
-         * html: true - Damit <br> ausgewertet wird
-         * trigger: manual - lösst Bug mit verschwindendem Tooltip
-         * @param {string} text -
-         * @returns {void}
-         */
-        render: function (text) {
-            var element = this.model.get("overlay").getElement();
+        $(element).tooltip("destroy");
+        $(element).tooltip({
+            html: true,
+            title: text,
+            template: "<div class='tooltip' role='tooltip'><div class='tooltip-inner mouseHover'></div></div>",
+            placement: "auto",
+            animation: true,
+            trigger: "manual",
+            viewport: "#map"
+        }, this);
+        $(element).tooltip("show");
+        return this;
+    },
 
-            $(element).tooltip("destroy");
-            $(element).tooltip({
-                html: true,
-                title: text,
-                template: "<div class='tooltip' role='tooltip'><div class='tooltip-inner mouseHover'></div></div>",
-                placement: "auto",
-                animation: true,
-                trigger: "manual",
-                viewport: "#map"
-            }, this);
-            $(element).tooltip("show");
-            return this;
-        },
+    /**
+     * creates the overlay element and appends it to the map element
+     * @returns {void}
+     */
+    createOverlayElement: function () {
+        var element = document.createElement("DIV");
 
-        /**
-         * creates the overlay element and appends it to the map element
-         * @returns {void}
-         */
-        createOverlayElement: function () {
-            var element = document.createElement("DIV");
-
-            document.getElementById("map").appendChild(element);
-            this.model.get("overlay").setElement(element);
-        }
-    });
-
-    return MouseHoverPopupView;
+        document.getElementById("map").appendChild(element);
+        this.model.get("overlay").setElement(element);
+    }
 });
+
+export default MouseHoverPopupView;
