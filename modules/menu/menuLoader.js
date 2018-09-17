cc0390a324bdbc160484310a0c79f320d09659a6
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-09-17 11:37:52 +0200

    update refactor caller deprecated

--- modules/menu/menuLoader.js
+++ modules/menu/menuLoader.js
@@ -1,59 +1,66 @@
 import LightMenu from "./desktop/listViewLight";
 import Menu from "./desktop/listView";
 import MobileMenu from "./mobile/listView";
+import TableMenu from "./table/view";
 
-function MenuLoader () {
-    var channel = Radio.channel("MenuLoader");
+const MenuLoader = Backbone.Model.extend({
+    defaults: {
+        treeType: "light",
+        currentMenu: ""
+    },
+    initialize: function () {
+        this.treeType = Radio.request("Parser", "getTreeType");
 
-    this.treeType = Radio.request("Parser", "getTreeType");
+        this.loadMenu();
 
+        // im Table-Style soll das ui nicht verändert werden
+        if (this.menuStyle === "DEFAULT") {
+            Radio.on("Util", {
+                "isViewMobileChanged": function () {
+                    this.currentMenu.removeView();
+                    this.loadMenu();
+                }
+            }, this);
+        }
+    },
     /**
      * Prüft initial und nach jedem Resize, ob und welches Menü geladen werden muss und lädt bzw. entfernt Module.
      * @param  {Object} caller this MenuLoader
      * @return {Object}        this
      */
-    this.loadMenu = function (caller) {
-        var isMobile = Radio.request("Util", "isViewMobile");
+    loadMenu: function () {
+        var isMobile = Radio.request("Util", "isViewMobile"),
+            channel = Radio.channel("Menuloader");
 
         if (!this.menuStyle) {
             this.menuStyle = Radio.request("Util", "getUiStyle");
         }
 
         if (this.menuStyle === "TABLE") {
-            caller.currentMenu = new Menu();
-            channel.trigger("ready", caller.currentMenu.id);
+            this.currentMenu = new TableMenu();
+            channel.trigger("ready", this.currentMenu.id);
         }
         else if (this.menuStyle === "DEFAULT") {
             $("#map").css("height", "calc(100% - 50px)");
             $("#main-nav").show();
 
             if (isMobile) {
-                caller.currentMenu = new MobileMenu();
+                this.currentMenu = new MobileMenu();
             }
             else if (this.treeType === "light") {
-                caller.currentMenu = new LightMenu();
+                this.currentMenu = new LightMenu();
                 channel.trigger("ready");
                 Radio.trigger("Map", "updateSize");
             }
             else {
-                caller.currentMenu = new Menu();
+                this.currentMenu = new Menu();
                 channel.trigger("ready");
                 Radio.trigger("Map", "updateSize");
             }
             // Nachdem die MapSize geändert wurde, muss die Map aktualisiert werden.
             Radio.trigger("Map", "updateSize");
         }
-    };
-    this.currentMenu = this.loadMenu(this);
-    // im Table-Style soll das ui nicht verändert werden
-    if (this.menuStyle === "DEFAULT") {
-        Radio.on("Util", {
-            "isViewMobileChanged": function () {
-                this.currentMenu.removeView();
-                this.currentMenu = this.loadMenu(this);
-            }
-        }, this);
     }
-}
+});
 
 export default MenuLoader;
