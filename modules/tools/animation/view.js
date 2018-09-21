ca7298eb17db4654947dd29a4fef5b0d7f28755c
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-09-21 10:22:39 +0200

    update convert animation to ES6

--- modules/tools/animation/view.js
+++ modules/tools/animation/view.js
@@ -1,57 +1,53 @@
-define(function (require) {
+import AnimationTemplate from "text-loader!./template.html";
 
-    var AnimationTemplate = require("text!modules/tools/animation/template.html"),
-        AnimationView;
-
-    AnimationView = Backbone.View.extend({
-        tagName: "form",
-        id: "animation-tool",
-        template: _.template(AnimationTemplate),
-        events: {
-            "click .start": "start",
-            "click .reset": "reset",
-            "change #select-kreis": "setKreis",
-            "change #select-gemeinde": "setGemeinde",
-            "change input[type=radio]": "setDirection"
-        },
-        initialize: function () {
-            this.listenTo(this.model, {
-                // ändert sich der Fensterstatus wird neu gezeichnet
-                "change:isActive": this.render,
-                // ändert sich eins dieser Attribute wird neu gezeichnet
-                "change:gemeinden change:gemeinde change:direction change:animating change:pendlerLegend": this.render
-            });
-        },
-        render: function (model, value) {
-            if (value || !model.get("animating")) {
-                this.setElement(document.getElementsByClassName("win-body")[0]);
-                this.$el.html(this.template(model.toJSON()));
-                this.delegateEvents();
-            }
-            else {
-                this.undelegateEvents();
-            }
-            return this;
-        },
-        start: function () {
-            this.model.prepareAnimation();
-        },
-        reset: function () {
-            this.model.stopAnimation();
-        },
-
-        setKreis: function (evt) {
-            this.model.setKreis(evt.target.value);
-        },
-
-        setGemeinde: function (evt) {
-            this.model.setGemeinde(evt.target.value);
-        },
-
-        setDirection: function (evt) {
-            this.model.setDirection(evt.target.value);
+const AnimationView = Backbone.View.extend({
+    events: {
+        "click .start": "start",
+        "click .reset": "reset",
+        "change #select-kreis": "setKreis",
+        "change #select-gemeinde": "setGemeinde",
+        "change input[type=radio]": "setDirection"
+    },
+    initialize: function () {
+        this.listenTo(this.model, {
+            // ändert sich der Fensterstatus wird neu gezeichnet
+            "change:isActive": this.render,
+            // ändert sich eins dieser Attribute wird neu gezeichnet
+            "change:gemeinden change:gemeinde change:direction change:animating change:pendlerLegend": this.render
+        });
+    },
+    tagName: "form",
+    id: "animation-tool",
+    template: _.template(AnimationTemplate),
+    render: function (model, value) {
+        if (value || !model.get("animating")) {
+            this.setElement(document.getElementsByClassName("win-body")[0]);
+            this.$el.html(this.template(model.toJSON()));
+            this.delegateEvents();
         }
-    });
+        else {
+            this.undelegateEvents();
+        }
+        return this;
+    },
+    start: function () {
+        this.model.prepareAnimation();
+    },
+    reset: function () {
+        this.model.stopAnimation();
+    },
 
-    return AnimationView;
+    setKreis: function (evt) {
+        this.model.setKreis(evt.target.value);
+    },
+
+    setGemeinde: function (evt) {
+        this.model.setGemeinde(evt.target.value);
+    },
+
+    setDirection: function (evt) {
+        this.model.setDirection(evt.target.value);
+    }
 });
+
+export default AnimationView;
