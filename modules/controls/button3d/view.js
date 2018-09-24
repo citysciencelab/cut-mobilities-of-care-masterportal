d34295db5434ce6541b6908431bc5df259e33414
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2018-09-24 15:45:28 +0200

    add - info for 3d modus

--- modules/controls/button3d/view.js
+++ modules/controls/button3d/view.js
@@ -1,34 +1,36 @@
 define(function (require) {
 
     var Button3dTemplate = require("text!modules/controls/button3d/template.html"),
-        $ = require("jquery"),
         Button3dView;
 
     Button3dView = Backbone.View.extend({
-        className: "row",
-        template: _.template(Button3dTemplate),
         events: {
             "click .button3D": "mapChange"
         },
         initialize: function () {
+            this.template = _.template(Button3dTemplate);
             this.render();
         },
         render: function () {
             this.$el.html(this.template);
             if (Radio.request("Map", "isMap3d")) {
-                $("#button3D").addClass("toggleButtonPressed");
+                this.$("#button3D").addClass("toggleButtonPressed");
             }
+
+            return this;
         },
         mapChange: function () {
             if (Radio.request("Map", "isMap3d")) {
                 Radio.trigger("Map", "deactivateMap3d");
-                $("#button3D").removeClass("toggleButtonPressed");
+                this.$("#button3D").removeClass("toggleButtonPressed");
+                Radio.trigger("Alert", "alert:remove");
             }
             else {
                 Radio.trigger("Map", "activateMap3d");
                 // Radio.trigger("ModelList", "setModelAttributesById", "3d_daten", {isExpanded: true});
                 // Radio.trigger("ModelList", "setModelAttributesById", "terrain", {isSelected: true});
-                $("#button3D").addClass("toggleButtonPressed");
+                this.$("#button3D").addClass("toggleButtonPressed");
+                Radio.trigger("Alert", "alert", "Der 3D-Modus befindet sich zur Zeit noch in der Beta-Version!");
             }
 
         }
