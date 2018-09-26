ed3a6fd40d12aff7dcbea75652ac0802f073c4ed
Sebastian Duden <sebastian.duden@gv.hamburg.de>
2018-09-26 10:47:12 +0000

    mapView.js edited online with Bitbucket

--- modules/core/mapView.js
+++ modules/core/mapView.js
@@ -131,10 +131,10 @@ const MapView = Backbone.Model.extend({
         this.setView();
 
         // Listener für ol.View
-        this.get("view").on("change:resolution", this.changedResolutionCallback.bind(this));
+        this.get("view").on("change:resolution", this.changedResolutionCallback.bind(this), this);
         this.get("view").on("change:center", function () {
             Radio.trigger("MapView", "changedCenter", this.getCenter());
-        });
+        }, this);
     },
 
     /**
