2e04f88346e61f8fb855ac45144c84a2477b7fbc
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2018-09-28 08:37:21 +0200

    fix - buildSpec unittest

--- modules/tools/print_/buildSpec.js
+++ modules/tools/print_/buildSpec.js
@@ -312,7 +312,9 @@ define(function (require) {
             var strokeColor = style.getColor();
 
             obj.strokeColor = this.rgbArrayToHex(strokeColor);
-            obj.strokeOpacity = strokeColor[3];
+            if (strokeColor[3] !== undefined) {
+                obj.strokeOpacity = strokeColor[3];
+            }
             if (_.indexOf(_.functions(style), "getWidth") !== -1 && style.getWidth() !== undefined) {
                 obj.strokeWidth = style.getWidth();
             }
