0b3d54004eacc90e4bed7be1c8ec3a10bfd7c308
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-10-08 11:49:05 +0200

    update add devserver

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,20 @@
 const merge = require("webpack-merge"),
     Visualizer = require("webpack-visualizer-plugin"),
-    common = require("./webpack.common.js");
+    common = require("./webpack.common.js"),
+    proxies = require("lgv-config/proxyconf.json");
+
 
 module.exports = merge(common, {
     mode: "development",
-    watch: true,
+    devServer: {
+        port: 9001,
+        publicPath: "/build/",
+        overlay: true,
+        https: true,
+        open: true,
+        openPage: "portal/master",
+        proxy: proxies
+    },
     plugins: [
         new Visualizer({
             filename: "./statistics.html"
