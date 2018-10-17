0b007593163a1ff8296e2af04bad49c5f363b571
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-10-17 12:06:43 +0200

    update convert to ES6

--- modules/tools/pendler/lines/model.js
+++ modules/tools/pendler/lines/model.js
@@ -1,146 +1,147 @@
-define(function (require) {
+import PendlerCoreModel from "../core/model";
+import {Stroke, Style} from "ol/style.js";
+import {Point} from "ol/geom.js";
+import VectorSource from "ol/source/Vector.js";
+import VectorLayer from "ol/layer/Vector.js";
+import Feature from "ol/Feature.js";
 
-    var PendlerCoreModel = require("modules/tools/pendler/core/model"),
-        ol = require("openlayers"),
-        Lines;
 
-    Lines = PendlerCoreModel.extend({
-        defaults: _.extend({}, PendlerCoreModel.prototype.defaults, {
-            zoomLevel: 0,
-            // Layer zur Darstellung der Linien / Strahlen
-            lineLayer: new ol.layer.Vector({
-                source: new ol.source.Vector(),
-                style: null,
-                name: "lineLayer"
-            }),
-            // Layer zur Darstellung der Beschriftung an Punkten am Strahlenende
-            labelLayer: new ol.layer.Vector({
-                source: new ol.source.Vector(),
-                style: null,
-                name: "labelLayer"
-            })
+const Lines = PendlerCoreModel.extend({
+    defaults: _.extend({}, PendlerCoreModel.prototype.defaults, {
+        zoomLevel: 0,
+        // Layer zur Darstellung der Linien / Strahlen
+        lineLayer: new VectorLayer({
+            source: new VectorSource(),
+            style: null,
+            name: "lineLayer"
         }),
+        // Layer zur Darstellung der Beschriftung an Punkten am Strahlenende
+        labelLayer: new VectorLayer({
+            source: new VectorSource(),
+            style: null,
+            name: "labelLayer"
+        })
+    }),
 
-        preparePendlerLegend: function (features) {
-            var pendlerLegend = [];
+    preparePendlerLegend: function (features) {
+        var pendlerLegend = [];
 
-            _.each(features, function (feature) {
-                // Ein Feature entspricht einer Gemeinde. Extraktion der für die Legende
-                // nötigen Attribute (abhängig von der gewünschten Richtung).
-                pendlerLegend.push({
-                    anzahlPendler: feature.get(this.get("attrAnzahl")),
-                    name: feature.get(this.get("attrGemeinde"))
-                });
-            }, this);
+        _.each(features, function (feature) {
+            // Ein Feature entspricht einer Gemeinde. Extraktion der für die Legende
+            // nötigen Attribute (abhängig von der gewünschten Richtung).
+            pendlerLegend.push({
+                anzahlPendler: feature.get(this.get("attrAnzahl")),
+                name: feature.get(this.get("attrGemeinde"))
+            });
+        }, this);
 
-            this.set("pendlerLegend", pendlerLegend);
-        },
+        this.set("pendlerLegend", pendlerLegend);
+    },
 
-        handleData: function () {
-            var rawFeatures = this.get("lineFeatures"),
-                topFeatures,
-                lineLayer,
-                labelLayer;
+    handleData: function () {
+        var rawFeatures = this.get("lineFeatures"),
+            topFeatures,
+            lineLayer,
+            labelLayer;
 
-            // Add layers for lines and labels if neccessary. If Layers
-            // are already exiting clean them.
-            labelLayer = Radio.request("Map", "createLayerIfNotExists", "labelLayer");
-            this.set("labelLayer", labelLayer);
-            this.get("labelLayer").getSource().clear();
-            lineLayer = Radio.request("Map", "createLayerIfNotExists", "lineLayer");
-            this.set("lineLayer", lineLayer);
-            this.get("lineLayer").getSource().clear();
+        // Add layers for lines and labels if neccessary. If Layers
+        // are already exiting clean them.
+        labelLayer = Radio.request("Map", "createLayerIfNotExists", "labelLayer");
+        this.set("labelLayer", labelLayer);
+        this.get("labelLayer").getSource().clear();
+        lineLayer = Radio.request("Map", "createLayerIfNotExists", "lineLayer");
+        this.set("lineLayer", lineLayer);
+        this.get("lineLayer").getSource().clear();
 
-            // Lege die Reihenfolge der Layer fest, damit die Beschriftungen nicht von den Zahlen überdeckt werden.
-            this.get("labelLayer").setZIndex(11);
-            this.get("lineLayer").setZIndex(10);
+        // Lege die Reihenfolge der Layer fest, damit die Beschriftungen nicht von den Zahlen überdeckt werden.
+        this.get("labelLayer").setZIndex(11);
+        this.get("lineLayer").setZIndex(10);
 
-            // Zentriere View auf die Gemeinde, zeichne aber keinen Marker ein.
-            this.centerGemeinde(false);
+        // Zentriere View auf die Gemeinde, zeichne aber keinen Marker ein.
+        this.centerGemeinde(false);
 
-            topFeatures = this.selectFeatures(rawFeatures);
+        topFeatures = this.selectFeatures(rawFeatures);
 
-            this.preparePendlerLegend(topFeatures);
-            this.createFeatures(topFeatures);
+        this.preparePendlerLegend(topFeatures);
+        this.createFeatures(topFeatures);
 
-            Radio.trigger("Map", "render");
-        },
+        Radio.trigger("Map", "render");
+    },
 
-        /**
-         * Erzeuge die Strahlen mit Beschriftung für jede Gemeinde.
-         * @param {Object[]} features Feature-Liste
-         * @returns {Void} Keine Rückgabe
-         */
-        createFeatures: function (features) {
+    /**
+     * Erzeuge die Strahlen mit Beschriftung für jede Gemeinde.
+     * @param {Object[]} features Feature-Liste
+     * @returns {Void} Keine Rückgabe
+     */
+    createFeatures: function (features) {
 
-            _.each(features, function (feature) {
-                var lineLayerFeature,
-                    labelCoordinates,
-                    labelLayerFeature;
+        _.each(features, function (feature) {
+            var lineLayerFeature,
+                labelCoordinates,
+                labelLayerFeature;
 
-                // Erzeuge die Strahlen
-                lineLayerFeature = new ol.Feature({
-                    geometry: feature.getGeometry()
-                });
+            // Erzeuge die Strahlen
+            lineLayerFeature = new Feature({
+                geometry: feature.getGeometry()
+            });
 
-                lineLayerFeature.setStyle(new ol.style.Style({
-                    stroke: new ol.style.Stroke({
-                        color: "#c00909",
-                        width: "3"
-                    })
-                }));
+            lineLayerFeature.setStyle(new Style({
+                stroke: new Stroke({
+                    color: "#c00909",
+                    width: "3"
+                })
+            }));
 
-                this.get("lineLayer").getSource().addFeature(lineLayerFeature);
+            this.get("lineLayer").getSource().addFeature(lineLayerFeature);
 
-                // Erzeuge die Beschriftung. Dafür wird ein (unsichtbarere) Punkt am Ende jeder Linie gesetzt.
-                // Wo das Ende ist (erste oder zweite Koordinate) entschreidet sich dabei aus der (Pendel-)Richtung
-                if (this.get("direction") === "wohnort") {
-                    labelCoordinates = _.last(feature.getGeometry().getCoordinates());
-                }
-                else {
-                    labelCoordinates = _.first(feature.getGeometry().getCoordinates());
-                }
-
-                labelLayerFeature = new ol.Feature({
-                    geometry: new ol.geom.Point(labelCoordinates)
-                });
-
-                labelLayerFeature.setStyle(new ol.style.Style({
-                    text: new ol.style.Text({
-                        text: feature.get(this.get("attrAnzahl")),
-                        font: "14pt sans-serif",
-                        placement: "point",
-                        stroke: new ol.style.Stroke({
-                            color: "#ffffff",
-                            width: 5
-                        })
-                    })
-                }));
-
-                this.get("labelLayer").getSource().addFeature(labelLayerFeature);
-
-            }, this);
-        },
-
-        /**
-         * Entefernt das Diagramm von der Karte
-         * @returns {Void} Kein Rückgabewert
-         */
-        clear: function () {
-            var lineLayer,
-                labelLayer;
-
-            lineLayer = this.get("lineLayer");
-            if (!_.isUndefined(lineLayer)) {
-                Radio.trigger("Map", "removeLayer", lineLayer);
+            // Erzeuge die Beschriftung. Dafür wird ein (unsichtbarere) Punkt am Ende jeder Linie gesetzt.
+            // Wo das Ende ist (erste oder zweite Koordinate) entschreidet sich dabei aus der (Pendel-)Richtung
+            if (this.get("direction") === "wohnort") {
+                labelCoordinates = _.last(feature.getGeometry().getCoordinates());
+            }
+            else {
+                labelCoordinates = _.first(feature.getGeometry().getCoordinates());
             }
 
-            labelLayer = this.get("labelLayer");
-            if (!_.isUndefined(lineLayer)) {
-                Radio.trigger("Map", "removeLayer", labelLayer);
-            }
+            labelLayerFeature = new Feature({
+                geometry: new Point(labelCoordinates)
+            });
+
+            labelLayerFeature.setStyle(new Style({
+                text: new Text({
+                    text: feature.get(this.get("attrAnzahl")),
+                    font: "14pt sans-serif",
+                    placement: "point",
+                    stroke: new Stroke({
+                        color: "#ffffff",
+                        width: 5
+                    })
+                })
+            }));
+
+            this.get("labelLayer").getSource().addFeature(labelLayerFeature);
+
+        }, this);
+    },
+
+    /**
+     * Entefernt das Diagramm von der Karte
+     * @returns {Void} Kein Rückgabewert
+     */
+    clear: function () {
+        var lineLayer,
+            labelLayer;
+
+        lineLayer = this.get("lineLayer");
+        if (!_.isUndefined(lineLayer)) {
+            Radio.trigger("Map", "removeLayer", lineLayer);
         }
-    });
 
-    return Lines;
+        labelLayer = this.get("labelLayer");
+        if (!_.isUndefined(lineLayer)) {
+            Radio.trigger("Map", "removeLayer", labelLayer);
+        }
+    }
 });
+
+export default Lines;
