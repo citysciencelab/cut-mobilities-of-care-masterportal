cb2660da936e6d654eaa251034b1e91a82f70279
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-10-23 09:52:36 +0200

    update add Internet/FHHnet replacefunction

--- devtools/tasks/replace.js
+++ devtools/tasks/replace.js
@@ -1,28 +1,58 @@
 var replace = require("replace-in-file"),
-    replacements = [{
-        "files": "dist/index.html",
+    replacements = [];
+
+module.exports = function (environment, destination) {
+    replacements.push({
+        "files": destination + "/index.html",
         "from": /..\/..\/build\//g,
         "to": "./"
     },
     {
-        "files": "dist/css/style.css",
+        "files": destination + "dist/css/style.css",
         "from": /css\/woffs/g,
         "to": "./woffs"
     },
     {
-        "files": "dist/config.json",
+        "files": destination + "/config.json",
         "from": /\.\.\/\.\.\/node_modules\/lgv-config/g,
         "to": "../lgv-config"
-    }
-];
+    });
 
-module.exports = function () {
+    if (environment === "Internet") {
+        replacements.push({
+            "files": destination + "/config.js",
+            "from": /rest-services-fhhnet/g,
+            "to": "rest-services-internet"
+        },
+        {
+            "files": destination + "/config.js",
+            "from": /services-fhhnet-ALL/g,
+            "to": "services-internet"
+        },
+        {
+            "files": destination + "/config.js",
+            "from": /services-fhhnet/g,
+            "to": "services-internet"
+        });
+    }
+    else {
+        replacements.push({
+            "files": destination + "/config.js",
+            "from": /rest-services-internet/g,
+            "to": "rest-services-fhhnet"
+        },
+        {
+            "files": destination + "/config.js",
+            "from": /services-internet/g,
+            "to": "services-fhhnet"
+        });
+    }
     replacements.forEach(function (replacement) {
         replace.sync({
             files: replacement.files,
             from: replacement.from,
             to: replacement.to
         });
-        console.log ("Successfully replaced '" + replacement.from + "' in Files '" + replacement.files + "' to '" + replacement.to + "!");
+        console.warn("Successfully replaced '" + replacement.from + "' in Files '" + replacement.files + "' to '" + replacement.to + "!");
     });
 };
