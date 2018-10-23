885d83c8baf25ea313789db20662510995d17279
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-10-23 09:53:27 +0200

    update provide Internet/FHHnet replacement, use fs-extra

--- devtools/tasks/copy.js
+++ devtools/tasks/copy.js
@@ -1,31 +1,32 @@
-const del = require("del"),
-    copy = require("copy-concurrently"),
+const fs = require("fs-extra"),
     inquirer = require("inquirer"),
+    replaceStrings = require("./replace"),
     questions = [
         {type: "input", name: "portalPath", message: "Bitte Pfad zum Portal angeben:", default: "portal/basic"},
-        {type: "input", name: "portalName", message: "Wie soll der Name des Ordners in dist heißen:", default: "Basic"}
+        {type: "input", name: "portalName", message: "Wie soll der Name des Ordners in dist heißen:", default: "Basic"},
+        {type: "input", name: "environment", message: "'Internet' oder 'FHHnet'?:", default: "Internet"}
     ];
 
 
-function copyFiles (source, destination) {
-    var copypathes = [];
+function copyFiles (source, destination, environment) {
 
-    copypathes.push({src: source, dest: destination}, {src: "./dist/build", dest: destination});
+    fs.copy(source, destination).then(() => {
+        console.warn("Successfully Copied '" + source + "' to '" + destination + "' !");
+        fs.move("./dist/build", destination, {overwrite: true}).then(() => {
+            console.warn("Successfully moved './dist/build' to '" + destination + "' !");
+            replaceStrings(environment, destination);
+        }).catch(err => console.error(err));
+    }).catch(err => console.error(err));
 
-    copypathes.forEach(function (path) {
-        copy(path.src, path.dest).then(() => {
-            console.log("Successfully Copied '" + path.src + "' to '" + path.dest + "' !");
-        }).catch();
-    });
 }
 
 function doit (answers) {
     var destination = "dist/" + answers.portalName,
         source = "./" + answers.portalPath;
 
-    del([destination]).then(() => {
-        console.log ("Successfully deleted '" + destination + "' directory");
-        copyFiles(source, destination);
+    fs.remove(destination).then(() => {
+        console.warn("Successfully deleted '" + destination + "' directory");
+        copyFiles(source, destination, answers.environment);
     });
 }
 
