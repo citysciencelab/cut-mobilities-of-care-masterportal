8ce3a599311c5852bb599f4d236879f2ba0cf065
sven_beyel <sven.beyel@gv.hamburg.de>
2018-10-25 11:01:41 +0200

    remove comments

--- devtools/webpack.dev.js
+++ devtools/webpack.dev.js
@@ -2,9 +2,7 @@ const merge = require("webpack-merge"),
     // auskommentieren um eine grafische Darstellung vom bundle als html zu erzeugen
     // Visualizer = require("webpack-visualizer-plugin"),
     common = require("./webpack.common.js"),
-    // fs = require("fs"),
     proxies = require("lgv-config/proxyconf.json");
-    // proxies = fs.existsSync("node_modules/lgv-config/proxyconf.json") ? require("lgv-config/proxyconf.json") : require("lgv-config-public/proxyconf.json");
 
 module.exports = merge(common, {
     mode: "development",
