757bf91561fc6c13ab42066507ec9074aeb111d6
sven_beyel <sven.beyel@gv.hamburg.de>
2018-11-07 15:27:28 +0100

    add TestModel

---
+++ test/unittests/modules/tools/draw/testModel.js
@@ -0,0 +1,16 @@
+import {assert} from "chai";
+import Model from "@modules/tools/draw/model.js";
+
+describe("drawModel", function () {
+    var model;
+
+    before(function () {
+        model = new Model();
+    });
+
+    describe("validateFilename", function () {
+        // it("validation should succeed when filename valid", function () {
+        //     assert.isTrue(model.validateFilename("name.jpg"), "validation should succeed when filename valid");
+        // });
+    });
+});
