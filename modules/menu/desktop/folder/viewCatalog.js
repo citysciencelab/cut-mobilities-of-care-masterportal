e46d3d393c54ea5ca1b304f7e91f8a6475d7fdc9
sven_beyel <sven.beyel@gv.hamburg.de>
2018-11-19 16:45:37 +0100

    add deactivate tool in viewCatalog

--- modules/menu/desktop/folder/viewCatalog.js
+++ modules/menu/desktop/folder/viewCatalog.js
@@ -12,6 +12,7 @@ const FolderView = Backbone.View.extend({
         "click .rotate-pin": "unfixTree",
         "click .rotate-pin-back": "fixTree",
         "click .layer-selection-save": function () {
+            this.model.collection.setActiveToolToFalse(this.model);
             Radio.trigger("ModelList", "setModelAttributesById", "saveSelection", {isActive: true});
             // Schließt den Baum
             $(".nav li:first-child").removeClass("open");
