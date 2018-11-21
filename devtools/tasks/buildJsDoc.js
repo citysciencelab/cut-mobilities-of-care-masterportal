bb256fc083d7ad6e633f07891ef7ab03ca3e1043
jonas_weiter <jonas.weiter@gv.hamburg.de>
2018-11-21 07:48:56 +0100

    write node task to build jsDoc

---
+++ devtools/tasks/buildJsDoc.js
@@ -0,0 +1,15 @@
+const execute = require("child-process-promise").exec,
+    path = require("path"),
+    pathToJsDocCmd = path.resolve(__dirname, "../../node_modules/.bin/jsdoc");
+
+/**
+ * Removes Folder jsdoc recusively if existent.
+ * Then it generates the JsDoc using:
+ * Config: -c jsdoc-config.json
+ * Destination: -d
+ * Recursion: -r
+ */
+execute("rm -rf jsdoc")
+    .then(function () {
+        execute(pathToJsDocCmd + " -c jsdoc-config.json -d jsdoc -r");
+    });
