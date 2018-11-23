3004580e50802f94c178969125e28755113908e8
robertdietzel <robert.dietzel@gv.hamburg.de>
2018-11-23 12:25:59 +0100

    update; 3rd approver issue solved

--- modules/tools/filter/query/source/wfs.js
+++ modules/tools/filter/query/source/wfs.js
@@ -8,7 +8,7 @@ const WfsQueryModel = SourceModel.extend({
     /**
      * Sends a DescriptFeatureType Request for the Layer asscociated with this Query
      * and proceeds to build the datastructure including the snippets for this query
-     * @param  {string} layerObject - WFS Url
+     * @param  {string} layerObject - WFS LayerObject
      * @param  {string} url - WFS Url
      * @param  {string} featureType - WFS FeatureType
      * @param  {string} version - WFS Version
