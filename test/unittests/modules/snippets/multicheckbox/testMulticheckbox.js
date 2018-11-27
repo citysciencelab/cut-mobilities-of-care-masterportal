a10057b5cdf09a638ddab387680e9908611c43c6
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-11-27 09:19:56 +0100

    update multicheckbox Test

--- test/unittests/modules/snippets/multicheckbox/testMulticheckbox.js
+++ test/unittests/modules/snippets/multicheckbox/testMulticheckbox.js
@@ -1,27 +1,13 @@
 import {expect} from "chai";
 import Model from "@modules/snippets/multicheckbox/model.js";
 
-describe("getIconPath", function () {
+describe("Multicheckbox Model", function () {
     var icons = [
             "http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-oeffentlicher-raum.png",
-            "http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-sport.png",
-            "http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-gewerbe.png",
-            "http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-soziales.png",
-            "http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-verkehr.png",
-            "http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-soziales.png",
-            "http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-soziales.png",
-            "http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-wirtschaft.png",
-            "http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-wohnen.png"],
+            "http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-sport.png"],
         valueArray = [
             "Freiraum und Grün",
-            "Freizeit",
-            "Gewerbe",
-            "Kultur",
-            "Mobilität und Infrastruktur",
-            "Sonstiges",
-            "Soziales",
-            "Wirtschaft",
-            "Wohnen"],
+            "Freizeit"],
         model;
 
     before(function () {
@@ -29,22 +15,15 @@ describe("getIconPath", function () {
     });
 
     describe("getIconPath", function () {
-        it("Freiraum und Grün with icon 40px-oeffentlicher-raum.png", function () {
+        it("should return right Icon Path", function () {
             expect(model.getIconPath(icons, valueArray, "Freiraum und Grün")).to.deep.equal("http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-oeffentlicher-raum.png");
-        });
-        it("Freizeit with icon 40px-sport.png", function () {
             expect(model.getIconPath(icons, valueArray, "Freizeit")).to.deep.equal("http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-sport.png");
         });
-        it("Wirtschaft with icon 40px-wirtschaft.png", function () {
-            expect(model.getIconPath(icons, valueArray, "Wirtschaft")).to.deep.equal("http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-wirtschaft.png");
-        });
-        it("Wohnen with icon 40px-wohnen.png", function () {
-            expect(model.getIconPath(icons, valueArray, "Wohnen")).to.deep.equal("http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-wohnen.png");
-        });
-        it("Undefined value returns undefined", function () {
+
+        it("should return undefined for undefined value", function () {
             expect(model.getIconPath(icons, valueArray, undefined)).to.deep.equal(undefined);
         });
-        it("Value is random string is undefined", function () {
+        it("should return undefined for not found value", function () {
             expect(model.getIconPath(icons, valueArray, "*42+")).to.deep.equal(undefined);
         });
     });
