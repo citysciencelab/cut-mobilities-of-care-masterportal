3172bf3cd1e51f8a71c70533aa36da3856f1edb4
mbgvhh <markus.besenfelder@gv.hamburg.de>
2018-11-28 15:08:02 +0100

    add tests for multicheckboxmodel

--- test/unittests/modules/snippets/multicheckbox/testMulticheckbox.js
+++ test/unittests/modules/snippets/multicheckbox/testMulticheckbox.js
@@ -2,29 +2,120 @@ import {expect} from "chai";
 import Model from "@modules/snippets/multicheckbox/model.js";
 
 describe("Multicheckbox Model", function () {
-    var icons = [
-            "http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-oeffentlicher-raum.png",
-            "http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-sport.png"],
-        valueArray = [
-            "Freiraum und Grün",
-            "Freizeit"],
-        model;
+    var model;
 
     before(function () {
-        model = new Model();
+        model = new Model({
+            values: ["Freizeit", "Freiraum und Grün"]
+        });
     });
 
-    describe("getIconPath", function () {
-        it("should return right Icon Path", function () {
-            expect(model.getIconPath(icons, valueArray, "Freiraum und Grün")).to.deep.equal("http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-oeffentlicher-raum.png");
-            expect(model.getIconPath(icons, valueArray, "Freizeit")).to.deep.equal("http://geoportal-hamburg.de/lgv-beteiligung/icons/40px-sport.png");
+
+    describe("updateSelectedValues", function () {
+        var initialValue, updatedValue;
+
+        before(function () {
+            initialValue = model.get("valuesCollection").models.filter(function (value) {
+                return value.get("value") === "Freizeit";
+            })[0].get("isSelected");
         });
 
-        it("should return undefined for undefined value", function () {
-            expect(model.getIconPath(icons, valueArray, undefined)).to.deep.equal(undefined);
+        it("should update Value", function () {
+
+            model.updateSelectedValues("Freizeit", false);
+            updatedValue = model.get("valuesCollection").models.filter(function (value) {
+                return value.get("value") === "Freizeit";
+            })[0].get("isSelected");
+
+
+            expect(initialValue).to.be.true;
+            expect(updatedValue).to.be.false;
         });
-        it("should return undefined for not found value", function () {
-            expect(model.getIconPath(icons, valueArray, "*42+")).to.deep.equal(undefined);
+    });
+
+    describe("resetValues", function () {
+        var initialValue, updatedValue;
+
+        before(function () {
+            var valueModel = model.get("valuesCollection").models.filter(function (value) {
+                return value.get("value") === "Freizeit";
+            });
+
+            valueModel[0].set("isSelectable", false);
+            initialValue = valueModel[0].get("isSelectable");
+        });
+
+        it("should reset Value", function () {
+            model.resetValues();
+            updatedValue = model.get("valuesCollection").models.filter(function (value) {
+                return value.get("value") === "Freizeit";
+            })[0].get("isSelectable");
+
+            expect(initialValue).to.be.false;
+            expect(updatedValue).to.be.true;
+        });
+    });
+
+    describe("addValueModels", function () {
+        var initialValue, updatedValue;
+
+        before(function () {
+            var countTest1, countTest2;
+
+            countTest1 = model.get("valuesCollection").models.filter(function (value) {
+                return value.get("value") === "Test1";
+            }).length;
+            countTest2 = model.get("valuesCollection").models.filter(function (value) {
+                return value.get("value") === "Test2";
+            }).length;
+
+            initialValue = countTest1 + countTest2;
+        });
+
+        it("should add Value Models", function () {
+            var countTest1, countTest2;
+
+            model.addValueModels(["Test1", "Test2"]);
+
+            countTest1 = model.get("valuesCollection").models.filter(function (value) {
+                return value.get("value") === "Test1";
+            }).length;
+            countTest2 = model.get("valuesCollection").models.filter(function (value) {
+                return value.get("value") === "Test2";
+            }).length;
+
+            updatedValue = countTest1 + countTest2;
+
+            expect(initialValue).to.equal(0);
+            expect(updatedValue).to.equal(2);
+        });
+    });
+
+    describe("updateSelectableValues", function () {
+        var initialValue, updatedValue;
+
+        before(function () {
+            var valueModel = model.get("valuesCollection").models.filter(function (value) {
+                return value.get("value") === "Freizeit";
+            });
+
+            valueModel[0].set("isSelected", false);
+
+            initialValue = valueModel[0].get("isSelectable");
+        });
+
+        it("should update Selectable Values", function () {
+            var valueModel;
+
+            model.updateSelectableValues("Freizeit");
+
+            valueModel = model.get("valuesCollection").models.filter(function (value) {
+                return value.get("value") === "Freizeit";
+            });
+            updatedValue = valueModel[0].get("isSelectable");
+
+            expect(initialValue).to.be.true;
+            expect(updatedValue).to.be.false;
         });
     });
 });
