988a017355499a1aed0221f2e6ef861bd9004e9a
Becker <christa.becker@gv.hamburg.de>
2018-12-20 09:34:48 +0100

    update Fehlerabfangung verbessert

--- modules/tools/gfi/themes/dipas/model.js
+++ modules/tools/gfi/themes/dipas/model.js
@@ -9,15 +9,15 @@ const DipasTheme = Theme.extend({
 
     getIconPath: function (value) {
         var styleModel = Radio.request("StyleList", "returnModelById", this.get("id")),
-            valueStyle,
-            iconPath;
+            valueStyle = null,
+            iconPath = "http://geoportal-hamburg.de/lgv-beteiligung/icons/einzelmarker_dunkel.png";
 
-        if (styleModel) {
+        if (styleModel && styleModel.has("styleFieldValues")) {
             valueStyle = styleModel.get("styleFieldValues").filter(function (styleFieldValue) {
                 return styleFieldValue.styleFieldValue === value;
             });
         }
-        if (valueStyle) {
+        if (valueStyle && valueStyle.length > 0) {
             iconPath = styleModel.get("imagePath") + valueStyle[0].imageName;
         }
         this.setIconPath(iconPath);
