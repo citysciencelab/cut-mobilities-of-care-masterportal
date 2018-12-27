0dc7039b6c9d02c7bf595068fb5ba72af0cfa2c2
jonas_weiter <jonas.weiter@gv.hamburg.de>
2018-12-27 16:06:36 +0100

    update zwischenstand clickcounter

--- modules/ClickCounter/model.js
+++ modules/ClickCounter/model.js
@@ -1,48 +1,64 @@
 /**
- * @description Module to count Clicks
- * @memberof ClickCounter
- * @extends Backbone.Model
+ * @namespace ClickCounter
  */
-const ClickCounter = Backbone.Model.extend({
-    defaults: {
-        countframeid: _.uniqueId("countframe"),
-        usedURL: "", // beutzte iFrame-URL, kann desktop oder mobile sein
-        desktopURL: "", // URL die verwendet wird, wenn nicht mobile
-        mobileURL: "" // URL die verwendet wird, wenn mobile
-    },
-    initialize: function (desktopURL, mobileURL) {
-        var isMobile = Radio.request("Util", "isViewMobile"),
-            usedURL = isMobile === true ? mobileURL : desktopURL;
+const ClickCounterModel = Backbone.Model.extend(
+/** @lends ClickCounterModel.prototype */
+    {
+        /**
+        * @class ClickCounterModel
+        * @memberof ClickCounter
+        * @extends Backbone.Model
+        * @constructs
+        * @param {String} countframeid [description]
+        * @param {String} usedURL [description]
+        * @param {String} desktopURL [description]
+        * @param {String} mobileURL [description]
+        */
+        defaults: {
+            countframeid: _.uniqueId("countframe"),
+            usedURL: "", // beutzte iFrame-URL, kann desktop oder mobile sein
+            desktopURL: "", // URL die verwendet wird, wenn nicht mobile
+            mobileURL: "" // URL die verwendet wird, wenn mobile
+        },
+        /**
+         * Initialize function for model
+         * @param  {String} desktopURL [description]
+         * @param  {String} mobileURL  [description]
+         * @return {void}
+         */
+        initialize: function (desktopURL, mobileURL) {
+            var isMobile = Radio.request("Util", "isViewMobile"),
+                usedURL = isMobile === true ? mobileURL : desktopURL;
 
-        this.set("desktopURL", desktopURL);
+            this.set("desktopURL", desktopURL);
 
-        this.set("mobileURL", mobileURL);
+            this.set("mobileURL", mobileURL);
 
-        this.set("usedURL", usedURL);
+            this.set("usedURL", usedURL);
 
-        this.listenTo(Radio.channel("Util"), {
-            "isViewMobileChanged": this.updateURL
-        });
-        // Erzeuge iFrame
-        $("<iframe style='display:none' src='" + this.get("usedURL") + "' id='" + this.get("countframeid") + "' width='0' height='0' frameborder='0'/>").appendTo("body");
-    },
-    updateURL: function (isMobile) {
-        var usedURL;
+            this.listenTo(Radio.channel("Util"), {
+                "isViewMobileChanged": this.updateURL
+            });
+            // Erzeuge iFrame
+            $("<iframe style='display:none' src='" + this.get("usedURL") + "' id='" + this.get("countframeid") + "' width='0' height='0' frameborder='0'/>").appendTo("body");
+        },
+        updateURL: function (isMobile) {
+            var usedURL;
 
-        if (isMobile) {
-            usedURL = this.get("mobileURL");
+            if (isMobile) {
+                usedURL = this.get("mobileURL");
+            }
+            else {
+                usedURL = this.get("desktopURL");
+            }
+            this.set("usedURL", usedURL);
+        },
+        refreshIframe: function () {
+            var id = this.get("countframeid"),
+                url = this.get("usedURL");
+
+            $("#" + id).attr("src", url);
         }
-        else {
-            usedURL = this.get("desktopURL");
-        }
-        this.set("usedURL", usedURL);
-    },
-    refreshIframe: function () {
-        var id = this.get("countframeid"),
-            url = this.get("usedURL");
+    });
 
-        $("#" + id).attr("src", url);
-    }
-});
-
-export default ClickCounter;
+export default ClickCounterModel;
--- modules/ClickCounter/view.js
+++ modules/ClickCounter/view.js
@@ -1,38 +1,49 @@
-/**
- * @description Module to count Clicks
- * @memberof ClickCounterView
- * @extends Backbone.View
- */
 import ClickCounterModel from "./model";
 
-const ClickCounterView = Backbone.View.extend({
-    initialize: function (desktopURL, mobileURL) {
-        var channel = Radio.channel("ClickCounter");
+const ClickCounterView = Backbone.View.extend(
+    /** @lends ClickCounterView.prototype */
+    {
+        /**
+        * @class ClickCounterView
+        * @memberof ClickCounter
+        * @extends Backbone.View
+        * @constructs
+        * @param {String} desktopURL [description]
+        * @param {String} mobileURL [description]
+        */
+        /**
+         * Initialize function for view
+         * @param  {String} desktopURL [description]
+         * @param  {String} mobileURL  [description]
+         * @return {void}
+         */
+        initialize2: function (desktopURL, mobileURL) {
+            var channel = Radio.channel("ClickCounter");
 
-        this.model = new ClickCounterModel(desktopURL, mobileURL);
+            this.model = new ClickCounterModel(desktopURL, mobileURL);
 
-        channel.on({
-            "toolChanged": this.registerClick,
-            "calcRoute": this.registerClick,
-            "zoomChanged": this.registerClick,
-            "layerVisibleChanged": this.registerClick,
-            "gfi": this.registerClick
-        }, this);
+            channel.on({
+                "toolChanged": this.registerClick,
+                "calcRoute": this.registerClick,
+                "zoomChanged": this.registerClick,
+                "layerVisibleChanged": this.registerClick,
+                "gfi": this.registerClick
+            }, this);
 
-        // fired beim Öffnen der Seite
-        this.registerClick();
-    },
-    registerLayerEvent: function (layertree) {
-        // fired beim LayerChange, Info-Button, Einstellungen auf dem Layertree
-        if (layertree.length > 0) {
-            layertree.click(function () {
-                this.registerClick();
-            }.bind(this));
+            // fired beim Öffnen der Seite
+            this.registerClick();
+        },
+        registerLayerEvent: function (layertree) {
+            // fired beim LayerChange, Info-Button, Einstellungen auf dem Layertree
+            if (layertree.length > 0) {
+                layertree.click(function () {
+                    this.registerClick();
+                }.bind(this));
+            }
+        },
+        registerClick: function () {
+            this.model.refreshIframe();
         }
-    },
-    registerClick: function () {
-        this.model.refreshIframe();
-    }
-});
+    });
 
 export default ClickCounterView;
