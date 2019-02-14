3e85b5567fe52c1f2ca0413e6a0ce040a1d50574
Maria <maria.dauwalter@hcu-hamburg.de>
2019-02-14 09:48:01 +0100

    delete console.log in ListViewLight

--- modules/menu/desktop/listViewLight.js
+++ modules/menu/desktop/listViewLight.js
@@ -4,7 +4,6 @@ import DesktopLayerViewLight from "./layer/viewLight";
 
 const LightMenu = listView.extend({
     initialize: function () {
-        console.log(Radio.request("Parser", "getPortalConfig").searchBar.gdi);
         this.collection = Radio.request("ModelList", "getCollection");
         Radio.on("Autostart", "startModul", this.startModul, this);
         this.listenTo(this.collection, {
