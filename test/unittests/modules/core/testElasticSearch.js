7185b993d3d1013f845b1ad59b1cfd451de43ec3
Timmermann <TimmerAn@fhhnet.stadt.hamburg.de>
2019-02-19 14:49:31 +0100

    elastic search tests modified to work with changes in model

--- test/unittests/modules/core/testElasticSearch.js
+++ test/unittests/modules/core/testElasticSearch.js
@@ -17,42 +17,42 @@ describe("check Elastic Search Helper Functions", function () {
         expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"})).to.be.an("string");
     });
     it("prepareSearchBody should return stringyfied JSON", function () {
-        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"})).to.be.equal("{\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
+        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"}, undefined, 10000)).to.be.equal("{\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
     });
     it("prepareSearchBody should have sort order", function () {
         model.setSorting("_score", "desc");
-        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"})).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
+        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"}, model.get("sorting"), 10000)).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
     });
     it("setSorting should ignore null key", function () {
         model.setSorting(null, "desc");
-        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"})).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
+        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"}, model.get("sorting"), 10000)).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
     });
     it("setSorting should ignore undefined key", function () {
         model.setSorting(undefined, "desc");
-        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"})).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
+        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"}, model.get("sorting"), 10000)).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
     });
     it("setSorting should ignore null value", function () {
         model.setSorting("key", null);
-        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"})).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
+        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"}, model.get("sorting"), 10000)).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
     });
     it("setSorting should ignore undefined value", function () {
         model.setSorting("key", undefined);
-        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"})).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
+        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"}, model.get("sorting"), 10000)).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
     });
     it("setSize should ignore non-numeric value", function () {
         model.setSize("abc");
-        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"})).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
+        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"}, model.get("sorting"), 10000)).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
     });
     it("setSize should ignore null value", function () {
         model.setSize(null);
-        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"})).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
+        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"}, model.get("sorting"), 10000)).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
     });
     it("setSize should ignore undefined value", function () {
         model.setSize(undefined);
-        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"})).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
+        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"}, model.get("sorting"), 10000)).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":10000,\"query\":{\"query\":\"meine Anfrage\"}}");
     });
     it("setSize should change size to 500", function () {
         model.setSize(500);
-        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"})).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":500,\"query\":{\"query\":\"meine Anfrage\"}}");
+        expect(ElasticSearch.prepareSearchBody({query: "meine Anfrage"}, {"_score": "desc"}, model.get("size"))).to.be.equal("{\"sort\":{\"_score\":\"desc\"},\"from\":0,\"size\":500,\"query\":{\"query\":\"meine Anfrage\"}}");
     });
 });
