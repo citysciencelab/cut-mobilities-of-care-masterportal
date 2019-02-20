02a6a9798c9ae4f157bd3e053526d8bd4d916d5e
Maria <maria.dauwalter@hcu-hamburg.de>
2019-02-20 13:43:31 +0100

    update View - auskommentierte Zeilen wurden gelöscht

--- modules/menu/mobile/layer/viewLight.js
+++ modules/menu/mobile/layer/viewLight.js
@@ -77,9 +77,6 @@ const LayerView = Backbone.View.extend({
             this.$el.find(".item-settings").hide();
             this.$el.find(".item-settings").slideDown();
         }
-        // if (Radio.request("Parser", "getPortalConfig").menu.tree.layersRemoveable === false) {
-        //     this.$el.find(".remove-layer").remove();
-        // }
     },
 
     toggleIsSelected: function () {
