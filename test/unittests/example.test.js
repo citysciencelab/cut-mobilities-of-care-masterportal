ff2c49ea584ca060cb00dc4ea03f49f0257b969d
Weiter <WeiterJo-Admin@fhhnet.stadt.hamburg.de>
2019-03-25 10:01:31 +0100

    update example tests

--- test/unittests/example.test.js
+++ test/unittests/example.test.js
@@ -1,10 +1,34 @@
 import {expect} from "chai";
 
-describe("example", function () {
-
-    describe("example1", function () {
-        it("true is true", function () {
-            expect(true).to.be.true;
+describe("Examples", function () {
+    describe("Types", function () {
+        describe("Boolean", function () {
+            it("True", function () {
+                expect(true).to.be.true;
+            });
+            it("False", function () {
+                expect(false).to.be.false;
+            });
+        });
+        describe("Number", function () {
+            it("1", function () {
+                expect(1).to.be.a("number");
+            });
+        });
+        describe("String", function () {
+            it("FooBar", function () {
+                expect("FooBar").to.be.a("string");
+            });
+        });
+        describe("Object", function () {
+            it("Object", function () {
+                expect({}).to.be.an("object");
+            });
+        });
+        describe("Array", function () {
+            it("Array", function () {
+                expect([]).to.be.an("array");
+            });
         });
     });
 });
