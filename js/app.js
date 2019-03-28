6a03171b2fbfe0bbe41e68b6c5919b5f465efd7d
Laura Meierkort <meierkort@grit.de>
2019-03-28 13:36:14 +0100

    reactivate custommodel

--- js/app.js
+++ js/app.js
@@ -389,9 +389,8 @@ function loadApp () {
     new HighlightFeature();
 
     // Variable CUSTOMMODULE wird im webpack.DefinePlugin gesetzt
-    /*
     if (CUSTOMMODULE !== "") {
-        return import(/* webpackMode: "eager" */ /* CUSTOMMODULE)
+        return import(/* webpackMode: "eager" */ CUSTOMMODULE)
         .then(module => {
             new module.default;
         })
@@ -399,7 +398,7 @@ function loadApp () {
             console.error(error);
             Radio.trigger("Alert", "alert", "Entschuldigung, diese Anwendung konnte nicht vollständig geladen werden. Bitte wenden sie sich an den Administrator.");
         });
-    }*/
+    }
 
     Radio.trigger("Util", "hideLoader");
 }
