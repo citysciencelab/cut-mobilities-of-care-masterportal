b556065a4f3ff5ffa2e16598047bd77d9d39dd23
Maria <maria.dauwalter@hcu-hamburg.de>
2019-04-15 13:13:24 +0200

    update style und view, select box wird in js defeniert

--- modules/tools/measure/view.js
+++ modules/tools/measure/view.js
@@ -4,6 +4,7 @@ import TableTemplate from "text-loader!./table/template.html";
 const MeasureView = Backbone.View.extend({
     events: {
         "change select#geomField": "setGeometryType",
+        "change select.styledSelect": "setGeometryType",
         "change select#unitField": "setUnit",
         "click button": "deleteFeatures",
         "click .form-horizontal > .form-group-sm > .col-sm-12 > .glyphicon-question-sign": function () {
@@ -19,14 +20,78 @@ const MeasureView = Backbone.View.extend({
         }
     },
     render: function (model, value) {
-        var template;
+        var template, i;
 
         if (value) {
             template = Radio.request("Util", "getUiStyle") === "TABLE" ? _.template(TableTemplate) : _.template(DefaultTemplate);
-
             this.setElement(document.getElementsByClassName("win-body")[0]);
             this.$el.html(template(model.toJSON()));
             this.delegateEvents();
+            if (Radio.request("Util", "getUiStyle") === "TABLE") {
+                // Iterate over each select element
+                $("select").each(function () {
+
+                    // Cache the number of options
+                    var $this = $(this),
+                        numberOfOptions = $(this).children("option").length;
+
+                    // Hides the select element
+                    $this.addClass("s-hidden");
+
+                    // Wrap the select element in a div
+                    $this.wrap("<div class='select'></div>");
+
+                    // Insert a styled div to sit over the top of the hidden select element
+                    $this.after("<select class='styledSelect'></select>");
+                    console.log($this);
+
+                    // Cache the styled div
+                    var $styledSelect = $this.next("select.styledSelect");
+
+                    // Show the first select option in the styled div
+                    $styledSelect.text($this.children("option").eq(0).text());
+
+                    // Insert an unordered list after the styled div and also cache the list
+                    var $list = $("<ul />", {
+                        "class": "options"
+                    }).insertAfter($styledSelect);
+
+                    // Insert a list item into the unordered list for each select option
+                    for (i = 0; i < numberOfOptions; i++) {
+                        $("<li />", {
+                            text: $this.children("option").eq(i).text(),
+                            rel: $this.children("option").eq(i).val()
+                        }).appendTo($list);
+                    }
+
+                    // Cache the list items
+                    var $listItems = $list.children("li");
+
+                    // Show the unordered list when the styled div is clicked (also hides it if the div is clicked again)
+                    $styledSelect.click(function (e) {
+                        e.stopPropagation();
+                        $("div.styledSelect.active").each(function () {
+                            $(this).removeClass("active").next("ul.options").hide();
+                        });
+                        $(this).toggleClass("active").next("ul.options").toggle();
+                    });
+
+                    // Hides the unordered list when a list item is clicked and updates the styled div to show the selected list item
+                    // Updates the select element to have the value of the equivalent option
+                    $listItems.click(function (e) {
+                        e.stopPropagation();
+                        $styledSelect.text($(this).text()).removeClass("active");
+                        $this.val($(this).attr("rel"));
+                        $list.hide();
+                        /* alert($this.val()); Uncomment this for demonstration! */
+                    });
+                    // Hides the unordered list when clicking outside of it
+                    $(document).click(function () {
+                        $styledSelect.removeClass("active");
+                        $list.hide();
+                    });
+                });
+            }
         }
         else {
             this.undelegateEvents();
@@ -38,6 +103,7 @@ const MeasureView = Backbone.View.extend({
 
     setGeometryType: function (evt) {
         this.model.setGeometryType(evt.target.value);
+        console.log("ich bin da");
     },
 
     setUnit: function (evt) {
