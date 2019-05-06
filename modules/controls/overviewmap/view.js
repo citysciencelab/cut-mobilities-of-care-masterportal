a176b0b2ae74ccca535c1c59fddabdfd3162d059
Maria <maria.dauwalter@hcu-hamburg.de>
2019-05-06 09:50:37 +0200

    update style and view for new mini Map tool

--- modules/controls/overviewmap/view.js
+++ modules/controls/overviewmap/view.js
@@ -1,9 +1,10 @@
 import OverviewMapModel from "./model";
 import template from "text-loader!./template.html";
 
-const OverviewMapView = Backbone.View.extend(/**@lends OverviewMapView.prototype */{
+const OverviewMapView = Backbone.View.extend(/** @lends OverviewMapView.prototype */{
     events: {
-        "click .glyphicon": "toggle"
+        "click .glyphicon": "toggle",
+        "click div#mini-map": "toggle"
     },
     /**
      * @class OverviewMapView
@@ -12,9 +13,30 @@ const OverviewMapView = Backbone.View.extend(/**@lends OverviewMapView.prototype
      * @constructs
      */
     initialize: function () {
-        this.render();
+        var channel = Radio.channel("Map"),
+            style = Radio.request("Util", "getUiStyle");
+
+        channel.on({
+            "change": this.change
+        }, this);
+        if (style === "DEFAULT") {
+            this.template = _.template(template);
+            this.render();
+        }
+        else if (style === "TABLE") {
+            this.listenTo(Radio.channel("MenuLoader"), {
+                "ready": function () {
+                    this.setElement("#table-tools-menu");
+                    this.renderToToolbar();
+                }
+            });
+            this.setElement("#table-tools-menu");
+            this.renderToToolbar();
+        }
+        // this.render();
         this.model = new OverviewMapModel();
     },
+    tabletemplate: _.template("<div id='mini-map' class='table-tool'><a href='#'><span class='glyphicon glyphicon-globe'></span><span id='mini-map_title'><%=ansicht %></span></a> </div>"),
 
     /**
      * Render function
@@ -22,6 +44,15 @@ const OverviewMapView = Backbone.View.extend(/**@lends OverviewMapView.prototype
      */
     render: function () {
         this.$el.html(this.template());
+        return this;
+    },
+    /**
+     * Render Function
+     * @fires Map#RadioRequestMap
+     * @returns {ButtonMapView} - Returns itself
+     */
+    renderToToolbar: function () {
+        this.$el.append(this.tabletemplate({ansicht: "Mini-Map ausschalten"}));
         return this;
     },
     id: "overviewmap",
@@ -37,13 +68,24 @@ const OverviewMapView = Backbone.View.extend(/**@lends OverviewMapView.prototype
      * @returns {void}
      */
     toggle: function () {
+        var ArrayObj = Array.from(document.getElementsByClassName("ol-custom-overviewmap"));
+
         if (this.$(".overviewmap-button > .glyphicon-globe").attr("title") === "Übersichtskarte ausblenden") {
             this.$(".ol-custom-overviewmap").hide("slow");
             this.$(".overviewmap-button > .glyphicon-globe").attr("title", "Übersichtskarte einblenden");
         }
-        else {
+        else if (this.$(".overviewmap-button > .glyphicon-globe").attr("title") === "Übersichtskarte einblenden") {
             this.$(".overviewmap-button > .glyphicon-globe").attr("title", "Übersichtskarte ausblenden");
             this.$(".ol-custom-overviewmap").show("slow");
+        }
+        else if (document.getElementById("mini-map_title").innerText === "Mini-Map ausschalten") {
+            ArrayObj.map(element => element.classList.add("hidden"));
+            document.getElementById("mini-map_title").innerText = "Mini-Map einschalten";
+        }
+        else if (document.getElementById("mini-map_title").innerText === "Mini-Map einschalten") {
+            document.getElementById("mini-map_title").innerText = "Mini-Map ausschalten";
+            ArrayObj.map(element => element.classList.remove("hidden"));
+
         }
     }
 });
