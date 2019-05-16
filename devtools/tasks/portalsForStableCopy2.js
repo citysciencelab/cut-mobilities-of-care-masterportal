09f7c0f959363a516552aa0e74109b6886cc6871
felixnagel87 <felix.nagel@gv.hamburg.de>
2019-05-16 12:26:35 +0200

    update: New buildPortalForStable Script - including Custommodules config and improved regex

---
+++ devtools/tasks/portalsForStableCopy2.js
@@ -0,0 +1,98 @@
+const fs = require("fs-extra"),
+    path = require("path"),
+    _ = require("underscore"),
+    stableVersion = require("../../package.json").version.replace(/\./g, "_"),
+    portalsForStableReplace = require("./portalsForStableReplace2"),
+    execute = require("child-process-promise").exec,
+    replaceStrings = require("./replace2");
+
+var
+    conf = {
+        sourceFolder: "portalconfigs/",
+        sourceFiles: ["config.js", "config.json", "index.html"],
+        targetFolder: "stablePortale/",
+        masterCodeFolder: "stablePortale/Mastercode/"+stableVersion+"/",
+        tempPortalFolder: "dist/build/",
+        basicPortalFolder: "dist/Basic/",
+        basicPortalFoldersToCopy: ["js/", "css/"],
+        customModules: {
+            sga: "gfionaddress/view"
+        },
+        environment: "Internet"
+    };
+
+function deleteStablePortalsFolder () {
+    // hardcoded to not accidently remove something unintended
+    fs.remove("stablePortale/").then(() => {
+        createMasterCodeFolder();
+    });
+}
+
+function createMasterCodeFolder () {
+    fs.mkdirs(conf.masterCodeFolder).then(() => {
+        conf.basicPortalFoldersToCopy.forEach(basicFolderToCopy => {
+            fs.mkdirs(conf.masterCodeFolder+basicFolderToCopy).then(() => {
+                fs.copy(conf.basicPortalFolder+basicFolderToCopy, conf.masterCodeFolder+basicFolderToCopy).then(() => {
+                    createStablePortalsFolder();
+                });
+            }).catch((error) => {
+                console.error(error);
+            });
+        });
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+
+function copyPortal (portalName) {
+    portalName = portalName+"/";
+    conf.sourceFiles.forEach((sourceFile) => {
+        if (!fs.existsSync(conf.sourceFolder+portalName+sourceFile)) {
+            return;
+        }
+        fs.copy(
+            conf.sourceFolder+portalName+sourceFile,
+            conf.targetFolder+portalName+sourceFile
+        ).then(() => {
+            portalsForStableReplace(conf.targetFolder+portalName+sourceFile, stableVersion);
+        }).catch((error) => {
+            console.error(error);
+        });
+    });
+}
+
+function buildCustomModulesPortal (portalName) {
+    var command = "webpack --config devtools/webpack.prod.js --CUSTOMMODULE ../"+conf.sourceFolder+portalName+"/"+conf.customModules[portalName];
+    execute(command).then(function (result) {
+        fs.copy(conf.tempPortalFolder, conf.targetFolder+portalName+"/").then(() => {
+            fs.copy(conf.sourceFolder+portalName+"/", conf.targetFolder+portalName+"/").then(() => {
+                replaceStrings(conf.environment, conf.targetFolder+portalName);
+            });
+        });
+    }).catch((error) => {
+        console.error(error);
+    });
+
+}
+
+function createStablePortalsFolder () {
+    fs.readdir(conf.sourceFolder, (error, portalNames) => {
+        if (!portalNames.length) {
+            console.error("No source folders available");
+        }
+        portalNames.forEach((portalName) => {
+            fs.mkdirs(conf.targetFolder+portalName).then(() => {
+                if (conf.customModules[portalName] === undefined) {
+                    copyPortal(portalName);
+                }
+                else {
+                    buildCustomModulesPortal(portalName);
+                }
+            }).catch((error) => {
+                console.error(error);
+            });
+        });
+    });
+}
+
+deleteStablePortalsFolder();
