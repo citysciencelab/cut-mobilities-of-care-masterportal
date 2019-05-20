64de099cddfd18ff2d65d93aa6a43de521336595
felixnagel87 <felix.nagel@gv.hamburg.de>
2019-05-20 13:30:16 +0200

    update: introduced whitelist for portals, updated logging messages

--- devtools/tasks/portalsForStableReplace2.js
+++ devtools/tasks/portalsForStableReplace2.js
@@ -3,13 +3,13 @@ var replace = require("replace-in-file");
 module.exports = function (destination, stableVersion) {
     var replacements = [{
         "files": destination,
-        "from": /\/lgv-config/g,
+        "from": /\/*(\.+\/)*lgv-config/g,
         "to": "../../lgv-config"
     },
     {
         "files": destination,
-        "from": /\.\.\/\.\.\/build\//g,
-        "to": "../Mastercode/" + stableVersion + "/"
+        "from": /\/*(\.+\/)*build/g,
+        "to": "../Mastercode/"+stableVersion
     }];
 
     replacements.forEach(function (replacement) {
