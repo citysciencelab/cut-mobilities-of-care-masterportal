78a3158aaaad2f35b4370947328158b3901d6122
Feng Feng <feng.feng@gv.hamburg.de>
2019-05-21 11:57:04 +0200

    COC-126 Js doc and comments in english finished

--- modules/tools/draw/model.js
+++ modules/tools/draw/model.js
@@ -314,6 +314,7 @@ const DrawTool = Tool.extend({
     },
     /**
      * finishes the draw interaction via Radio
+     * @param {String} cursor check and receive the parameter from Cockpit
      * @returns {void}
      */
     cancelDrawWithoutGUI: function (cursor) {
@@ -321,7 +322,7 @@ const DrawTool = Tool.extend({
         this.deactivateSelectInteraction();
         this.deactivateModifyInteraction();
         this.resetModule();
-        // GFI wieder einschalten nach dem Zeichnen
+        // Turn GFI on again after drawing
         this.setIsActive(false);
         if (cursor !== undefined && cursor.cursor) {
             $("#map").removeClass("no-cursor");
