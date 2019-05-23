fd564b9a60052919f82948a68fb1cc0cc661b075
FHHNET\WeiterJo <jonas.weiter@gv.hamburg.de>
2019-05-23 10:08:17 +0200

    update basic portal

--- portal/basic/config.js
+++ portal/basic/config.js
@@ -1,27 +1,27 @@
 const Config = {
-	wfsImgPath: "/lgv-config/img/",
-	namedProjections: [
-		["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
-	],
-	footer: {
-		urls: [
-			{
-				"bezeichnung": "Kartographie und Gestaltung: ",
-				"url": "https://www.geoinfo.hamburg.de/",
-				"alias": "Landesbetrieb Geoinformation und Vermessung",
-				"alias_mobil": "LGV"
-			}
-		]
-	},
-	quickHelp: {
-		imgPath: "/lgv-config/img/"
-	},
-	layerConf: "/lgv-config/services-internet.json",
-	restConf: "/lgv-config/rest-services-internet.json",
-	styleConf: "/lgv-config/style_v2.json",
-	scaleLine: true,
-	mouseHover: {
-		numFeaturesToShow: 2,
-		infoText: "(weitere Objekte. Bitte zoomen.)"
-	}
+    wfsImgPath: "/lgv-config/img/",
+    namedProjections: [
+        ["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
+    ],
+    footer: {
+        urls: [
+            {
+                "bezeichnung": "Kartographie und Gestaltung: ",
+                "url": "https://www.geoinfo.hamburg.de/",
+                "alias": "Landesbetrieb Geoinformation und Vermessung",
+                "alias_mobil": "LGV"
+            }
+        ]
+    },
+    quickHelp: {
+        imgPath: "/lgv-config/img/"
+    },
+    layerConf: "/lgv-config/services-internet.json",
+    restConf: "/lgv-config/rest-services-internet.json",
+    styleConf: "/lgv-config/style_v2.json",
+    scaleLine: true,
+    mouseHover: {
+        numFeaturesToShow: 2,
+        infoText: "(weitere Objekte. Bitte zoomen.)"
+    }
 };
