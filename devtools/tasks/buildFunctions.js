667ca42e84d1a879fac916a397ae91aacb040a31
felixnagel87 <felix.nagel@gv.hamburg.de>
2019-07-02 16:39:51 +0200

    refactor: step 1

--- devtools/tasks/buildFunctions.js
+++ devtools/tasks/buildFunctions.js
@@ -1,9 +1,9 @@
 const fs = require("fs-extra"),
-    replaceStrings = require("./replace"),
     execute = require("child-process-promise").exec;
 
-function copyFiles (source, destination, environment) {
+function copyFiles (source, destination) {
     fs.copy(source, destination).then(() => {
+<<<<<<< HEAD
         console.warn("NOTICE: Successfully Copied '" + source + "' to '" + destination + "' !");
         fs.copy("./dist/build", destination).then(() => {
             fs.remove("./dist/build").catch(err => console.error(err));
@@ -11,15 +11,30 @@ function copyFiles (source, destination, environment) {
             replaceStrings(environment, destination);
         }).catch(err => console.error(err));
     }).catch(err => console.error(err));
+=======
+        console.warn("NOTE: Successfully Copied \"" + source + "\" to \"" + destination + "\".");
+        fs.copy("./dist/build", destination).then(() => {
+            fs.remove("./dist/build").catch(error => console.error(error));
+            console.warn("NOTE: Successfully moved \"./dist/build\" to \"" + destination + "\".");
+        }).catch(error => console.error(error));
+    }).catch(error => console.error(error));
+>>>>>>> refactor: step 1
 }
 
 function removeFiles (answers) {
-    var destination = "dist/" + answers.portalName,
+    var
+        portalName = answers.portalPath.split("/").pop(),
+        destination = "dist/" + portalName,
         source = "./" + answers.portalPath;
 
     fs.remove(destination).then(() => {
+<<<<<<< HEAD
         console.warn("NOTICE: Successfully deleted '" + destination + "' directory");
         copyFiles(source, destination, answers.environment);
+=======
+        console.warn("NOTE: Successfully deleted \"" + destination + "\" directory.");
+        copyFiles(source, destination);
+>>>>>>> refactor: step 1
     });
 }
 
