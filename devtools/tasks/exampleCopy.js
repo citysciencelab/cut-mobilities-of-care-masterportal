a586f264a2387312d3766bc02969dac1d6ee7d8b
sven_beyel <sven.beyel@gv.hamburg.de>
2019-07-03 09:27:51 +0200

    update buildExamples without lgv-config

--- devtools/tasks/exampleCopy.js
+++ devtools/tasks/exampleCopy.js
@@ -1,109 +1,63 @@
 const fs = require("fs-extra"),
-    _ = require("underscore"),
-    exampleReplace = require("./exampleReplace"),
+    zipAFolder = require("zip-a-folder"),
     packageJSON = require("../../package.json");
 
-function createFolders (folder, name) {
-    fs.mkdir(name).then(() => {
-        if (_.has(folder, "subFolders")) {
-            folder.subFolders.forEach(function (subFolder) {
-                createFolders(subFolder, name + "/" + subFolder.name);
-            });
-        }
-        if (_.has(folder, "files")) {
-            folder.files.forEach(function (file) {
-                fs.copy(folder.source + "/" + file, name + "/" + file)
-                    .then(() => {
-                        if (file === "config.js" || file === "config.json" || file === "index.html") {
-                            exampleReplace(name + "/" + file);
-                        }
-                    })
-                    .catch(err => console.error(err));
-            });
-        }
+/**
+ * zips a given folder and deletes it afterwards
+ * @param {String} folder folder to be zipped
+ * @returns {void}
+ */
+function zipFolder (folder) {
+    zipAFolder.zip(folder, folder + ".zip").then(() => {
+        fs.remove(folder).catch(error => console.error(error));
     });
 }
 
-function buildFolderStructure (structure) {
-    structure.forEach(function (folder) {
-        fs.remove(folder.name).then(() => {
-            createFolders(folder, folder.name);
+/**
+ * creates the folder which contains the example portal
+ * @param {String} folder folder to create
+ * @param {Object} portal for the exmaples
+ * @returns {void}
+ */
+function createFolders (folder, portal) {
+    const destinationFolder = folder + "/" + portal.name;
+
+    fs.mkdir(folder).then(() => {
+        fs.mkdir(destinationFolder).then(() => {
+            fs.copy(portal.source, destinationFolder).then(() => {
+                zipFolder(folder);
+            }).catch(err => console.error(err));
+        }).catch(err => console.error(err));
+    }).catch(err => console.error(err));
+}
+
+/**
+ * Deletes the folders if they already exist.
+ * @param {String[]} folders folders to create
+ * @param {Object} portal portal for the exmaples
+ * @returns {void}
+ */
+function removeFolders (folders, portal) {
+    folders.forEach(function (folder) {
+        fs.remove(folder).then(() => {
+            createFolders(folder, portal);
         }).catch(err => console.error(err));
     });
 }
 
-function createDataStructure () {
-    var structure = [],
-        folder = {
-            name: "examples",
-            subFolders: [
-                {
-                    name: "lgv-config",
-                    subFolders: [
-                        {
-                            name: "img",
-                            source: "node_modules/lgv-config/img",
-                            files: [
-                                "hh-logo.png",
-                                "Icon-Badeseen.png",
-                                "Icon-Schwimmbaeder.png",
-                                "Icon-Spaß-am-Wasser.png",
-                                "Krankenhaus.png",
-                                "ajax-loader.gif",
-                                "dira.png",
-                                "bikeandride.png",
-                                "grundschulen.png",
-                                "gymnasien.png",
-                                "stadtteilschulen.png",
-                                "sonderschulen.png",
-                                "berufliche_schulen.png",
-                                "langformschulen.png",
-                                "backgroundCanvas.jpeg",
-                                "krippe.gif",
-                                "krippe_ele_eh.gif",
-                                "krippe_ele.gif",
-                                "ele.gif",
-                                "ele_eh.gif",
-                                "krippe_ele_hort.gif",
-                                "paemi.gif"
-                            ]
-                        }
-                    ],
-                    source: "node_modules/lgv-config",
-                    files: [
-                        "rest-services-internet.json",
-                        "services-internet.json",
-                        "style_v2.json"
-                    ]
-                },
-                {
-                    name: "portal",
-                    subFolders: [
-                        {
-                            name: "Basic",
-                            source: "./dist/Basic",
-                            files: [
-                                "config.js",
-                                "config.json",
-                                "index.html",
-                                "css",
-                                "js"
-                            ]
-                        }
-                    ]
-                }
-            ]
-        },
-        folderVersion = _.clone(folder),
-        version = packageJSON.version;
+/**
+ * Defines the folders to be created and the portal for the examples.
+ * @returns {void}
+ */
+function createFolderStructure () {
+    const folders = ["examples", "examples-" + packageJSON.version],
+        portal = {
+            name: "Basic",
+            source: "./dist/basic"
+        };
 
-    folderVersion.name = "examples-" + version;
-
-    structure.push(folder);
-    structure.push(folderVersion);
-
-    buildFolderStructure(structure);
+    removeFolders(folders, portal);
 }
 
 console.warn("create example folders, copy portal and dependencies");
-createDataStructure();
+createFolderStructure();
