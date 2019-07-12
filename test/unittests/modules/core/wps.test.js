f82690ab350e4b66b6db08619ec70d3d1360496a
Feng Feng <feng.feng@gv.hamburg.de>
2019-07-12 13:36:56 +0200

    2093 no-console and no-sync rule removed

--- test/unittests/modules/core/wps.test.js
+++ test/unittests/modules/core/wps.test.js
@@ -43,6 +43,7 @@ describe("core/WPS", function () {
         });
     });
     describe("buildUrl", function () {
+        // eslint-disable-next-line no-unused-vars
         var restModel = new Backbone.Model({
             "id": "1001",
             "name": "Deegree WPS RZ2 Produktion",
