c9daae22e6a4cff717f084f35d6e525ef5f4c1c2
Feng Feng <feng.feng@gv.hamburg.de>
2019-07-26 16:20:39 +0200

    2197 dev conflict

--- modules/controls/attributions/model.js
+++ modules/controls/attributions/model.js
@@ -138,7 +138,7 @@ const AttributionsModel = Backbone.Model.extend(/** @lends AttributionsModel.pro
      * @returns {Array} models with configured layer attributions
      */
     filterModelsWithLayerAttribution: function (modelList) {
-        let childModelsWithLayerAttributions = [],
+        const childModelsWithLayerAttributions = [],
             filteredModelList = modelList.filter(function (model) {
                 if (model.get("typ") === "GROUP") {
                     model.get("layerSource").forEach(childLayerSource => {
