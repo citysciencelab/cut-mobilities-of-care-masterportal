ad108e35b7e4cde2712b1655b922971204545a73
mbgvhh <markus.besenfelder@gv.hamburg.de>
2019-08-21 11:34:19 +0200

    update browserstack end2endtesting via env File

--- test/end2end/TestRunner.js
+++ test/end2end/TestRunner.js
@@ -1,18 +1,49 @@
+require("dotenv").config();
 var webdriver = require("selenium-webdriver"),
     path = require("path"),
     tests = require(path.resolve(__dirname, "./tests.js")),
-    driver,
-    driver_chrome,
-    driver_ff,
-    driver_ie,
-    loader;
+    browser = process.env.browser || "chrome",
+    browserstackuser = process.env.bs_user,
+    browserstackkey = process.env.bs_key,
+    proxy = process.env.proxy || '',
+    url = process.env.url || "https://localhost:9001/portal/basic",
+    driver;
 
-// driver_ie = new webdriver.Builder().withCapabilities(webdriver.Capabilities.ie()).build();
-// tests(driver_ie);
+switch (browser) {
+    case "firefox":
+        driver = new webdriver.Builder().
+                    withCapabilities({'browserName': 'firefox', acceptSslCerts: true, acceptInsecureCerts: true}).
+                    build();
+        break;
+    case "ie":
+        driver = new webdriver.Builder().
+                    withCapabilities(webdriver.Capabilities.ie()).
+                    build();
+        break;
+    case "bs":
+        var capabilities = {
+            'browserName' : 'Chrome',
+            'browser_version' : '74.0',
+            'os' : 'Windows',
+            'os_version' : '10',
+            'resolution' : '1024x768',
+            'browserstack.user' : browserstackuser,
+            'browserstack.key' : browserstackkey
+            }
 
-// driver_ff = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
-// tests(driver_ff);
+        driver = new webdriver.Builder().
+                    usingServer('http://hub-cloud.browserstack.com/wd/hub').
+                    withCapabilities(capabilities).
+                    usingWebDriverProxy(proxy).
+                    build();
+        break;
+    default:
+        driver = new webdriver.Builder().
+                    withCapabilities(webdriver.Capabilities.chrome()).
+                    build();
+        break;
+};
 
-driver_chrome = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
-tests(driver_chrome);
+
+tests(driver, url);
 
