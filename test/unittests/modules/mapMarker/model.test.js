3d30d1324f6129260911b1e8fe78352e2e8e3ec3
sven_beyel <sven.beyel@gv.hamburg.de>
2019-09-19 18:50:40 +0200

    update unittest

--- test/unittests/modules/mapMarker/model.test.js
+++ test/unittests/modules/mapMarker/model.test.js
@@ -13,7 +13,7 @@ describe("mapMarker/model", function () {
             expect(mapMarkerModel.convertCoordinatesToInteger(undefined)).to.be.an("array").that.is.empty;
         });
         it("should return an array with integers by string with brackets input", function () {
-            expect(mapMarkerModel.convertCoordinatesToInteger(["123)", "345"])).to.be.an("array")
+            expect(mapMarkerModel.convertCoordinatesToInteger(["123)", "345("])).to.be.an("array")
                 .that.includes(123, 345);
         });
         it("should return an array with integers by string without input", function () {
@@ -21,7 +21,7 @@ describe("mapMarker/model", function () {
                 .that.includes(123, 345);
         });
         it("should return an array with integers by integer input", function () {
-            expect(mapMarkerModel.convertCoordinatesToInteger(["123", "345"])).to.be.an("array")
+            expect(mapMarkerModel.convertCoordinatesToInteger([123, 345])).to.be.an("array")
                 .that.includes(123, 345);
         });
         it("should return an empty array by empty array input", function () {
