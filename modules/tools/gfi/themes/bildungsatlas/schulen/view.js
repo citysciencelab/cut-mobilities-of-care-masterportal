cb0a394fd1ede204c8136b68b5e70b582c4566f7
Feng Feng <feng.feng@gv.hamburg.de>
2019-09-25 11:34:48 +0200

    gfi theme with info and data

--- modules/tools/gfi/themes/bildungsatlas/schulen/view.js
+++ modules/tools/gfi/themes/bildungsatlas/schulen/view.js
@@ -2,9 +2,24 @@ import ThemeView from "../../view";
 import DefaultTemplate from "text-loader!./template.html";
 
 const SchulenThemeView = ThemeView.extend({
-    tagName: "table",
-    className: "table table-condensed table-hover",
-    template: _.template(DefaultTemplate)
+    tagName: "div",
+    className: "gif-schule",
+    template: _.template(DefaultTemplate),
+    events: {
+        "click .gfi-daten .title": "toggleTab",
+        "click .gfi-info .title": "toggleTab"
+    },
+
+    /**
+     * toggle the current tab to decide to show the information or data
+     * @param  {event} evt - event that is triggered
+     * @returns {void}
+     */
+    toggleTab: function (evt) {
+        $(evt.currentTarget).parent().parent().find(".title").toggleClass("show");
+        $(evt.currentTarget).parent().parent().find(".details").slideToggle(300);
+    }
+
 });
 
 export default SchulenThemeView;
