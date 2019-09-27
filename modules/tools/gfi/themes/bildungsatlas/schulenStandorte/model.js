dc8fed9042cb9e14dca3323c50e88ab40e3ae13e
Feng Feng <feng.feng@gv.hamburg.de>
2019-09-27 14:07:58 +0200

    theme changed to a meaningful name

--- modules/tools/gfi/themes/bildungsatlas/schulenStandorte/model.js
+++ modules/tools/gfi/themes/bildungsatlas/schulenStandorte/model.js
@@ -1,36 +1,14 @@
 import Theme from "../../model";
-import RoutableView from "../../../objects/routingButton/view";
 
-const DefaultTheme = Theme.extend({
+const SchulenStandorteViewTheme = Theme.extend({
     initialize: function () {
-        var channel = Radio.channel("defaultTheme");
-
         this.listenTo(this, {
             "change:isReady": function () {
                 this.replaceValuesWithChildObjects();
-                this.checkRoutable();
-            }
-        });
-
-        // render gfi new when changing properties of the associated features
-        this.listenTo(channel, {
-            "changeGfi": function () {
-                Radio.trigger("gfiView", "render");
             }
         });
     },
 
-    /**
-     * Prüft, ob der Button zum Routen angezeigt werden soll
-     * @returns {void}
-     */
-    checkRoutable: function () {
-        if (_.isUndefined(Radio.request("Parser", "getItemByAttributes", {id: "routing"})) === false) {
-            if (this.get("routable") === true) {
-                this.set("routable", new RoutableView());
-            }
-        }
-    },
     /**
      * Hier werden bei bestimmten Keywords Objekte anstatt von Texten für das template erzeugt. Damit können Bilder oder Videos als eigenständige Objekte erzeugt und komplex
      * gesteuert werden. Im Template werden diese Keywords mit # ersetzt und rausgefiltert. Im view.render() werden diese Objekte attached.
@@ -51,9 +29,7 @@ const DefaultTheme = Theme.extend({
                 this.set(idx, element[idx1][key]);
             }
         }
-
-        this.set("gfiContent", element);
     }
 });
 
-export default DefaultTheme;
+export default SchulenStandorteViewTheme;
