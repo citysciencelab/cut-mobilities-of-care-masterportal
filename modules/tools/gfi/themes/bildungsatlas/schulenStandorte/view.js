94469b6b46fc562120a3081532c1d713dff9c338
Feng Feng <feng.feng@gv.hamburg.de>
2019-09-27 14:11:48 +0200

    eslint error removed

--- modules/tools/gfi/themes/bildungsatlas/schulenStandorte/view.js
+++ modules/tools/gfi/themes/bildungsatlas/schulenStandorte/view.js
@@ -4,7 +4,7 @@ import DefaultTemplate from "text-loader!./template.html";
 const SchulenStandorteThemeView = ThemeView.extend({
     tagName: "div",
     className: "gfi-schule",
-    template: _.template(DefaultTemplate),
+    template: _.template(DefaultTemplate)
 });
 
 export default SchulenStandorteThemeView;
