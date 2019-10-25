f0b25b1a516343ed6defabfcac8da7aed64df3a6
Raj_hamburg <rajesh.mahalingam@gv.hamburg.de>
2019-10-25 08:35:43 +0200

    update gfi theme dipas

--- modules/tools/gfi/themes/dipas/model.js
+++ modules/tools/gfi/themes/dipas/model.js
@@ -1,14 +1,54 @@
 import Theme from "../model";
 
-const DipasTheme = Theme.extend({
+const DipasTheme = Theme.extend(/** @lends DipasTheme.prototype */{
+    defaults: {
+        gfiAttributesDipas: {
+            "Thema": "",
+            "name": "",
+            "description": "",
+            "link": "",
+            "nid": "",
+            "Rubric": ""
+        }
+
+    },
+    /**
+     * @class DipasTheme
+     * @extends Theme
+     * @memberof Tools.GFI.Themes.Dipas
+     * @constructs
+     * @listens Theme#changeIsReady
+     */
     initialize: function () {
         var featureList = this.get("gfiFeatureList");
 
         this.listenTo(this, {
-            "change:isReady": this.getIconPath(featureList[0].get("Thema"))
+            "change:isReady": function () {
+                this.getIconPath(featureList[0].get("Thema"));
+                this.getGfiTheme();
+            }
         });
     },
 
+    /**
+     * generates the gfi Attributes when gfi is active
+     * @returns {void}
+     */
+    getGfiTheme: function () {
+        var gfiContent = this.get("gfiContent"),
+            gfiAttributes = this.get("gfiAttributes");
+
+        _.each(gfiAttributes, function (value, key) {
+            this.get("gfiAttributesDipas")[key] = gfiContent[0][value] || key;
+        }, this);
+    },
+
+    /**
+     * generates the path for gfi icons
+     * @param  {String} value - gfi feature attribute values
+     * @fires StyleList#RadioRequestStyleListReturnModeById
+     * @returns {void}
+     */
     getIconPath: function (value) {
         var styleModel = Radio.request("StyleList", "returnModelById", this.get("themeId")),
             valueStyle = null,
@@ -24,6 +64,8 @@ const DipasTheme = Theme.extend({
         }
         this.setIconPath(iconPath);
     },
+
+    // setter for icon path
     setIconPath: function (value) {
         this.set("iconPath", value);
     }
