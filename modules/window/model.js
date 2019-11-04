bfebbc2d58fa186466c5adcf376846042b67b86b
Timmermann <anke.timmermann@gv.hamburg.de>
2019-11-04 17:09:15 +0100

    DPS-10 add jsdoc

--- modules/window/model.js
+++ modules/window/model.js
@@ -1,4 +1,4 @@
-const Window = Backbone.Model.extend({
+const Window = Backbone.Model.extend(/** @lends Window.prototype */{
     defaults: {
         isCollapsed: false,
         isVisible: false,
@@ -6,6 +6,17 @@ const Window = Backbone.Model.extend({
         maxPosTop: "60px",
         rotationAngle: 0
     },
+
+     /**
+     * @class Window
+     * @description Model for MapMarker and Highlighting
+     * @extends Backbone.Model
+     * @memberof Window
+     * @constructs
+     * @listens Window#RadioTriggerWindowSetIsVisible
+     * @listens Window#RadioTriggerWindowShowTool
+     * @listens Window#RadioTriggerWindowCollapseWin
+     */
     initialize: function () {
         var channel = Radio.channel("Window");
 
@@ -18,15 +29,36 @@ const Window = Backbone.Model.extend({
             "collapseWin": this.collapseWindow
         }, this);
     },
+
+    /**
+     * Collapses the Window
+     *  @return {void}
+     */
     collapseWindow: function () {
         this.setCollapse(true);
     },
+
+    /**
+     * Sets the value for "isCollapsed"
+     *  @return {void}
+     */
     setCollapse: function (value) {
         this.set("isCollapsed", value);
     },
+
+    /**
+     * Sets the value for "isVisible"
+     *  @return {void}
+     */
     setIsVisible: function (value) {
         this.set("isVisible", value);
     },
+
+    /**
+     * Sets the parameters for the Window
+     * @param {Object} value Object containing name, glyphicon and id
+     *  @return {void}
+     */
     setParams: function (value) {
         this.set("title", value.get("name"));
         this.set("icon", value.get("glyphicon"));
