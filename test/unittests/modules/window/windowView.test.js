19b400a3c7dd34f4fae061a8534ef5d72928c789
Timmermann <anke.timmermann@gv.hamburg.de>
2019-11-06 09:28:58 +0100

    DPS-10 add further tests

--- test/unittests/modules/window/windowView.test.js
+++ test/unittests/modules/window/windowView.test.js
@@ -28,5 +28,34 @@ describe("getNewPosition", function () {
             view.model.set("rotationAngle", 270);
             expect(view.getNewPosition({clientX: 727, clientY: 190}, 260, 231, 4000, 2000)).to.eql({left: "562px", top: "369px"});
         });
+        it("getNewPosition calculates values within the map width and height on angle 0", function () {
+            view.model.set("rotationAngle", 0);
+            expect(parseInt(view.getNewPosition({clientX: 4050, clientY: -50}, 260, 231, 4000, 2000).left, 10)).to.be.below(4000);
+            expect(parseInt(view.getNewPosition({clientX: 4050, clientY: -50}, 260, 231, 4000, 2000).top, 10)).to.be.above(0);
+            expect(parseInt(view.getNewPosition({clientX: -50, clientY: -50}, 260, 231, 4000, 2000).left, 10)).to.be.above(0);
+            expect(parseInt(view.getNewPosition({clientX: -50, clientY: 2200}, 260, 231, 4000, 2000).top, 10)).to.be.below(2000);
+        });
+        it("getNewPosition calculates values within the map width and height on angle 90", function () {
+            view.model.set("rotationAngle", 90);
+            expect(parseInt(view.getNewPosition({clientX: 4050, clientY: -50}, 260, 231, 4000, 2000).left, 10)).to.be.below(4000);
+            expect(parseInt(view.getNewPosition({clientX: 4050, clientY: -50}, 260, 231, 4000, 2000).top, 10)).to.be.above(0);
+            expect(parseInt(view.getNewPosition({clientX: -50, clientY: -50}, 260, 231, 4000, 2000).left, 10)).to.be.above(0);
+            expect(parseInt(view.getNewPosition({clientX: -50, clientY: 2200}, 260, 231, 4000, 2000).top, 10)).to.be.below(2000);
+        });
+        it("getNewPosition calculates values within the map width and height on angle 180", function () {
+            view.model.set("rotationAngle", 180);
+            expect(parseInt(view.getNewPosition({clientX: 4050, clientY: -50}, 260, 231, 4000, 2000).left, 10)).to.be.below(4000);
+            expect(parseInt(view.getNewPosition({clientX: 4050, clientY: -50}, 260, 231, 4000, 2000).top, 10)).to.be.above(0);
+            expect(parseInt(view.getNewPosition({clientX: -50, clientY: -50}, 260, 231, 4000, 2000).left, 10)).to.be.above(0);
+            expect(parseInt(view.getNewPosition({clientX: -50, clientY: 2200}, 260, 231, 4000, 2000).top, 10)).to.be.below(2000);
+        });
+        it("getNewPosition calculates values within the map width and height on angle 270", function () {
+            view.model.set("rotationAngle", 270);
+            expect(parseInt(view.getNewPosition({clientX: 4050, clientY: -50}, 260, 231, 4000, 2000).left, 10)).to.be.below(4000);
+            expect(parseInt(view.getNewPosition({clientX: 4050, clientY: -50}, 260, 231, 4000, 2000).top, 10)).to.be.above(0);
+            expect(parseInt(view.getNewPosition({clientX: -50, clientY: -50}, 260, 231, 4000, 2000).left, 10)).to.be.above(0);
+            expect(parseInt(view.getNewPosition({clientX: -50, clientY: 2200}, 260, 231, 4000, 2000).top, 10)).to.be.below(2000);
+        });
     });
+
 });
