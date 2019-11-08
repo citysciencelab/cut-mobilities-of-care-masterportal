a5ff27276932edec7ed16518a31e26695e6449f4
Timmermann <anke.timmermann@gv.hamburg.de>
2019-11-08 17:35:55 +0100

    DPS-10 adjust tests

--- test/unittests/modules/window/windowView.test.js
+++ test/unittests/modules/window/windowView.test.js
@@ -5,9 +5,8 @@ var view;
 
 before(function () {
     view = new View();
-    view.startX = 165;
-    view.startY = 81;
-
+    view.model.set("startX", 165);
+    view.model.set("startY", 81);
 });
 
 describe("getNewPosition", function () {
@@ -17,15 +16,15 @@ describe("getNewPosition", function () {
             expect(view.getNewPosition({clientX: 727, clientY: 190}, 260, 231, 4000, 2000)).to.eql({left: "562px", top: "109px"});
         });
         it("getNewPosition calculates the correct movement values on rotation angle 90", function () {
-            view.model.set("rotationAngle", 90);
+            view.model.set("rotationAngle", -90);
             expect(view.getNewPosition({clientX: 727, clientY: 190}, 260, 231, 4000, 2000)).to.eql({left: "793px", top: "109px"});
         });
         it("getNewPosition calculates the correct movement values on rotation angle 180", function () {
-            view.model.set("rotationAngle", 180);
+            view.model.set("rotationAngle", -180);
             expect(view.getNewPosition({clientX: 727, clientY: 190}, 260, 231, 4000, 2000)).to.eql({left: "822px", top: "340px"});
         });
         it("getNewPosition calculates the correct movement values on rotation angle 270", function () {
-            view.model.set("rotationAngle", 270);
+            view.model.set("rotationAngle", -270);
             expect(view.getNewPosition({clientX: 727, clientY: 190}, 260, 231, 4000, 2000)).to.eql({left: "562px", top: "369px"});
         });
         it("getNewPosition calculates values within the map width and height on angle 0", function () {
@@ -36,21 +35,21 @@ describe("getNewPosition", function () {
             expect(parseInt(view.getNewPosition({clientX: -50, clientY: 2200}, 260, 231, 4000, 2000).top, 10)).to.be.below(2000);
         });
         it("getNewPosition calculates values within the map width and height on angle 90", function () {
-            view.model.set("rotationAngle", 90);
+            view.model.set("rotationAngle", -90);
             expect(parseInt(view.getNewPosition({clientX: 4050, clientY: -50}, 260, 231, 4000, 2000).left, 10)).to.be.below(4000);
             expect(parseInt(view.getNewPosition({clientX: 4050, clientY: -50}, 260, 231, 4000, 2000).top, 10)).to.be.above(0);
             expect(parseInt(view.getNewPosition({clientX: -50, clientY: -50}, 260, 231, 4000, 2000).left, 10)).to.be.above(0);
             expect(parseInt(view.getNewPosition({clientX: -50, clientY: 2200}, 260, 231, 4000, 2000).top, 10)).to.be.below(2000);
         });
         it("getNewPosition calculates values within the map width and height on angle 180", function () {
-            view.model.set("rotationAngle", 180);
+            view.model.set("rotationAngle", -180);
             expect(parseInt(view.getNewPosition({clientX: 4050, clientY: -50}, 260, 231, 4000, 2000).left, 10)).to.be.below(4000);
             expect(parseInt(view.getNewPosition({clientX: 4050, clientY: -50}, 260, 231, 4000, 2000).top, 10)).to.be.above(0);
             expect(parseInt(view.getNewPosition({clientX: -50, clientY: -50}, 260, 231, 4000, 2000).left, 10)).to.be.above(0);
             expect(parseInt(view.getNewPosition({clientX: -50, clientY: 2200}, 260, 231, 4000, 2000).top, 10)).to.be.below(2000);
         });
         it("getNewPosition calculates values within the map width and height on angle 270", function () {
-            view.model.set("rotationAngle", 270);
+            view.model.set("rotationAngle", -270);
             expect(parseInt(view.getNewPosition({clientX: 4050, clientY: -50}, 260, 231, 4000, 2000).left, 10)).to.be.below(4000);
             expect(parseInt(view.getNewPosition({clientX: 4050, clientY: -50}, 260, 231, 4000, 2000).top, 10)).to.be.above(0);
             expect(parseInt(view.getNewPosition({clientX: -50, clientY: -50}, 260, 231, 4000, 2000).left, 10)).to.be.above(0);
