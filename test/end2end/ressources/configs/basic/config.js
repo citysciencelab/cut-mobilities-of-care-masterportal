7ba6b8a6a1934f20829ca030a8c21db9098d63c0
mbgvhh <markus.besenfelder@gv.hamburg.de>
2019-11-28 14:34:54 +0100

    add configs for end2endTesting

---
+++ test/end2end/ressources/configs/basic/config.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+
+const Config = {
+    wfsImgPath: "./ressources/img/",
+    namedProjections: [
+        ["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
+    ],
+    footer: {
+        urls: [
+            {
+                "bezeichnung": "Kartographie und Gestaltung: ",
+                "url": "https://www.geoinfo.hamburg.de/",
+                "alias": "Landesbetrieb Geoinformation und Vermessung",
+                "alias_mobil": "LGV"
+            }
+        ]
+    },
+    quickHelp: {
+        imgPath: "./ressources/img/"
+    },
+    layerConf: "./ressources/services-internet.json",
+    restConf: "./ressources/rest-services-internet.json",
+    styleConf: "./ressources/style_v2.json",
+    scaleLine: true,
+    mouseHover: {
+        numFeaturesToShow: 2,
+        infoText: "(weitere Objekte. Bitte zoomen.)"
+    }
+};
+
+/* eslint-enable no-unused-vars */
