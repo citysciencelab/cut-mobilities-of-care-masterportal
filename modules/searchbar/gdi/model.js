aa447455eee01914d4cf15692141f17b8442ff3f
M-JW <jonas.weiter@muenchen.de>
2019-12-02 12:09:07 +0100

    update gdi

--- modules/searchbar/gdi/model.js
+++ modules/searchbar/gdi/model.js
@@ -5,31 +5,22 @@ const GdiModel = Backbone.Model.extend(/** @lends GdiModel.prototype */{
     defaults: {
         minChars: 3,
         serviceId: "",
+        queryObject: {
+            id: "query",
+            params: {
+                query_string: "%%searchString%%"
+            }
+        },
         elasticSearch: new ElasticSearch()
     },
     /**
      * @description Initialise GDI-Search via ElasticSearch
-     * @param {Object} config - gdi-ConfigObject
-     * @param {integer} [config.minChars=3] - minimal number of searchcharacters
-     * @param {string} config.serviceId - rest-services Id
      * @returns {void}
      */
-    initialize: function (config) {
-        var channel = Radio.channel("GDI-Search");
-
-        channel.on({
-            "addLayer": this.addLayer
-        }, this);
-
+    initialize: function () {
         this.listenTo(Radio.channel("Searchbar"), {
             "search": this.search
         });
-        this.listenTo(Radio.channel("Elastic"), {
-            "triggerHitList": this.triggerHitList
-        });
-        if (config.minChars) {
-            this.set("minChars", config.minChars);
-        }
     },
     /**
      * Searchs layer if enough characters have been touched (if >="minChars")
@@ -37,12 +28,127 @@ const GdiModel = Backbone.Model.extend(/** @lends GdiModel.prototype */{
      * @returns {void}
      */
     search: function (searchString) {
-        var query = this.createQuery(searchString, Radio.request("Parser", "getPortalConfig").searchBar.gdi);
+        const payload = this.appendSearchStringToPayload(this.get("queryObject"), "query_string", searchString),
+            xhrConfig = {
+                serviceId: this.get("serviceId"),
+                type: "GET",
+                async: false,
+                payload: payload,
+                responseEntryPath: "hits.hits"
+            };
+        let result;
 
         if (searchString.length >= this.get("minChars")) {
-            this.get("elasticSearch").search(this.get("serviceId"), query);
+            result = this.get("elasticSearch").searchNew(xhrConfig);
+            this.createRecommendedList(result.hits);
         }
     },
+
+    /**
+     * Creates the reccommended List
+     * @param {Object[]} responseData Response data.
+     * @fires Searchbar#RadioTriggerSearchbarPushHits
+     * @fires Searchbar#RadioTriggerSearchbarRemoveHits
+     * @fires Searchbar#RadioTriggerSearchbarCreateRecommendedList
+     * @returns {void}
+     */
+    createRecommendedList: function (responseData) {
+        const triggerEvent = {
+                channel: "Parser",
+                event: "addGdiLayer"
+            },
+            hitMap = {
+                name: "_source.name",
+                id: "_source.id",
+                source: "_source"
+            },
+            hitType = "Fachthema",
+            hitGlyphicon = "glyphicon-list";
+
+        if (responseData.length > 0) {
+            responseData.forEach(result => {
+                const hit = this.createHit(result, hitMap, hitType, hitGlyphicon, triggerEvent);
+
+                Radio.trigger("Searchbar", "pushHits", "hitList", hit);
+            });
+        }
+        else {
+            Radio.trigger("Searchbar", "removeHits", "hitList", {type: "Straße"});
+        }
+        Radio.trigger("Searchbar", "createRecommendedList", "elasticSearch");
+    },
+
+    /**
+     * Creates hit that is sent to the hitList.
+     * @param {Object} result Result object from elastcisearch request.
+     * @param {Object} hitMap Mapping object. Used to map results attributes to neccessary hit attributes.
+     * @param {String} hitType Type of hit.
+     * @param {String} hitGlyphicon Glyphicon class to show in reccomendedList
+     * @param {Object} triggerEvent Object defining channel and event. used to fire event on mouseover and click in recommendedList.
+     * @returns {Object} - hit.
+     */
+    createHit: function (result, hitMap, hitType, hitGlyphicon, triggerEvent) {
+        let hit = {};
+
+        Object.keys(hitMap).forEach(key => {
+            hit[key] = this.findAttributeByPath(result, hitMap[key]);
+        });
+        hit.type = hitType;
+        hit.glyphicon = hitGlyphicon;
+        if (Object.keys(triggerEvent).length > 0) {
+            hit = Object.assign(hit, {triggerEvent: triggerEvent});
+        }
+        return hit;
+    },
+
+    /**
+     * Returns the attribute value of the given object by path.
+     * If path is an array, the function recursively iterates over the object for each part and pushes the value in an array.
+     * Otherwise only the value of the given attribute path will be returned.
+     * @param {Object} object Object to derive value from.
+     * @param {String|String[]} path Path of the attribute. "." in the path indicates the next deeper level.
+     * @returns {*} - Value that is at position of given path.
+     */
+    findAttributeByPath: function (object, path) {
+        let attribute = object,
+            paths;
+
+        if (Array.isArray(path)) {
+            attribute = [];
+            path.forEach(pathPart => {
+                attribute.push(this.findAttributeByPath(object, pathPart));
+            });
+        }
+        else {
+            paths = path.split(".");
+            paths.forEach(pathPart => {
+                attribute = attribute[pathPart];
+            });
+        }
+        return attribute;
+    },
+
+    /**
+     * Recursively searches for the searchStringAttribute key and sets the searchString.
+     * Adds the search string to the payload using the given key
+     * @param {Object} payload Payload as Object
+     * @param {String} searchStringAttribute Attribute key to be added to the payload object.
+     * @param {String} searchString Search string to be added using the searchStringAttribute.
+     * @returns {Object} - the payload with the search string.
+     */
+    appendSearchStringToPayload: function (payload, searchStringAttribute, searchString) {
+        Object.keys(payload).forEach(key => {
+            if (typeof payload[key] === "object") {
+                payload[key] = this.appendSearchStringToPayload(payload[key], searchStringAttribute, searchString);
+            }
+            if (key === searchStringAttribute) {
+                payload[searchStringAttribute] = searchString;
+            }
+        });
+
+        return payload;
+    },
+
     /**
      * function for the layers that are being searched for
      * @param {String} datasources - layers that have been found
@@ -70,83 +176,16 @@ const GdiModel = Backbone.Model.extend(/** @lends GdiModel.prototype */{
     /**
      * creates query for searched string (layer)
      * @param {String} searchString - string that will be touched
-     * @param {Object} config - config Parameter
+     * @param {Object} queryObject - Query object.
      * @returns {Oject} result
      */
-    createQuery: function (searchString, config) {
-        var query_object,
-            string_query,
-            replace_object,
-            result;
-
-        if (config) {
-            query_object = config.queryObject;
-            string_query = JSON.stringify(query_object);
-            replace_object = string_query.replace("%%searchString%%", searchString);
+    createQuery: function (searchString, queryObject) {
+        const query_object = queryObject,
+            string_query = JSON.stringify(query_object),
+            replace_object = string_query.replace("%%searchString%%", searchString),
             result = JSON.parse(replace_object);
-        }
+
         return result;
-    },
-    /**
-     * Adds found layer to layer tree
-     * @param {Object} hit layer to be added
-     * @returns {void}
-     */
-    addLayer: function (hit) {
-        var treeType = Radio.request("Parser", "getTreeType"),
-            parentId = "tree",
-            level = 0,
-            layerTreeId;
-
-        if (hit.source) {
-
-            /* Erst mal prüfen, ob es den Layer schon im Themenbaum gibt */
-            layerTreeId = Radio.request("Parser", "getItemByAttributes", {id: hit.source.id});
-            /* wenn es den Layer noch nicht gibt, diesen aus dem ElasticSearch-Ergebnis erzeugen */
-            if (_.isUndefined(layerTreeId)) {
-
-                if (treeType === "custom") {
-                    /* Im Custom-Tree erst mal einen neuen Folder erzeugen und diesem den Folder "Ext.Thema" hinzufügen (falls es diese noch nicht gibt) */
-                    parentId = "extthema";
-                    level = 2;
-                    if (_.isUndefined(Radio.request("Parser", "getItemByAttributes", {id: "ExternalLayer"}))) {
-                        Radio.trigger("Parser", "addFolder", "Externe Fachdaten", "ExternalLayer", "tree", 0);
-                        Radio.trigger("ModelList", "renderTree");
-                        $("#Overlayer").parent().after($("#ExternalLayer").parent());
-                    }
-                    if (_.isUndefined(Radio.request("Parser", "getItemByAttributes", {id: parentId}))) {
-                        Radio.trigger("Parser", "addFolder", "Fachthema", parentId, "ExternalLayer", 1, true);
-                    }
-                }
-
-                /* Dann den neuen Layer aus dem ElasicSearch-Ergebnis erzeugen */
-                Radio.trigger("Parser",
-                    "addGDILayer",
-                    {
-                        name: hit.source.name,
-                        id: hit.source.id,
-                        parentId: parentId,
-                        level: level,
-                        layers: hit.source.layers,
-                        url: hit.source.url,
-                        version: hit.source.version,
-                        gfiAttributes: hit.source.gfiAttributes ? hit.source.gfiAttributes : "showAll",
-                        datasets: hit.source.datasets,
-                        isJustAdded: true
-                    });
-
-                /* und der ModelList hinzufügen */
-                Radio.trigger("ModelList", "addModelsByAttributes", {id: hit.source.id});
-            }
-
-            Radio.trigger("ModelList", "showModelInTree", hit.source.id);
-            if (treeType === "light") {
-                Radio.trigger("ModelList", "refreshLightTree");
-            }
-        }
-        else {
-            console.error("Es konnte kein Eintrag für Layer " + hit.id + " in ElasticSearch gefunden werden.");
-        }
     },
     /**
      * Setter for MinChars
