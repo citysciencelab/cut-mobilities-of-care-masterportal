dce1104ace1ec974790d39b0791f8734df3b9551
M-JW <jonas.weiter@muenchen.de>
2019-12-03 10:15:13 +0100

    update extent unittests for addons

--- devtools/webpack.test.js
+++ devtools/webpack.test.js
@@ -10,6 +10,7 @@ module.exports = {
     resolve: {
         alias: {
             "@modules": path.resolve(__dirname, "../modules"),
+            "@addons": path.resolve(__dirname, "../addons"),
             "@testUtil": path.resolve(__dirname, "../test/unittests/Util"),
             "@portalconfigs": path.resolve(__dirname, "../portalconfigs")
         }
