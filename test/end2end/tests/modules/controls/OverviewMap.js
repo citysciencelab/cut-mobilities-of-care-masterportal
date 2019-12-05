ab23f24e8a4ad70cfdd317033681c5c6209e2b30
Dennis Sen <dennis.sen@dataport.de>
2019-12-05 09:36:00 +0100

    MPLGV-45 fix test

--- test/end2end/tests/modules/controls/OverviewMap.js
+++ test/end2end/tests/modules/controls/OverviewMap.js
@@ -14,10 +14,10 @@ function OverviewMap ({builder, url, resolution, browsername}) {
     // canvas panning is currently broken in Chrome, see https://github.com/SeleniumHQ/selenium/issues/6332
     const skipCanvasPan = browsername.toLowerCase().includes("chrome"),
         skipMouseWheel = isMobile(resolution), // no mouse wheel on mobile devices
-        skipAll = !isCustom(url); // overview map button only configured in CT
+        skipAll = isMobile(resolution) || !isCustom(url); // overview map button only configured in CT
 
     (skipAll ? describe.skip : describe)("Modules Controls OverviewMap", function () {
-        let driver, overviewMapButton, customOverviewMap;
+        let driver, overviewMapButton, customOverviewMap, overviewMapViewport;
 
         before(async function () {
             driver = await initDriver(builder, url, resolution);
@@ -34,23 +34,29 @@ function OverviewMap ({builder, url, resolution, browsername}) {
             expect(overviewMapButton).to.exist;
         });
 
-        it("opens overview map on clicking overview map button", async function () {
-            await driver.actions({bridge: true})
-                .click(overviewMapButton)
-                .perform();
-            customOverviewMap = await driver.wait(until.elementLocated(By.css(".ol-overviewmap.ol-custom-overviewmap")), 9000);
+        it("closes/opens overview map on clicking overview map button", async function () {
+            const click = driver.actions({bridge: true}).click(overviewMapButton);
+
+            // close - is open initially
+            await click.perform();
+            expect((await driver.findElements(By.css(".ol-overviewmap.ol-custom-overviewmap"))).length).to.equal(0);
+
+            // open
+            await click.perform();
+            customOverviewMap = await driver.findElement(By.css(".ol-overviewmap.ol-custom-overviewmap"));
 
             expect(customOverviewMap).to.exist;
         });
 
         (skipCanvasPan ? it.skip : it)("allows panning the map from the overview map", async function () {
-            const center = await driver.executeScript(getCenter),
-                viewport = await driver.findElement(By.css(".ol-overviewmap.ol-custom-overviewmap .ol-viewport"));
+            const center = await driver.executeScript(getCenter);
+
+            overviewMapViewport = await driver.findElement(By.css(".ol-overviewmap.ol-custom-overviewmap .ol-viewport"));
 
             await driver.actions({bridge: true})
-                .move({origin: viewport})
+                .move({origin: overviewMapViewport})
                 .press(Button.LEFT)
-                .move({origin: viewport, x: 5, y: 5})
+                .move({origin: overviewMapViewport, x: 5, y: 5})
                 .release(Button.LEFT)
                 .perform();
 
@@ -62,7 +68,7 @@ function OverviewMap ({builder, url, resolution, browsername}) {
         (skipMouseWheel ? it.skip : it)("should zoom full map in on mouse wheel up on overview", async function () {
             const res = await driver.executeScript(getResolution);
 
-            await driver.executeScript(mouseWheelUp, customOverviewMap);
+            await driver.executeScript(mouseWheelUp, overviewMapViewport);
             await driver.executeAsyncScript(onMoveEnd);
 
             expect(res).to.be.above(await driver.executeScript(getResolution));
@@ -71,7 +77,7 @@ function OverviewMap ({builder, url, resolution, browsername}) {
         (skipMouseWheel ? it.skip : it)("should zoom full map out on mouse wheel down on overview", async function () {
             const res = await driver.executeScript(getResolution);
 
-            await driver.executeScript(mouseWheelDown, customOverviewMap);
+            await driver.executeScript(mouseWheelDown, overviewMapViewport);
             await driver.executeAsyncScript(onMoveEnd);
 
             expect(res).to.be.below(await driver.executeScript(getResolution));
