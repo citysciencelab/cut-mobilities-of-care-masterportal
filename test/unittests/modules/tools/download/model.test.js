10ca7019a8c84777067936edd9155eb903bcccd8
M-JW <jonas.weiter@muenchen.de>
2019-12-10 12:26:08 +0100

    update tests for download

--- test/unittests/modules/tools/download/model.test.js
+++ test/unittests/modules/tools/download/model.test.js
@@ -7,10 +7,29 @@ describe("downloadModel", function () {
     before(function () {
         model = new Model();
     });
-    describe("TODO", function () {
-        it("FOOBAR", function () {
-            model.set("foo", "bar");
-            expect(true).to.be.true;
+    describe("validateFileNameAndExtension", function () {
+        it("should return undefined for unset fileName and unset selectedFormat", function () {
+            expect(model.validateFileNameAndExtension()).to.be.undefined;
+        });
+        it("should return filename with format", function () {
+            model.setFileName("Foo");
+            model.setSelectedFormat("bar");
+            expect(model.validateFileNameAndExtension()).to.equal("Foo.bar");
+        });
+        it("should return filename with format on user input with fileextension", function () {
+            model.setFileName("Foo.bar");
+            model.setSelectedFormat("bar");
+            expect(model.validateFileNameAndExtension()).to.equal("Foo.bar");
+        });
+        it("should return undefined for empty filename", function () {
+            model.setFileName("");
+            model.setSelectedFormat("bar");
+            expect(model.validateFileNameAndExtension()).to.be.undefined;
+        });
+        it("should return undefined for empty format", function () {
+            model.setFileName("foo");
+            model.setSelectedFormat("");
+            expect(model.validateFileNameAndExtension()).to.be.undefined;
         });
     });
 });
