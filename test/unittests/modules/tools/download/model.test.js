d00e140307e0dba6c0c0cfe81832db7fbd44a247
M-JW <jonas.weiter@muenchen.de>
2019-12-10 10:34:43 +0100

    undo tests

--- test/unittests/modules/tools/download/model.test.js
+++ test/unittests/modules/tools/download/model.test.js
@@ -7,27 +7,10 @@ describe("downloadModel", function () {
     before(function () {
         model = new Model();
     });
-    describe("validateFilename", function () {
-        it("validation should succeed when filename valid", function () {
-            expect(model.validateFilename("name.jpg")).to.be.true;
-        });
-        it("validation should fail when filename starts with \".\"", function () {
-            expect(model.validateFilename(".name.jpg")).to.be.false;
-        });
-        it("validation should fail when filename ends with \".\"", function () {
-            expect(model.validateFilename("name.")).to.be.false;
-        });
-        it("validation should fail when filename contains \"?\"", function () {
-            expect(model.validateFilename("nam?e.jpg")).to.be.false;
-        });
-        it("validation should fail when filename isEmpty", function () {
-            expect(model.validateFilename("")).to.be.false;
-        });
-        it("validation should fail when filename undefined", function () {
-            expect(model.validateFilename()).to.be.false;
-        });
-        it("validation should fail when filename contains blank", function () {
-            expect(model.validateFilename(" ")).to.be.false;
+    describe("TODO", function () {
+        it("FOOBAR", function () {
+            model.set("foo", "bar");
+            expect(true).to.be.true;
         });
     });
 });
