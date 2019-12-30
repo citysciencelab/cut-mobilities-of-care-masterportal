297cddea5d5aaa9956294e455d0303bf073744e2
Beyel <BeyelSv@fhhnet.stadt.hamburg.de>
2019-12-30 16:49:09 +0100

    add alerting in vue

---
+++ src/components/Alerting.vue
@@ -0,0 +1,87 @@
+<template lang="html">
+    <div id="messages" v-bind:class="[position]">
+        <div v-for="alert in alerts">
+            <div :id="alert.id" :class="['alert', alert.category]" role="alert">
+                <p>{{alert.isDismissable}}</p>
+                <button v-if="alert.isDismissable===true" v-on:click="closeAlert('closed', $event)" type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <p>{{ alert.message }}</p>
+                <button v-if="alert.isConfirmable" v-on:click="closeAlert('confirmed', $event)" type="button" class="btn btn-primary alert-confirm" data-dismiss="alert" aria-label="Close">OK</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+// todo:
+// position dynamisch
+// fade out
+// daten aus config übernehmen
+
+export default {
+    data() {
+        return {
+            position: "top-center",
+            fadeOut: 5000,
+        }
+    },
+    created() {
+        const that = this,
+            myBus = Backbone.Events;
+
+        myBus.listenTo(Radio.channel("Alert"), {
+            "alert": function (alert) {
+                that.$store.dispatch("addAlert", alert);
+            },
+            "alert:remove": function (removeAlertId) {
+                that.$store.dispatch("removeAlert", removeAlertId)
+            }
+        });
+    },
+    mounted() {
+        document.getElementsByTagName("body")[0].append(this.$el);
+    },
+    computed: {
+        alerts () {
+            return this.$store.state.Alert.alerts
+        },
+    },
+    methods: {
+        closeAlert: function (mode, event) {
+            const div = $(event.currentTarget).parent();
+
+            this.$store.dispatch("removeAlert", $(div[0]).attr("id"));
+            Radio.trigger("Alert", mode, $(div[0]).attr("id"));
+        }
+    }
+}
+</script>
+
+<style lang="css">
+div#messages {
+    max-width: 100%;
+    z-index: 2001;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    .alert {
+        box-shadow: 0 6px 12px rgba(0, 0, 0, 0.176);
+    }
+}
+div#messages.top-center {
+    top: 25%;
+    left: 50%;
+}
+div#messages.center-center {
+    top: 50%;
+    left: 50%;
+}
+div#messages.bottom-center {
+    top: 75%;
+    left: 50%;
+}
+.alert-confirm {
+    margin-top: 10px;
+    width: 100%;
+}
+</style>
---
+++ src/components/FooBar.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+    <h1>FooBar!</h1>
+</template>
+
+<script>
+export default {
+    mounted() {
+        // console.log ('FooBar was mounted!!');
+        // document.getElementById('main-nav').appendChild(this.$el);
+        // const modelList = Radio.request("ModelList", "getCollection");
+        // console.log(modelList);
+        // console.log(this.$store.state.User.user);
+        // this.$store.dispatch("addAlert", "FooBar alert");
+        // this.$store.dispatch("addAlert", {text:"FooBar2 alert", id:"124"});
+        // Radio.trigger("Alert", "alert:remove", "124");
+        //Radio.tigger("Alert", "alert:remove");
+        // this.$store.dispatch("removeAlert");
+        // this.$store.dispatch("removeAlert", "124");
+        Radio.trigger("Alert", "alert", "Textnachricht als alert!");
+        Radio.trigger("Alert", "alert", "Textnachricht als alert!");
+        Radio.trigger("Alert", "alert", "Textnachricht als alert222!");
+        Radio.trigger("Alert", "alert", "Textnachricht als alert!");
+                Radio.trigger("Alert", "alert", {
+            id: "999999",
+            text: "confirm Example from pendler",
+            dismissable: false,
+            confirmable: true
+        });
+        
+    }
+}
+</script>
+
+<style lang="css">
+</style>
