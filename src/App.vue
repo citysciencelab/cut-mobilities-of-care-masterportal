07741a981e35dbe965e9e6a08b9794769060d4c4
Beyel <BeyelSv@fhhnet.stadt.hamburg.de>
2019-12-30 16:45:29 +0100

    add vue instance to app. js

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FooBar msg="Welcome to Your Vue.js App"/>
+    <Alerting/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import FooBar from './components/FooBar.vue'
+import Alerting from './components/Alerting.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    FooBar,
+    Alerting
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
