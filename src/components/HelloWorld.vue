364a3795a31c177d6ba9bbb556c06682b7e48036
Beyel <BeyelSv@fhhnet.stadt.hamburg.de>
2019-12-30 16:49:09 +0100

    add alerting in vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+    <h1>HALLOO!</h1>
+</template>
+
+<script>
+export default {
+    created() {
+        console.log("Hello World was created!");
+    },
+    mounted() {
+        console.log ('Hello World was mounted!!');
+        console.log(this.$store.state.user);
+        document.getElementById('main-nav').appendChild(this.$el);
+        Radio.trigger("Alert", "alert", {text:"Hello World!", kategorie: "alert-danger"});
+        // this.$store.commit("setAlert", "Text")
+        this.$store.dispatch('storeUser', "XYUser");
+    }
+}
+</script>
+
+<style lang="css">
+</style>
