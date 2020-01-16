a0ac9eac1a86aa0c4d7848f2ed1901d89fb73998
heinz l mann <jonas.weiter@muenchen.de>
2020-01-16 08:00:30 +0100

    make alert also accept html

--- src/components/Alerting.vue
+++ src/components/Alerting.vue
@@ -5,7 +5,9 @@
                 <button v-if="alert.isDismissable===true" v-on:click="closeAlert('closed', $event)" type="button" class="close" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
-                <p>{{ alert.message }}</p>
+                <p>
+                    <span v-html="alert.message"></span>
+                </p>
                 <button v-if="alert.isConfirmable" v-on:click="closeAlert('confirmed', $event)" type="button" class="btn btn-primary alert-confirm" aria-label="Close">OK</button>
             </div>
         </div>
