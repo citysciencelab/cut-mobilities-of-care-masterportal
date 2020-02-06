9752f84c3b00f1294a7e65fbbae1cef2976f7bf7
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2020-02-06 09:16:50 +0100

    update - eslint for vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
-<template>
-  <div id="app">
-    <Alerting/>
-    <SupplyCoord/>
-  </div>
-</template>
-
 <script>
 import Alerting from './components/Alerting.vue'
 import Tool from './components/Tool.vue'
 import SupplyCoord from './components/SupplyCoord.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
     Alerting,
     Tool,
@@ -19,6 +12,12 @@ export default {
   }
 }
 </script>
+<template>
+    <div id="app">
+        <Alerting />
+        <SupplyCoord/>
+    </div>
+</template>
 
 <style>
 </style>
