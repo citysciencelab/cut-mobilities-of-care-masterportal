2eb24e445e2be6dc1e6c38d858dce465504156a0
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2020-02-06 09:16:50 +0100

    update - eslint for vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script>
-import Alerting from "./components/Alerting.vue";
-import SupplyCoord from "./components/SupplyCoord.vue";
+import Alerting from './components/Alerting.vue'
+import SupplyCoord from './components/SupplyCoord.vue'
 import ScaleLine from "./components/ScaleLine.vue";
 
 export default {
@@ -12,6 +12,7 @@ export default {
     }
 };
 </script>
+
 <template>
     <div id="app">
         <Alerting />
@@ -19,5 +20,6 @@ export default {
         <ScaleLine />
     </div>
 </template>
+
 <style>
 </style>
