a38481e039d6eb19e3f1219977321f9f942aa4c7
Inka Dudek <inka.dudek@gv.hamburg.de>
2020-02-07 12:54:59 +0100

    update: handling of fetch error

--- modules/core/configLoader/preparser.js
+++ modules/core/configLoader/preparser.js
@@ -21,7 +21,8 @@ const Preparser = Backbone.Model.extend(/** @lends Preparser.prototype */{
                 const statusText = xhr.statusText;
                 let message,
                     position,
-                    snippet;
+                    snippet,
+                    textStatus;
 
                 // SyntaxError for consoletesting, propably because of older version.
                 if (statusText === "Not Found" || statusText.indexOf("SyntaxError") !== -1) {
@@ -34,13 +35,14 @@ const Preparser = Backbone.Model.extend(/** @lends Preparser.prototype */{
                     message = error.errorThrown.message;
                     position = parseInt(message.substring(message.lastIndexOf(" ")), 10);
                     snippet = xhr.responseText.substring(position - 30, position + 30);
+                    textStatus = error.textStatus;
                     Radio.trigger("Alert", "alert", {
                         text: "<strong>Die Datei '" + model.url + "' konnte leider nicht geladen werden!</strong> <br> " +
-                        "<small>Details: " + error.textStatus + " - " + error.errorThrown.message + ".</small><br>" +
+                        "<small>Details: " + textStatus + " - " + error.errorThrown.message + ".</small><br>" +
                         "<small>Auszug:" + snippet + "</small>",
                         kategorie: "alert-warning"
                     });
-                    if (error.textStatus === "parsererror" && error.errorThrown.message === "Syntaxfehler") {
+                    if (textStatus === "parsererror"){
                         // reload page once
                         if (window.localStorage) {
                             if (!localStorage.getItem("firstLoad")) {
