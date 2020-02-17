9e4d12586b1fa6aadee197416a3477ab98fc433c
Christopher Reder <christopher.reder@gv.hamburg.de>
2020-02-17 13:42:52 +0100

    update layerinformation visible legend  if njot lightTree  and layer not viisible

--- modules/core/modelList/layer/model.js
+++ modules/core/modelList/layer/model.js
@@ -483,10 +483,14 @@ const Layer = Item.extend(/** @lends Layer.prototype */{
      */
     showLayerInformation: function () {
         var metaID = [],
-            legend = Radio.request("Legend", "getLegend", this),
+            legend = "",
             name = this.get("name"),
             layerMetaId = this.get("datasets") && this.get("datasets")[0] ? this.get("datasets")[0].md_id : null;
 
+        if (this.get("legendURL") === "") {
+            this.createLegendURL();
+        }
+        legend = Radio.request("Legend", "getLegend", this);
         metaID.push(layerMetaId);
 
         Radio.trigger("LayerInformation", "add", {
