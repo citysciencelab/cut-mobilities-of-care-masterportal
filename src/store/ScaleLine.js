bc9c595d4f994a22738796801649e44a89760b7f
Christopher Reder <christopher.reder@gv.hamburg.de>
2020-02-20 10:11:34 +0100

    add vue scaleline jsdoc

--- src/store/ScaleLine.js
+++ src/store/ScaleLine.js
@@ -14,6 +14,12 @@ export default {
         }
     },
     actions: {
+        /**
+         * Modifies the scale number if it has more than three digits and adds it to the store
+         * @param {ActionContext} param0 - context passed by vuex
+         * @param  {object} obj - contains resolution, zoomlevel and scale from MapView
+         * @returns {void}
+         */
         modifyScale ({commit}, obj) {
             var scaleNumber = obj.scale.toString();
 
@@ -23,6 +29,13 @@ export default {
 
             commit("updateScaleNumber", scaleNumber);
         },
+
+        /**
+         * Calculates the scaleLineValue für the scale bar in relation to a 2cm long line
+         * Ff the scaleLineValue is greater than 1000 meter, it is given as km
+         * @param {ActionContext} param0 - context passed by vuex
+         * @returns {void}
+        */
         updateScaleLineValue: function ({state, commit}) {
             var scaleLineValue,
                 scaleNumber = Math.round(0.02 * state.scaleNumber.replace(" ", ""));
