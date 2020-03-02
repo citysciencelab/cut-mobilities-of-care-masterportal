eb900083aea3209ba64ccbc24bae4dbbfa32cd51
Burmester <BurmesKi@fhhnet.stadt.hamburg.de>
2020-03-02 10:05:08 +0100

    update BG-272 display titel depending on space

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -28,66 +28,9 @@ export default {
             }
             return undefined;
         },
-            
-        // /**
-        // * Renders the title if enough space available.
-        // * @returns {void}
-        // */
-        // renderDependingOnSpace: function () {
-        //     let menueBreite,
-        //         sucheBreite,
-        //         gesamtBreite,
-        //         platzFuerTitel,
-        //         // navMenuWidth,
-        //         // searchbarWidth,
-        //         // navBarWidth,
-        //         titleWidth,
-        //         rest,
-        //         doRender = false;
-        //     const titleElement = document.getElementsByClassName("portal-title"),
-        //         titlePadding = 10;
-
-        //     if (document.getElementById("searchbar")) {
-        //         menueBreite = document.getElementById("root").offsetWidth;
-        //         sucheBreite = document.getElementById("searchForm").offsetWidth + 40; // add width of searchbarIcon
-        //         gesamtBreite = document.getElementById("main-nav").offsetWidth;
-        //         // navMenuWidth = document.getElementById("root").offsetWidth;
-        //         // searchbarWidth = document.getElementById("searchForm").offsetWidth + 40; // add width of searchbarIcon
-        //         // navBarWidth = document.getElementById("main-nav").offsetWidth;
-        //         console.log(titleElement);
-        //         titleWidth = titleElement ? titleElement[0].offsetWidth : 0; // Ermittlung wie breit der Titel eigentlich ist.
-        //         this.$store.state.Title.titleWidth = titleWidth;
-
-        //         platzFuerTitel = gesamtBreite - menueBreite - sucheBreite;
-        //         console.log(platzFuerTitel, this.$store.state.Title.titleWidth);
-
-        //         // Fall 1: volle Breite, alle Elemente können nebeneinander bestehen. Keine Einschränkungen.
-        //         if (platzFuerTitel > this.$store.state.Title.titleWidth) {
-        //             console.log(titleElement[0].style.display);
-        //             // wenn wieder genug Platz vorhanden sein sollte, so werden die vormals ausgeblendeten Titleelmente wieder eingeblendet.
-        //             if (!titleElement) {
-        //                 console.log("nope");
-        //                 titleElement[0].style.display = "visible";
-        //             }
-        //             console.log(titleElement[0].style.display);
-        //         }
-
-        //         // Fall 2: Eingeschränkte Breite, es passt nur noch das Logo. Der Titel-Text wird ausgeblendet.
-        //         // if (platzFuerTitel < this.$store.state.Title.titleWidth) {
-
-        //         // }
-
-        //         // Fall 3: Stark eingeschränkte Breite. Titel und Logo sind nicht mehr zu sehen. Sie werden beide ausgeblendet.
-        //         else if (platzFuerTitel < titleWidth) {
-        //             console.log(titleElement);                
-        //             document.getElementsByClassName("portal-title")[0].remove();
-        //         }
-        //     }
-        // }
-    // },
 
         /**
-        * Renders the title if enough space available.
+        * Depending on the available space, the titletext and titlelogo is rendered.
         * @returns {void}
         */
         renderDependingOnSpace: function () {
@@ -95,70 +38,47 @@ export default {
                 searchbarWidth,
                 navBarWidth,
                 titleWidth,
+                titleTextWidth,
                 rest,
+                logo,
                 doRender = false;
-            const searchBarIconEl = document.getElementById("searchbar"),
-                titleEl = document.getElementById("portalTitle"),
+            const titleEl = document.getElementsByClassName("portal-title"),
                 titlePadding = 10;
+            
+            this.$el.style.display = "block";
+            document.getElementById('title-text').style.display = 'inline-block';
 
             if (document.getElementById("searchbar")) {
                 navMenuWidth = document.getElementById("root").offsetWidth;
-                searchbarWidth = document.getElementById("searchForm").offsetWidth + searchBarIconEl.offsetWidth;
+                searchbarWidth = document.getElementById("searchbar").offsetWidth;
                 navBarWidth = document.getElementById("main-nav").offsetWidth;
-                titleWidth = titleEl ? titleEl.offsetWidth : 0;
-                this.$store.state.Title.titleWidth = titleWidth;
+                titleTextWidth = document.getElementById("title-text").offsetWidth;
+                logo = document.getElementById("logo").offsetWidth;
+                titleWidth = titleEl ? titleEl[0].offsetWidth : 0;
+
                 if (!this.$store.state.Title.titleWidth) {
                     if (titleWidth > titlePadding) {
                         this.$store.state.Title.titleWidth = titleWidth;
-                        this.$el.width(titleWidth);
                     }
                 }
-                else if (titleWidth > this.$store.state.Title.titleWidth) {
-                    this.$store.state.Title.titleWidth = titleWidth;
-                    this.$el.width(titleWidth);
-                }
-                else {
-                    titleWidth = this.$store.state.Title.titleWidth;
-                }
-                rest = navBarWidth - navMenuWidth - searchbarWidth;
 
-                // check if title is smaller than the rest: set new width at el to visualize ... at the end of the title
-                if ((rest - titleWidth) < 0 && rest > 0 && (rest - titleWidth) > -(titleWidth - 30)) {
-                    this.$el.width(rest);
-                    doRender = true;
+                rest = navBarWidth - navMenuWidth - searchbarWidth;
+                document.getElementById("title-text").style.width = (rest - logo - titlePadding).toString() + "px";
+
+                if (logo < rest && this.$el.style.display === 'none') {
+                    this.$el.style.display = 'block';
                 }
-                else if (titleWidth > 0) {
-                    this.$el.width(titleWidth);
-                }
-                if (doRender) {
-                    console.log("X)");
-                    this.render();
-                }
-                else if (titleWidth < rest && rest > 50) {
-                    this.render();
-                    console.log("Y");
-                }
-                else {
-                    console.log(this.$el);
+                else if (rest < logo && this.$el.style.display === "block") {
                     this.$el.style.display = 'none';
-                    // reset width at title, else the header may be wrapped
-                    this.$el.style.width = "auto";
+                }
+                if (rest - titleTextWidth - logo - titlePadding < (30/titleTextWidth * 100) - titleTextWidth && document.getElementById('title-text').style.display === 'inline-block') {
+                    document.getElementById('title-text').style.display = 'none';
+                }
+                else if (rest - titleTextWidth - logo - titlePadding > (30/titleTextWidth * 100) && document.getElementById('title-text').style.display === 'none') {
+                    document.getElementById('title-text').style.display = 'inline-block';
                 }
             }
         },
-        /**
-        * Render function for title.
-        * @returns {void}
-        */
-        render: function () {
-            const attr = this.$store.state.Title;
-            console.log(attr);
-            console.log(this.$el);
-            this.$el.style.display = 'block';
-            // this.$el.html(this.template(attr));
-
-            return this;
-        }
     },
     computed: {
         /**
@@ -187,10 +107,7 @@ export default {
         */
         logo () {
             const deprecatedParameter = this.getDeprecatedParameters('PortalLogo');
-            this.$store.watch(
-                (state) => state.Title.TitleWidth, test => {console.log("changes")});
 
-            
             return ((deprecatedParameter === undefined) ? this.$store.state.Title.logo : deprecatedParameter);
         },
         /**
@@ -209,45 +126,20 @@ export default {
 
         myBus.listenTo(Radio.channel("Title"), {
             "setSize": function () {
-                console.log("listenTo");
-                that.renderDependingOnSpace();
+                setTimeout(function() {
+                    that.renderDependingOnSpace();
+                }, 500);
             }
         })
-
-        // console.log(this.$store.state);
-        // this.$store.subscribe((mutation, state) => {
-        //     console.log(mutation.type);
-        //     if (mutation.type === "changedTitleWidth") {
-        //         console.log(`Updating to ${state.TitleWidth}`)
-        //     }
-        // })
-        // this.$store.watch(
-        //     (state, getters) => getters.TitleWidth,
-        //     (newValue, oldValue) => {
-        //         console.log('Updating from ${oldValue} to ${newValue}');
-        //     }
-        // )
-        // this.$store.commit('changedTitleWidth', 10);
-        // this.$store.commit('changedTitleWidth', 6);
     }
-    // updated: function () {
-    //     this.$nextTick(function () {
-    //         this.renderDependingOnSpace();
-    //     })
-    // },
-    // watch: {
-    //     TitleWidth: function () {
-    //         console.log("Hello");
-    //     }
-    // }
 }
 </script>
 
 <template>
     <div class="portal-title">
         <a :href="link" target="_blank" :data-toggle="title" data-placement="bottom" :title="toolTip">
-            <img :src="logo">
-            <span v-html="title"/>
+            <img id="logo" :src="logo">
+            <span id="title-text" v-html="title"/>
         </a>
     </div>
 </template>
@@ -276,6 +168,7 @@ export default {
             font-size: 26px;
             font-family: @font_family_1;
             white-space: nowrap;
+            overflow: hidden;
             text-overflow: ellipsis;
             display: inline-block;
             vertical-align: middle;
