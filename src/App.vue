8934ac6f248ee69545d08562d8a1f34b69c33e9b
Burmester <BurmesKi@fhhnet.stadt.hamburg.de>
2020-03-04 08:10:28 +0100

    fix errors after rebasing BG-272

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,10 @@
 <script>
-import Alerting from './components/Alerting.vue'
-import SupplyCoord from './components/SupplyCoord.vue'
-import ScaleLine from "./components/ScaleLine.vue";
-import Title from "./components/Title.vue";
+import Alerting from './components/Alerting.vue';
+import SupplyCoord from './components/SupplyCoord.vue';
+import ScaleLine from './components/ScaleLine.vue';
+import Title from './components/Title.vue';
 
 export default {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    name: "App",
-    components: {
-        Alerting,
-=======
-    name: 'App',
-    components: {
-        Alerting,
-        Title,
->>>>>>> refactor scaleLine from backbone to vue
-        SupplyCoord,
-        ScaleLine
-    }
-};
-<<<<<<< HEAD
-=======
   name: 'App',
   components: {
     Alerting,
@@ -30,13 +13,10 @@ export default {
     ScaleLine
   }
 }
->>>>>>> update - eslint for vue
-=======
->>>>>>> refactor scaleLine from backbone to vue
 </script>
 
 <template>
-    <div id="app">
+    <div id='app'>
         <Alerting />
         <SupplyCoord />
         <ScaleLine />
