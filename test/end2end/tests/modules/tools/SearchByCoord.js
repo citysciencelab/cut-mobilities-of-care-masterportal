51c7ca49c9c4e7eb23a9f8f9f186e894d43986fd
Dennis Sen <dennis.sen@dataport.de>
2020-03-10 10:14:31 +0100

    MPLGV-40 remove test isolation

--- test/end2end/tests/modules/tools/SearchByCoord.js
+++ test/end2end/tests/modules/tools/SearchByCoord.js
@@ -11,7 +11,7 @@ const webdriver = require("selenium-webdriver"),
  * @returns {void}
  */
 async function SearchByCoordTests ({builder, url, resolution}) {
-    describe.only("SearchByCoord", function () {
+    describe("SearchByCoord", function () {
         const selectors = {
                 tools: By.xpath("//span[contains(.,'Werkzeuge')]"),
                 toolSearchByCoord: By.xpath("//a[contains(.,'Koordinatensuche')]"),
