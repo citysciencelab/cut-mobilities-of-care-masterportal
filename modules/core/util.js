4da1776e8f4e2513ccc948d29fd37c1131f3444c
Burmester <BurmesKi@fhhnet.stadt.hamburg.de>
2020-03-24 13:53:29 +0100

    update BG-395 correct jsDoc params

--- modules/core/util.js
+++ modules/core/util.js
@@ -783,7 +783,7 @@ const Util = Backbone.Model.extend(/** @lends Util.prototype */{
      * Return a copy of the object, filtered to only have values for the whitelisted keys
      * (or array of valid keys).
      * @param {Object} object - the object.
-     * @param {Array} keys - the key(s) to search for.
+     * @param {Array<Number>} keys - the key(s) to search for.
      * @returns {Object} - returns the entry/entries with the right key/keys.
      */
     pick: function (object, keys) {
@@ -797,21 +797,16 @@ const Util = Backbone.Model.extend(/** @lends Util.prototype */{
 
     /** Looks through the list and returns the firts value that matches all of the key-value pairs
      * listed in hitId.
-     * @param {list} list - the list.
+     * @param {Array<Object>} list - the list.
      * @param {Object} findId - the id/entry to search for.
      * @returns {Object} - returns the first value/entry, that matches.
      */
-    findWhereJs: function (list, findId) {
-        let result;
-
-        if (findId !== undefined && list !== undefined) {
-            result = list.find(
-                item => Object.keys({id: findId}).every(
-                    key => item[key] === {id: findId}[key]
-                )
-            );
-        }
-        return result;
+    findWhereJs: function (list = [], findId = "") {
+        return list.find(
+            item => Object.keys({id: findId}).every(
+                key => item[key] === {id: findId}[key]
+            )
+        );
     }
 });
 
