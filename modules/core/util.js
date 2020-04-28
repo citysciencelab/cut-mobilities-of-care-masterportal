d1719ae1b1386d68f57f69953943219d0ed7d1ab
Feng Feng <feng.feng@gv.hamburg.de>
2020-04-28 09:29:06 +0200

    add - new central function to replace of undercore

--- modules/core/util.js
+++ modules/core/util.js
@@ -87,7 +87,10 @@ const Util = Backbone.Model.extend(/** @lends Util.prototype */{
             "groupBy": this.groupBy,
             "pick": this.pick,
             "omit": this.omit,
-            "findWhereJs": this.findWhereJs
+            "findWhereJs": this.findWhereJs,
+            "isEqual": this.isEqual,
+            "toObject": this.toObject,
+            "isEmpty": this.isEmpty
         }, this);
 
         channel.on({
@@ -623,8 +626,8 @@ const Util = Backbone.Model.extend(/** @lends Util.prototype */{
             else {
                 Radio.trigger("Alert", "alert", {
                     text: "<strong>Der Parametrisierte Aufruf des Portals ist leider schief gelaufen!</strong>"
-                    + "<br> Der URL-Paramater <strong>Config</strong> verlangt eine Datei mit der Endung \".json\"."
-                    + "<br> Es wird versucht die config.json unter dem Standardpfad zu laden",
+                        + "<br> Der URL-Paramater <strong>Config</strong> verlangt eine Datei mit der Endung \".json\"."
+                        + "<br> Es wird versucht die config.json unter dem Standardpfad zu laden",
                     kategorie: "alert-warning"
                 });
             }
@@ -816,7 +819,6 @@ const Util = Backbone.Model.extend(/** @lends Util.prototype */{
         return filteredObj;
     },
 
-
     /** Looks through the list and returns the firts value that matches all of the key-value pairs
      * listed in hitId.
      * @param {Object[]} [list=[]] - the list.
@@ -829,6 +831,92 @@ const Util = Backbone.Model.extend(/** @lends Util.prototype */{
                 key => item[key] === {id: findId}[key]
             )
         );
+    },
+
+    /**
+     * Check if two objects are same
+     * @param {Object} first the first object
+     * @param {Object} second the second object
+     * @returns {Boolean} true or false
+     */
+    isEqual: function (first, second) {
+        // If the value of either variable is empty, we can instantly compare them and check for equality.
+        if (first === null || first === undefined || second === null || second === undefined) {
+            return first === second;
+        }
+
+        // If neither are empty, we can check if their constructors are equal. Because constructors are objects, if they are equal, we know the objects are of the same type (though not necessarily of the same value).
+        if (first.constructor !== second.constructor) {
+            return false;
+        }
+
+        // If we reach this point, we know both objects are of the same type so all we need to do is check what type one of the objects is, and then compare them
+        if (first instanceof Function || first instanceof RegExp) {
+            return first === second;
+        }
+
+        // Throught back to the equlity check we started with. Just incase we are comparing simple objects.
+        if (first === second || first.valueOf() === second.valueOf()) {
+            return true;
+        }
+
+        // If the value of check we saw above failed and the objects are Dates, we know they are not Dates because Dates would have equal valueOf() values.
+        if (first instanceof Date) {
+            return false;
+        }
+
+        // If the objects are arrays, we know they are not equal if their lengths are not the same.
+        if (Array.isArray(first) && first.length !== second.length) {
+            return false;
+        }
+
+        // If we have gotten to this point, we need to just make sure that we are working with objects so that we can do a recursive check of the keys and values.
+        if (!(first instanceof Object) || !(second instanceof Object)) {
+            return false;
+        }
+
+        // We now need to do a recursive check on all children of the object to make sure they are deeply equal
+        const firstKeys = Object.keys(first),
+            // Here we just make sure that all the object keys on this level of the object are the same.
+            allKeysExist = Object.keys(second).every(
+                i => firstKeys.indexOf(i) !== -1
+            ),
+
+            // Finally, we pass all the values of our of each object into this function to make sure everything matches
+            allKeyValuesMatch = firstKeys.every(
+                i => this.isEqual(first[i], second[i])
+            );
+
+        return allKeysExist && allKeyValuesMatch;
+    },
+
+    /**
+     * Converts lists into objects
+     * @param {Array} list to be converted
+     * @param {Array} values the corresponding values of parallel array
+     * @returns {Object} result
+     */
+    toObject: function (list, values) {
+        const result = {};
+
+        for (let i = 0, length = list.length; i < length; i++) {
+            if (values) {
+                result[list[i]] = values[i];
+            }
+            else {
+                result[list[i][0]] = list[i][1];
+            }
+        }
+        return result;
+    },
+
+    /**
+     * Checks if value is an empty object or collection.
+     * @param {Object} obj the object to be checked
+     * @returns {boolean} true or false
+     */
+    isEmpty: function (obj) {
+        return [Object, Array].includes((obj || {}).constructor) && !Object.entries(obj || {}).length;
     }
 });
 
