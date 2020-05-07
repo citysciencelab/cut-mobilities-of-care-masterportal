979adf97ee280b1b0ae2710727a255597186ee34
felixnagel87 <felix.nagel@gv.hamburg.de>
2020-05-07 15:39:29 +0200

    fix: layer info pane was too wide

--- modules/core/modelList/folder/model.js
+++ modules/core/modelList/folder/model.js
@@ -61,11 +61,7 @@ const Folder = Item.extend(/** @lends Folder.prototype */{
         // Wenn alle Layer in einem Folder selektiert sind, wird der Folder auch selektiert
         if (this.get("parentId") === "Overlayer") {
             items = Radio.request("Parser", "getItemsByAttributes", {parentId: this.get("id")});
-            items.forEach(function (item) {
-                if (item.isSelected === false) {
-                    isEveryLayerSelected = false;
-                }
-            });
+            isEveryLayerSelected = items.every(item => item.isSelected === true);
 
             if (isEveryLayerSelected === true) {
                 this.setIsSelected(true);
@@ -73,11 +69,7 @@ const Folder = Item.extend(/** @lends Folder.prototype */{
         }
         if (this.get("id") === "tools") {
             items = Radio.request("Parser", "getItemsByAttributes", {parentId: this.get("id")});
-            items.forEach(function (item) {
-                if (item.isVisibleInMenu === true) {
-                    isEveryToolInvisible = false;
-                }
-            });
+            isEveryToolInvisible = items.every(item => item.isVisibleInMenu === false);
 
             if (isEveryToolInvisible === true) {
                 this.setIsVisibleInMenu(false);
