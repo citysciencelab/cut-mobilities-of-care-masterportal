be30740bcea1ed801fdbc26c20585839fb92d1d2
Dennis Sen <dennis.sen@dataport.de>
2020-05-13 13:44:09 +0200

    MPR-102 implement and localize zoom; delete old files

--- src/modules/controls/zoom/components/Zoom.vue
+++ src/modules/controls/zoom/components/Zoom.vue
@@ -3,35 +3,61 @@
 import {mapGetters, mapActions} from "vuex";
 import ControlIcon from "../../ControlIcon.vue";
 
+/**
+ * Zoom Component.
+ * NOTE This should eventually listen to the VueX map module
+ * as soon as 3D/OB mode are implemented in it. The flag
+ * "mapMode" is already prepared in it, but not used yet.
+ * @listens Map#RadioTriggerMapChange
+ */
 export default {
     name: "Zoom",
     components: {
         ControlIcon
     },
     props: {},
+    data () {
+        return {
+            mapChannel: Radio.channel("Map"),
+            visibleInMapMode: null // set in .created
+        };
+    },
     computed: {
         ...mapGetters(
             "Map",
-            ["zoomLevel", "maximumZoomLevelActive", "minimumZoomLevelActive"]
+            ["maximumZoomLevelActive", "minimumZoomLevelActive"]
         )
     },
+    created () {
+        this.checkModeVisibility();
+        this.mapChannel.on("change", this.checkModeVisibility);
+    },
+    beforeDestroy () {
+        this.mapChannel.off("change", this.checkModeVisibility);
+    },
     methods: {
-        ...mapActions("Map", ["increaseZoomLevel", "decreaseZoomLevel"])
+        ...mapActions("Map", ["increaseZoomLevel", "decreaseZoomLevel"]),
+        checkModeVisibility () {
+            this.visibleInMapMode = Radio.request("Map", "getMapMode") === "2D";
+        }
     }
 };
 </script>
 
 <template>
-    <div class="zoom-buttons">
+    <div
+        v-if="visibleInMapMode"
+        class="zoom-buttons"
+    >
         <ControlIcon
             icon-name="plus"
-            title="Zoom In"
+            :title="$t(`common:modules.controls.zoom.zoomIn`)"
             :active="!maximumZoomLevelActive"
             :on-click="increaseZoomLevel"
         />
         <ControlIcon
             icon-name="minus"
-            title="Zoom Out"
+            :title="$t(`common:modules.controls.zoom.zoomOut`)"
             :active="!minimumZoomLevelActive"
             :on-click="decreaseZoomLevel"
         />
