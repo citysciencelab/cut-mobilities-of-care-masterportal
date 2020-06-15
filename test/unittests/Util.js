4cb85a0126930e112ba9972fe1db1aa39cb04bf3
Burmester <BurmesKi@fhhnet.stadt.hamburg.de>
2020-06-15 07:28:29 +0200

    update Util.js BG-677

--- test/unittests/Util.js
+++ test/unittests/Util.js
@@ -36,8 +36,8 @@ const fs = require("fs"),
 
             return xml;
         },
-        getDescribeFeatureTypeResponse: function () {
-            const xml = fs.readFileSync(this.get("basepath") + "resources/testDescribeFeatureTypeResponse.xml", "utf8"),
+        getDescribeFeatureTypeResponse: function (path) {
+            const xml = fs.readFileSync(this.get("basepath") + path, "utf8"),
                 xmlObject = new window.DOMParser().parseFromString(xml, "text/xml");
 
             return xmlObject;
@@ -53,41 +53,5 @@ const fs = require("fs"),
     });
 
 addProjection(proj);
-
-
-Util = Backbone.Model.extend({
-    defaults: {
-        basepath: "./test/unittests/"
-    },
-    initialize: function () {
-        return null;
-    },
-    createTestFeatures: function (path) {
-        const format = new WFS({
-                featureNS: "http://www.deegree.org/app"
-            }),
-            data = fs.readFileSync(this.get("basepath") + path, "utf8"),
-            features = format.readFeatures(data);
-
-        return features;
-    },
-
-    getGeoJsonTestFeatures: function () {
-        const geojson = JSON.parse(fs.readFileSync(this.get("basepath") + "resources/testFeatures.json", "utf8"));
-
-        return geojson;
-    },
-    getCswResponse: function () {
-        const xml = fs.readFileSync(this.get("basepath") + "resources/testCswResponse.xml", "utf8");
-
-        return xml;
-    },
-    getDescribeFeatureTypeResponse: function (path) {
-        const xml = fs.readFileSync(this.get("basepath") + path, "utf8"),
-            xmlObject = new window.DOMParser().parseFromString(xml, "text/xml");
-
-        return xmlObject;
-    }
-});
 
 export default Util;
