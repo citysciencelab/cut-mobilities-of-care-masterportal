97d9652a45dd0d8ae58bfb4d33e1f54877235c74
felixnagel87 <felix.nagel@gv.hamburg.de>
2020-07-16 17:30:27 +0200

    update: linter

--- src/modules/tools/kmlImport/store/indexKmlImport.js
+++ src/modules/tools/kmlImport/store/indexKmlImport.js
@@ -4,10 +4,6 @@ import state from "./stateKmlImport";
 import actions from "./actionsKmlImport";
 import {generateSimpleGetters, generateSimpleMutations} from "../../../../app-store/utils/generators";
 
-
-console.log({...generateSimpleMutations(state)});
-
-
 export default composeModules([GenericTool, {
     namespaced: true,
     state,
