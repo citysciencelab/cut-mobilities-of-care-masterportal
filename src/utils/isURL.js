b22b6c30da41a5fc7decf51abc43212e6da02bb6
Darge <DargeFi@fhhnet.stadt.hamburg.de>
2020-07-29 14:10:17 +0200

    LGVP1-61 fixes after code review

--- src/utils/isURL.js
+++ src/utils/isURL.js
@@ -1,12 +1,9 @@
 /**
- * @returns {boolean} true if screen is considered mobile device
+ * @returns {boolean} true if incoming string matches RegExp for website links or html files
  * @param {string} str incoming string to test against regex
  */
 function isURL (str) {
-    if ((/(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])/i).test(str) || new RegExp(/[^/]+(?:\.html)$/i).test(str)) {
-        return true;
-    }
-    return false;
+    return (/(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])/i).test(str) || (/[^/]+(?:\.html)$/i).test(str);
 }
 
 export default isURL;
