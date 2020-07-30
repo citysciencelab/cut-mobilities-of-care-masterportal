49e2d56a38eed4b727303471417e9fee9e6e5a3f
Darge <DargeFi@fhhnet.stadt.hamburg.de>
2020-07-30 08:34:25 +0200

    LGVP1-61 updated JSDOC for isURL function

--- src/utils/isURL.js
+++ src/utils/isURL.js
@@ -1,6 +1,7 @@
 /**
- * @returns {boolean} true if incoming string matches RegExp for website links or html files
- * @param {string} str incoming string to test against regex
+ * Returns true if the RegExp is matched.
+ * @param {string} str Incoming string to test against RegExp.
+ * @returns {boolean} True if incoming string matches RegExp for website links or html files.
  */
 function isURL (str) {
     return (/(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])/i).test(str) || (/[^/]+(?:\.html)$/i).test(str);
