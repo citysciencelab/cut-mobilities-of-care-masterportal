6ec501e92920bd2c7e31db52be9bb9de29feaffe
Darge <DargeFi@fhhnet.stadt.hamburg.de>
2020-07-31 11:21:43 +0200

    LGVP1-61 updated test

---
+++ src/utils/isURL.spec.js
@@ -0,0 +1,31 @@
+import isURL from "./isURL.js";
+import {expect} from "chai";
+
+describe("isURL", function () {
+    it("detects an URL in an incoming string", function () {
+        const incomingString = "https://test.example.com";
+
+        expect(isURL(incomingString)).to.be.true;
+    });
+    it("detects an URL in an incoming string", function () {
+        const incomingString = "http://test.example.com";
+
+        expect(isURL(incomingString)).to.be.true;
+    });
+    it("detects an HTML file in an incoming string", function () {
+        const incomingString = "test.example.com/index.html";
+
+        expect(isURL(incomingString)).to.be.true;
+    });
+    it("detects that an incoming string is not an URL or HTML file", function () {
+        const incomingString = "test@mail.com";
+
+        expect(isURL(incomingString)).to.be.false;
+    });
+    it("detects that an incoming string is not an URL or HTML file", function () {
+        const incomingString = "089 / 1234567";
+
+        expect(isURL(incomingString)).to.be.false;
+    });
+});
+
