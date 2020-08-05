1f0f465430ebb6ad57f2ee325672509571fa9e61
Burmester <BurmesKi@fhhnet.stadt.hamburg.de>
2020-08-05 15:21:01 +0200

    central test instance for deprecated code BG-837

--- src/app-store/mutations.js
+++ src/app-store/mutations.js
@@ -1,4 +1,98 @@
-const mutations = {
+
+// beachten, dass es mehrer Pfade für die Tools geben kann. Alle müssen angegeben werden.
+const deprecatedCode = {
+    "toolTip": ["Portalconfig.portalTitle.tooltip"],
+    "supplyCoord": ["Portalconfig.menu.coord", "Portalconfig.menu.tools.children.coord"]
+};
+
+/**
+ * Function to check if the deprecated parameters could be specified for more than one location e.g. they (location of the parameter or tool) have multiple possible paths.
+ * Furthermore the function checks whether the given paths for the parameters are defined or undefined.
+ * @param {String} deprecatedPath - dotted string. The path in the config of the old and deprecated parameter.
+ * @param {config} config - the config.json.
+ * @returns {Object} - returns a new config.json without the deprecated parameters. They were replaced by the actual ones.
+*/
+function checkWhereDeprecated (deprecatedPath, config) {
+    let updatedConfig = {...config};
+
+    Object.entries(deprecatedPath).forEach((entry) => {
+        const currentCode = entry[0];
+
+        if (entry[1].length === 1) {
+            const deprecatedParameters = getDeprecatedParameters(entry[1][0], config);
+
+            updatedConfig = findDeprecatedCode(currentCode, deprecatedParameters, config);
+        }
+        else {
+            for (const path of entry[1]) {
+                const deprecatedParameters = getDeprecatedParameters(path, config);
+
+                if (deprecatedParameters.output !== undefined) {
+                    updatedConfig = findDeprecatedCode(currentCode, deprecatedParameters, config);
+                }
+            }
+        }
+    });
+    return updatedConfig;
+}
+
+/**
+ * Function to determine:
+ * Firstly: the path as dotted string.
+ * Secondly: the output given by the config.json for the path with the deprecated parameter.
+ * Thirdly: the deprecated key/parameter itself.
+ * @param {Array} entry - Array with the single "steps" / elements of the deprecated path.
+ * @param {Object} config - The config.json.
+ * @returns {Object} - returns an object with the three mentioned above parameters.
+*/
+function getDeprecatedParameters (entry, config) {
+    const splittedPath = entry.split("."),
+        output = splittedPath.reduce((object, index) => object[index], config),
+        deprecatedKey = splittedPath[splittedPath.length - 1];
+
+    return {
+        "splittedPath": splittedPath,
+        "output": output,
+        "deprecatedKey": deprecatedKey
+    };
+}
+
+/**
+ * Function to find and replace the old deprecated path.
+ * Inserts the new and current key into the config instead of the deprecated parameter.
+ * The deprecated parameter is deleted. The content is allocated to the new key.
+ * @param {String} currentParameter - the new current parameter to repalce the deprecated parameter.
+ * @param {Object} deprecatedParameters - this object contains the path with the deprecated parameter, the output/content of the deprecated parameter and the deprecated parameter itself.
+ * @param {Object} config - the config.json.
+ * @returns {Object} - returns a updated config where the deprecated parameters are replaced by the new and current ones.
+*/
+function findDeprecatedCode (currentParameter, deprecatedParameters, config) {
+    const updatedConfig = {...config},
+        path = deprecatedParameters.splittedPath,
+        output = deprecatedParameters.output,
+        deprecatedKey = deprecatedParameters.deprecatedKey;
+    let current = updatedConfig;
+
+    path.pop();
+    path.push(currentParameter);
+    path.forEach((element, index) => {
+        if (index === path.length - 1 && output !== undefined) {
+            current[currentParameter] = output;
+            delete current[deprecatedKey];
+        }
+        else {
+            if (!current[element]) {
+                current[element] = {};
+            }
+            current = current[element];
+        }
+    });
+    // });
+
+    return updatedConfig;
+}
+
+export default {
     /**
      * Sets config.json.
      * @param {object} state store state
@@ -6,7 +100,7 @@ const mutations = {
      * @returns {void}
      */
     setConfigJson (state, config) {
-        state.configJson = config;
+        state.configJson = checkWhereDeprecated(deprecatedCode, config);
     },
     /**
      * Sets config.js.
@@ -36,5 +130,3 @@ const mutations = {
         state.i18NextInitialized = isInitialized;
     }
 };
-
-export default mutations;
