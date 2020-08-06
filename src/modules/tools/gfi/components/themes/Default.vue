a89e2d27432d96aed546b3d3f94a2aa0a0b6b93b
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2020-08-06 10:41:56 +0200

    MPR 232: add GFI Parent Component

---
+++ src/modules/tools/gfi/components/themes/Default.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default {
+    name: "Default"
+};
+</script>
+
+<template>
+    <div>
+        <h1>
+            Default
+        </h1>
+    </div>
+</template>
+
+
+<style>
+
+</style>
