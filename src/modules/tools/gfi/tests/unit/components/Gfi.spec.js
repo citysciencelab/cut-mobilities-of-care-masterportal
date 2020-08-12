505bb4ef3f393afcf743396a8fe13d5707b65dbb
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2020-08-12 10:11:03 +0200

    MPR-234: add unittests for gfi mobile

--- src/modules/tools/gfi/tests/unit/components/Gfi.spec.js
+++ src/modules/tools/gfi/tests/unit/components/Gfi.spec.js
@@ -50,7 +50,7 @@ describe("Gfi.vue", () => {
         expect(wrapper.findComponent({name: "Detached"}).exists()).to.be.false;
     });
 
-    it("no child component should be found if gfi has no features", async () => {
+    it("no child component should be found if gfi has no features", () => {
         const wrapper = shallowMount(GfiComponent, {
             computed: {
                 gfiFeatures: () => null
@@ -83,9 +83,18 @@ describe("Gfi.vue", () => {
                 computed: {
                     isMobile: () => true,
                     isActive: () => true,
-                    gfiFeatures: () => [{}, {
+                    gfiFeatures: () => [{
                         getTheme: () => "default",
                         getTitle: () => "Hallo",
+                        "attributesToShow": "showAll",
+                        "olFeature": {
+                            getProperties: sinon.stub()
+                        },
+                        "html": null
+                    },
+                    {
+                        getTheme: () => "default",
+                        getTitle: () => "Hallo2",
                         "attributesToShow": "showAll",
                         "olFeature": {
                             getProperties: sinon.stub()
@@ -97,9 +106,218 @@ describe("Gfi.vue", () => {
                 store
             });
 
-
-        await wrapper.find(Mobile).vm.$emit("close");
+        await wrapper.findComponent(Mobile).vm.$emit("close");
         expect(mockMapMutations.setGfiFeatures.calledOnce).to.be.true;
-        expect(wrapper.vm.pagerIndex).to.be.equal(0);
+        expect(wrapper.vm.pagerIndex).to.equal(0);
+    });
+
+    it("should display the footer", () => {
+        const wrapper = mount(GfiComponent, {
+            computed: {
+                isMobile: () => true,
+                isActive: () => true,
+                gfiFeatures: () => [{
+                    getTheme: () => "default",
+                    getTitle: () => "Feature 1",
+                    "attributesToShow": "showAll",
+                    "olFeature": {
+                        getProperties: sinon.stub()
+                    },
+                    "html": null
+                },
+                {}]
+            },
+            localVue
+        });
+
+        expect(wrapper.find(".pager-left").exists()).to.be.true;
+        expect(wrapper.find(".pager-right").exists()).to.be.true;
+    });
+
+    it("should display the next feature if pager-right is clicked", async () => {
+        const wrapper = mount(GfiComponent, {
+            computed: {
+                isMobile: () => true,
+                isActive: () => true,
+                gfiFeatures: () => [{
+                    getTheme: () => "default",
+                    getTitle: () => "Feature 1",
+                    "attributesToShow": "showAll",
+                    "olFeature": {
+                        getProperties: sinon.stub()
+                    },
+                    "html": null
+                },
+                {
+                    getTheme: () => "default",
+                    getTitle: () => "Feature 2",
+                    "attributesToShow": "showAll",
+                    "olFeature": {
+                        getProperties: sinon.stub()
+                    },
+                    "html": null
+                }]
+            },
+            localVue
+        });
+
+        await wrapper.find(".pager-right").trigger("click");
+        expect(wrapper.find(".modal-title").text()).to.equal("Feature 2");
+    });
+
+    it("should display the previous feature if pager-left is clicked", async () => {
+        const wrapper = mount(GfiComponent, {
+            computed: {
+                isMobile: () => true,
+                isActive: () => true,
+                gfiFeatures: () => [{
+                    getTheme: () => "default",
+                    getTitle: () => "Feature 1",
+                    "attributesToShow": "showAll",
+                    "olFeature": {
+                        getProperties: sinon.stub()
+                    },
+                    "html": null
+                },
+                {
+                    getTheme: () => "default",
+                    getTitle: () => "Feature 2",
+                    "attributesToShow": "showAll",
+                    "olFeature": {
+                        getProperties: sinon.stub()
+                    },
+                    "html": null
+                }]
+            },
+            localVue
+        });
+
+        wrapper.setData({pagerIndex: 1});
+        await wrapper.find(".pager-left").trigger("click");
+        expect(wrapper.find(".modal-title").text()).to.equal("Feature 1");
+    });
+
+    it("should disabled left pager if pagerIndex is zero", () => {
+        const wrapper = mount(GfiComponent, {
+            computed: {
+                isMobile: () => true,
+                isActive: () => true,
+                gfiFeatures: () => [{
+                    getTheme: () => "default",
+                    getTitle: () => "Feature 1",
+                    "attributesToShow": "showAll",
+                    "olFeature": {
+                        getProperties: sinon.stub()
+                    },
+                    "html": null
+                },
+                {}]
+            },
+            localVue
+        });
+
+        expect(wrapper.find(".pager-left").classes("disabled")).to.be.true;
+    });
+
+    it("should enabled right pager if pagerIndex is zero", () => {
+        const wrapper = mount(GfiComponent, {
+            computed: {
+                isMobile: () => true,
+                isActive: () => true,
+                gfiFeatures: () => [{
+                    getTheme: () => "default",
+                    getTitle: () => "Feature 1",
+                    "attributesToShow": "showAll",
+                    "olFeature": {
+                        getProperties: sinon.stub()
+                    },
+                    "html": null
+                },
+                {}]
+            },
+            localVue
+        });
+
+        expect(wrapper.find(".pager-right").classes("disabled")).to.be.false;
+    });
+
+    it("should disabled right pager if pagerIndex === gfiFeatures.length - 1", () => {
+        const wrapper = mount(GfiComponent, {
+            data () {
+                return {
+                    pagerIndex: 1
+                };
+            },
+            computed: {
+                isMobile: () => true,
+                isActive: () => true,
+                gfiFeatures: () => [{}, {
+                    getTheme: () => "default",
+                    getTitle: () => "Feature 1",
+                    "attributesToShow": "showAll",
+                    "olFeature": {
+                        getProperties: sinon.stub()
+                    },
+                    "html": null
+                }]
+            },
+            localVue
+        });
+
+        expect(wrapper.find(".pager-right").classes("disabled")).to.be.true;
+    });
+
+    it("should enable left pager if pagerIndex === gfiFeatures.length - 1", () => {
+        const wrapper = mount(GfiComponent, {
+            data () {
+                return {
+                    pagerIndex: 1
+                };
+            },
+            computed: {
+                isMobile: () => true,
+                isActive: () => true,
+                gfiFeatures: () => [{}, {
+                    getTheme: () => "default",
+                    getTitle: () => "Feature 1",
+                    "attributesToShow": "showAll",
+                    "olFeature": {
+                        getProperties: sinon.stub()
+                    },
+                    "html": null
+                }]
+            },
+            localVue
+        });
+
+        expect(wrapper.find(".pager-left").classes("disabled")).to.be.false;
+    });
+
+
+    it("should enable left pager and right pager if pagerIndex is between zero and gfiFeature.length - 1", () => {
+        const wrapper = mount(GfiComponent, {
+            data () {
+                return {
+                    pagerIndex: 1
+                };
+            },
+            computed: {
+                isMobile: () => true,
+                isActive: () => true,
+                gfiFeatures: () => [{}, {
+                    getTheme: () => "default",
+                    getTitle: () => "Feature 1",
+                    "attributesToShow": "showAll",
+                    "olFeature": {
+                        getProperties: sinon.stub()
+                    },
+                    "html": null
+                }, {}]
+            },
+            localVue
+        });
+
+        expect(wrapper.find(".pager-left").classes("disabled")).to.be.false;
+        expect(wrapper.find(".pager-right").classes("disabled")).to.be.false;
     });
 });
