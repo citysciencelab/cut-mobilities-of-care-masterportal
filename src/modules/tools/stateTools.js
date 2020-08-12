eaa0a44708f1c1fb824a5cfba3445b85291a3b40
Beyel <BeyelSv@fhhnet.stadt.hamburg.de>
2020-08-12 10:48:01 +0200

    BG-779 add jsdoc and changes coord to supplyCoord in portals

--- src/modules/tools/stateTools.js
+++ src/modules/tools/stateTools.js
@@ -2,6 +2,12 @@ import ScaleSwitcher from "./scale/components/ScaleSwitcher.vue";
 import SupplyCoord from "./supplyCoord/components/SupplyCoord.vue";
 import KmlImport from "./kmlImport/components/KmlImport.vue";
 
+/**
+ * User type definition
+ * @typedef {object} ToolsState
+ * @property {object} componentMap contains all tool components
+ * @property {object[]} configuredTools gets all tools that should be initialized
+ */
 const state = {
     componentMap: {
         scaleSwitcher: ScaleSwitcher,
