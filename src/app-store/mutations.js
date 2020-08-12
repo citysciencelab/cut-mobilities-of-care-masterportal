1296e8336d9de0c6dd7c7848689f90ce12011412
Burmester <BurmesKi@fhhnet.stadt.hamburg.de>
2020-08-12 12:52:55 +0200

    move functions from mutations to actions BG-837

--- src/app-store/mutations.js
+++ src/app-store/mutations.js
@@ -1,3 +1,4 @@
+import actions from "../app-store/actions"; // https://stackoverflow.com/questions/40487627/can-i-call-commit-from-one-of-mutations-in-vuex-store
 
 // The objects deprecatedParamsConfigJson and deprecatedParamsConfigJs store the current respectively new parameters and the related deprecated parameters.
 // The key describes the current parameter or more precisely the path to the new/current path.
@@ -11,7 +12,7 @@ const deprecatedParamsConfigJson = {
         "Portalconfig.portalTitle.title": ["Portalconfig.PortalTitle"],
         "Portalconfig.portalTitle.logo": ["Portalconfig.PortalLogo"],
         "Portalconfig.portalTitle.link": ["Portalconfig.LogoLink"],
-        "Portalconfig.portalTitle.toolTip": ["Portalconfig.LogoToolTip", "Portalconfig.portalTitle.tooltip"],
+        "Portalconfig.portalTitle.toolTip": ["Portalconfig.portalTitle.tooltip", "Portalconfig.LogoToolTip"],
         "Portalconfig.searchBar.bkg.zoomToResultOnHover": ["Portalconfig.searchBar.bkg.zoomToResult"],
         "Portalconfig.treeType": ["Portalconfig.Baumtyp"],
         "Portalconfig.controls.overviewMap.layerId": ["Portalconfig.controls.overviewMap.baselayer"],
@@ -22,92 +23,8 @@ const deprecatedParamsConfigJson = {
         "startUpModul": ["isInitOpen"]
     };
 
-/**
- * Function to check if the deprecated parameters could be specified for more than one location e.g. they (location of the parameter or tool) have multiple possible paths.
- * Furthermore the function checks whether the given paths for the parameters are defined or undefined.
- * @param {String} deprecatedPath - dotted string. The path in the config of the old and deprecated parameter.
- * @param {config} config - the config.json or config.js.
- * @returns {Object} - returns a new config (.json or .js) without the deprecated parameters. They were replaced by the actual ones.
-*/
-function checkWhereDeprecated (deprecatedPath, config) {
-    let updatedConfig = {},
-        parameters = {};
-
-    Object.entries(deprecatedPath).forEach((entry) => {
-        parameters = getDeprecatedParameters(entry, config);
-        if (parameters !== undefined && parameters.output !== undefined) {
-            updatedConfig = replaceDeprecatedCode(parameters, config);
-        }
-    });
-    return updatedConfig;
-}
-
-/**
- * Function to determine:
- * Firstly: the path as dotted string.
- * Secondly: the output given by the config.json for the path with the deprecated parameter.
- * Thirdly: the deprecated key/parameter itself.
- * @param {Array} entry - Array with the single "steps" / elements of the deprecated path.
- * @param {Object} config - The config.json or config.js.
- * @returns {Object} - returns an object with the three mentioned above parameters.
-*/
-function getDeprecatedParameters (entry, config) {
-    const newSplittedPath = entry[0].split(".");
-    let oldSplittedPath = "",
-        output = "",
-        deprecatedKey = "",
-        parameters;
-
-    entry[1].forEach((oldPathes) => {
-        oldSplittedPath = oldPathes.split(".");
-        output = oldSplittedPath.reduce((object, index) => object[index], config);
-        if (output === undefined) {
-            return;
-        }
-        deprecatedKey = oldSplittedPath[oldSplittedPath.length - 1];
-        parameters = {
-            "newSplittedPath": newSplittedPath,
-            "oldSplittedPath": oldSplittedPath,
-            "output": output,
-            "deprecatedKey": deprecatedKey
-        };
-    });
-    return parameters;
-}
-
-/**
- * Function to find and replace the old deprecated path.
- * Inserts the new and current key into the config instead of the deprecated parameter.
- * The deprecated parameter is deleted. The content is allocated to the new key.
- * @param {Array} parameters - contains the new current parameter to replace the deprecated parameter. Contains also an object which lists the path of the deprecated parameter, the output/content of the deprecated parameter and the deprecated parameter itself.
- * @param {Object} config - the config.json or config.js.
- * @returns {Object} - returns a updated config where the deprecated parameters are replaced by the new and current ones.
-*/
-function replaceDeprecatedCode (parameters, config) {
-    const updatedConfig = {...config},
-        output = parameters.output,
-        deprecatedKey = parameters.deprecatedKey,
-        splittedCurrentPath = parameters.newSplittedPath;
-    let current = updatedConfig;
-
-    splittedCurrentPath.forEach((element, index) => {
-        if (index === splittedCurrentPath.length - 1 && output !== undefined) {
-            current[element] = output;
-            console.warn(parameters.deprecatedKey + " is deprecated. Instead, please use the following path/parameter: " + String(parameters.newSplittedPath).replace(/,/g, ".") + " in the config.json. For this session it is automatically replaced.");
-        }
-        else {
-            if (!current[element]) {
-                current[element] = {};
-            }
-            current = current[element];
-        }
-        delete current[deprecatedKey];
-    });
-
-    return updatedConfig;
-}
-
 export default {
+
     /**
      * Sets config.json.
      * @param {object} state store state
@@ -115,7 +32,7 @@ export default {
      * @returns {void}
      */
     setConfigJson (state, config) {
-        state.configJson = checkWhereDeprecated(deprecatedParamsConfigJson, config);
+        state.configJson = actions.checkWhereDeprecated(deprecatedParamsConfigJson, config);
     },
     /**
      * Sets config.js.
@@ -124,7 +41,7 @@ export default {
      * @returns {void}
      */
     setConfigJs (state, config) {
-        state.configJs = checkWhereDeprecated(deprecatedParamsConfigJs, config);
+        state.configJs = actions.checkWhereDeprecated(deprecatedParamsConfigJs, config);
     },
     /**
      * Sets mobile flag.
