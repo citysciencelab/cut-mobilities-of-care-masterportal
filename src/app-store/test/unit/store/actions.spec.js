e3bdd13eb24e28999a519baf0352ae86a566fea9
Burmester <BurmesKi@fhhnet.stadt.hamburg.de>
2020-08-12 14:59:40 +0200

    unittest have been written for actions BG-837

--- src/app-store/test/unit/store/actions.spec.js
+++ src/app-store/test/unit/store/actions.spec.js
@@ -1,7 +1,18 @@
 import {expect} from "chai";
 import actions from "../../../actions";
 
-const singleDeprecatedEntryCase1 = ["Portalconfig.portalTitle.title", ["Portalconfig.PortalTitle"]],
+const deprecatedParamsConfigJson = {
+        "Portalconfig.portalTitle.title": ["Portalconfig.PortalTitle"],
+        "Portalconfig.portalTitle.logo": ["Portalconfig.PortalLogo"],
+        "Portalconfig.portalTitle.link": ["Portalconfig.LogoLink"],
+        "Portalconfig.portalTitle.toolTip": ["Portalconfig.portalTitle.tooltip", "Portalconfig.LogoToolTip"],
+        "Portalconfig.searchBar.bkg.zoomToResultOnHover": ["Portalconfig.searchBar.bkg.zoomToResult"],
+        "Portalconfig.treeType": ["Portalconfig.Baumtyp"],
+        "Portalconfig.controls.overviewMap.layerId": ["Portalconfig.controls.overviewMap.baselayer"],
+        "Portalconfig.mapView.startResolution": ["Portalconfig.mapView.resolution"],
+        "Portalconfig.searchBar.startZoomLevel": ["Portalconfig.searchBar.zoomLevel"]
+    },
+    singleDeprecatedEntryCase1 = ["Portalconfig.portalTitle.title", ["Portalconfig.PortalTitle"]],
     singleDeprecatedEntryCase2 = ["Portalconfig.treeType", ["Portalconfig.Baumtyp"]],
     singleDeprecatedEntryCase3 = ["Portalconfig.portalTitle.toolTip", ["Portalconfig.LogoToolTip", "Portalconfig.portalTitle.tooltip"]],
 
@@ -10,7 +21,7 @@ const singleDeprecatedEntryCase1 = ["Portalconfig.portalTitle.title", ["Portalco
             PortalTitle: "Testname",
             PortalLogo: "Testlink.de",
             LogoLink: "Testlogo",
-            LogoToolTip: "TTT = Test-Tool-Tip"
+            LogoToolTip: "Testtooltip"
         }
     },
     configCase2 = {
@@ -19,7 +30,7 @@ const singleDeprecatedEntryCase1 = ["Portalconfig.portalTitle.title", ["Portalco
                 title: "Testname",
                 logo: "Testlink.de",
                 link: "Testlogo",
-                tooltip: "TTT = Test-Tool-Tip"
+                tooltip: "Testtooltip"
             }
         }
     },
@@ -28,7 +39,7 @@ const singleDeprecatedEntryCase1 = ["Portalconfig.portalTitle.title", ["Portalco
             PortalTitle: "Testname",
             PortalLogo: "Testlink.de",
             LogoLink: "Testlogo",
-            LogoToolTip: "TTT = Test-Tool-Tip"
+            LogoToolTip: "Testtooltip"
         }
     },
 
@@ -43,6 +54,12 @@ const singleDeprecatedEntryCase1 = ["Portalconfig.portalTitle.title", ["Portalco
         "oldSplittedPath": ["Portalconfig", "Baumtyp"],
         "output": undefined,
         "deprecatedKey": "Baumtyp"
+    },
+    parametersCase3 = {
+        "newSplittedPath": ["Portalconfig", "portalTitle", "toolTip"],
+        "oldSplittedPath": ["Portalconfig", "portalTitle", "tooltip"],
+        "output": "Testtooltip",
+        "deprecatedKey": "tooltip"
     };
 
 describe("getDeprecatedParameters", function () {
@@ -70,7 +87,7 @@ describe("getDeprecatedParameters", function () {
         expect(testReturn).to.be.an("Object");
         expect(testReturn.newSplittedPath).to.deep.equal(["Portalconfig", "portalTitle", "toolTip"]);
         expect(testReturn.oldSplittedPath).to.deep.equal(["Portalconfig", "portalTitle", "tooltip"]);
-        expect(testReturn.output).to.equal("TTT = Test-Tool-Tip");
+        expect(testReturn.output).to.equal("Testtooltip");
         expect(testReturn.deprecatedKey).to.equal("tooltip");
     });
     it("should return an object with defined parameters", function () {
@@ -80,10 +97,11 @@ describe("getDeprecatedParameters", function () {
         expect(testReturn).to.be.an("Object");
         expect(testReturn.newSplittedPath).to.deep.equal(["Portalconfig", "portalTitle", "toolTip"]);
         expect(testReturn.oldSplittedPath).to.deep.equal(["Portalconfig", "LogoToolTip"]);
-        expect(testReturn.output).to.equal("TTT = Test-Tool-Tip");
+        expect(testReturn.output).to.equal("Testtooltip");
         expect(testReturn.deprecatedKey).to.equal("LogoToolTip");
     });
 });
+
 describe("replaceDeprecatedCode", function () {
     it("should return an updated config with replaced deprecated parameters", function () {
         const configCopy = {...configCase1},
@@ -93,10 +111,93 @@ describe("replaceDeprecatedCode", function () {
         expect(testReturn.Portalconfig.portalTitle.title).to.equal("Testname");
         expect(testReturn.Portalconfig.PortalTitle).to.be.undefined;
     });
+    it("should return an updated config with replaced deprecated parameters", function () {
+        const configCopy = {...configCase2},
+            testReturn = actions.replaceDeprecatedCode(parametersCase3, configCopy);
+
+        expect(testReturn).to.be.an("Object");
+        expect(testReturn.Portalconfig.portalTitle.toolTip).to.equal("Testtooltip");
+        expect(testReturn.Portalconfig.tooltip).to.be.undefined;
+    });
     it("should return an unchanged object compared to the transferred config", function () {
         const configCopy = {...configCase3},
             testReturn = actions.replaceDeprecatedCode(parametersCase2, configCopy);
 
         expect(testReturn).to.deep.equal(configCopy);
+    });
+});
+
+describe("checkWhereDeprecated", function () {
+    it("it should return an object with the correct keys and values", function () {
+        const config = {
+                Portalconfig: {
+                    PortalTitle: "Testname",
+                    PortalLogo: "Testlogo",
+                    LogoLink: "Testlink",
+                    LogoToolTip: "Testtooltip",
+                    Baumtyp: true,
+                    searchBar: {
+                        bkg: {
+                            zoomToResult: true
+                        },
+                        zoomLevel: 9
+                    },
+                    controls: {
+                        overviewMap: {
+                            baselayer: false
+                        }
+                    },
+                    mapView: {
+                        resolution: 40
+                    }
+                }
+            },
+
+            testReturn = actions.checkWhereDeprecated(deprecatedParamsConfigJson, config);
+
+        expect(testReturn.Portalconfig).to.be.an("Object");
+        expect(testReturn.Portalconfig.portalTitle.title).to.equal("Testname");
+        expect(testReturn.Portalconfig.portalTitle.logo).to.equal("Testlogo");
+        expect(testReturn.Portalconfig.portalTitle.link).to.equal("Testlink");
+        expect(testReturn.Portalconfig.portalTitle.toolTip).to.equal("Testtooltip");
+        expect(testReturn.Portalconfig.treeType).to.be.true;
+        expect(testReturn.Portalconfig.searchBar.bkg.zoomToResultOnHover).to.be.true;
+        expect(testReturn.Portalconfig.searchBar.startZoomLevel).to.equal(9);
+        expect(testReturn.Portalconfig.controls.overviewMap.layerId).to.be.false;
+        expect(testReturn.Portalconfig.mapView.startResolution).to.equal(40);
+
+        expect(testReturn.Portalconfig.PortalTitle).to.be.undefined;
+        expect(testReturn.Portalconfig.PortalLogo).to.be.undefined;
+        expect(testReturn.Portalconfig.LogoLink).to.be.undefined;
+        expect(testReturn.Portalconfig.LogoToolTip).to.undefined;
+        expect(testReturn.Portalconfig.searchBar.bkg.zoomToResult).to.be.undefined;
+        expect(testReturn.Portalconfig.Baumtyp).to.be.undefined;
+        expect(testReturn.Portalconfig.controls.overviewMap.baselayer).to.be.undefined;
+        expect(testReturn.Portalconfig.mapView.resolution).to.be.undefined;
+        expect(testReturn.Portalconfig.searchBar.zoomLevel).to.be.undefined;
+    });
+});
+describe("checkWhereDeprecated", function () {
+    it("it should return an object with the correct keys and values", function () {
+        const config = {
+                Portalconfig: {
+                    portalTitle: {
+                        title: "Testname",
+                        logo: "Testlogo",
+                        link: "Testlink",
+                        tooltip: "tooltip mit kleinem t"
+                    }
+                }
+            },
+
+            testReturn = actions.checkWhereDeprecated(deprecatedParamsConfigJson, config);
+
+        expect(testReturn.Portalconfig).to.be.an("Object");
+        expect(testReturn.Portalconfig.portalTitle.title).to.equal("Testname");
+        expect(testReturn.Portalconfig.portalTitle.logo).to.equal("Testlogo");
+        expect(testReturn.Portalconfig.portalTitle.link).to.equal("Testlink");
+        expect(testReturn.Portalconfig.portalTitle.toolTip).to.equal("tooltip mit kleinem t");
+
+        expect(testReturn.Portalconfig.portalTitle.tooltip).to.undefined;
     });
 });
