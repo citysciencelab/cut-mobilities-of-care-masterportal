4526abc4d6d9f7811cc5aee9471b1091ef17edf0
Micha Pikora <Micha.Pikora@gv.hamburg.de>
2020-09-18 14:44:32 +0200

    MPR fix vue: remove transformNaNtoNull - no extra utils for one-liners

--- src/utils/transformNaNToNull.js
+++
@@ -1,9 +0,0 @@
-/**
- * Transforms the given value to null, if it is not a number.
- *
- * @param {*} value value to check whether its a number.
- * @returns {Number|null} Returns null, if the input value is not a number.
- */
-export function transformNaNToNull (value) {
-    return isNaN(value) ? null : value;
-}
