9ef155b0b4f881be1d8f8ad90f1d82eefe09fd5b
sven_junker-beyel <sven.beyel@gv.hamburg.de>
2020-09-21 14:54:09 +0200

    MPR-286 add unit tests

--- src/modules/tools/gfi/components/themes/Solaratlas.vue
+++ src/modules/tools/gfi/components/themes/Solaratlas.vue
@@ -11,9 +11,9 @@ export default {
 </script>
 
 <template>
-    <div v-if="feature.getMappedProperties().area_pv_m2 > 0">
+    <div>
         <h6><strong>Solarpotential des Gebäudes: {{ feature.getMappedProperties().adrfull }}</strong></h6>
-        <p>
+        <p v-if="feature.getMappedProperties().area_pv_m2 > 0">
             Die Gesamteignungsfläche des Gebäudes beträgt {{ feature.getMappedProperties().area_pv_m2 }} m².
             Die mittlere Strahlungsmenge unter Berücksichtigung der Verschattung für dieses Gebäude beträgt {{ feature.getMappedProperties().ic_mean }} kWh/m²a.<br><br>
             <b>Photovoltaik</b> - Installierbare PV-Fläche<br>
@@ -30,10 +30,7 @@ export default {
             gut geeignet (EK1): {{ feature.getMappedProperties().area_st_1a }} m²<br><br>
             Der mögliche Wärmeertrag beträgt  {{ feature.getMappedProperties().p_st_mwh_a }} MWh/a.
         </p>
-    </div>
-    <div v-else>
-        <h6><strong>Solarpotential des Gebäudes: {{ feature.getMappedProperties().adrfull }}</strong></h6>
-        <p>
+        <p v-else>
             Dieses Gebäude ist für die Einrichtung einer Photovoltaik-Anlage nicht geeignet. Gegebenenfalls kann jedoch eine Solarthermie-Anlage nutzbringend betrieben werden.<br><br>
             <b>Solarthermie </b> - Installierbare Solarthermiefläche<br>
             sehr gut geeignet (EK2): {{ feature.getMappedProperties().area_st_2a }} m²<br>
