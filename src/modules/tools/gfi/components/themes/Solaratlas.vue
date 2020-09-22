5b8fa518430c833ed4409662057768496d5f6417
sven_junker-beyel <sven.beyel@gv.hamburg.de>
2020-09-22 09:43:47 +0200

    temp

--- src/modules/tools/gfi/components/themes/Solaratlas.vue
+++ src/modules/tools/gfi/components/themes/Solaratlas.vue
@@ -12,24 +12,23 @@ export default {
 
 <template>
     <div>
-        <h6><strong>Solarpotential des Gebäudes: {{ feature.getMappedProperties().adrfull }}</strong></h6>
-        <p v-if="feature.getMappedProperties().area_pv_m2 > 0">
-            Die Gesamteignungsfläche des Gebäudes beträgt {{ feature.getMappedProperties().area_pv_m2 }} m².
-            Die mittlere Strahlungsmenge unter Berücksichtigung der Verschattung für dieses Gebäude beträgt {{ feature.getMappedProperties().ic_mean }} kWh/m²a.<br><br>
-            <b>Photovoltaik</b> - Installierbare PV-Fläche<br>
-            sehr gut geeignet (EK4): {{ feature.getMappedProperties().area_ek4_p }} m²<br>
-            gut geeignet (EK3): {{ feature.getMappedProperties().area_ek3_p }} m²<br>
-            geeignet (EK2): {{ feature.getMappedProperties().area_ek2_p }} m²<br>
-            bedingt geeignet (EK1): {{ feature.getMappedProperties().area_ek1_p }} m²<br><br>
-            Auf der Dachfläche kann eine PV-Anlage mit einer Peakleistung von ca. {{ feature.getMappedProperties().powerp_15 }} kWp installiert werden.<br>
-            Der prognostizierte Stromertrag der PV-Anlage beträgt ca. {{ feature.getMappedProperties().p_pv_15 }} MWh/a.<br>
-            Die mögliche CO2-Einsparung beträgt {{ feature.getMappedProperties().co2_pv_15 }} t/a.<br><br>
-            Die Angaben beziehen sich auf die Verwendung eines Solarmoduls mit 15 % Wirkungsgrad und einer Anlageneffektivität (Performance Ratio) von 80 %.<br><br>
-            <b>Solarthermie </b> - Installierbare Solarthermiefläche
-            sehr gut geeignet (EK2): {{ feature.getMappedProperties().area_st_2a }} m²<br>
-            gut geeignet (EK1): {{ feature.getMappedProperties().area_st_1a }} m²<br><br>
-            Der mögliche Wärmeertrag beträgt  {{ feature.getMappedProperties().p_st_mwh_a }} MWh/a.
-        </p>
+        <h6><strong>{{ $t("modules.tools.gfi.themes.solaratlas.solarPotential", {ardfull: feature.getMappedProperties().adrfull}) }}</strong></h6>
+        <div v-if="feature.getMappedProperties().area_pv_m2 > 0">
+            {{ $t("modules.tools.gfi.themes.solaratlas.buildingSuitable", {
+                area_pv_m2: feature.getMappedProperties().area_pv_m2,
+                ic_mean: feature.getMappedProperties().ic_mean,
+                area_ek4_p: feature.getMappedProperties().area_ek4_p,
+                area_ek3_p: feature.getMappedProperties().area_ek3_p,
+                area_ek2_p: feature.getMappedProperties().area_ek2_p,
+                area_ek1_p: feature.getMappedProperties().area_ek1_p,
+                powerp_15: feature.getMappedProperties().powerp_15,
+                p_pv_15: feature.getMappedProperties().p_pv_15,
+                co2_pv_15: feature.getMappedProperties().co2_pv_15,
+                area_st_2a: feature.getMappedProperties().area_st_2a,
+                area_st_1a: feature.getMappedProperties().area_st_1a,
+                p_st_mwh_a: feature.getMappedProperties().p_st_mwh_a
+            }) }}
+        </div>
         <p v-else>
             Dieses Gebäude ist für die Einrichtung einer Photovoltaik-Anlage nicht geeignet. Gegebenenfalls kann jedoch eine Solarthermie-Anlage nutzbringend betrieben werden.<br><br>
             <b>Solarthermie </b> - Installierbare Solarthermiefläche<br>
