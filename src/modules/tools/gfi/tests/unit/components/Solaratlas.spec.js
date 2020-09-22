c9bbf2d5d313aa79250a54b49a883c6ab184cb6f
sven_junker-beyel <sven.beyel@gv.hamburg.de>
2020-09-22 13:49:26 +0200

    MPR-286 add multilangugae for gfitheme Solaratlas

--- src/modules/tools/gfi/tests/unit/components/Solaratlas.spec.js
+++ src/modules/tools/gfi/tests/unit/components/Solaratlas.spec.js
@@ -33,14 +33,60 @@ describe("src/modules/tools/gfi/components/themes/Solaratlas.vue", () => {
                     }
                 }
             },
-            localVue
+            localVue,
+            mocks: {
+                $t: (msg) => msg
+            }
         });
     });
 
-    it("should render html elements", () => {
+    it("should render three html elements", () => {
         expect(wrapper.find("h6").exists()).to.be.true;
         expect(wrapper.find("strong").exists()).to.be.true;
         expect(wrapper.find("p").exists()).to.be.true;
+    });
+
+    it("should render the html strong tag with text solarPotential", () => {
+        expect(wrapper.find("strong").text()).to.equals("modules.tools.gfi.themes.solaratlas.solarPotential");
+    });
+
+    it("should render the html p tag with text buildingSuitable for parameter area_pv_m2 > 0", () => {
+        expect(wrapper.find("p").text()).to.equals("modules.tools.gfi.themes.solaratlas.buildingSuitable");
+    });
+
+    it("should render the html p tag with text buildingUnsuitable for parameter area_pv_m2 === 0.0", () => {
+        wrapper = shallowMount(SolaratlasTheme, {
+            propsData: {
+                feature: {
+                    getMappedProperties: function () {
+                        return {
+                            adrfull: "Alstertor 5",
+                            area_ek1_p: "0.0",
+                            area_ek2_p: "0.0",
+                            area_ek3_p: "0.0",
+                            area_ek4_p: "307.7",
+                            area_pv_m2: "0.0",
+                            area_st_1a: "23.9",
+                            area_st_2a: "573.2",
+                            co2_pv_15: "19.3",
+                            ic_mean: "1025.8",
+                            p_pv_15: "37.8",
+                            p_st_mwh_a: "208.7",
+                            powerp_15: "46.0"
+                        };
+                    }
+                }
+            },
+            localVue,
+            mocks: {
+                $t: (msg) => msg
+            }
+        });
+
+        expect(wrapper.find("h6").exists()).to.be.true;
+        expect(wrapper.find("strong").exists()).to.be.true;
+        expect(wrapper.find("p").exists()).to.be.true;
+        expect(wrapper.find("p").text()).to.equals("modules.tools.gfi.themes.solaratlas.buildingUnsuitable");
     });
 
 });
