e183d1ac1c3bfb591709b0b26cd8dd8b8319fc04
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2020-09-23 09:41:47 +0200

    MPR-313: update communication between new gfi and print module

---
+++ modules/tools/print_/gfiEventBus.js
@@ -0,0 +1,28 @@
+/**
+ * The Radio Channel GFI is no longer available in the "new" Gfi (src/modules/tools/gfi).
+ * This file is used for communication between this module and the new Gfi.
+ * It can be deleted, if this module has been refactored.
+ */
+
+import store from "../../../src/app-store";
+
+store.watch((state, getters) => getters["Tools/Gfi/currentFeature"], feature => {
+    if (feature !== null) {
+        Radio.trigger("GFI", "isVisible", true);
+    }
+    else {
+        Radio.trigger("GFI", "isVisible", false);
+    }
+});
+
+Radio.channel("GFI").reply({
+    "getGfiForPrint": function () {
+        const feature = store.getters["Tools/Gfi/currentFeature"],
+            coordinate = store.getters["Map/clickCoord"];
+
+        if (feature !== null) {
+            return [feature.getMappedProperties(), feature.getTitle(), coordinate];
+        }
+        return [];
+    }
+});
