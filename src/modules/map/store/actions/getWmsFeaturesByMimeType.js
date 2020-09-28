94600b52770520a6c5902e738ca437bb381e32e9
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2020-09-28 10:59:34 +0200

    MPR-314: update doc

--- src/modules/map/store/actions/getWmsFeaturesByMimeType.js
+++ src/modules/map/store/actions/getWmsFeaturesByMimeType.js
@@ -24,7 +24,7 @@ export function getWmsFeaturesByMimeType (mimeType, url, layerName, gfiTheme, at
     }
 
     // mimeType === "text/html"
-    return [createGfiFeature(layerName, gfiTheme, attributesToShow, null, null, url)];
+    return [createGfiFeature(layerName, gfiTheme, attributesToShow, null, undefined, url)];
 }
 
 /**
@@ -100,9 +100,9 @@ export function getXmlFeatures (url, layerName, gfiTheme, attributesToShow, call
  * @param {String} gfiTheme the title of the theme - it does not check if the theme exists
  * @param {(Object|String)} attributesToShow an object of attributes to show or a string "showAll" or "ignore"
  * @param {Object} featureProperties an object with the data of the feature as simple key/value pairs
- * @param {String} id the id of the feature
+ * @param {String} [id=""] id the id of the feature
  * @param {String} [url=""] the url to call the wms features from
- * @returns {Object}  an object{getTheme, getTitle, getAttributesToShow, getProperties, getGfiUrl}
+ * @returns {Object} an object{getTheme, getTitle, getAttributesToShow, getProperties, getId, getGfiUrl}
  */
 export function createGfiFeature (layerName, gfiTheme, attributesToShow, featureProperties, id = "", url = "") {
     return {
