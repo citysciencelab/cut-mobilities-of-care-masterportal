64ceeac0314321caedaba69bb01023812d9ed15c
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2020-09-29 13:53:26 +0200

    MPR-304: open gfi by clicking on a feature in the search

---
+++ modules/searchbar/RadioBridge.js
@@ -0,0 +1,26 @@
+/**
+ * The Radio Channel GFI is no longer available in the "new" Gfi (src/modules/tools/gfi).
+ * This file is used for communication between this module and the new Gfi.
+ * It can be deleted, if this module has been refactored.
+ */
+import store from "../../src/app-store";
+import {createGfiFeature} from "../../src/modules/map/store/actions/getWmsFeaturesByMimeType";
+
+Radio.channel("VisibleVector").on({
+    "gfiOnClick": function (hit) {
+        const layerList = store.getters["Map/layerList"],
+            foundLayer = layerList.find(function (layer) {
+                return layer.get("id") === hit.layer_id;
+            }),
+            feature = createGfiFeature(
+                hit.name,
+                foundLayer.get("gfiTheme"),
+                hit.gfiAttributes,
+                hit.feature.getProperties(),
+                hit.id
+            );
+
+        store.commit("Map/setClickCoord", [hit.coordinate[0], hit.coordinate[1]]);
+        store.commit("Map/setGfiFeatures", [feature]);
+    }
+});
