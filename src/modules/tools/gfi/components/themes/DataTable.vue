0f4ff0882da7389401df1b545a9d4d6531fc142b
felixnagel87 <felix.nagel@gv.hamburg.de>
2020-09-30 17:41:41 +0200

    wip

--- src/modules/tools/gfi/components/themes/DataTable.vue
+++ src/modules/tools/gfi/components/themes/DataTable.vue
@@ -1,5 +1,8 @@
 <script>
 
+import {mapGetters, mapMutations} from "vuex";
+import getters from "../../store/gettersGfi";
+
 export default {
     name: "DataTable",
     props: {
@@ -9,41 +12,109 @@ export default {
         }
     },
     methods: {
-        
+        ...mapMutations("Tools/Gfi", ["setUsePager"]),
+        ...mapGetters("Tools/Gfi", Object.keys(getters))
     },
     
     mounted () {
-        console.log(this.feature.getAttributesToShow());
-        console.log(this.feature.getMappedProperties());
+        this.setUsePager(false);
+        console.log(this.refinedData);
+        
+
+
+        this.gfiFeatures.forEach(singleFeature => {
+            //console.log(singleFeature.getAttributesToShow());
+            
+        });
+    },
+    
+    computed: {
+        ...mapGetters("Map", {
+            gfiFeatures: "gfiFeatures"
+        }),
+        
+        refinedData: function () {
+            const result = {
+                mapping: {},
+                head: [],
+                rows: [],
+            };
+            
+            this.gfiFeatures.forEach(singleFeature => {
+                const attrsToShow = singleFeature.getAttributesToShow(),
+                    valsToShow = singleFeature.getProperties();
+                
+                Object.keys(attrsToShow).forEach(attrKey => {
+                    if (valsToShow[attrKey] !== undefined && typeof valsToShow[attrKey] === "string" && result.head.indexOf(attrKey) === -1) {
+                        result.head.push(attrKey);
+                        result.mapping[attrKey] = attrsToShow[attrKey];
+                    }
+                });
+
+                result.rows.push(valsToShow);
+            });
+            
+            return result;
+        },
+        
+        console: () => console
     },
     
     watch: {
         feature (newFeature) {
-            console.log(this.feature);
-            console.log(newFeature);
+            //console.log(this.feature);
+            //console.log(newFeature);
         }
     }
 };
 </script>
 
 <template>
-    <div></div>
+    <div id="table-data-container">
+        <table 
+            v-if="refinedData.rows.length > 0"
+            class="table table-hover"
+        >
+            <thead>
+                <th
+                    v-for="propNameKey in refinedData.head"
+                    :key="propNameKey"
+                >
+                    {{ refinedData.mapping[propNameKey] }}
+                </th>
+            </thead>
+        
+            <tbody>
+                <tr
+                    v-for="singleRow in refinedData.rows"
+                >
+                    <td
+                        :load="console.log(singleRow)"
+                        v-for="propKey in refinedData.head"
+                    >
+                        {{ singleRow[propKey] ? singleRow[propKey] : '' }}
+                    </td>
+                </tr>            
+            </tbody>
+        </table>
+    </div>
 </template>
 
 
 <style lang="less" scoped>
 @import "~variables";
 
-.table > tbody > tr > td {
-    padding: 5px 8px;
-    font-size: 12px;
-    &.bold{
-        font-family: @font_family_accent;
+#table-data-container {
+    margin:6px 15px 0 12px;
+    
+    table {
+        margin: 0;
+        
+        td, th {
+            padding: 6px;
+        }
     }
 }
-.gfi-iFrame {
-    width: 100%;
-    min-height: 40vh;
-}
+
 
 </style>
