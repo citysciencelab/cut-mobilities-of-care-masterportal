28ce74707d1b60a82f323345c6c7c83283e0bc3e
Feng Feng <feng.feng@gv.hamburg.de>
2020-10-01 10:05:48 +0200

    MPR 259 register day and center language code

---
+++ src/modules/language/store/indexLanguage.js
@@ -0,0 +1,10 @@
+import state from "./stateLanguage";
+import getters from "./gettersLanguage";
+import mutations from "./mutationsLanguage";
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    mutations
+};
