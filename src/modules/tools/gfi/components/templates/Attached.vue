7f6af25c35541921bae8605b501d2fb678fb2bcf
felixnagel87 <felix.nagel@gv.hamburg.de>
2020-10-05 15:03:20 +0200

    update: gfi theme "table"

--- src/modules/tools/gfi/components/templates/Attached.vue
+++ src/modules/tools/gfi/components/templates/Attached.vue
@@ -34,6 +34,11 @@ export default {
         feature: {
             type: Object,
             required: true
+        },
+        gfiFeatures: {
+            type: Array,
+            required: false,
+            default: () => [this.feature]
         }
     },
     data () {
@@ -191,6 +196,7 @@ export default {
             <component
                 :is="theme"
                 :feature="feature"
+                :gfiFeatures="gfiFeatures"
             />
         </div>
         <!-- footer -->
