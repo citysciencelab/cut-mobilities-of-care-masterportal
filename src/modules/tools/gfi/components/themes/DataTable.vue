d15745ea6b9c865a4156aceac174e2e0f02e4d04
felixnagel87 <felix.nagel@gv.hamburg.de>
2020-10-07 11:59:13 +0200

    update: linter

--- src/modules/tools/gfi/components/themes/DataTable.vue
+++ src/modules/tools/gfi/components/themes/DataTable.vue
@@ -19,7 +19,7 @@ export default {
             };
 
             this.gfiFeatures.forEach(singleFeature => {
-            
+
                 const mappedProps = singleFeature.getMappedProperties();
 
                 Object.keys(mappedProps).forEach(attrKey => {
@@ -30,7 +30,7 @@ export default {
 
                 result.rows.push(mappedProps);
             });
-            
+
             return result;
         }
     },
