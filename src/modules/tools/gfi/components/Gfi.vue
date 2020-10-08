7bd7b6d042d3f2de518ab592ca69cafa65cfc30b
Dudek <DudekIn@fhhnet.stadt.hamburg.de>
2020-10-08 09:15:18 +0200

    MPR-293: do not show mapMarker in gfi

--- src/modules/tools/gfi/components/Gfi.vue
+++ src/modules/tools/gfi/components/Gfi.vue
@@ -101,7 +101,7 @@ export default {
         }
     },
     beforeUpdate () {
-        if (this.feature !== null && this.feature.hasOwnProperty("getProperties") && this.feature.getProperties() !== null) {
+        if (this.feature !== null && this.feature !== undefined && this.feature.hasOwnProperty("getProperties") && this.feature.getProperties() !== null) {
             const mappedProperties = this.prepareProperties(this.feature.getProperties(), this.feature.getAttributesToShow(), this.ignoredKeys);
 
             this.feature.getMappedProperties = () => mappedProperties;
