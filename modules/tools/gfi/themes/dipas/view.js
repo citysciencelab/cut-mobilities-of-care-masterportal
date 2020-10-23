491de94c66563a889b9d6f94d4f174d2aa46b83e
sven_junker-beyel <sven.beyel@gv.hamburg.de>
2020-10-23 12:54:00 +0200

    remove sensor gfitheme and other gfiThemes that have already been migrated to vue
    The SensorTheme is to be outsourced in an addon that is developed and hosted by Munich.
    
    Jira-Issu: MPR-284

--- modules/tools/gfi/themes/dipas/view.js
+++
@@ -1,9 +0,0 @@
-import ThemeView from "../view";
-import DipasThemeTemplate from "text-loader!./template.html";
-
-const DipasThemeView = ThemeView.extend({
-    className: "dipas-gfi-theme",
-    template: _.template(DipasThemeTemplate)
-});
-
-export default DipasThemeView;
