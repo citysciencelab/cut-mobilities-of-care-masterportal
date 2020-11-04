1d00bc76811ce965c003e9daf94b1671a74373b1
sven_beyel <sven.beyel@gv.hamburg.de>
2020-11-04 14:02:53 +0100

    update unified digrams in the gfithemes
    Jira-Issue: MPR-342

--- src/modules/tools/gfi/components/templates/Detached.vue
+++ src/modules/tools/gfi/components/templates/Detached.vue
@@ -242,8 +242,13 @@ export default {
     }
     .gfi-content {
         overflow: auto;
+        margin: 5px 20px 5px 20px;
         table {
             margin-bottom: 0;
         }
+    }
+    .gfi-footer {
+        color: #646262;
+        font-size: 22px;
     }
 </style>
