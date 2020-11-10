e0576291de187978269094fb59a8137cc3ca63cd
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2020-11-10 12:15:19 +0100

    MRH-72: update jsdoc for getNestedValues

--- src/utils/getNestedValues.js
+++ src/utils/getNestedValues.js
@@ -2,9 +2,9 @@
  * Gets nested values recursively from an object by the given key if it exists, up to a depth of 500.
  * @param {Object} obj - The object to search.
  * @param {String} searchKey - The key for the searched values.
- * @param {Object|String[]} nestedValues - Given nested values.
- * @param {Number} depth - number of self calls
- * @param {Number} maxDepth  - maximum number of self calls
+ * @param {Object|String[]} [nestedValues=[]] - Given nested values.
+ * @param {Number} [depth=0] - number of self calls
+ * @param {Number} [maxDepth=500]  - maximum number of self calls
  * @returns {Object|String[]} results - The found nested values.
  */
 export default function getNestedValues (obj, searchKey, nestedValues = [], depth = 0, maxDepth = 500) {
