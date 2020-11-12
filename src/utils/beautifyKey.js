6117df03060948d1a96b3d363eb0420d60e38ecf
sven_beyel <sven.beyel@gv.hamburg.de>
2020-11-12 12:03:20 +0100

    update rename the util function beautifyWfskeyx to beautifyKey, because it is also used by data other than WFS

--- src/utils/beautifyKey.js
+++ src/utils/beautifyKey.js
@@ -1,13 +1,13 @@
 /**
  * parses a string: replaces letters and uppercases the very first letter (by default)
  * @info common wfs data in the MP has keys that will be comfortable to read with the default options of this function
- * @param {String} value the string (the key of wfs data) to parse and "beautify"
+ * @param {String} value the string (the key of wfs or other data) to parse and "beautify"
  * @param {Object} [optionsOpt={}] options to give if you wish to alter the behavior
  * @param {Boolean} [optionsOpt.uppercase=true] if you wish not to use uppercase of the very first letter, set this to false
  * @param {Object} [optionsOpt.replacements={"_": " "}] an object with key/value pairs for alternative replacements (e.g. {"_": " ", "+": " "})
  * @returns {String}  a new string with the given replacements, the very first letter may be uppercase based on options
  */
-export default function beautifyWfsKey (value, optionsOpt = null) {
+export default function beautifyKey (value, optionsOpt = null) {
     if (typeof value !== "string") {
         return "";
     }
