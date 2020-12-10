1d43de72995c64b9976def2a47dc96c3cbcd0d4d
felixnagel87 <felix.nagel@gv.hamburg.de>
2020-12-10 19:57:01 +0100

    update: test may now succeed

--- src/utils/loaderOverlay.js
+++ src/utils/loaderOverlay.js
@@ -11,12 +11,21 @@ export default {
      * @returns {int}           - Count of virtual loader stacks
      */
     show: function (maxWait = 25000) {
+        const loader = document.getElementById("loader"),
+            masterportalContainer = document.getElementById("masterportal-container");
+
         clearTimeout(this.loaderTimeoutReference);
         this.loaderTimeoutReference = setTimeout(() => {
             this.hide();
         }, maxWait);
-        document.getElementById("loader").classList.add("loader-is-loading");
-        document.getElementById("masterportal-container").classList.add("blurry");
+
+        if (loader !== null) {
+            document.getElementById("loader").classList.add("loader-is-loading");
+        }
+        if (masterportalContainer !== null) {
+            document.getElementById("masterportal-container").classList.add("blurry");
+        }
+
         return ++this.loaderOverlayCount;
     },
     /**
@@ -24,11 +33,19 @@ export default {
      * @returns {int}   - Count of virtual loader stacks
      */
     hide: function () {
+        const loader = document.getElementById("loader"),
+            masterportalContainer = document.getElementById("masterportal-container");
+
         this.loaderOverlayCount--;
         if (this.loaderOverlayCount <= 0) {
             this.loaderOverlayCount = 0;
-            document.getElementById("loader").classList.remove("loader-is-loading");
-            document.getElementById("masterportal-container").classList.remove("blurry");
+
+            if (loader !== null) {
+                document.getElementById("loader").classList.remove("loader-is-loading");
+            }
+            if (masterportalContainer !== null) {
+                document.getElementById("masterportal-container").classList.remove("blurry");
+            }
             if (!this.initialLoaderIsHidden) {
                 this.removePreLoadLogos();
             }
@@ -41,11 +58,19 @@ export default {
      */
     removePreLoadLogos: function () {
         const portalLogoBox = document.getElementById("portal-logo-box"),
+            loader = document.getElementById("loader"),
             genericMasterPortalLogo = document.getElementById("generic-masterportal-logo");
 
-        portalLogoBox.parentNode.removeChild(portalLogoBox);
-        genericMasterPortalLogo.parentNode.removeChild(genericMasterPortalLogo);
-        document.getElementById("loader").classList.remove("loader-has-solid-bg");
+        if (portalLogoBox !== null) {
+            portalLogoBox.parentNode.removeChild(portalLogoBox);
+        }
+        if (genericMasterPortalLogo !== null) {
+            genericMasterPortalLogo.parentNode.removeChild(genericMasterPortalLogo);
+        }
+        if (loader !== null) {
+            document.getElementById("loader").classList.remove("loader-has-solid-bg");
+        }
+
         this.initialLoaderIsHidden = true;
     }
 };
