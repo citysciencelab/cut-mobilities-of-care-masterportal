7998dc5b091a026591854e5ce2bb0c3bd75e6b19
sven_beyel <sven.beyel@gv.hamburg.de>
2020-12-11 07:06:33 +0100

    add default value isVisibleInMenu in the state of the tool gfi
    Because this attribute could be configured for everything tool.
    
    BG-1112

--- src/modules/tools/gfi/store/stateGfi.js
+++ src/modules/tools/gfi/store/stateGfi.js
@@ -1,13 +1,14 @@
 /**
  * User type definition
  * @typedef {Object} GfiState
- * @property {String} id id of the gfi component
- * @property {String} name displayed as title (config-param)
- * @property {String} glyphicon icon next to title (config-param)
- * @property {Boolean} active - true if the gfi is active
- * @property {Object} currentFeature - the current feature that is displayed
- * @property {String} desktopType - specifies which template is used in desktop mode
- * @property {Boolean} centerMapToClickPoint - specifies if the map should be centered when clicking on a feature
+ * @property {String} id Id of the gfi component.
+ * @property {String} name Displayed as title (config-param).
+ * @property {String} glyphicon Icon next to title (config-param).
+ * @property {Boolean} active True if the gfi is active.
+ * @property {Object} currentFeature The current feature that is displayed.
+ * @property {String} desktopType Specifies which template is used in desktop mode.
+ * @property {Boolean} centerMapToClickPoint specifies if the map should be centered when clicking on a feature.
+ * @property {Boolean} isVisibleInMenu Indicates whether there is a menu entry for the GFI.
  *
  */
 const state = {
@@ -19,7 +20,8 @@ const state = {
     desktopType: "",
     centerMapToClickPoint: false,
     showMarker: true,
-    highlightVectorRules: null
+    highlightVectorRules: null,
+    isVisibleInMenu: true
 };
 
 export default state;
