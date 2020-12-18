e597f6542cdcae0082972f1c09444eeb435169e5
Dudek <DudekIn@fhhnet.stadt.hamburg.de>
2020-12-18 11:58:07 +0100

    BG-1123: placingPointMarker: if 3D round coordinates

--- src/modules/mapMarker/store/actionsMapMarker.js
+++ src/modules/mapMarker/store/actionsMapMarker.js
@@ -1,6 +1,7 @@
 import {fetchFirstModuleConfig} from "../../../utils/fetchFirstModuleConfig";
 import Point from "ol/geom/Point.js";
 import Feature from "ol/Feature.js";
+import {MapMode} from "../../map/store/enums";
 
 /**
  * @const {String} configPaths an array of possible config locations. First one found will be used
@@ -37,24 +38,24 @@ export default {
      * @param {String[]} value The array with the markable coordinate pair.
      * @returns {void}
      */
-    placingPointMarker ({state, commit, dispatch, rootState}, value) {
+    placingPointMarker ({state, rootState, commit, dispatch}, value) {
         const styleListModel = Radio.request("StyleList", "returnModelById", state.pointStyleId);
         let coordValues = [];
 
         dispatch("removePointMarker");
 
         if (styleListModel) {
-            if(rootState.Map.mapMode === MapMode.MODE_3D){
-                // else an error is thrown in proj4/lib/checkSanity: coordinates must be finite numbers 
+            if (rootState.Map.mapMode === MapMode.MODE_3D) {
+                // else an error is thrown in proj4/lib/checkSanity: coordinates must be finite numbers
                 value.forEach(val => {
-                    coordValues.push(Math.round(val)); 
+                    coordValues.push(Math.round(val));
                 });
             }
-            else{
+            else {
                 coordValues = value;
             }
             const iconfeature = new Feature({
-                    geometry: new Point(valuesRounded)
+                    geometry: new Point(coordValues)
                 }),
                 featureStyle = styleListModel.createStyle(iconfeature, false);
 
