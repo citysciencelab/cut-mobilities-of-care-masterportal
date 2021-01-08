7fe5b51eae5bf2192469ff3b7c0292e6fc610b0d
sven_beyel <sven.beyel@gv.hamburg.de>
2021-01-08 09:23:28 +0100

    update consolidate duplicate warnings so that the console is not too full when starting the portal

--- modules/core/modelList/layer/tileset.js
+++ modules/core/modelList/layer/tileset.js
@@ -167,7 +167,6 @@ TileSetLayer = Layer.extend(/** @lends TileSetLayer.prototype */{
          * @deprecated in 3.0.0
          */
         if (this.get("legendURL")) {
-            console.warn("legendURL ist deprecated in 3.0.0. Please use attribute \"legend\" als Boolean or String with path to legend image or pdf");
             if (this.get("legendURL") === "") {
                 legend = true;
             }
--- modules/core/modelList/layer/wms.js
+++ modules/core/modelList/layer/wms.js
@@ -147,7 +147,6 @@ const WMSLayer = Layer.extend({
          * @deprecated in 3.0.0
          */
         if (this.get("legendURL")) {
-            console.warn("legendURL ist deprecated in 3.0.0. Please use attribute \"legend\" als Boolean or String with path to legend image or pdf");
             if (this.get("legendURL") === "") {
                 legend = true;
             }
