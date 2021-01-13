608a0a63909ae646cc982f39109a1fa1001553e9
Darge <DargeFi@fhhnet.stadt.hamburg.de>
2021-01-13 14:54:44 +0100

    MPR-112 added tests for validateInput action

--- src/modules/tools/searchByCoord/test/unit/store/actionsSearchByCoord.spec.js
+++ src/modules/tools/searchByCoord/test/unit/store/actionsSearchByCoord.spec.js
@@ -23,25 +23,74 @@ describe("src/modules/tools/supplyCoord/store/actionsSearchByCoord.js", () => {
             actions.validateInput({state, commit, dispatch}, [state.coordinatesEasting, state.coordinatesNorthing]);
 
             expect(commit.firstCall.args[0]).to.equal("resetSelectedCoordinates");
-            expect(state.coordinatesEasting.errorMessage).to.equal("");
-            expect(state.coordinatesNorthing.errorMessage).to.equal("");
             expect(commit.secondCall.args[0]).to.equal("resetErrorMessages");
             expect(commit.thirdCall.args[0]).to.equal("pushCoordinates");
             expect(commit.thirdCall.args[1]).to.equal("564459.13");
         });
-        it("Validates the coordinates according to the WGS84 coordinate system", () => {
+        it("Throws an Error for wrong coordinates (ETRS89)", () => {
             const state = {
-                currentSelection: "WGS84(Dezimalgrad)",
-                coordinatesEasting: {id: "easting", name: "", value: "53° 33′ 25″", errorMessage: ""},
-                coordinatesNorthing: {id: "northing", name: "", value: "9° 59′ 50″", errorMessage: ""}
+                currentSelection: "ETRS89",
+                coordinatesEasting: {id: "easting", name: "", value: "", errorMessage: ""},
+                coordinatesNorthing: {id: "northing", name: "", value: "falsche Eingabe", errorMessage: ""}
             };
 
             actions.validateInput({state, commit, dispatch}, [state.coordinatesEasting, state.coordinatesNorthing]);
 
             expect(commit.firstCall.args[0]).to.equal("resetSelectedCoordinates");
-            expect(state.coordinatesEasting.errorMessage).to.equals("");
-            // expect(state.coordinatesNorthing.errorMessage).to.equals("");
-            // expect(commit.secondCall.args[0]).to.equal("resetErrorMessages");
+            expect(commit.secondCall.args[0]).to.equal("setEastingErrorNoCoord");
+            expect(commit.thirdCall.args[0]).to.equal("setNorthingErrorNoMatch");
+        });
+        it("Validates the coordinates according to the WGS84 coordinate system", () => {
+            const state = {
+                currentSelection: "WGS84",
+                coordinatesEasting: {id: "easting", name: "", value: "53° 33′ 25", errorMessage: ""},
+                coordinatesNorthing: {id: "northing", name: "", value: "9° 59′ 50", errorMessage: ""}
+            };
+
+            actions.validateInput({state, commit, dispatch}, [state.coordinatesEasting, state.coordinatesNorthing]);
+
+            expect(commit.firstCall.args[0]).to.equal("resetSelectedCoordinates");
+            expect(commit.secondCall.args[0]).to.equal("resetErrorMessages");
+            expect(commit.thirdCall.args[0]).to.equal("pushCoordinates");
+        });
+        it("Throws an Error for wrong coordinates (WGS84)", () => {
+            const state = {
+                currentSelection: "WGS84",
+                coordinatesEasting: {id: "easting", name: "", value: "", errorMessage: ""},
+                coordinatesNorthing: {id: "northing", name: "", value: "falsche Eingabe", errorMessage: ""}
+            };
+
+            actions.validateInput({state, commit, dispatch}, [state.coordinatesEasting, state.coordinatesNorthing]);
+
+            expect(commit.firstCall.args[0]).to.equal("resetSelectedCoordinates");
+            expect(commit.secondCall.args[0]).to.equal("setEastingErrorNoCoord");
+            expect(commit.thirdCall.args[0]).to.equal("setNorthingErrorNoMatch");
+        });
+        it("Validates the coordinates according to the WGS84(Dezimalgrad) coordinate system", () => {
+            const state = {
+                currentSelection: "WGS84(Dezimalgrad)",
+                coordinatesEasting: {id: "easting", name: "", value: "53.55555°", errorMessage: ""},
+                coordinatesNorthing: {id: "northing", name: "", value: "10.01234°", errorMessage: ""}
+            };
+
+            actions.validateInput({state, commit, dispatch}, [state.coordinatesEasting, state.coordinatesNorthing]);
+
+            expect(commit.firstCall.args[0]).to.equal("resetSelectedCoordinates");
+            expect(commit.secondCall.args[0]).to.equal("resetErrorMessages");
+            expect(commit.thirdCall.args[0]).to.equal("pushCoordinates");
+        });
+        it("Throws an Error for wrong coordinates (WGS84(Dezimalgrad))", () => {
+            const state = {
+                currentSelection: "WGS84(Dezimalgrad)",
+                coordinatesEasting: {id: "easting", name: "", value: "", errorMessage: ""},
+                coordinatesNorthing: {id: "northing", name: "", value: "falsche Eingabe", errorMessage: ""}
+            };
+
+            actions.validateInput({state, commit, dispatch}, [state.coordinatesEasting, state.coordinatesNorthing]);
+
+            expect(commit.firstCall.args[0]).to.equal("resetSelectedCoordinates");
+            expect(commit.secondCall.args[0]).to.equal("setEastingErrorNoCoord");
+            expect(commit.thirdCall.args[0]).to.equal("setNorthingErrorNoMatch");
         });
     });
 });
