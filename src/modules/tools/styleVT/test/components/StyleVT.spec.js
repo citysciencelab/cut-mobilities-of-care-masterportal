74dc277c4b7e73dfa2d682209f01a67777148aaf
Pascal Röhling <pascal.roehling@dataport.de>
2021-01-14 10:46:49 +0100

    MPR-347: Implemented tests for the component of the StyleVT tool.

--- src/modules/tools/styleVT/test/components/StyleVT.spec.js
+++ src/modules/tools/styleVT/test/components/StyleVT.spec.js
@@ -3,28 +3,36 @@ import {createLocalVue, shallowMount} from "@vue/test-utils";
 import sinon from "sinon";
 import Vuex from "vuex";
 
-import StyleVT from "../../components/StyleVT.vue";
+import StyleVT from "../../store/indexStyleVT";
+import StyleVTComponent from "../../components/StyleVT.vue";
 
 const localVue = createLocalVue();
 
 localVue.use(Vuex);
 
-describe.only("src/modules/tools/styleVT/components/StyleVT.vue", () => {
-    let wrapper,
-        wrapperOptions;
+describe("src/modules/tools/styleVT/components/StyleVT.vue", () => {
+    let store,
+        wrapper;
 
     beforeEach(() => {
-        wrapperOptions = {
-            propsData: {
-                vectorTileLayerList: () => []
+        store = new Vuex.Store({
+            namespaces: true,
+            modules: {
+                Tools: {
+                    namespaced: true,
+                    modules: {
+                        StyleVT
+                    }
+                }
             }
-        };
+        });
+        store.commit("Tools/StyleVT/setActive", true);
     });
 
     afterEach(sinon.restore);
 
     it("should render a paragraph informing the user that no styleable layers are available if none are", () => {
-        wrapper = shallowMount(StyleVT, {wrapperOptions, localVue});
+        wrapper = shallowMount(StyleVTComponent, {store, localVue});
 
         expect(wrapper.find("#tool-styleVT-noStyleableLayers").exists()).to.be.true;
         expect(wrapper.find("#tool-styleVT-styleableLayersAvailable").exists()).to.be.false;
@@ -33,8 +41,8 @@ describe.only("src/modules/tools/styleVT/components/StyleVT.vue", () => {
         const layerOne = Symbol(),
             layerTwo = Symbol();
 
-        state.vectorTileLayerList.push(layerOne, layerTwo);
-        wrapper = shallowMount(StyleVT, {wrapperOptions, localVue});
+        store.commit("Tools/StyleVT/setVectorTileLayerList", [layerOne, layerTwo]);
+        wrapper = shallowMount(StyleVTComponent, {store, localVue});
 
         expect(wrapper.find("#tool-styleVT-styleableLayersAvailable").exists()).to.be.true;
         expect(wrapper.find("#tool-styleVT-noStyleableLayers").exists()).to.be.false;
