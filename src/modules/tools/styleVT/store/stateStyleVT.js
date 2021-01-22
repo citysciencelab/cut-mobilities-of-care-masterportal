85f31a781bd7f1fb76caefcf36fc4063ae58c7e2
Pascal Röhling <pascal.roehling@dataport.de>
2021-01-22 10:39:18 +0100

    MPR-347: Changed type of property layerModel, as it is nullable.

--- src/modules/tools/styleVT/store/stateStyleVT.js
+++ src/modules/tools/styleVT/store/stateStyleVT.js
@@ -3,7 +3,7 @@
  * @property {Boolean} deactivateGFI If set to true, the activation of this Tool deactivates the GFI Tool.
  * @property {String} glyphicon Glyphicon used in the header of the window.
  * @property {String} id Internal identifier for the Tool.
- * @property {VTLayer} layerModel Currently selected model of a Vector Tile Layer to style.
+ * @property {?VTLayer} layerModel Currently selected model of a Vector Tile Layer to style.
  * @property {String} name Title displayed at the top of the window of the Tool; can be configured through the config.json.
  * @property {Boolean} renderToWindow Decides whether the Tool should be displayed as a window or as a sidebar.
  * @property {Boolean} resizableWindow Determines whether the Tool window can be resized.
