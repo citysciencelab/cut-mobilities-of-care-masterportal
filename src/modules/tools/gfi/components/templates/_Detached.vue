726f94db7e8939637515bb85d16f24d9cee3c13c
felixnagel87 <felix.nagel@gv.hamburg.de>
2021-01-28 13:58:08 +0100

    update: created demo draggable resizable tool window component

--- src/modules/tools/gfi/components/templates/_Detached.vue
+++ src/modules/tools/gfi/components/templates/_Detached.vue
@@ -3,14 +3,16 @@ import Default from "../themes/default/components/Default.vue";
 import Sensor from "../themes/sensor/components/Sensor.vue";
 import getTheme from "../../utils/getTheme";
 import {mapGetters, mapMutations, mapActions} from "vuex";
-import ToolWindow from "../../../../../share-components/ToolWindow.vue";
+import BasicDragHandle from "../../../../../share-components/BasicDragHandle.vue";
+import BasicResizeHandle from "../../../../../share-components/BasicResizeHandle.vue";
 
 export default {
     name: "Detached",
     components: {
+        BasicDragHandle,
+        BasicResizeHandle,
         Default,
-        Sensor,
-        ToolWindow
+        Sensor
     },
     props: {
         feature: {
@@ -42,6 +44,24 @@ export default {
          */
         theme: function () {
             return getTheme(this.feature.getTheme(), this.$options.components, this.$gfiThemeAddons);
+        },
+
+        /**
+         * Returns the custom style for the gfi content.
+         * it will make the content croll.
+         * @returns {Object} the style in the object
+         */
+        styleContent: function () {
+            if (this.isContentHtml) {
+                return {
+                    "max-width": "inherit",
+                    "max-height": "inherit"
+                };
+            }
+            return {
+                "max-width": Math.round(document.getElementById("map").offsetWidth / 2.2) + "px",
+                "max-height": window.innerHeight - 100 - 34 - 83 + "px" // 100 pixer for the navi. 34 for header, 83 is the distance from bottom
+            };
         }
     },
     watch: {
@@ -134,16 +154,93 @@ export default {
 </script>
 
 <template>
-    <ToolWindow @close="close">
-        <template v-slot:title>
-            <span>{{ $t(title) }}</span>
-        </template>
-        <template v-slot:body>
+    <div
+        class="gfi-detached"
+    >
+        <BasicDragHandle
+            targetEl=".gfi-detached"
+            :margin="20"
+        >
+            <!-- header -->
+            <div class="gfi-header">
+                <button
+                    type="button"
+                    class="close"
+                    aria-label="Close"
+                    @click="close"
+                >
+                    <span
+                        class="glyphicon glyphicon-remove"
+                    >
+                    </span>
+                </button>
+                <h5>
+                    {{ $t(title) }}
+                </h5>
+            </div>
+        </BasicDragHandle>
+        <!-- theme -->
+        <div
+            class="gfi-content"
+            :style="styleContent"
+        >
             <component
                 :is="theme"
                 :feature="feature"
             />
+        </div>
+        <!-- footer -->
+        <div class="gfi-footer">
             <slot name="footer" />
-        </template>
-    </ToolWindow>
+        </div>
+        <BasicResizeHandle
+            v-for="hPos in ['tl', 'tr', 'br', 'bl']"
+            :id="'basic-resize-handle-' + hPos"
+            :key="hPos"
+            :hPos="hPos"
+            targetEl=".gfi-detached"
+            :minW="250"
+            :minH="100"
+        />
+    </div>
 </template>
+
+<style lang="less" scoped>
+    .gfi-detached {
+        position: absolute;
+        min-width: 250px;
+        top: 50px;
+        //todo felix: warum funktioniert right nicht? oder: welchen Wert für left nutzen?
+        left: calc(100vw - 45%);
+        margin: 10px 10px 30px 10px;
+        background-color: #ffffff;
+        box-shadow: 8px 8px 12px rgba(0, 0, 0, 0.3);
+         .basic-resize-handle {
+            position:absolute;
+            width:6px;
+            height:6px;
+        }
+        #basic-resize-handle-tl { top:0px; left:0px; }
+        #basic-resize-handle-tr { top:0px; right:0px;}
+        #basic-resize-handle-br { bottom:0px; right:0px;}
+        #basic-resize-handle-bl { bottom:0px; left:0px;}
+    }
+    .gfi-header {
+        font-size: 13px;
+        font-weight: normal;
+        line-height: 17px;
+        color: #646262;
+        padding: 0px 15px;
+        border-bottom: 1px solid #e5e5e5;
+        button {
+            font-size: 16px;
+            opacity: 0.6;
+        }
+    }
+    .gfi-content {
+        overflow: auto;
+        table {
+            margin-bottom: 0;
+        }
+    }
+</style>
