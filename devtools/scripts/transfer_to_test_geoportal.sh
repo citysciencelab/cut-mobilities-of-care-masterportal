9493b8410979953f29c4ebc4e4a5f027792a47d4
Dudek <DudekIn@fhhnet.stadt.hamburg.de>
2021-02-16 13:40:36 +0100

    test 3

--- devtools/scripts/transfer_to_test_geoportal.sh
+++ devtools/scripts/transfer_to_test_geoportal.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 set -e
+# install "git-ftp"
 apt-get -qq install git-ftp
+# store configs for ftp transfer
 git config git-ftp.user $LGV_TEST_FTP_USERNAME
 git config git-ftp.password $LGV_TEST_FTP_PASSWORD
 git config git-ftp.syncroot dist
 git config git-ftp.url ftp://test.geoportal-hamburg.de/
 # do init once per ftp target-folder to create and transport the .git-ftp.log file containing the SHA1 of the latest commit, after that do always push.
-git ftp init -vv
+# - git ftp init -vv
+#replace slashes in branchname with underscores and
 tmp=$BITBUCKET_BRANCH
-#replace slashes in branchname with underscores
 branchname=${tmp//[\/]/_}
+#create temporariliy a file called ".git-ftp-include" for the subfolders in folder ./dist
 STR=$'!dist/master_'$branchname$'\n!dist/basic_'$branchname$'\n!dist/masterCustom_'$branchname$'\n!dist/masterDefault_'$branchname$'\n!dist/mastercode'
 echo "$STR"  >> .git-ftp-include
 git add .git-ftp-include
