4d824c6b0e4d5f0e695549d4eb331af577ff52fe
Dudek <DudekIn@fhhnet.stadt.hamburg.de>
2021-02-17 10:09:09 +0100

    BG-1320: adaptions to dev

--- devtools/scripts/transfer_to_test_geoportal.sh
+++ devtools/scripts/transfer_to_test_geoportal.sh
@@ -7,8 +7,6 @@ git config git-ftp.user $LGV_TEST_FTP_USERNAME
 git config git-ftp.password $LGV_TEST_FTP_PASSWORD
 git config git-ftp.syncroot dist
 git config git-ftp.url ftp://test.geoportal-hamburg.de/
-# ATTENTION: (next line) do init once per ftp target-folder to create and transport the .git-ftp.log file containing the SHA1 of the latest commit, after that comment out next line again
-# - git ftp init -vv
 #replace slashes in branchname with underscores and
 tmp=$BITBUCKET_BRANCH
 branchname=${tmp//[\/]/_}
@@ -17,5 +15,8 @@ STR=$'!dist/master_'$branchname$'\n!dist/basic_'$branchname$'\n!dist/masterCusto
 echo "$STR"  >> .git-ftp-include
 git add .git-ftp-include
 git commit .git-ftp-include -m "Add new content for e2e test"
+git commit package-lock.json -m "is sometimes modified after install"
+# ATTENTION: (next line) do init once per ftp target-folder to create and transport the .git-ftp.log file containing the SHA1 of the latest commit, after that comment out next line again
+# - git ftp init -vv
 # push all folders defined in ".git-ftp-include" of the current branch to ftp host
 git ftp push -b $BITBUCKET_BRANCH --all
