ce0fb1e1db9cdcabe99691e44219626618c8a780
Dudek <DudekIn@fhhnet.stadt.hamburg.de>
2021-02-25 14:43:04 +0100

    more log git status

--- devtools/scripts/transfer_to_test_geoportal.sh
+++ devtools/scripts/transfer_to_test_geoportal.sh
@@ -15,10 +15,18 @@ STR=$'!dist/master_'$branchname$'\n!dist/basic_'$branchname$'\n!dist/masterCusto
 echo "$STR"  >> .git-ftp-include
 echo "git status 1:"
 git status
+echo "ls -al:"
+ls -al
 git add .git-ftp-include
 git commit .git-ftp-include -m "Add new content for e2e test"
-git commit package-lock.json -m "is sometimes modified after install"
 echo "git status 2:"
+git status
+git add addons/
+git commit addons/ -m "is sometimes modified after install"
+echo "git status 3:"
+git status
+git commit package-lock.json -m "is sometimes modified after install"
+echo "git status 4:"
 git status
 # ATTENTION: (next line) do init once per ftp target-folder to create and transport the .git-ftp.log file containing the SHA1 of the latest commit, after that comment out next line again
 # - git ftp init -vv
