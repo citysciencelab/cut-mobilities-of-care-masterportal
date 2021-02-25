e265e0963e10174da407d260fa7e1aa6d3f5a6c9
Dudek <DudekIn@fhhnet.stadt.hamburg.de>
2021-02-25 14:34:16 +0100

    log git status

--- devtools/scripts/transfer_to_test_geoportal.sh
+++ devtools/scripts/transfer_to_test_geoportal.sh
@@ -13,9 +13,13 @@ branchname=${tmp//[\/]/_}
 #create temporariliy a file called ".git-ftp-include" containing the subfolders in folder ./dist, see https://github.com/git-ftp/git-ftp/blob/master/man/git-ftp.1.md
 STR=$'!dist/master_'$branchname$'\n!dist/basic_'$branchname$'\n!dist/masterCustom_'$branchname$'\n!dist/masterDefault_'$branchname$'\n!dist/mastercode'
 echo "$STR"  >> .git-ftp-include
+echo "git status 1:"
+git status
 git add .git-ftp-include
 git commit .git-ftp-include -m "Add new content for e2e test"
 git commit package-lock.json -m "is sometimes modified after install"
+echo "git status 2:"
+git status
 # ATTENTION: (next line) do init once per ftp target-folder to create and transport the .git-ftp.log file containing the SHA1 of the latest commit, after that comment out next line again
 # - git ftp init -vv
 # push all folders defined in ".git-ftp-include" of the current branch to ftp host
