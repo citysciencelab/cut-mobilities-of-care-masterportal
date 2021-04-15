01800984c1cd294ae160da906c779009aa40f736
Laura Meierkort <meierkort@grit.de>
2021-04-15 15:56:21 +0200

    rename parameter "startZoomLevel" to "zoomLevel"

--- modules/searchbar/view.js
+++ modules/searchbar/view.js
@@ -490,13 +490,13 @@ const SearchbarView = Backbone.View.extend(/** @lends SearchbarView.prototype */
     setMarkerZoom: function (hit) {
         const resolutions = Radio.request("MapView", "getResolutions"),
             index = resolutions.indexOf(0.2645831904584105) === -1 ? resolutions.length : resolutions.indexOf(0.2645831904584105),
-            startZoomLevel = this.model.get("startZoomLevel") !== undefined ? this.model.get("startZoomLevel") : index;
+            zoomLevel = this.model.get("zoomLevel") !== undefined ? this.model.get("zoomLevel") : index;
         let extent = [];
 
         if (hit.coordinate.length === 2) {
             store.dispatch("MapMarker/removePolygonMarker");
             store.dispatch("MapMarker/placingPointMarker", hit.coordinate);
-            Radio.trigger("MapView", "setCenter", hit.coordinate, startZoomLevel);
+            Radio.trigger("MapView", "setCenter", hit.coordinate, zoomLevel);
         }
         else {
             store.dispatch("MapMarker/removePolygonMarker");
