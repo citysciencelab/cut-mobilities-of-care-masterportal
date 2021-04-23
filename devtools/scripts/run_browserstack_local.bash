f5a2049e34324f51472a0ccd4f7c91791f9492a2
Dudek <DudekIn@fhhnet.stadt.hamburg.de>
2021-04-23 12:51:19 +0200

    BG-1507: moved files to ci_scripts

--- devtools/scripts/run_browserstack_local.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-#File: run_browserstack_local.bash
-
-wget http://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
-unzip BrowserStackLocal-linux-x64.zip
-./BrowserStackLocal ${bs_key} > /dev/null &
-sleep 10
